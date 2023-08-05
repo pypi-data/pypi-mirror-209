548c80c0fb77e0667feb389d32f18c144bb13150
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:11:04 +0000

    Add package gp2/0.5.18.23/gp2-0.5.18.23.tar.gz

---
+++ packages/gp2/gp2-0.5.18.23.tar.gz/gp2-0.5.18.23/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+
+from gp2.runner import __version__
+
+setup(
+    name='gp2',
+    version=__version__,
+    url='https://github.com/RyanZurrin/CS410-GP2',
+    description='A framework for tuning segmentation classifiers and '
+                'discriminators',
+    author='mpsych lab',
+    packages=find_packages(),
+    classifiers=[
+        'Environment :: GPU :: NVIDIA CUDA :: 11.3',
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Science/Research',
+        'Intended Audience :: Education',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.9',
+        'Topic :: Scientific/Engineering :: Artificial Intelligence',
+        ],
+    install_requires=[
+        'tensorflow',
+        'matplotlib',
+        'keras_unet_collection',
+        'keras-unet'
+    ],
+
+)
