fccbe529af86589f9d5537f7b95072665f42108f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:11:07 +0000

    Add package OmeSliCC/0.5.1b0/OmeSliCC-0.5.1b0.tar.gz

---
+++ packages/OmeSliCC/OmeSliCC-0.5.1b0.tar.gz/OmeSliCC-0.5.1b0/src/OmeSliCC/util.py
@@ -0,0 +1,190 @@
+import ast
+import os
+import re
+import numpy as np
+import tifffile
+
+
+def get_default(x, default):
+    return default if x is None else x
+
+
+def ensure_list(x) -> list:
+    if isinstance(x, list):
+        return x
+    else:
+        return [x]
+
+
+def filter_dict(dict0):
+    new_dict = {}
+    for key, value0 in dict0.items():
+        if value0 is not None:
+            values = []
+            for value in ensure_list(value0):
+                if isinstance(value, dict):
+                    value = filter_dict(value)
+                values.append(value)
+            if len(values) == 1:
+                values = values[0]
+            new_dict[key] = values
+    return new_dict
+
+
+def tags_to_dict(tags: tifffile.TiffTags) -> dict:
+    tag_dict = {}
+    for tag in tags.values():
+        tag_dict[tag.name] = tag.value
+    return tag_dict
+
+
+def desc_to_dict(desc: str) -> dict:
+    desc_dict = {}
+    if desc.startswith('{'):
+        try:
+            metadata = ast.literal_eval(desc)
+            return metadata
+        except:
+            pass
+    for item in re.split(r'[\r\n\t|]', desc):
+        item_sep = '='
+        if ':' in item:
+            item_sep = ':'
+        if item_sep in item:
+            items = item.split(item_sep)
+            key = items[0].strip()
+            value = items[1].strip()
+            try:
+                if '.' in value:
+                    value = float(value)
+                else:
+                    value = int(value)
+            except:
+                try:
+                    value = bool(value)
+                except:
+                    pass
+            desc_dict[key] = value
+    return desc_dict
+
+
+def print_dict(dct: dict, compact: bool = False, indent: int = 0):
+    s = ''
+    for key, value in dct.items():
+        if not isinstance(value, list):
+            if not compact: s += '\t' * indent
+            s += str(key)
+            s += ':' if compact else '\n'
+        if isinstance(value, dict):
+            s += print_dict(value, indent=indent + 1)
+        elif isinstance(value, list):
+            for v in value:
+                s += print_dict(v)
+        else:
+            if not compact: s += '\t' * (indent + 1)
+            s += str(value)
+        s += ' ' if compact else '\n'
+    return s
+
+
+def print_hbytes(bytes: int) -> str:
+    exps = ['', 'K', 'M', 'G', 'T']
+    div = 1024
+    exp = 0
+
+    while bytes > div:
+        bytes /= div
+        exp +=1
+    return f'{bytes:.1f}{exps[exp]}B'
+
+
+def check_round_significants(a: float, significant_digits: int) -> float:
+    rounded = round_significants(a, significant_digits)
+    if a != 0:
+        dif = 1 - rounded / a
+    else:
+        dif = rounded - a
+    if abs(dif) < 10 ** -significant_digits:
+        return rounded
+    return a
+
+
+def round_significants(a: float, significant_digits: int) -> float:
+    if a != 0:
+        round_decimals = significant_digits - int(np.floor(np.log10(abs(a)))) - 1
+        return round(a, round_decimals)
+    return a
+
+
+def get_filetitle(filename: str, remove_all_ext: bool = False) -> str:
+    filebase = os.path.basename(filename)
+    if remove_all_ext:
+        return filebase.split('.')[0]
+    else:
+        return os.path.splitext(filebase)[0]
+
+
+def split_num_text(text):
+    num_texts = []
+    block = ''
+    is_num0 = None
+    if text is None:
+        return None
+
+    for c in text:
+        is_num = (c.isnumeric() or c == '.')
+        if is_num0 is not None and is_num != is_num0:
+            num_texts.append(block)
+            block = ''
+        block += c
+        is_num0 = is_num
+    if block != '':
+        num_texts.append(block)
+
+    num_texts2 = []
+    for block in num_texts:
+        block = block.strip()
+        try:
+            block = float(block)
+        except:
+            pass
+        if block not in [' ', ',', '|']:
+            num_texts2.append(block)
+    return num_texts2
+
+
+def split_value_unit_list(text):
+    value_units = []
+    if text is None:
+        return None
+
+    items = split_num_text(text)
+    if isinstance(items[-1], str):
+        def_unit = items[-1]
+    else:
+        def_unit = ''
+
+    i = 0
+    while i < len(items):
+        value = items[i]
+        if i + 1 < len(items):
+            unit = items[i + 1]
+        else:
+            unit = ''
+        if not isinstance(value, str):
+            if isinstance(unit, str):
+                i += 1
+            else:
+                unit = def_unit
+            value_units.append((value, unit))
+        i += 1
+    return value_units
+
+
+def get_value_units_micrometer(value_units0: list):
+    conversions = {'nm': 1e-3, 'µm': 1, 'um': 1, 'mm': 1e3, 'cm': 1e4, 'm': 1e6}
+    if value_units0 is None:
+        return None
+
+    value_units = [value_unit[0] * conversions.get(value_unit[1], 1) for value_unit in value_units0]
+    return value_units
