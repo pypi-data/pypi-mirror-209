0dbf8563c2f2adba14c639318b9c271512a826bd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:30:58 +0000

    Add package pm-slack/0.2.12/pm_slack-0.2.12.tar.gz

---
+++ packages/pm-slack/pm_slack-0.2.12.tar.gz/pm_slack-0.2.12/pm_slack/templates/__init__.py
@@ -0,0 +1,17 @@
+from ._templates import (
+    CantSaveOrderTemplate,
+    FutureOrderTemplate,
+    NoOrdersFromBrandTemplate,
+    OrderCantConfirmedTemplate,
+    PlatformConnectionsFailedTemplate,
+    Template,
+)
+
+__all__ = [
+    "Template",
+    "FutureOrderTemplate",
+    "NoOrdersFromBrandTemplate",
+    "CantSaveOrderTemplate",
+    "OrderCantConfirmedTemplate",
+    "PlatformConnectionsFailedTemplate",
+]
