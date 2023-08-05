7d5dab05981b86fc9d91a98b967c98309819dcef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:06:41 +0000

    Add package academyAI/1.0.3/academyAI-1.0.3.tar.gz

---
+++ packages/academyAI/academyAI-1.0.3.tar.gz/academyAI-1.0.3/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+setup(
+    name='academyAI',
+    version='1.0.3',
+    description='Academy 4 Summer Course',
+    py_modules=["academyAI"],
+    package_dir={'':'src'},
+)
