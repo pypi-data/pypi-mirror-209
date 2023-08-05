bb37f8682d70338623683ccc023165b1e22fedf4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:06:48 +0000

    Add package pmbootstrap/1.53.0/pmbootstrap-1.53.0-py3-none-any.whl

---
+++ packages/pmbootstrap/pmbootstrap-1.53.0-py3-none-any.whl/pmb/aportgen/binutils.py
@@ -0,0 +1,27 @@
+# Copyright 2023 Oliver Smith
+# SPDX-License-Identifier: GPL-3.0-or-later
+import pmb.aportgen.core
+import pmb.helpers.git
+import pmb.helpers.run
+
+
+def generate(args, pkgname):
+    # Copy original aport
+    arch = pkgname.split("-")[1]
+    upstream = pmb.aportgen.core.get_upstream_aport(args, "binutils")
+    pmb.helpers.run.user(args, ["cp", "-r", upstream, args.work + "/aportgen"])
+
+    # Rewrite APKBUILD
+    fields = {
+        "pkgname": pkgname,
+        "pkgdesc": f"Tools necessary to build programs for {arch} targets",
+        "arch": pmb.config.arch_native,
+    }
+
+    below_header = """
+        CTARGET_ARCH=""" + arch + """
+        CTARGET="$(arch_to_hostspec $CTARGET_ARCH)"
+    """
+
+    pmb.aportgen.core.rewrite(args, pkgname, "main/binutils", fields,
+                              "binutils", below_header=below_header)
