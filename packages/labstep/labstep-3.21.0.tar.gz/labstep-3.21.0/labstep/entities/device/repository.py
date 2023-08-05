60e2af6682c80da6904075caaae2593ad1905f79
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:26:18 +0000

    Add package labstep/3.21.0/labstep-3.21.0.tar.gz

---
+++ packages/labstep/labstep-3.21.0.tar.gz/labstep-3.21.0/labstep/entities/device/repository.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# Author: Labstep <dev@labstep.com>
+
+from labstep.entities.device.model import Device
+import labstep.generic.entity.repository as entityRepository
+from labstep.constants import UNSPECIFIED
+
+
+def getDevice(user, device_id):
+    return entityRepository.getEntity(user, Device, id=device_id)
+
+
+def getDevices(user, count=100, search_query=UNSPECIFIED, extraParams={}):
+    params = {"search_query": search_query, **extraParams}
+    return entityRepository.getEntities(user, Device, count, params)
+
+
+def newDevice(user, name, extraParams={}):
+    params = {"name": name, **extraParams}
+    return entityRepository.newEntity(user, Device, params)
+
+
+def editDevice(device, name=UNSPECIFIED, deleted_at=UNSPECIFIED, extraParams={}):
+    params = {"name": name, "deleted_at": deleted_at, **extraParams}
+    return entityRepository.editEntity(device, params)
---
+++ packages/labstep/labstep-3.21.0.tar.gz/labstep-3.21.0/labstep/entities/organizationUser/repository.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# Author: Labstep <dev@labstep.com>
+
+from labstep.service.helpers import getTime
+from labstep.entities.organizationUser.model import OrganizationUser
+import labstep.generic.entity.repository as entityRepository
+from labstep.entities.user.model import User
+
+
+def getOrganizationUsers(organization, count=100, extraParams={}):
+
+    params = {
+        'organization_id': organization.id,
+        **extraParams,
+    }
+
+    return entityRepository.getEntities(
+        organization.__user__,
+        OrganizationUser, count,
+        filterParams=params)
+
+
+def promoteUser(organizationUser):
+    params = {
+        "type": "admin",
+    }
+    return entityRepository.editEntity(organizationUser, params)
+
+
+def demoteUser(organizationUser):
+    params = {
+        "type": "member",
+    }
+    return entityRepository.editEntity(organizationUser, params)
+
+
+def disableUser(organizationUser):
+    params = {
+        "deleted_at": getTime(),
+    }
+    user = User({"id": organizationUser.user['username']})
+    user.__user__ = organizationUser.__user__
+    return entityRepository.editEntity(user, params)
