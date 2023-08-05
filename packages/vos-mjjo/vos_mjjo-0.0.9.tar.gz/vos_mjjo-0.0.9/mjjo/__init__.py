488da6f5eadedbe21a0f3f3c639aa8ef4529be6f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:08:51 +0000

    Add package vos-mjjo/0.0.9/vos_mjjo-0.0.9.tar.gz

---
+++ packages/vos-mjjo/vos_mjjo-0.0.9.tar.gz/vos_mjjo-0.0.9/mjjo/__init__.py
@@ -0,0 +1,13 @@
+# /package/__init__.py
+"""
+Description for Package
+"""
+
+from mjjo.cordate import (
+    CorDate,
+    get_correct_array,
+    get_correct_one
+)
+
+__all__ = ['mjjo']
+__version__ = '0.0.8' 
