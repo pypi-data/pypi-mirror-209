8ec4b6eb9a5d1bfee64cb1016b3e57dcb2d9778b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:30:02 +0000

    Add package tab-aaron-alphabet/0.0.1/tab_aaron_alphabet-0.0.1-py3-none-any.whl

---
+++ packages/tab-aaron-alphabet/tab_aaron_alphabet-0.0.1-py3-none-any.whl/tab_aaron_alphabet/__init__.py
@@ -0,0 +1,22 @@
+import argparse as _argparse
+import os_aaron_alphabet as _os
+
+parser = _argparse.ArgumentParser(
+    fromfile_prefix_chars='@',
+    allow_abbrev=False,
+)
+parser.add_argument('targets', nargs='*')
+
+def main(args=None):
+    ns = parser.parse_args(args)
+    if len(ns.targets):
+        targets = ns.targets
+    else:
+        targets = ['.']
+    files = _os.walk(targets)
+    for file in targets:
+        with open(file, 'r') as s:
+            text = s.read()
+        text = text.replace('\t', "    ")
+        with open(file, 'w') as s:
+            s.write(text) 
