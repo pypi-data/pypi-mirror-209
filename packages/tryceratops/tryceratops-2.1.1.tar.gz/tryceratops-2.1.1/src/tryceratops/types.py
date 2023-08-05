1c0989078dcf8b614cabd9e3f8f4e9a7ae5a4e0d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:28:17 +0000

    Add package tryceratops/2.1.1/tryceratops-2.1.1.tar.gz

---
+++ packages/tryceratops/tryceratops-2.1.1.tar.gz/tryceratops-2.1.1/src/tryceratops/types.py
@@ -0,0 +1,21 @@
+import ast
+from typing import Collection, List, Tuple, TypedDict
+
+from tryceratops.filters import FileFilter
+
+ParsedFileType = Tuple[str, ast.AST, FileFilter]
+ParsedFilesType = Collection[ParsedFileType]
+
+
+class PyprojectConfig(TypedDict):
+    """
+    Represents the expected pyproject config to be loaded
+        exclude: a list of path patterns to be excluded e.g. [/tests, /fixtures]
+        ignore: a list of violations to be completely ignored e.g. [TRY002, TRY300]
+        experimental: whether to enable experimental analyzers
+    """
+
+    exclude: List[str]
+    ignore: List[str]
+    experimental: bool
+    autofix: bool
