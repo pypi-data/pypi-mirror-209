3401ef46754d7f4b82fed16ecc255dc36acef6c4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:10:50 +0000

    Add package mergepythonclient/0.0.1/mergepythonclient-0.0.1-py3-none-any.whl

---
+++ packages/mergepythonclient/mergepythonclient-0.0.1-py3-none-any.whl/merge/types/shared/remote_key.py
@@ -0,0 +1,12 @@
+# File generated from our OpenAPI spec by Stainless.
+
+
+from ..._models import BaseModel
+
+__all__ = ["RemoteKey"]
+
+
+class RemoteKey(BaseModel):
+    key: str
+
+    name: str
