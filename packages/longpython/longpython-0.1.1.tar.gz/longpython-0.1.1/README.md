cc9f014836638a1b62401c449e08c6959e7d390c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:30:27 +0000

    Add package longpython/0.1.1/longpython-0.1.1.tar.gz

---
+++ packages/longpython/longpython-0.1.1.tar.gz/longpython-0.1.1/README.md
@@ -0,0 +1,64 @@
+# longpython
+
+[![PyPI](https://img.shields.io/pypi/v/longpython?color=blue)](https://pypi.org/project/longpython/)
+
+_looooooooooooooooooooong python!_
+
+## Usage
+
+```shellsession
+$ longpython
+  _
+(" ヽ
+   \ \
+   / /
+   \ \ _ ,
+    \___/
+
+$ longpython -h
+usage: longpython [-h] [-l INT]
+
+CLI tool to print long python
+
+optional arguments:
+  -h, --help            show this help message and exit
+  -l INT, --length INT  length of python (default: 1)
+```
+
+## Installation
+
+longpython requires python 3.9 or later.
+
+### Via pipx (recommended)
+
+I recommend using [pipx](https://github.com/pypa/pipx) to install this project.
+
+```sh
+pipx install longpython
+```
+
+### Via pip
+
+```sh
+pip install longpython
+```
+
+### Download Single File
+
+The project does not have any dependencies, you can download
+[this file](./longpython/main.py) and use as soon as.
+
+## Derived Projects
+
+- [syumai/longify](https://github.com/syumai/longify): A command to output
+  longified any ascii art
+- [arrow2nd/longdeno](https://github.com/arrow2nd/longdeno):
+  Looooooooooooooooooooooooooooooooooooooooooooooong [Deno](https://deno.land)
+- [ikanago/longferris](https://github.com/ikanago/longferris): Long
+  [Ferris](https://github.com/ciusji/ferris) written in Rust
+- [sheepla/longgopher](https://github.com/sheepla/longgopher): ʕ◉ϖ◉ʔ
+  loooooooooooooooooooooong gopher
+
+## License
+
+MIT
---
+++ packages/longpython/longpython-0.1.1.tar.gz/longpython-0.1.1/longpython/main.py
@@ -0,0 +1,69 @@
+"""CLI tool to print long python."""
+
+import argparse
+from textwrap import dedent
+
+HEAD = dedent(
+    r"""
+      _
+    (" ヽ
+       \ \
+    """
+).strip("\n")
+BODY = (
+    "   / /",
+    r"   \ \ ".rstrip(),
+)
+TAIL = dedent(
+    r"""
+    {0}   \ \ _ ,
+    {0}    \___/
+    """
+).strip("\n")
+
+
+def check_natural(v: str) -> int:
+    v_int = int(v)
+    if v_int < 0:
+        raise argparse.ArgumentTypeError("length should be greater than or equal to 0.")
+    return v_int
+
+
+def parse_args() -> argparse.Namespace:
+    """Parse CLI arguments."""
+    parser = argparse.ArgumentParser(
+        prog="longpython",
+        description="CLI tool to print long python",
+    )
+    parser.add_argument(
+        "-l",
+        "--length",
+        metavar="INT",
+        type=check_natural,
+        default=1,
+        help="length of python (default: %(default)r)",
+    )
+    return parser.parse_args()
+
+
+def generate_python(length: int) -> str:
+    """Return an ascii art of python."""
+    lines = []
+    lines.append(HEAD)
+    for x in range(length):
+        lines.append(BODY[x % 2])
+    if len(lines) % 2 == 1:
+        lines.append(TAIL.format(" "))
+    else:
+        lines.append(TAIL.format(""))
+    return "\n".join(lines)
+
+
+def main() -> None:
+    """Print an ascii art of python."""
+    args = parse_args()
+    print(generate_python(args.length))
+
+
+if __name__ == "__main__":
+    main()
