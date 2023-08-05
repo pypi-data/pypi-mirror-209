2451e346de365ad04e59db035fd950dee3d72d9a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:24:58 +0000

    Add package flight-maneuvers/1.0.1/flight_maneuvers-1.0.1.tar.gz

---
+++ packages/flight-maneuvers/flight_maneuvers-1.0.1.tar.gz/flight_maneuvers-1.0.1/flight_maneuvers/modules/se2_resnet.py
@@ -0,0 +1,16 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from flight_maneuvers.utils import *
+
+
+class SE2_ResNetBlock(torch.nn.Module):
+    pass
+
+
+class SE2_PreActResNetBlock(torch.nn.Module):
+    pass
+
+
+class SE2_ResNet(torch.nn.Module):
+    pass
