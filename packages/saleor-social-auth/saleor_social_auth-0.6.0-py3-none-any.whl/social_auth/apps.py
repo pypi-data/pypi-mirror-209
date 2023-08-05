baf31b7a0d03b3cd4de33d3faa42361a96a4ff7c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:10:13 +0000

    Add package saleor-social-auth/0.6.0/saleor_social_auth-0.6.0-py3-none-any.whl

---
+++ packages/saleor-social-auth/saleor_social_auth-0.6.0-py3-none-any.whl/social_auth/apps.py
@@ -0,0 +1,45 @@
+from collections import OrderedDict
+import logging
+import os
+import time
+from django.apps import apps, AppConfig
+from django.conf import settings
+from django.core import management
+
+logger = logging.getLogger(__name__)
+
+class SocialAuthConfig(AppConfig):
+    name = 'social_auth'
+    verbose_name = "Social Auth"
+
+    def do_db_init_stuff(self):
+        start_time = time.time()
+        # for patch_app_name in patch_app_names:
+        #     management.call_command('migrate', patch_app_name.rsplit('.', maxsplit=1)[-1], interactive=False)
+        management.call_command('migrate', interactive=False)
+        logger.info('python manage.py migrate %s, elasped: %s sec', patch_app_names, time.time() - start_time)
+
+    def ready(self):
+        patch_app_names = ('social_django', 'django.contrib.sessions', )
+        patch_app_installed = apps.is_installed(patch_app_names[0])
+
+        if patch_app_installed:
+            logger.info('SocialAuthConfig.ready, app: %s, is_installed: %s', patch_app_names[0], patch_app_installed)
+        else:
+            logger.info('SocialAuthConfig.ready, app: %s, is_installed: %s, doing initialization...', patch_app_names[0], patch_app_installed)
+            # refer to
+            # https://stackoverflow.com/questions/24027901/dynamically-loading-django-apps-at-runtime#answer-57897422
+            settings.INSTALLED_APPS += patch_app_names
+            apps.app_configs = OrderedDict()
+            apps.apps_ready = apps.models_ready = apps.loading = apps.ready = False
+            apps.clear_cache()
+            apps.populate(settings.INSTALLED_APPS)
+
+        init_str = os.environ.get("SOCIAL_AUTH_DB_INIT") or ''
+        if init_str.lower() in ['true', '1', 't', 'y', 'yes', 'yeah', 'yup', 'certainly', 'uh-huh']:
+            self.do_db_init_stuff()
+        else:
+            logger.info(
+                'SocialAuthConfig no db init. \n'
+                'Try setting env variable `SOCIAL_AUTH_DB_INIT=true` to do the db init stuff.'
+            )
---
+++ packages/saleor-social-auth/saleor_social_auth-0.6.0-py3-none-any.whl/social_auth/strategy.py
@@ -0,0 +1,34 @@
+from django.shortcuts import resolve_url
+from django.utils.encoding import force_str
+from django.utils.functional import Promise
+from social_django.strategy import DjangoStrategy
+
+class SaleorPluginStrategy(DjangoStrategy):
+
+    def __init__(self, storage, settings, request_data, *args, **kwargs):
+        # loading settings from dashboard admin
+        self.settings = settings
+        self.req_data = request_data
+        super().__init__(storage, *args, **kwargs)
+
+    def get_setting(self, name):
+        # copy from social_django.strategy.DjangoStrategy
+        value = self.settings[name]
+        # Force text on URL named settings that are instance of Promise
+        if name.endswith('_URL'):
+            if isinstance(value, Promise):
+                value = force_str(value)
+            value = resolve_url(value)
+        return value
+
+    def request_data(self, merge=True):
+        # graphql may not include query_string or data in request directly
+        return self.req_data
+
+    def authenticate(self, backend, *args, **kwargs):
+        kwargs['strategy'] = self
+        kwargs['storage'] = self.storage
+        kwargs['backend'] = backend
+        # we need to assign request as `args` first element according to debug
+        args = args + (kwargs.pop('request'), )
+        return backend.authenticate(*args, **kwargs)
