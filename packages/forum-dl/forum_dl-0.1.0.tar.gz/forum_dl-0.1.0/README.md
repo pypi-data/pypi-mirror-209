6787017e1be74356a7b22260e7766201f87e9abe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:09:02 +0000

    Add package forum-dl/0.1.0/forum_dl-0.1.0.tar.gz

---
+++ packages/forum-dl/forum_dl-0.1.0.tar.gz/forum_dl-0.1.0/README.md
@@ -0,0 +1,105 @@
+# forum-dl
+
+Forum-dl is a downloader (scraper) for forums, mailing lists, and news aggregators. It can be used to crawl, extract, and archive individual threads and entire boards into a variety of output formats.
+
+# Installation
+
+Clone the repository:
+
+```
+git clone https://github.com/mikwielgus/forum-dl
+```
+
+Then, in the same directory, install the repository directly: 
+
+```
+pip install -e forum-dl
+```
+
+# Quick start
+
+Download a Simple Machines forum thread in JSONL format:
+
+```
+forum-dl "https://www.simplemachines.org/community/index.php?topic=584230.0"
+```
+
+Download an entire PhpBB forum board in JSONL format, write to stdout (`-o -`).
+
+```
+forum-dl -o - "https://www.phpbb.com/community/viewforum.php?f=696"
+```
+
+<sub>(due to current architecture limitations, `forum-dl` will shallowly scan the entire forum hierarchy before downloading the board. This will be fixed in future releases)</sub>
+
+Download Hacker News top stories and write them to a Maildir directory `hn`:
+
+```
+forum-dl --textify --content-as-title -f maildir -o hn "https://news.ycombinator.com/news"
+```
+
+- `--textify` converts HTML to plaintext (useful for text-only mail clients),
+- `--content-as-title` puts the beginning of each message's content in its title (useful for mail clients that don't display content in index view),
+- `-f maildir` changes the output format to `maildir`,
+- `-o hn` changes the output directory name to `hn`.
+
+# What is supported
+
+## Forum software
+
+- Discourse
+- Hacker News
+- Hyperkitty
+- Hypermail
+- Invision Power Board
+- PhpBB
+- Pipermail
+- Proboards
+- Simple Machines Forum
+- vBulletin
+- Xenforo
+
+## Output formats
+
+- Babyl
+- JSONL
+- Maildir
+- Mbox
+- MH
+- MMDF
+
+# Usage
+
+```
+forum-dl [--help] [--version] [--list-extractors] [--list-output-formats] [--user-agent USER_AGENT] [-q] [-v] [-g] [-o FILE]
+         [-f FORMAT] [--no-boards] [--no-threads] [--no-posts] [--textify] [--content-as-title]
+```
+
+## General Options:
+
+```
+  --help                Show this help message and exit
+  --version             Print program version and exit
+  --list-extractors     List all supported extractors and exit
+  --list-output-formats
+                        List all supported output formats and exit
+  --user-agent USER_AGENT
+                        User-Agent request header
+```
+
+## Output Options:
+
+```
+  -q, --quiet           Activate quiet mode
+  -v, --verbose         Print various debugging information
+  -g, --get-urls        Print URLs instead of downloading
+  -o FILE, --output FILE
+                        Output all results concatenated to FILE, or stdout if FILE is -
+  -f FORMAT, --output-format FORMAT
+                        Output format
+  --no-boards           Do not write board objects
+  --no-threads          Do not write thread objects
+  --no-posts            Do not write post objects
+  --textify             Lossily convert HTML content to plaintext
+  --content-as-title    Write 98 initial characters of content in title field of each post
+```
---
+++ packages/forum-dl/forum_dl-0.1.0.tar.gz/forum_dl-0.1.0/forum_dl/options.py
@@ -0,0 +1,126 @@
+# pyright: strict
+from __future__ import annotations
+from typing import *  # type: ignore
+
+import argparse
+import logging
+
+from .version import __version__
+
+
+def build_parser():
+    parser = argparse.ArgumentParser(
+        add_help=False,
+    )
+
+    general = parser.add_argument_group("General Options")
+    general.add_argument(
+        "--help",
+        action="help",
+        help="Show this help message and exit",
+    )
+    general.add_argument(
+        "--version",
+        action="version",
+        version=__version__,
+        help="Print program version and exit",
+    )
+    general.add_argument(
+        "--list-extractors",
+        dest="list_extractors",
+        action="store_true",
+        help="List all supported extractors and exit",
+    )
+    general.add_argument(
+        "--list-output-formats",
+        dest="list_output_formats",
+        action="store_true",
+        help="List all supported output formats and exit",
+    )
+
+    general.add_argument(
+        "--user-agent",
+        dest="user_agent",
+        default=f"Forum-dl {__version__}",
+        help="User-Agent request header",
+    )
+
+    output = parser.add_argument_group("Output Options")
+    output.add_argument(
+        "-q",
+        "--quiet",
+        dest="loglevel",
+        default=logging.INFO,
+        action="store_const",
+        const=logging.ERROR,
+        help="Activate quiet mode",
+    )
+    output.add_argument(
+        "-v",
+        "--verbose",
+        dest="loglevel",
+        action="store_const",
+        const=logging.DEBUG,
+        help="Print various debugging information",
+    )
+    output.add_argument(
+        "-g",
+        "--get-urls",
+        dest="get_urls",
+        action="store_true",
+        help="Print URLs instead of downloading",
+    )
+    output.add_argument(
+        "-o",
+        "--output",
+        metavar="FILE",
+        dest="output",
+        help="Output all results concatenated to FILE, or stdout if FILE is -",
+    )
+    output.add_argument(
+        "-f",
+        "--output-format",
+        metavar="FORMAT",
+        dest="output_format",
+        default="jsonl",
+        help="Output format. Use --list-output-formats for a list of possible arguments",
+    )
+    output.add_argument(
+        "--no-boards",
+        dest="boards",
+        action="store_false",
+        help="Do not write board objects",
+    )
+    output.add_argument(
+        "--no-threads",
+        dest="threads",
+        action="store_false",
+        help="Do not write thread objects",
+    )
+    output.add_argument(
+        "--no-posts",
+        dest="posts",
+        action="store_false",
+        help="Do not write post objects",
+    )
+    output.add_argument(
+        "--textify",
+        dest="textify",
+        action="store_true",
+        help="Lossily convert HTML content to plaintext",
+    )
+    output.add_argument(
+        "--content-as-title",
+        dest="content_as_title",
+        action="store_true",
+        help="Write 98 initial characters of content in title field of each post",
+    )
+
+    parser.add_argument(
+        "urls",
+        metavar="URL",
+        nargs="*",
+        help=argparse.SUPPRESS,
+    )
+
+    return parser
