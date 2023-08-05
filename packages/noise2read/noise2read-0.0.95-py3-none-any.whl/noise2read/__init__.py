020d6921359c66cc15f9287b7b829d944f20c328
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:09:21 +0000

    Add package noise2read/0.0.95/noise2read-0.0.95-py3-none-any.whl

---
+++ packages/noise2read/noise2read-0.0.95-py3-none-any.whl/noise2read/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+# @Author: Pengyao Ping
+# @Date:   2023-01-24 15:47:23
+# @Last Modified by:   Pengyao Ping
+# @Last Modified time: 2023-05-19 10:53:35
+
+__version__ = "0.0.95"
