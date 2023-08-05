b88dfe3a3f6beae2b1b9a1d843f83c407f4f5a5f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:01:59 +0000

    Add package yalexs/1.4.5/yalexs-1.4.5.tar.gz

---
+++ packages/yalexs/yalexs-1.4.5.tar.gz/yalexs-1.4.5/setup.py
@@ -0,0 +1,33 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="yalexs",
+    version="1.4.5",
+    python_requires=">=3.9",
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Natural Language :: English",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+    ],
+    url="https://github.com/bdraco/yalexs",
+    license="MIT",
+    author="bdraco",
+    author_email="nick@koston.org",
+    description="Python API for Yale Access (formerly August) Smart Lock and Doorbell",
+    packages=find_packages(include=["yalexs", "yalexs.backports", "yalexs.*"]),
+    install_requires=[
+        "pyjwt",
+        "requests",
+        "vol",
+        "python-dateutil",
+        "aiohttp",
+        "aiofiles",
+        "pubnub>=7.1.0",
+        "typing_extensions>=4.5.0"
+    ],
+)
