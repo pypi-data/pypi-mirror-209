eb4de7f574efb31fcf38cd1d293a3975cd9a689d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:59:26 +0000

    Add package halig/0.3.2/halig-0.3.2.tar.gz

---
+++ packages/halig/halig-0.3.2.tar.gz/halig-0.3.2/halig/commands/base.py
@@ -0,0 +1,14 @@
+from abc import ABC, abstractmethod
+
+from halig.settings import Settings
+
+
+class ICommand(ABC):
+    @abstractmethod
+    def run(self):
+        ...  # pragma: no cover
+
+
+class BaseCommand(ICommand):
+    def __init__(self, settings: Settings, *args, **kwargs):
+        self.settings = settings
