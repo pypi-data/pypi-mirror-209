65b318cf2685b9c044ec5947d5da7ed3ecacec35
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:08:49 +0000

    Add package pypura/0.1.0/pypura-0.1.0.tar.gz

---
+++ packages/pypura/pypura-0.1.0.tar.gz/pypura-0.1.0/pypura/const.py
@@ -0,0 +1,22 @@
+"""Constants."""
+from __future__ import annotations
+
+from typing import Final
+
+USER_POOL_ID: Final = "dXMtZWFzdC0xX0xhQjcxOGhZdg=="
+CLIENT_ID: Final = "NGlla3ViYXQwamI1aWxqZmJhYWxzaXFmOWo="
+
+FRAGRANCES: Final[dict[str, str]] = {
+    "AJV": "Birchwood Pine",
+    "BMC": "Balsam & Cedar",
+    "JRG": "Elsa Journeys to Ahtohallan",
+    "JZB": "Velvet Wineberry",
+    "JZJ": "Egyptian Sandalwood",
+    "KDR": "Ocean Mist & Sea Salt",
+    "KYV": "Tuscan Neroli",
+    "LHT": "Anna & Olaf in the Enchanted Forest",
+    "MBZ": "Pumpkin Chai",
+    "TWL": "Blue Water Lotus",
+    "UDZ": "Bamboo",
+    "WRT": "Charcoal",
+}
