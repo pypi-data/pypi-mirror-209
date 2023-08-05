fee65dee492695765db22a922a9dcef055780c14
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:25:25 +0000

    Add package canals/0.2.0/canals-0.2.0-py3-none-any.whl

---
+++ packages/canals/canals-0.2.0-py3-none-any.whl/canals/pipeline/__init__.py
@@ -0,0 +1,16 @@
+# SPDX-FileCopyrightText: 2022-present deepset GmbH <info@deepset.ai>
+#
+# SPDX-License-Identifier: Apache-2.0
+from canals.pipeline.pipeline import Pipeline
+from canals.errors import (
+    PipelineError,
+    PipelineRuntimeError,
+    PipelineValidationError,
+)
+from canals.pipeline.save_load import (
+    save_pipelines,
+    load_pipelines,
+    marshal_pipelines,
+    unmarshal_pipelines,
+    _find_decorated_classes,
+)
