d9860146c4c4632b10bc89529eed006a94232daa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:04:18 +0000

    Add package dbt-coves/1.4.18/dbt_coves-1.4.18-py3-none-any.whl

---
+++ packages/dbt-coves/dbt_coves-1.4.18-py3-none-any.whl/dbt_coves/tasks/setup/main.py
@@ -0,0 +1,42 @@
+from rich.console import Console
+
+from dbt_coves.tasks.base import NonDbtBaseTask
+
+from .all import SetupAllTask
+from .dbt import SetupDbtTask
+from .git import SetupGitTask
+from .pre_commit import SetupPrecommitTask
+from .ssh import SetupSSHTask
+
+console = Console()
+
+
+class DbtCovesSetupException(Exception):
+    pass
+
+
+class SetupTask(NonDbtBaseTask):
+    """
+    Task that code-gen dbt resources
+    """
+
+    key_column_with = 20
+    value_column_with = 50
+    arg_parser = None
+
+    @classmethod
+    def register_parser(cls, sub_parsers, base_subparser):
+        ext_subparser = sub_parsers.add_parser(
+            "setup",
+            parents=[base_subparser],
+            help="Set up project components (git, dbt, vscode, sqlfluff, pre-commit, etc)",
+        )
+        ext_subparser.set_defaults(cls=cls, which="setup")
+        sub_parsers = ext_subparser.add_subparsers(title="dbt-coves setup commands", dest="task")
+        SetupAllTask.register_parser(sub_parsers, base_subparser)
+        SetupGitTask.register_parser(sub_parsers, base_subparser)
+        SetupDbtTask.register_parser(sub_parsers, base_subparser)
+        SetupSSHTask.register_parser(sub_parsers, base_subparser)
+        SetupPrecommitTask.register_parser(sub_parsers, base_subparser)
+        cls.arg_parser = ext_subparser
+        return ext_subparser
