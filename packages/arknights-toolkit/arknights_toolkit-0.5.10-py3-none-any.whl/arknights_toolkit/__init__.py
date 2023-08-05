834d69d59331cc206ac9cca8408386ed907b12b3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:08:05 +0000

    Add package arknights-toolkit/0.5.10/arknights_toolkit-0.5.10-py3-none-any.whl

---
+++ packages/arknights-toolkit/arknights_toolkit-0.5.10-py3-none-any.whl/arknights_toolkit/__init__.py
@@ -0,0 +1,17 @@
+from pathlib import Path
+from packaging import version
+from .gacha import ArknightsGacha, GachaUser
+from .random_operator import RandomOperator
+from .recruit import recruitment
+
+__version__ = "0.5.10"
+
+
+def need_init():
+    file = Path(__file__).parent / "resource" / "ops_initialized"
+    with file.open("r", encoding="utf-8") as f:
+        target = version.parse(f.read())
+    source = version.parse(__version__)
+    return not file.exists() or (
+        target.major < source.major or target.minor < source.minor
+    )
