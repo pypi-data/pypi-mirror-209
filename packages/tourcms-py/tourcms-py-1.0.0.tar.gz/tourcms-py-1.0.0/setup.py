36e23fb10afd8daba2e69d1594bd61b2dcb5fee9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:13:34 +0000

    Add package tourcms-py/1.0.0/tourcms-py-1.0.0.tar.gz

---
+++ packages/tourcms-py/tourcms-py-1.0.0.tar.gz/tourcms-py-1.0.0/setup.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+from distutils.core import setup
+import tourcms
+
+
+setup(name='tourcms-py',
+      version=tourcms.__version__,
+      description="Python wrapper class for TourCMS Rest API",
+      long_description="A simple wrapper for connecting to the TourCMS Marketplace API (http://www.tourcms.com/support/api/mp/). Forked from /prio/tourcms. This wrapper mirrors the TourCMS PHP library. See https://github.com/TourCMS/tourcms-py for more details.",
+      author=tourcms.__author__,
+      author_email='support@palisis.com',
+      url='https://github.com/TourCMS/tourcms-py',
+      download_url='https://github.com/TourCMS/tourcms-py',
+      license=tourcms.__license__,
+      platforms=['all'],
+      classifiers=[
+          'Intended Audience :: Developers',
+          'License :: OSI Approved :: BSD License',
+          'Operating System :: OS Independent',
+          'Programming Language :: Python',
+          'Programming Language :: Python :: 2.6',
+          'Programming Language :: Python :: 2.7',
+          'Programming Language :: Python :: 3',
+      ],
+      py_modules=['tourcms'],
+      )
