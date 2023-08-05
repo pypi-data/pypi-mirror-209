85b05b6666a355a80963673cadab20db2d38c5dd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:08:45 +0000

    Add package eule/1.0.0/eule-1.0.0.tar.gz

---
+++ packages/eule/eule-1.0.0.tar.gz/eule-1.0.0/eule/__init__.py
@@ -0,0 +1,10 @@
+""" Package Eule  """
+
+# -*- coding: utf-8 -*-
+
+from __future__ import annotations
+
+from .eule import euler, euler_generator, euler_keys, euler_boundaries
+
+__author__ = """Bruno Peixoto"""
+__email__ = 'brunolnetto@gmail.com'
