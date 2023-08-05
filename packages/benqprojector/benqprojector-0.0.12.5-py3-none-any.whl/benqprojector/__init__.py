edd9e79438e67fbeca390f23d42aab76dd33729d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:01:42 +0000

    Add package benqprojector/0.0.12.5/benqprojector-0.0.12.5-py3-none-any.whl

---
+++ packages/benqprojector/benqprojector-0.0.12.5-py3-none-any.whl/benqprojector/__init__.py
@@ -0,0 +1,10 @@
+"""
+Implements the BenQ projector library for controlling BenQ projector over the serial interface.
+
+Created on 27 Nov 2022
+
+@author: Rogier van Staveren
+"""
+__version__ = "0.0.12.5"
+
+from benqprojector.benqprojector import BAUD_RATES, BenQProjector
