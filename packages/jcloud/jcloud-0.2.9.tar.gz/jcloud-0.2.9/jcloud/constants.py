cf6d856b44bf225d143160333072e08d6e26d43f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:24:12 +0000

    Add package jcloud/0.2.9/jcloud-0.2.9.tar.gz

---
+++ packages/jcloud/jcloud-0.2.9.tar.gz/jcloud-0.2.9/jcloud/constants.py
@@ -0,0 +1,37 @@
+import os
+from enum import Enum
+from pathlib import Path
+from typing import Dict, Optional
+
+JCLOUD_API = os.getenv('JCLOUD_API', 'https://api-v2.wolf.jina.ai/')
+FLOWS_API = os.path.join(JCLOUD_API, 'flows')
+
+
+class Phase(str, Enum):
+    Empty = ''
+    Pending = 'Pending'
+    Starting = 'Starting'
+    Serving = 'Serving'
+    Failed = 'Failed'
+    Updating = 'Updating'
+    Deleted = 'Deleted'
+    Paused = 'Paused'
+
+
+class CONSTANTS:
+    DEFAULT_FLOW_FILENAME = 'flow.yml'
+    DEFAULT_ENV_FILENAME = '.env'
+    NORMED_FLOWS_DIR = Path('/tmp/flows')
+
+
+class CustomAction(str, Enum):
+    NoAction = ''
+    Restart = 'restart'
+    Pause = 'pause'
+    Resume = 'resume'
+    Scale = 'scale'
+    Recreate = 'recreate'
+
+
+def get_phase_from_response(response: Dict) -> Optional[Phase]:
+    return Phase(response.get('status', {}).get('phase'))
