9265059318e509c75e71ad3c83d0c989f7dbf0a7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:12:57 +0000

    Add package mycalcpackage/0.1.0/mycalcpackage-0.1.0.tar.gz

---
+++ packages/mycalcpackage/mycalcpackage-0.1.0.tar.gz/mycalcpackage-0.1.0/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+
+setup(
+    name='mycalcpackage',
+    version='0.1.0',
+    description='A package for calculations',
+    author='Your Name',
+    author_email='your@email.com',
+    packages=['mycalcpackage'],
+    install_requires=['toml'],
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+    ],
+    entry_points={
+        'console_scripts': [
+            'mycalc = mycalcpackage.calculator:main',
+        ],
+    },
+)
