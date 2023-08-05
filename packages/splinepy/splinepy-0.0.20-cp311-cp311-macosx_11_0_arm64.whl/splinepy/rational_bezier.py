60c7cf79c31fcc620f8be3e1725b35298d8806a1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:07:55 +0000

    Add package splinepy/0.0.20/splinepy-0.0.20-cp311-cp311-macosx_11_0_arm64.whl

---
+++ packages/splinepy/splinepy-0.0.20-cp311-cp311-macosx_11_0_arm64.whl/splinepy/rational_bezier.py
@@ -0,0 +1,29 @@
+from splinepy.bezier import BezierBase
+
+
+class RationalBezier(BezierBase):
+    __slots__ = ()
+
+    def __init__(
+        self, degrees=None, control_points=None, weights=None, spline=None
+    ):
+        """
+        RationalBezier (Spline).
+
+        Parameters
+        -----------
+        degrees: (para_dim,) array-like
+        control_points: (m, dim) array-like
+        weights: (m) array-like
+        spline: Spline
+
+        Returns
+        --------
+        None
+        """
+        super().__init__(
+            spline=spline,
+            degrees=degrees,
+            control_points=control_points,
+            weights=weights,
+        )
