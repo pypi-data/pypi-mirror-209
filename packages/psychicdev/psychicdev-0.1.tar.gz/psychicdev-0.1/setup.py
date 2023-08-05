c0ab5f92d888782761f9471b638b8617e6615b25
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:08:30 +0000

    Add package psychicdev/0.1/psychicdev-0.1.tar.gz

---
+++ packages/psychicdev/psychicdev-0.1.tar.gz/psychicdev-0.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='psychicdev',
+    version='0.1',
+    description='Psychic.dev is an open-source universal data connector for knowledgebases.',
+    author='Ayan Bandyopadhyay',
+    author_email='ayan@getsidekick.ai',
+    packages=['psychicdev'],
+    install_requires=[
+        'requests',
+    ],
+)
