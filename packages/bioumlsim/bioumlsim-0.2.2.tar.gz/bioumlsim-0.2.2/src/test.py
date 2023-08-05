bccc76b727107f8e2b652052959745c0fd97bdc8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:10:23 +0000

    Add package bioumlsim/0.2.2/bioumlsim-0.2.2.tar.gz

---
+++ packages/bioumlsim/bioumlsim-0.2.2.tar.gz/bioumlsim-0.2.2/src/test.py
@@ -0,0 +1,15 @@
+import bioumlsim
+
+sim = bioumlsim.BioUMLSim()
+model = sim.load("C:/Users/Damag/BioUML_Scripts/models_selected/BIOMD0000000003.xml")
+result = model.simulate(100, 10)
+
+print(result)
+print()
+print(result.getTimes())
+print()
+print(result.getNames())
+print()
+print(result.getValues())
+print()
+print(result.getValues('X'))
