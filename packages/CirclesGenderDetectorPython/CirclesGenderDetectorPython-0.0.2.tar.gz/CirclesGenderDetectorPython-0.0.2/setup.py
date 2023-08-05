22c002b9c2191cc01416dc07e00cfd1eece4625a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:26:59 +0000

    Add package CirclesGenderDetectorPython/0.0.2/CirclesGenderDetectorPython-0.0.2.tar.gz

---
+++ packages/CirclesGenderDetectorPython/CirclesGenderDetectorPython-0.0.2.tar.gz/CirclesGenderDetectorPython-0.0.2/setup.py
@@ -0,0 +1,19 @@
+import setuptools
+
+print(setuptools.find_packages())
+setuptools.setup(
+     name='CirclesGenderDetectorPython',  
+     version='0.0.2',
+     author="Circles",
+     author_email="info@circles.zone",
+     description="PyPI Package for gender detection",
+     long_description="This is a package for running gender detection and predicting gender",
+     long_description_content_type="text/markdown",
+     url="https://github.com/circles-zone/gender-detection-backend",
+     packages=setuptools.find_packages(),
+     classifiers=[
+         "Programming Language :: Python :: 3",
+         "License :: Other/Proprietary License",
+         "Operating System :: OS Independent",
+     ],
+ )
