e3148e1c4fc405e7937f128e4af6061822a666b5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:13:11 +0000

    Add package render-engine/2023.5.2a2/render_engine-2023.5.2a2.tar.gz

---
+++ packages/render-engine/render_engine-2023.5.2a2.tar.gz/render_engine-2023.5.2a2/src/render_engine/blog.py
@@ -0,0 +1,26 @@
+from .collection import Collection
+from .feeds import RSSFeed
+from .page import Page
+from .parsers.markdown import MarkdownPageParser
+
+
+class BlogPost(Page):
+    """Page Like object with slight modifications to work with BlogPosts."""
+    invalid_attrs = ["slug"]
+
+
+class Blog(Collection):
+    """
+    Custom :py:class:`collection.Collection` class with archiving enabled, sort by `date` by default.
+
+    Todos:
+        - Add Support for JSON Feeds
+        - Rename the archive items so they are not private
+    """
+
+    BasePageParser = MarkdownPageParser
+    content_type: BlogPost = BlogPost
+    sort_reverse: bool = True
+    sort_by = "date"
+    has_archive = True
+    Feed = RSSFeed
