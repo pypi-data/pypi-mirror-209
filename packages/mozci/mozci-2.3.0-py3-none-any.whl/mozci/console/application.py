66fa45607d7e3830ca472021b7ba696c07e755e1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:04:31 +0000

    Add package mozci/2.3.0/mozci-2.3.0-py3-none-any.whl

---
+++ packages/mozci/mozci-2.3.0-py3-none-any.whl/mozci/console/application.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+
+import sys
+
+from cleo.application import Application
+
+from mozci.console.commands.batch_execution import BatchExecutionCommands
+from mozci.console.commands.check_backfills import CheckBackfillsCommand
+from mozci.console.commands.decision import DecisionCommand
+from mozci.console.commands.push import PushCommands
+
+
+def cli():
+    application = Application()
+    application.add(BatchExecutionCommands())
+    application.add(CheckBackfillsCommand())
+    application.add(PushCommands())
+    application.add(DecisionCommand())
+    application.run()
+
+
+if __name__ == "__main__":
+    sys.exit(cli())
