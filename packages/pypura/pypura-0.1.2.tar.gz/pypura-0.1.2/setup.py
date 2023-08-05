d064782251741eb77385f3b3f7fc7a6a7238b83e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:11:16 +0000

    Add package pypura/0.1.2/pypura-0.1.2.tar.gz

---
+++ packages/pypura/pypura-0.1.2.tar.gz/pypura-0.1.2/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['pypura']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['boto3>=1.26.135,<2.0.0', 'pycognito>=2022.12.0,<2023.0.0']
+
+setup_kwargs = {
+    'name': 'pypura',
+    'version': '0.1.2',
+    'description': 'Python package for interacting with Pura smart fragrance diffuser',
+    'long_description': '# pypura\nPython package for interacting with Pura smart fragrance diffusers\n',
+    'author': 'Nathan Spencer',
+    'author_email': 'natekspencer@gmail.com',
+    'maintainer': 'None',
+    'maintainer_email': 'None',
+    'url': 'None',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.8.1,<4.0.0',
+}
+
+
+setup(**setup_kwargs)
