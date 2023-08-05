d23806750f8aa1fabc0f5631e41547cd4fdc06d9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:06:21 +0000

    Add package interactivenlp/0.0.1/interactivenlp-0.0.1-py3-none-any.whl

---
+++ packages/interactivenlp/interactivenlp-0.0.1-py3-none-any.whl/__init__.py
@@ -0,0 +1,8 @@
+__title__ = "interactivenlp"
+__author__ = "Chunxu Yang"
+__license__ = "MIT"
+
+from .typedef.block import Block, BlockTypeEnum, BlockLevelEnum
+from .typedef.interactive import InteractiveFunctionType
+from .typedef.rss import InteractiveRssType
+from .web.server import Server
