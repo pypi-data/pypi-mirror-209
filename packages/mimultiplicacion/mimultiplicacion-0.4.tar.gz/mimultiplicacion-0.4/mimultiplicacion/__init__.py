40f6915bdcfddd12d48ce7861abd84f31072b976
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:08:50 +0000

    Add package mimultiplicacion/0.4/mimultiplicacion-0.4.tar.gz

---
+++ packages/mimultiplicacion/mimultiplicacion-0.4.tar.gz/mimultiplicacion-0.4/mimultiplicacion/__init__.py
@@ -0,0 +1,8 @@
+def saludo_inicial():
+    a = 'Hola, ¿Qué multiplicación quieres realizar?, Por ahora tenemos: multiplica_dos_numeros, que realiza la multiplicacion entre dos numeros'
+    return a
+    
+def multiplica_dos_numeros(a,b):
+    return a*b
+    
+    
---
+++ packages/mimultiplicacion/mimultiplicacion-0.4.tar.gz/mimultiplicacion-0.4/setup.py
@@ -0,0 +1,59 @@
+import setuptools
+
+with open('README.md', 'r') as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    # ######################################################################
+    # BASIC DESCRIPTION
+    # ######################################################################
+    name='mimultiplicacion',
+    author='Juani',
+    author_email='juanita.agudelo@udea.edu.co',
+    description='Make a multiplicacion',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://pypi.org/project/mimultiplicacion',
+    keywords='multiplication maths',
+    license='MIT',
+
+    # ######################################################################
+    # CLASSIFIER
+    # ######################################################################
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        ],
+    version='0.4',
+
+    # ######################################################################
+    # FILES
+    # ######################################################################
+    package_dir={'': '.'},
+    packages=setuptools.find_packages(where='.'),
+    
+    # ######################################################################
+    # ENTRY POINTS
+    # ######################################################################
+    entry_points={
+        'console_scripts': ['install=mimultiplicacion.install:main'],
+    },
+
+    # ######################################################################
+    # TESTS
+    # ######################################################################
+    test_suite='nose.collector',
+    tests_require=['nose'],
+
+    # ######################################################################
+    # DEPENDENCIES
+    # ######################################################################
+    install_requires=['numpy', 'matplotlib'],
+
+    # ######################################################################
+    # OPTIONS
+    # ######################################################################
+    include_package_data=True,
+    package_data={'': ['data/*.*', 'tests/*.*']},
+)
