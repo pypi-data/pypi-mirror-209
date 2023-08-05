98b728b0fdb56e361cc51ffc0215ecb69000ff92
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:29:27 +0000

    Add package jijzeptlab-client/0.2.0/jijzeptlab_client-0.2.0-cp38-cp38-manylinux_2_5_x86_64.manylinux1_x86_64.manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/jijzeptlab-client/jijzeptlab_client-0.2.0-cp38-cp38-manylinux_2_5_x86_64.manylinux1_x86_64.manylinux_2_17_x86_64.manylinux2014_x86_64.whl/jijzeptlab/__init__.py
@@ -0,0 +1,13 @@
+from jijzeptlab.jijzeptlab import FixedVariables, InstanceData
+from jijzeptlab.compile import CompileOption, CompiledInstance, compile_model
+from jijzeptlab import client, response
+
+__all__ = [
+    "client",
+    "response",
+    "FixedVariables",
+    "InstanceData",
+    "CompileOption",
+    "CompiledInstance",
+    "compile_model",
+]
