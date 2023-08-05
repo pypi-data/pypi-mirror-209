85b2ebaafb88831f4b44a9caf160bf27a438d3ff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:09:56 +0000

    Add package fairbench/0.2.0/fairbench-0.2.0-py3-none-any.whl

---
+++ packages/fairbench/fairbench-0.2.0-py3-none-any.whl/fairbench/__init__.py
@@ -0,0 +1,6 @@
+from fairbench.forks import *
+from fairbench.metrics import *
+from fairbench.reports import *
+from fairbench.export import *
+from fairbench.algorithms import *
+from fairbench.bench import *
---
+++ packages/fairbench/fairbench-0.2.0-py3-none-any.whl/fairbench/forks/explanation.py
@@ -0,0 +1,66 @@
+from typing import Any
+import eagerpy as ep
+
+
+def tofloat(value):
+    if isinstance(value, ep.Tensor):
+        return float(value.raw)
+    return float(value)
+
+
+class Explainable:
+    def __init__(self, value, explain: Any = None, desc: str = None, **kwargs):
+        from fairbench.forks import Fork
+
+        self.value = value
+        self.explain = Fork(kwargs) if explain is None else explain
+        self.desc = desc
+        if (
+            not isinstance(value, float)
+            and not isinstance(value, int)
+            and "tensor" not in value.__class__.__name__.lower()
+            and "array" not in value.__class__.__name__
+        ):
+            raise Exception("Can not set non-numeric as explainable", value)
+        if explain is not None and kwargs:
+            raise Exception("Cannot create explainable with both todict and a Fork")
+
+    def __float__(self):
+        return tofloat(self.value)
+
+    def numpy(self):
+        return self.value.numpy()
+
+    def __str__(self):
+        return str(self.value)
+
+    def __repr__(self):
+        return self.__str__()
+
+    def sum(self):
+        return self.value.sum()
+
+    def __sub__(self, other):
+        if isinstance(other, Explainable):
+            other = other.value
+        return self.value - other
+
+    def __mul__(self, other):
+        if isinstance(other, Explainable):
+            other = other.value
+        return self.value * other
+
+    def __rmul__(self, other):
+        if isinstance(other, Explainable):
+            other = other.value
+        return other * self.value
+
+    def __add__(self, other):
+        if isinstance(other, Explainable):
+            other = other.value
+        return self.value + other
+
+    def __radd__(self, other):
+        if isinstance(other, Explainable):
+            other = other.value
+        return other + self.value
