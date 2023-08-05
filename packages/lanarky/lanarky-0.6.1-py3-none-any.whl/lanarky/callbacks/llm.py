50d9769159897113a55b3f75d4fe21efc0630fa7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:07:20 +0000

    Add package lanarky/0.6.1/lanarky-0.6.1-py3-none-any.whl

---
+++ packages/lanarky/lanarky-0.6.1-py3-none-any.whl/lanarky/callbacks/llm.py
@@ -0,0 +1,37 @@
+from typing import Any
+
+from lanarky.register import register_streaming_callback, register_websocket_callback
+
+from .base import AsyncStreamingResponseCallback, AsyncWebsocketCallback
+
+
+@register_streaming_callback("LLMChain")
+class AsyncLLMChainStreamingCallback(AsyncStreamingResponseCallback):
+    """AsyncStreamingResponseCallback handler for LLMChain."""
+
+    async def on_llm_new_token(self, token: str, **kwargs: Any) -> None:
+        """Run on new LLM token. Only available when streaming is enabled."""
+        await self.send(token)
+
+
+@register_websocket_callback("LLMChain")
+class AsyncLLMChainWebsocketCallback(AsyncWebsocketCallback):
+    """AsyncWebsocketCallback handler for LLMChain."""
+
+    async def on_llm_new_token(self, token: str, **kwargs: Any) -> None:
+        """Run on new LLM token. Only available when streaming is enabled."""
+        await self.websocket.send_json({**self.response.dict(), **{"message": token}})
+
+
+@register_streaming_callback("ConversationChain")
+class AsyncConversationChainStreamingCallback(AsyncLLMChainStreamingCallback):
+    """AsyncStreamingResponseCallback handler for ConversationChain."""
+
+    pass
+
+
+@register_websocket_callback("ConversationChain")
+class AsyncConversationChainWebsocketCallback(AsyncLLMChainWebsocketCallback):
+    """AsyncWebsocketCallback handler for ConversationChain."""
+
+    pass
---
+++ packages/lanarky/lanarky-0.6.1-py3-none-any.whl/lanarky/responses/streaming.py
@@ -0,0 +1,86 @@
+"""
+Credits:
+- https://gist.github.com/ninely/88485b2e265d852d3feb8bd115065b1a
+- https://github.com/hwchase17/langchain/discussions/1706
+"""
+from typing import Any, Awaitable, Callable, Dict, Optional, Union
+
+from fastapi.responses import StreamingResponse as _StreamingResponse
+from langchain.chains.base import Chain
+from starlette.background import BackgroundTask
+from starlette.types import Send
+
+from lanarky.callbacks import get_streaming_callback
+
+
+class StreamingResponse(_StreamingResponse):
+    """StreamingResponse class wrapper for langchain chains."""
+
+    def __init__(
+        self,
+        chain_executor: Callable[[Send], Awaitable[Any]],
+        background: Optional[BackgroundTask] = None,
+        **kwargs: Any,
+    ) -> None:
+        super().__init__(content=iter(()), background=background, **kwargs)
+
+        self.chain_executor = chain_executor
+
+    async def stream_response(self, send: Send) -> None:
+        await send(
+            {
+                "type": "http.response.start",
+                "status": self.status_code,
+                "headers": self.raw_headers,
+            }
+        )
+
+        async def send_token(token: Union[str, bytes]):
+            if not isinstance(token, bytes):
+                token = token.encode(self.charset)
+            await send({"type": "http.response.body", "body": token, "more_body": True})
+
+        try:
+            outputs = await self.chain_executor(send_token)
+            if self.background is not None:
+                self.background.kwargs["outputs"] = outputs
+        except Exception as e:
+            if self.background is not None:
+                self.background.kwargs["outputs"] = str(e)
+            await send(
+                {
+                    "type": "http.response.body",
+                    "body": str(e).encode(self.charset),
+                    "more_body": False,
+                }
+            )
+            return
+
+        await send({"type": "http.response.body", "body": b"", "more_body": False})
+
+    @staticmethod
+    def _create_chain_executor(
+        chain: Chain, inputs: Union[Dict[str, Any], Any]
+    ) -> Callable[[Send], Awaitable[Any]]:
+        async def wrapper(send: Send):
+            return await chain.acall(
+                inputs=inputs, callbacks=[get_streaming_callback(chain, send=send)]
+            )
+
+        return wrapper
+
+    @classmethod
+    def from_chain(
+        cls,
+        chain: Chain,
+        inputs: Union[Dict[str, Any], Any],
+        background: Optional[BackgroundTask] = None,
+        **kwargs: Any,
+    ) -> "StreamingResponse":
+        chain_executor = cls._create_chain_executor(chain, inputs)
+
+        return cls(
+            chain_executor=chain_executor,
+            background=background,
+            **kwargs,
+        )
