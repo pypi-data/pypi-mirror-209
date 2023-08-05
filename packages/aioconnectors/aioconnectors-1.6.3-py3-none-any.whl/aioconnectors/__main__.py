330a5715dbe09faa9a615968c51e55a664b08824
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:07:56 +0000

    Add package aioconnectors/1.6.3/aioconnectors-1.6.3-py3-none-any.whl

---
+++ packages/aioconnectors/aioconnectors-1.6.3-py3-none-any.whl/aioconnectors/__main__.py
@@ -0,0 +1,225 @@
+import sys
+import logging
+import json
+import argparse
+
+import aioconnectors
+
+logger = logging.getLogger('aioconnectors_main')
+logger.setLevel(logging.DEBUG)
+handler = logging.StreamHandler()
+logger.addHandler(handler)
+
+    
+HELP = '''
+aioconnectors supported commands :
+    
+    - print_config_templates
+    - create_certificates [--ca] [optional dirpath] [--help]
+    - create_connector <config file path>    
+    - cli (start, stop, restart, show_connected_peers, ignore_peer_traffic, peek_queues, delete_client_certificate, delete_client_token)
+    - replace_server_certificate new_pem_path [optional dirpath] [--revert] [--help]
+    - ping <config file path>
+    - chat [--target <server_ip>] [--upload <path>] [--help]    
+    - test_receive_messages <config file path>
+    - test_send_messages <config file path>
+    - test_publish_messages <config file path>    
+    - --help
+    - --version
+'''
+    
+    
+if len(sys.argv) > 1:
+    if sys.argv[1] == 'create_certificates':
+        no_ca = True
+        if '--ca' in sys.argv:
+            sys.argv.remove('--ca')
+            no_ca = False
+        if len(sys.argv) == 3:
+            if sys.argv[2] == '--help':
+                print('create_certificates without argument will create client and server certificates directories '
+                      f'under {aioconnectors.core.Connector.CONNECTOR_FILES_DIRPATH}.\n'
+                      'You can specify a target directory as an optional argument.\n'
+                      '(Use "create_certificates ." to create your target directory in your current working directory.)\n'
+                      'Use --ca to create certificates for a server having server_ca=True (default is server_ca=False)')
+                sys.exit(0)
+            certificates_directory_path = aioconnectors.helpers.full_path(sys.argv[2])
+        else:
+            certificates_directory_path = None
+        logger.info('Starting create_certificates')            
+        res = aioconnectors.ssl_helper.create_certificates(logger, certificates_directory_path=certificates_directory_path,
+                                                           no_ca=no_ca)
+        if res is False:
+            sys.exit(1)
+
+    elif sys.argv[1] == 'replace_server_certificate':
+        help_test = 'For server only, replace_server_certificate puts your custom server certificate in the server certificates directories.\n' \
+              'First argument (mandatory) : the path of you server pem (the server key should be there too)\n' \
+              f'Second argument (optional) : the certificates directory path. If not provided it is : {aioconnectors.core.Connector.CONNECTOR_FILES_DIRPATH}.\n' \
+              '(Use "." to create your target directory in your current working directory).\n' \
+              'Use "--revert" to put back the original certificates and delete the custom certificate copy.'
+        
+        if len(sys.argv) == 3:
+            if sys.argv[2] == '--help':
+                print(help_test)
+                sys.exit(0)
+            if sys.argv[2] == '--revert':
+                res = aioconnectors.ssl_helper.replace_server_certificate(logger, revert=True)
+                sys.exit(0)                
+            server_certificate_path = aioconnectors.helpers.full_path(sys.argv[2])
+            certificates_directory_path = None
+        elif len(sys.argv) == 4:
+            server_certificate_path = aioconnectors.helpers.full_path(sys.argv[2])            
+            certificates_directory_path = aioconnectors.helpers.full_path(sys.argv[3])
+        else:
+            print(help_test)
+            sys.exit(0)
+        logger.info('Starting replace_server_certificate')            
+        res = aioconnectors.ssl_helper.replace_server_certificate(logger, server_certificate_path=server_certificate_path,
+                                                        certificates_directory_path=certificates_directory_path)
+        if res is False:
+            sys.exit(1)
+        
+    elif sys.argv[1] == 'print_config_templates':
+        Connector = aioconnectors.core.Connector
+    
+        manager_config_template = dict(default_logger_log_level='INFO', default_logger_rotate=True,
+                        default_logger_dirpath=Connector.CONNECTOR_FILES_DIRPATH, 
+                        default_logger_bk_count=aioconnectors.helpers.LOG_BK_COUNT,
+                        connector_files_dirpath=Connector.CONNECTOR_FILES_DIRPATH,
+                        is_server=True, server_sockaddr=Connector.SERVER_ADDR, reuse_server_sockaddr=False,
+                        reuse_uds_path_commander_server=False, reuse_uds_path_send_to_connector=False,
+                        use_ssl=Connector.USE_SSL, ssl_allow_all=False, use_token=Connector.USE_TOKEN,
+                        server_ca=Connector.SERVER_CA, server_ca_certs_not_stored=True, server_secure_tls=True,
+                        certificates_directory_path=Connector.CONNECTOR_FILES_DIRPATH,
+                        tokens_directory_path=Connector.CONNECTOR_FILES_DIRPATH,                        
+                        client_name=None, client_bind_ip=None, 
+                        send_message_types=Connector.DEFAULT_MESSAGE_TYPES, 
+                        recv_message_types=Connector.DEFAULT_MESSAGE_TYPES,
+                        subscribe_message_types=[], pubsub_central_broker=False,
+                        disk_persistence_send=Connector.DISK_PERSISTENCE_SEND, 
+                        disk_persistence_recv=Connector.DISK_PERSISTENCE_RECV, 
+                        max_size_persistence_path=Connector.MAX_SIZE_PERSISTENCE_PATH,
+                        file_recv_config={}, debug_msg_counts=Connector.DEBUG_MSG_COUNTS, silent=Connector.SILENT, 
+                        uds_path_receive_preserve_socket=Connector.UDS_PATH_RECEIVE_PRESERVE_SOCKET, 
+                        uds_path_send_preserve_socket=Connector.UDS_PATH_SEND_PRESERVE_SOCKET,
+                        enable_client_try_reconnect=True, keep_alive_period=None, keep_alive_timeout=Connector.KEEP_ALIVE_TIMEOUT,
+                        max_number_of_unanswered_keep_alive=Connector.MAX_NUMBER_OF_UNANSWERED_KEEP_ALIVE,
+                        connect_timeout = Connector.CONNECT_TIMEOUT,
+                        send_timeout=Connector.SEND_TIMEOUT, max_size_file_upload_send=Connector.MAX_SIZE_FILE_UPLOAD_SEND,
+                        max_size_file_upload_recv=Connector.MAX_SIZE_FILE_UPLOAD_RECV, max_certs=Connector.MAX_CERTS,
+                        everybody_can_send_messages=Connector.EVERYBODY_CAN_SEND_MESSAGES, send_message_types_priorities={},
+                        proxy={}, alternate_client_default_cert=Connector.ALTERNATE_CLIENT_DEFAULT_CERT,
+                        blacklisted_clients_id=None, blacklisted_clients_ip=None, blacklisted_clients_subnet=None,
+                        whitelisted_clients_id=None, whitelisted_clients_ip=None, whitelisted_clients_subnet=None,
+                        ignore_peer_traffic=False, client_cafile_verify_server=None,
+                        token_verify_peer_cert=Connector.TOKEN_VERIFY_PEER_CERT,
+                        token_client_send_cert=Connector.TOKEN_CLIENT_SEND_CERT,
+                        token_client_verify_server_hostname=Connector.TOKEN_CLIENT_VERIFY_SERVER_HOSTNAME,
+                        token_server_allow_authorized_non_default_cert=False)
+                        
+        print('\n- MANAGER TEMPLATE, used to create a connector')
+        print(json.dumps(manager_config_template, indent=4, sort_keys=True))
+        file_recv_config = {'any': {'target_directory':'/var/tmp/aioconnectors/{message_type}/{source_id}/',
+                                    'owner':'user:user', 'override_existing':False}}
+        print('\n- file_recv_config example, used inside MANAGER TEMPLATE')
+        print(json.dumps(file_recv_config, indent=4, sort_keys=True))                
+        
+        api_config_template = dict(default_logger_log_level='INFO', default_logger_rotate=True,
+                        default_logger_dirpath=Connector.CONNECTOR_FILES_DIRPATH,
+                        default_logger_bk_count=aioconnectors.helpers.LOG_BK_COUNT,
+                        connector_files_dirpath=Connector.CONNECTOR_FILES_DIRPATH, 
+                        is_server=True, server_sockaddr=Connector.SERVER_ADDR, client_name=None,
+                        uds_path_receive_preserve_socket=Connector.UDS_PATH_RECEIVE_PRESERVE_SOCKET, 
+                        uds_path_send_preserve_socket=Connector.UDS_PATH_SEND_PRESERVE_SOCKET,
+                        send_message_types=Connector.DEFAULT_MESSAGE_TYPES, 
+                        recv_message_types=Connector.DEFAULT_MESSAGE_TYPES,
+                        receive_from_any_connector_owner=True, pubsub_central_broker=False,
+                        max_size_chunk_upload= 209715200)                 
+        print('\n- API TEMPLATE, used to send/receive messages')
+        print(json.dumps(api_config_template, indent=4, sort_keys=True))
+
+    elif sys.argv[1] == 'create_connector':
+        if len(sys.argv) != 3:
+            print('Usage : create_connector <config file path>')
+            sys.exit(1)
+        if sys.argv[2] == '--help':
+            print('Usage : create_connector <config file path>')
+            sys.exit(0)
+        config_file_path=sys.argv[2]            
+        aioconnectors.applications.create_connector(config_file_path, logger)        
+
+    elif sys.argv[1] == 'cli':
+        aioconnectors.applications.cli(logger)
+
+    elif sys.argv[1] == 'test_receive_messages':
+        if len(sys.argv) != 3:
+            print('Usage : test_receive_messages <config file path>')
+            sys.exit(1)
+        if sys.argv[2] == '--help':
+            print('Usage : test_receive_messages <config file path>')
+            sys.exit(0)
+        config_file_path=sys.argv[2]       
+        aioconnectors.applications.test_receive_messages(config_file_path, logger)
+
+    elif sys.argv[1] == 'test_send_messages':
+        if len(sys.argv) != 3:
+            print('Usage : test_send_messages <config file path>')
+            sys.exit(1)
+        if sys.argv[2] == '--help':
+            print('Usage : test_send_messages <config file path>')
+            sys.exit(0)
+        config_file_path=sys.argv[2]            
+        aioconnectors.applications.test_send_messages(config_file_path, logger)
+
+    elif sys.argv[1] == 'test_publish_messages':
+        if len(sys.argv) != 3:
+            print('Usage : test_publish_messages <config file path>')
+            sys.exit(1)
+        if sys.argv[2] == '--help':
+            print('Usage : test_publish_messages <config file path>')
+            sys.exit(0)
+        config_file_path=sys.argv[2]            
+        aioconnectors.applications.test_publish_messages(config_file_path, logger)
+                
+    elif sys.argv[1] == 'ping':
+        if len(sys.argv) != 3:
+            print('Usage : ping <config file path>')
+            sys.exit(1)
+        if sys.argv[2] == '--help':
+            print('Usage : ping <config file path>')
+            sys.exit(0)
+        config_file_path=sys.argv[2]
+        aioconnectors.applications.ping(config_file_path, logger)
+
+    elif sys.argv[1] == 'chat':
+        #usage
+        #python3 -m aioconnectors chat
+        #python3 -m aioconnectors chat --target 127.0.0.1 [--upload <path>]
+        #inside chat, prepend "!" to call a local shell command, !exit" to exit, "!upload <path>" to upload to cwd, 
+        #"!dezip" to unzip an uploaded file.
+        print('\nWelcome to aioconnectors chat !')
+        print('Usage :\n- Type messages, or !exit to exit, or any shell command preceded by a ! to execute locally\n'
+              '- !upload <file or dir path> to upload to peer\'s current working directory\n'
+              '- !dezip <downloaded file name> to unzip a file\n')
+        parser = argparse.ArgumentParser()
+        parser.add_argument('chat')
+        parser.add_argument('--target', nargs='?', default=None, help="server ip, mandatory for client")
+        parser.add_argument('--accept', action='store_true', help="accept all clients if specified, optional for server")        
+        parser.add_argument('--port', nargs='?', default=None, help="server port, optional for server and client")
+        parser.add_argument('--bind_server_ip', nargs='?', default=None, help="bind to ip, optional for server")        
+        parser.add_argument('--upload', nargs='?', default=False, help="path of directory or file to upload")
+        parser.add_argument('--nowrap', action='store_true', help="disable tab completion")        
+        parser.add_argument('--exec', nargs='?', default=None, help="executable path like /bin/bash, optional for server and client")        
+        
+        args = parser.parse_args()
+        aioconnectors.applications.chat(args)
+                        
+    elif sys.argv[1] == '--help':
+        print(HELP)
+    elif sys.argv[1] == '--version':
+        print(aioconnectors.__version__)
+    else:
+        print('Unknown command : '+str(sys.argv[1]))
+else:
+    print(HELP)
---
+++ packages/aioconnectors/aioconnectors-1.6.3-py3-none-any.whl/aioconnectors/helpers.py
@@ -0,0 +1,148 @@
+import os
+import pwd,grp
+import time
+import logging
+from logging.handlers import RotatingFileHandler
+import sys
+import stat
+import subprocess
+import re
+import gzip
+
+
+PYTHON_VERSION = (sys.version_info.major,sys.version_info.minor)
+if PYTHON_VERSION < (3,6):
+    print('aioconnectors minimum requirement : Python 3.6')
+    sys.exit(1)
+PYTHON_GREATER_37 = (PYTHON_VERSION >= (3,7))
+
+DEFAULT_LOGGER_NAME = 'aioconnector'
+LOGFILE_DEFAULT_NAME = 'aioconnectors.log'
+LOG_LEVEL = 'INFO'
+LOG_ROTATE = True
+LOG_FORMAT = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+LOG_FORMAT_SHORT = '%(asctime)s - %(levelname)s - %(message)s'
+LOG_BK_COUNT = 5
+LOG_MAX_SIZE = 67108864 # 2**26 = 64 MB
+
+SOURCE_ID_REGEX = re.compile('^[0-9a-zA-Z-_:]+$')
+SOURCE_ID_DEFAULT_REGEX = re.compile('^default[0-9]*$')
+SOURCE_ID_MAX_LENGTH = 128
+TAG_REGEX = re.compile('^[0-9a-zA-Z-_:]+$')
+TAG_MAX_LENGTH = 128
+
+def full_path(the_path):
+    if the_path is not None:
+        return os.path.abspath(os.path.normpath(os.path.expandvars(os.path.expanduser(the_path))))
+
+def get_tmp_dir():
+    if os.path.exists('/var/tmp'):
+        return '/var/tmp/aioconnectors'
+    else:
+        candidate1 = full_path('~/aioconnectors_tmp')
+        candidate2 = full_path('aioconnectors_tmp')
+        return min(candidate1, candidate2, key=lambda x:len(x))
+
+def get_logger(logfile_path=LOGFILE_DEFAULT_NAME, first_run=False, silent=True, logger_name=DEFAULT_LOGGER_NAME, 
+               log_format=LOG_FORMAT, level=LOG_LEVEL, rotate=LOG_ROTATE, bk_count=LOG_BK_COUNT):
+    
+    def namer(name):
+        return name + '.gz'
+    
+    def rotator(source, dest):
+        with open(source, 'rb') as sf:
+            data = sf.read()
+        compressed = gzip.compress(data)
+        with open(dest, 'wb') as df:
+            df.write(compressed)
+        os.truncate(source, 0)
+    
+    logger = logging.getLogger(logger_name)
+    logger.handlers = []
+    if not first_run:
+        handlers = []
+        if logfile_path:    #could be '' if no config file provided
+            if rotate:
+                if rotate is True:
+                    rotate = LOG_MAX_SIZE
+                else:
+                    #use user defined value                    
+                    try:
+                        rotate = int(rotate)
+                    except Exception:
+                        rotate = LOG_MAX_SIZE
+                fh = RotatingFileHandler(logfile_path, maxBytes=rotate, backupCount=bk_count)
+                fh.rotator = rotator
+                fh.namer = namer                
+                handlers.append(fh)
+            else:
+                handlers.append(logging.FileHandler(logfile_path))
+        if not silent:
+            handlers.append(logging.StreamHandler(sys.stdout))
+        if not handlers:
+            logger.addHandler(logging.NullHandler())
+            return logger
+
+        log_level = getattr(logging, level, logging.INFO)
+        logger.setLevel(log_level)        
+        
+        formatter = logging.Formatter(log_format)
+        formatter.converter = time.gmtime
+        for fh in handlers:
+            fh.setFormatter(formatter)
+            fh.setLevel(logging.DEBUG)        
+            logger.addHandler(fh)
+    else:
+        logger.addHandler(logging.NullHandler())    
+    
+    return logger
+
+def chown_file(filepath, username, groupname, logger=None):
+    try:
+        uid = pwd.getpwnam(username).pw_uid
+        gid = grp.getgrnam(groupname).gr_gid
+        os.chown(filepath, uid, gid, follow_symlinks = False)
+    except Exception:
+        if logger:
+            logger.exception('chown_file')        
+
+def chown_nobody_permissions(directory_path, logger=None):
+    try:
+        UID_NOBODY = pwd.getpwnam("nobody").pw_uid
+        try:
+            GID_NOGROUP = grp.getgrnam("nogroup").gr_gid
+        except Exception:
+            GID_NOGROUP = grp.getgrnam("nobody").gr_gid
+        os.chown(directory_path, UID_NOBODY, GID_NOGROUP, follow_symlinks = False)
+        os.chmod(directory_path, stat.S_IRWXU | stat.S_IRWXG)# | stat.S_IRWXO)
+    except Exception as exc:
+        if logger:
+            logger.info('chown_nobody_permissions : '+str(exc))  
+    
+def iface_to_ip(iface, logger=None):
+    try:
+        ifconfig_output = subprocess.check_output(['ip', 'addr', 'show', iface], encoding='utf8', timeout=5)
+        return re.search(f'inet (?P<ipaddr>[\d\.]+).*{iface}$', ifconfig_output, re.MULTILINE).group('ipaddr')
+    except Exception:
+        if logger:
+            logger.exception('iface_to_ip')
+        return iface
+
+def validate_source_id(source_id):
+    if not SOURCE_ID_REGEX.match(source_id):
+    #if '.' in source_id or '/' in source_id:
+        #protect against path traversal
+        raise Exception(f'Invalid source_id : {source_id} - please use only {SOURCE_ID_REGEX.pattern}')
+    if len(source_id) > SOURCE_ID_MAX_LENGTH:
+        raise Exception(f'Invalid source_id : {source_id} - of length {len(source_id)}')        
+    if SOURCE_ID_DEFAULT_REGEX.match(source_id):
+        raise Exception(f'Invalid source_id : {source_id} - cannot match {SOURCE_ID_DEFAULT_REGEX.pattern}')
+
+def validate_tag(tag):
+    if not TAG_REGEX.match(tag):
+        raise Exception(f'Invalid tag : {tag} - please use only {TAG_REGEX.pattern}')
+    if len(tag) > TAG_MAX_LENGTH:
+        raise Exception(f'Invalid tag : {tag} - of length {len(tag)}')        
+        
+class CustomException(Exception):
+    pass
