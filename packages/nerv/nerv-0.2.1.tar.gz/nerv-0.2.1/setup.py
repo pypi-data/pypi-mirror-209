6a2917464fffdf16f5b12fdb9f3400522007ba60
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:14:17 +0000

    Add package nerv/0.2.1/nerv-0.2.1.tar.gz

---
+++ packages/nerv/nerv-0.2.1.tar.gz/nerv-0.2.1/setup.py
@@ -0,0 +1,53 @@
+import os
+
+from setuptools import find_packages, setup
+
+dir = os.path.abspath(os.path.dirname(__file__))
+
+with open(os.path.join(dir, "CHANGELOG.md"), "r") as file:
+    changelog = file.readlines()[9:12]
+
+
+with open(os.path.join(dir, "README.md"), "r") as file:
+    readme = file.read()
+
+
+with open("requirements.txt", "r") as file:
+    install_requires = file.read().split("\n")
+
+VERSION = "0.2.1"
+DESCRIPTION = "Neuroimaging Results Visualization"
+LONG_DESCRIPTION = f"## What's new in {VERSION}\n\n{''.join(changelog)}\n\n{readme}"
+
+setup(
+    name="nerv",
+    version=VERSION,
+    author="rmanaem",
+    author_email="<rmanaem@protonmail.ch>",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=LONG_DESCRIPTION,
+    url="https://github.com/rmanaem/nerv",
+    packages=find_packages(),
+    include_package_data=True,
+    package_data={"nerv": ["assets/*"]},
+    install_requires=install_requires,
+    license="MIT",
+    keywords=[
+        "python",
+        "dash",
+        "pandas",
+        "data visualization",
+        "plotly",
+        "neuroscience",
+    ],
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+        "License :: OSI Approved :: MIT License",
+    ],
+)
