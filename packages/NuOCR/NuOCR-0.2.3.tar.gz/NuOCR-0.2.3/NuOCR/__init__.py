f9552756961200734240e51f5d6c02501f96c50a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:24:51 +0000

    Add package NuOCR/0.2.3/NuOCR-0.2.3.tar.gz

---
+++ packages/NuOCR/NuOCR-0.2.3.tar.gz/NuOCR-0.2.3/NuOCR/__init__.py
@@ -0,0 +1,9 @@
+from .extractor import Extractor
+from .io_adaptor import FTP, S3, Blob, Email
+from .user import User
+from .util import *
+from .admin import Admin
+from .templated_flow import TemplatedFlow
+from .text import TextProcessors
+from .monitor import Analysis
+from .preprocessors import Preprocessor
