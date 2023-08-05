45ba296b378fbf0deccca3050643be283a83e5ef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:02:31 +0000

    Add package smb3-video-autosplitter/0.0.2/smb3_video_autosplitter-0.0.2-py3-none-any.whl

---
+++ packages/smb3-video-autosplitter/smb3_video_autosplitter-0.0.2-py3-none-any.whl/smb3_video_autosplitter/main.py
@@ -0,0 +1,52 @@
+"""
+video based autosplitter for smb3
+"""
+import logging
+
+from pygrabber.dshow_graph import FilterGraph
+
+from smb3_video_autosplitter import settings
+from smb3_video_autosplitter.autosplitter import (
+    Autosplitter,
+    LivesplitConnectFailedException,
+)
+from smb3_video_autosplitter.opencv import OpenCV
+
+from smb3_video_autosplitter.logging import initialize_logging
+
+LOGGER = logging.getLogger(__name__)
+
+
+def print_camera_info():
+    graph = FilterGraph()
+    input_devices = graph.get_input_devices()
+    video_capture_source = settings.get("video_capture_source")
+    if (
+        video_capture_source == None
+        or video_capture_source == -1
+        or video_capture_source >= len(input_devices)
+    ):
+        LOGGER.warning(
+            "No camera selected or invalid, please update to one of the below:"
+        )
+        LOGGER.warning(input_devices)
+        exit()
+    LOGGER.info(f"Selected video source: {input_devices[video_capture_source]}")
+
+
+def main():
+    initialize_logging()
+    print_camera_info()
+    opencv = OpenCV()
+    try:
+        autosplitter = Autosplitter()
+    except LivesplitConnectFailedException:
+        LOGGER.warning("Failed to connect to livesplit, is it running?")
+        exit()
+    while True:
+        opencv.tick()
+        autosplitter.tick(opencv.frame)
+
+
+if __name__ == "__main__":
+    main()
