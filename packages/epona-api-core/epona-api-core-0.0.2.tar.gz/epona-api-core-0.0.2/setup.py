20e7ebb527987f596ab04f7de1a45555cf679183
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:10:06 +0000

    Add package epona-api-core/0.0.2/epona-api-core-0.0.2.tar.gz

---
+++ packages/epona-api-core/epona-api-core-0.0.2.tar.gz/epona-api-core-0.0.2/setup.py
@@ -0,0 +1,54 @@
+
+import io
+import os
+
+from setuptools import find_packages, setup
+
+# Package metadata
+NAME = "epona-api-core"
+DESCRIPTION = "Epona API-Core"
+EMAIL = "marcos@eponaconsultoria.com.br"
+AUTHOR = "Antonio Marcos"
+REQUIRES_PYTHON = ">=3.9.0"
+VERSION = "0.0.2"
+
+# Required packages
+REQUIRED = [
+    "fastapi==0.95.2"
+]
+
+current_dir = os.path.abspath(os.path.dirname(__file__))
+
+# Import the README and use it as long-description
+try:
+    with io.open(os.path.join(current_dir, "README.md"), encoding="utf-8") as f:
+        long_description = f"\n{f.read()}"
+except FileNotFoundError:
+    long_description = DESCRIPTION
+
+# Load the package's __version__.py module as a dictionary
+about = {}
+if not VERSION:
+    project_slug = NAME.lower().replace("-", "_").replace(" ", "_")
+    with open(os.path.join(current_dir, project_slug, "__version__.py")) as f:
+        exec(f.read(), about)
+else:
+    about['__version__'] = VERSION
+
+setup(
+    name=NAME,
+    version=about['__version__'],
+    description=DESCRIPTION,
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author=AUTHOR,
+    author_email=EMAIL,
+    python_requires=REQUIRES_PYTHON,
+    packages=find_packages(exclude=["test_*"]),
+    install_requires=REQUIRED,
+    include_package_data=True,
+    classifiers=[
+        "Programming Language :: Python :: 3.9",
+    ],
+    setup_requires=['wheel']
+)
