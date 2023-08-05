63cf2f90a389f2af1ec7691a18655f3ed2e2810b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:12:28 +0000

    Add package GB-Chat-project-client/0.0.1/GB-Chat-project-client-0.0.1.tar.gz

---
+++ packages/GB-Chat-project-client/GB-Chat-project-client-0.0.1.tar.gz/GB-Chat-project-client-0.0.1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name="GB-Chat-project-client",
+    version="0.0.1",
+    description="A Simple asinc chat client",
+    author="Ilya Kulikov",
+    author_email="ilyatrg@gmail.com",
+    url="https://github.com/KulikovIK/GB-AsincChat",
+    packages=find_packages()
+)
