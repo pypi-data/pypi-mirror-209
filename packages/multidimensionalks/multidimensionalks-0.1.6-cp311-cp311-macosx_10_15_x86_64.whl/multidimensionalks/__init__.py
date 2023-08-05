82c504d43dabe57deb4276cfee9fb173fe018dfa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:25:00 +0000

    Add package multidimensionalks/0.1.6/multidimensionalks-0.1.6-cp311-cp311-macosx_10_15_x86_64.whl

---
+++ packages/multidimensionalks/multidimensionalks-0.1.6-cp311-cp311-macosx_10_15_x86_64.whl/multidimensionalks/__init__.py
@@ -0,0 +1,24 @@
+from .pure import test as test_pure
+from .avx2 import test as test_avx2
+from .avx512 import test as test_avx512
+from cpufeature.extension import CPUFeature
+
+
+def test(rvs, use_avx=3, **kwargs):
+    method = test_pure
+    if use_avx == 1 and not CPUFeature['AVX512vl']:
+        print("!!! Warning: AVX512vl instruction set is not supported by your CPU, backing up to pure implementation")
+        use_avx = 0
+    elif use_avx in (1, 3) and CPUFeature['AVX512vl']:
+        method = test_avx512
+        use_avx = 1
+    elif use_avx == 2 and not CPUFeature['AVX2']:
+        print("!!! Warning: AVX2 instruction set is not supported by your CPU, backing up to pure implementation")
+        use_avx = 0
+    elif use_avx in (2, 3) and CPUFeature['AVX2']:
+        method = test_avx2
+        use_avx = 2
+    else:  # use pure implementation
+        use_avx = 0
+
+    return method(rvs, use_avx=use_avx, **kwargs)
