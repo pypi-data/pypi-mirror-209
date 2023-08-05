6ac94eaffe961a6cea877d2ba702c3718db55f23
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:08:04 +0000

    Add package daliuge-engine/3.0.0/daliuge-engine-3.0.0.tar.gz

---
+++ packages/daliuge-engine/daliuge-engine-3.0.0.tar.gz/daliuge-engine-3.0.0/dlg/apps/constructs.py
@@ -0,0 +1,152 @@
+from dlg.apps.app_base import BarrierAppDROP
+
+
+##
+# @brief Scatter
+# @details A Scatter template drop
+# @par EAGLE_START
+# @param category Scatter
+# @param tag template
+# @param num_of_copies Scatter dimension/4/Integer/ComponentParameter/readwrite//False/False/Specifies the number of replications of the content of the scatter construct
+# @param dropclass dropclass/dlg.apps.constructs.ScatterDrop/String/ComponentParameter/readwrite//False/False/Drop class
+# @par EAGLE_END
+class ScatterDrop(BarrierAppDROP):
+    """
+    This only exists to make sure we have a Scatter in the template palette
+    """
+
+    pass
+
+
+##
+# @brief Gather
+# @details A Gather template drop
+# @par EAGLE_START
+# @param category Gather
+# @param tag template
+# @param num_of_inputs No. of inputs/2/Integer/ApplicationArgument/readwrite//False/False/Number of inputs
+# @param gather_axis Index of gather axis/0/Integer/ApplicationArgument/readwrite//False/False/Index of gather axis
+# @param dropclass dropclass/dlg.apps.constructs.GatherDrop/String/ComponentParameter/readwrite//False/False/Drop class
+# @par EAGLE_END
+class GatherDrop(BarrierAppDROP):
+    """
+    This only exists to make sure we have a GroupBy in the template palette
+    """
+
+    pass
+
+
+##
+# @brief Loop
+# @details A loop template drop
+# @par EAGLE_START
+# @param category Loop
+# @param tag template
+# @param num_of_iter No. of iterations/2/Integer/ApplicationArguments/readwrite//False/False/Number of iterations
+# @param dropclass dropclass/dlg.apps.constructs.LoopDrop/String/ComponentParameter/readwrite//False/False/Drop class
+# @par EAGLE_END
+class LoopDrop(BarrierAppDROP):
+    """
+    This only exists to make sure we have a loop in the template palette
+    """
+
+    pass
+
+
+##
+# @brief MKN
+# @details A MKN template drop
+# @par EAGLE_START
+# @param category MKN
+# @param tag template
+# @param k K/1/Integer/ApplicationArgument/readwrite//False/False/Internal multiplicity
+# @param dropclass dropclass/dlg.apps.constructs.MKNDrop/String/ComponentParameter/readwrite//False/False/Drop class
+# @par EAGLE_END
+class MKNDrop(BarrierAppDROP):
+    """
+    This only exists to make sure we have a MKN in the template palette
+    """
+
+    pass
+
+
+##
+# @brief GroupBy
+# @details A GroupBy template drop
+# @par EAGLE_START
+# @param category GroupBy
+# @param tag template
+# @param num_of_inputs No. of inputs/2/Integer/ApplicationArgument/readwrite//False/False/Number of inputs
+# @param gather_axis Index of gather axis/0/Integer/ApplicationArgument/readwrite//False/False/Index of gather axis
+# @param dropclass dropclass/dlg.apps.constructs.GroupByDrop/String/ComponentParameter/readwrite//False/False/Drop class
+# @par EAGLE_END
+class GroupByDrop(BarrierAppDROP):
+    """
+    This only exists to make sure we have a GroupBy in the template palette
+    """
+
+    pass
+
+
+##
+# @brief SubGraph
+# @details A SubGraph template drop
+# @par EAGLE_START
+# @param category SubGraph
+# @param dropclass dropclass/dlg.apps.constructs.SubGraphDrop/String/ComponentParameter/readwrite//False/False/Drop class
+# @param tag template
+# @par EAGLE_END
+class SubGraphDrop(BarrierAppDROP):
+    """
+    This only exists to make sure we have a SubGraph in the template palette
+    """
+
+    pass
+
+
+##
+# @brief Comment
+# @details A comment template drop
+# @par EAGLE_START
+# @param category Comment
+# @param dropclass dropclass/dlg.apps.constructs.CommentDrop/String/ComponentParameter/readwrite//False/False/Drop class
+# @param tag template
+# @par EAGLE_END
+class CommentDrop(BarrierAppDROP):
+    """
+    This only exists to make sure we have a comment in the template palette
+    """
+
+    pass
+
+
+##
+# @brief Description
+# @details A description template drop
+# @par EAGLE_START
+# @param category Description
+# @param dropclass dropclass/dlg.apps.constructs.DescriptionDrop/String/ComponentParameter/readwrite//False/False/Drop class
+# @param tag template
+# @par EAGLE_END
+class DescriptionDrop(BarrierAppDROP):
+    """
+    This only exists to make sure we have a description in the template palette
+    """
+
+    pass
+
+
+##
+# @brief Exclusive Force Node
+# @details An Exclusive Force Node
+# @par EAGLE_START
+# @param category ExclusiveForceNode
+# @param dropclass dropclass/dlg.apps.constructs.ExclusiveForceDrop/String/ComponentParameter/readwrite//False/False/Drop class
+# @param tag template
+# @par EAGLE_END
+class ExclusiveForceDrop(BarrierAppDROP):
+    """
+    This only exists to make sure we have an exclusive force node in the template palette
+    """
+
+    pass
---
+++ packages/daliuge-engine/daliuge-engine-3.0.0.tar.gz/daliuge-engine-3.0.0/dlg/data/drops/parset_drop.py
@@ -0,0 +1,97 @@
+#
+#    ICRAR - International Centre for Radio Astronomy Research
+#    (c) UWA - The University of Western Australia, 2014
+#    Copyright by UWA (in the framework of the ICRAR)
+#    All rights reserved
+#
+#    This library is free software; you can redistribute it and/or
+#    modify it under the terms of the GNU Lesser General Public
+#    License as published by the Free Software Foundation; either
+#    version 2.1 of the License, or (at your option) any later version.
+#
+#    This library is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+#    Lesser General Public License for more details.
+#
+#    You should have received a copy of the GNU Lesser General Public
+#    License along with this library; if not, write to the Free Software
+#    Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+#    MA 02111-1307  USA
+#
+import io
+import os
+
+from dlg.data.drops.data_base import DataDROP
+from dlg.drop import DEFAULT_INTERNAL_PARAMETERS
+from dlg.data.io import MemoryIO
+from dlg.meta import dlg_string_param
+
+
+##
+# @brief ParameterSet
+# @details A set of parameters, wholly specified in EAGLE
+# @par EAGLE_START
+# @param category ParameterSet
+# @param tag daliuge
+# @param data_volume Data volume/5/Float/ComponentParameter/readwrite//False/False/Estimated size of the data contained in this node
+# @param group_end Group end/False/Boolean/ComponentParameter/readwrite//False/False/Is this node the end of a group?
+# @param mode Parset mode/"YANDA"/String/ComponentParameter/readonly//False/False/To what standard DALiuGE should filter and serialize the parameters.
+# @param config_data ConfigData/""/String/ComponentParameter/readwrite//False/False/Additional configuration information to be mixed in with the initial data
+# @param dropclass dropclass/dlg.data.drops.parset_drop.ParameterSetDROP/String/ComponentParameter/readwrite//False/False/Drop class
+# @param streaming Streaming/False/Boolean/ComponentParameter/readwrite//False/False/Specifies whether this data component streams input and output data
+# @param persist Persist/False/Boolean/ComponentParameter/readwrite//False/False/Specifies whether this data component contains data that should not be deleted after execution
+# @param dropclass dropclass//dlg.data.drops.parset_drop.ParameterSetDROP//readonly//False/False/default class for this DROP
+# @param Config ConfigFile//Object.File/OutputPort/readwrite//False/False/The output configuration file
+# @par EAGLE_END
+class ParameterSetDROP(DataDROP):
+    """
+    A generic configuration file template wrapper
+    This drop opens an (optional) file containing some initial configuration information, then
+    appends any additional specified parameters to it, finally serving it as a data object.
+    """
+
+    config_data = b""
+
+    mode = dlg_string_param("mode", None)
+
+    def serialize_parameters(self, parameters: dict, mode):
+        """
+        Returns a string representing a serialization of the parameters.
+        """
+        if mode == "YANDA":
+            # TODO: Add more complex value checking
+            return "\n".join(f"{x}={y}" for x, y in parameters.items())
+        # Add more formats (.ini for example)
+        return "\n".join(f"{x}={y}" for x, y in parameters.items())
+
+    def filter_parameters(self, parameters: dict, mode):
+        """
+        Returns a dictionary of parameters, with daliuge-internal or other parameters filtered out
+        """
+        if mode == "YANDA":
+            forbidden_params = list(DEFAULT_INTERNAL_PARAMETERS)
+            if parameters["config_data"] == "":
+                forbidden_params.append("configData")
+            return {
+                key: val
+                for key, val in parameters.items()
+                if key not in DEFAULT_INTERNAL_PARAMETERS
+            }
+        return parameters
+
+    def initialize(self, **kwargs):
+        """
+        TODO: Open input file
+        """
+        self.config_data = self.serialize_parameters(
+            self.filter_parameters(self.parameters, self.mode), self.mode
+        ).encode("utf-8")
+
+    def getIO(self):
+        return MemoryIO(io.BytesIO(self.config_data))
+
+    @property
+    def dataURL(self) -> str:
+        hostname = os.uname()[1]
+        return f"config://{hostname}/{os.getpid()}/{id(self.config_data)}"
