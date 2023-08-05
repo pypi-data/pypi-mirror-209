ea26ad861e88467d8289531b2d8268e857bde6d8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:06:24 +0000

    Add package psychic-link/0.2/psychic_link-0.2.tar.gz

---
+++ packages/psychic-link/psychic_link-0.2.tar.gz/psychic_link-0.2/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='psychic_link',
+    version='0.2',
+    description='Psychic.dev is an open-source universal data connector for knowledgebases.',
+    author='Ayan Bandyopadhyay',
+    author_email='ayan@getsidekick.ai',
+    packages=['psychic_link'],
+    install_requires=[
+        'requests',
+    ],
+)
