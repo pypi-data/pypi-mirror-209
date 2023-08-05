4195bf6e1a5a0e860aaaccebfb7aec4b59684d7a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:14:17 +0000

    Add package marvin/0.9.1/marvin-0.9.1-py3-none-any.whl

---
+++ packages/marvin/marvin-0.9.1-py3-none-any.whl/marvin/utilities/meta.py
@@ -0,0 +1,23 @@
+import pendulum
+
+import marvin
+from marvin.infra.chroma import Chroma
+from marvin.models.documents import Document
+
+
+async def record_feedback(feedback: str, topic: str = None):
+    """Record feedback on a given topic."""
+
+    processed_feedback = await Document(
+        text=feedback,
+        metadata={
+            "source": "user feedback",
+            "created_at": pendulum.now().isoformat(),
+        },
+    ).to_excerpts()
+
+    async with Chroma(topic or marvin.settings.default_topic) as chroma:
+        await chroma.add(
+            documents=processed_feedback,
+            skip_existing=True,
+        )
