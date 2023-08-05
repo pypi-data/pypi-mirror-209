ea7072fe3e72658304ef6b9473fc641c5a389230
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:23:05 +0000

    Add package dragonfirerequests/0.1.1/dragonfirerequests-0.1.1.tar.gz

---
+++ packages/dragonfirerequests/dragonfirerequests-0.1.1.tar.gz/dragonfirerequests-0.1.1/dragonfirerequests/response.py
@@ -0,0 +1,16 @@
+from typing import Optional, Union
+
+from requests import Response, PreparedRequest
+
+
+class DragonResponse:
+    origin: Response
+    data: dict
+    success: bool
+    request: PreparedRequest
+
+    def __init__(self, response: Response, data: dict, success: bool, request: PreparedRequest):
+        self.origin = response
+        self.data = data
+        self.success = success
+        self.request = request
