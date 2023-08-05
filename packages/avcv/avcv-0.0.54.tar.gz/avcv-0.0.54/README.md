467e58da3ffc8d7f9f3d0b68cc611fa853297073
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:23:04 +0000

    Add package avcv/0.0.54/avcv-0.0.54.tar.gz

---
+++ packages/avcv/avcv-0.0.54.tar.gz/avcv-0.0.54/README.md
@@ -0,0 +1,9 @@
+# AVCV
+> Optimized functions for vision problems
+
+
+## Install
+
+`pip install avcv`
+
+Docs: https://anhvth.github.io/avcv/utils.html
---
+++ packages/avcv/avcv-0.0.54.tar.gz/avcv-0.0.54/avcv/all.py
@@ -0,0 +1,12 @@
+from avcv._imports import *
+from avcv.utils import *
+from avcv.process import *
+from avcv.visualize import *
+from avcv.coco import *
+
+# import cv2, matplotlib.pyplot as plt, mmcv, numpy as np, os, os.path as osp
+# from glob import glob
+# from tqdm import tqdm
+# import pandas as pd
+# from PIL import Image
+# from loguru import logger
