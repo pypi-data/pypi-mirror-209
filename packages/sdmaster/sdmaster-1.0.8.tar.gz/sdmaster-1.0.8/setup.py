dd03bcfddcd50ff63438ae18950b22b57e1a183e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:11:29 +0000

    Add package sdmaster/1.0.8/sdmaster-1.0.8.tar.gz

---
+++ packages/sdmaster/sdmaster-1.0.8.tar.gz/sdmaster-1.0.8/setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='sdmaster',
+    version='1.0.8',
+    packages=setuptools.find_packages(),
+    url='https://github.com/RedrumSherlock/stockdatamaster',
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    license='MIT',
+    author='Mike Wang',
+    author_email='wml816@gmail.com',
+    description='Stock Data Master',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    python_requires='>=3.5',
+    install_requires=["pandas", "plotly", "matplotlib", "numpy", "pytz"],
+    package_dir={'sdmaster':'sdm'},
+)
