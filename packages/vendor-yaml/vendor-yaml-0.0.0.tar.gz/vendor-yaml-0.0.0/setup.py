b8806f5e69dcab846a9c4d3ac742cf350a8e3378
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:11:02 +0000

    Add package vendor-yaml/0.0.0/vendor-yaml-0.0.0.tar.gz

---
+++ packages/vendor-yaml/vendor-yaml-0.0.0.tar.gz/vendor-yaml-0.0.0/setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+from setuptools import setup
+from codecs import open
+from os import path
+
+setup(
+    name='vendor-yaml',
+    version='0.0.0',
+    description='',
+    long_description='',
+    url='',
+    packages=[],
+)
