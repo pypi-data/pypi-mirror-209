c6508575679e4bd12afc3b9d081b216798173736
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:02:31 +0000

    Add package robothub-oak/1.2.0/robothub_oak-1.2.0-py3-none-any.whl

---
+++ packages/robothub-oak/robothub_oak-1.2.0-py3-none-any.whl/robothub_oak/__init__.py
@@ -0,0 +1,12 @@
+try:
+    import blobconverter
+
+    blobconverter.set_defaults(silent=True)
+except:
+    pass
+
+from .device import *
+from .hub_camera import *
+from .manager import *
+
+__version__ = '1.2.0'
