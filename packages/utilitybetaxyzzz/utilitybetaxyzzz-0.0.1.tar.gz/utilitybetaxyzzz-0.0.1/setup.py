26537a74311ae31db85a64468718b013885ce8d2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:12:33 +0000

    Add package utilitybetaxyzzz/0.0.1/utilitybetaxyzzz-0.0.1.tar.gz

---
+++ packages/utilitybetaxyzzz/utilitybetaxyzzz-0.0.1.tar.gz/utilitybetaxyzzz-0.0.1/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+VERSION = '0.0.1'
+DESCRIPTION = 'ABC Desc'
+
+# Setting up
+setup(
+    name="utilitybetaxyzzz",
+    version=VERSION,
+    author="NeuralNine (Florian Dedov)",
+    author_email="<mail@neuralnine.com>",
+    description=DESCRIPTION,
+    packages=find_packages(),
+    install_requires=['opencv-python', 'pyautogui', 'pyaudio'],
+    keywords=['python', 'video', 'stream', 'video stream', 'camera stream', 'sockets'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
