30fcad599d77f76540d3f9d11f15f1df497ccf51
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:26:51 +0000

    Add package data2latex/1.0.5/data2latex-1.0.5-py3-none-any.whl

---
+++ packages/data2latex/data2latex-1.0.5-py3-none-any.whl/data2latex/__init__.py
@@ -0,0 +1,14 @@
+from .dm import DocumentManager, gd, gdm
+from .features import (
+    finish,
+    latex,
+    pdf,
+    reset,
+    section,
+    setup,
+    text,
+    use_multi_page_standalone,
+    use_one_page_standalone,
+)
+from .plot import plot
+from .table import table
