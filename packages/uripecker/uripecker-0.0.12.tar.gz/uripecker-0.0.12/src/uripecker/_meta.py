f10923f266f313ea9f43c7271eadfb355d8034e7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:12:51 +0000

    Add package uripecker/0.0.12/uripecker-0.0.12.tar.gz

---
+++ packages/uripecker/uripecker-0.0.12.tar.gz/uripecker-0.0.12/src/uripecker/_meta.py
@@ -0,0 +1,39 @@
+CODENAME: str = """
+
+""".strip()
+
+GIT_LASTHASH: str = """
+9487965a449088989a4f5a501d09fbab6c48b6c2
+""".strip()
+
+GIT_LASTSUMMARY: str = """
+Bump version to 0.0.12
+""".strip()
+
+LICENSE: str = """
+LGPLv2
+""".strip()
+
+MAINTAINER: str = """
+Alois Mahdal <netvor+uripecker@vornet.cz>
+""".strip()
+
+NAME: str = """
+uripecker
+""".strip()
+
+PKGNAME: str = """
+python-uripecker
+""".strip()
+
+TAGLINE: str = """
+Peck them URIs out
+""".strip()
+
+VCS_BROWSER: str = """
+https://gitlab.com/vornet/python/python-uripecker
+""".strip()
+
+VERSION: str = """
+0.0.12
+""".strip()
