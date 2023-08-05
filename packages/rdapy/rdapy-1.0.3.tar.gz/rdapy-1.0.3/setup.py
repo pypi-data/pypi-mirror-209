621731bb7adc65045a5e43e709d1bab3788016af
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:30:12 +0000

    Add package rdapy/1.0.3/rdapy-1.0.3.tar.gz

---
+++ packages/rdapy/rdapy-1.0.3.tar.gz/rdapy-1.0.3/setup.py
@@ -0,0 +1,47 @@
+from setuptools import setup, find_packages
+
+"""
+python setup.py register
+python setup.py register sdist upload
+"""
+
+setup(
+    name="rdapy",
+    version="1.0.3",
+    description="Redistricting analytics in Python",
+    url="https://github.com/dra2020/rdapy",
+    author="alecramsay",
+    author_email="alec@davesredistricting.org",
+    license="MIT",
+    packages=[
+        "rdapy",
+        "rdapy.compactness",
+        "rdapy.equal",
+        "rdapy.graph",
+        "rdapy.partisan",
+        "rdapy.splitting",
+    ],
+    install_requires=[
+        "attrs",
+        "certifi",
+        "click",
+        "click-plugins",
+        "cligj",
+        "exceptiongroup",
+        "Fiona",
+        "geographiclib",
+        "iniconfig",
+        "munch",
+        "nptyping",
+        "numpy",
+        "packaging",
+        "pluggy",
+        "pyproj",
+        "pytest",
+        "scipy",
+        "shapely",
+        "six",
+        "tomli",
+    ],
+    zip_safe=False,
+)
