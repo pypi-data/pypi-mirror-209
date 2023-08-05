bd3662141d63f5c251d11737ded42cfef47dfc72
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:26:48 +0000

    Add package web3cli/0.19.1/web3cli-0.19.1.tar.gz

---
+++ packages/web3cli/web3cli-0.19.1.tar.gz/web3cli-0.19.1/src/web3cli/controllers/crud/rpc_controller.py
@@ -0,0 +1,116 @@
+from cement import ex
+
+from web3cli.controllers.controller import Controller
+from web3cli.exceptions import Web3CliError
+from web3cli.helpers.render import render_table
+from web3core.exceptions import ChainNotFound
+from web3core.models.chain import Chain, Rpc
+
+
+class RpcController(Controller):
+    """Handler of the `w3 rpc` CRUD commands"""
+
+    class Meta:
+        label = "rpc"
+        help = "add, list or delete rpcs"
+        stacked_type = "nested"
+        stacked_on = "base"
+
+    @ex(
+        help="add a new rpc to the given chain",
+        arguments=[
+            (["chain_name"], {"help": "name of the chain of the rpc"}),
+            (
+                ["rpcs"],
+                {
+                    "help": "url of the RPC to add; you can add more than one",
+                    "nargs": "+",
+                },
+            ),
+        ],
+    )
+    def add(self) -> None:
+        chain = Chain.get_by_name(self.app.pargs.chain_name)
+        if not chain:
+            raise ChainNotFound(
+                f"Chain '{self.app.pargs.chain_name}' does not exist, add it with `w3 chain add`"
+            )
+
+        for rpc_url in self.app.pargs.rpcs:
+            chain.add_rpc(rpc_url, self.app.log.info)
+
+    @ex(
+        help="list available rpcs and their chains",
+    )
+    def list(self) -> None:
+        render_table(
+            self.app,
+            headers=["ID", "RPC", "CHAIN"],
+            data=[
+                [r.id, r.url, ",".join([c.name for c in r.get_chains()])]
+                for r in Rpc.get_all()
+            ],
+        )
+
+    @ex(
+        help="show the full URL of the RPC with the given ID",
+        arguments=[
+            (
+                ["id"],
+                {
+                    "help": "ID of the rpc; run `w3 rpc list` to list the IDs",
+                    "type": int,
+                },
+            ),
+        ],
+    )
+    def get_url(self) -> None:
+        rpc = Rpc.get(self.app.pargs.id)
+        self.app.print(rpc.url)
+
+    @ex(
+        help="show the URL of an RPC by its ID; without arguments, shows the RPC that will be used by the CLI",
+        arguments=[
+            (
+                ["id"],
+                {
+                    "help": "ID of the RPC to show; run `w3 rpc list` to list the IDs",
+                    "nargs": "?",
+                    "type": int,
+                },
+            ),
+        ],
+    )
+    def get(self) -> None:
+        # Case 1: Show the URL of the RPC with the given ID
+        if self.app.pargs.id:
+            rpc = Rpc.get_or_none(self.app.pargs.id)
+            if not rpc:
+                raise Web3CliError(f"RPC with ID {self.app.pargs.id} does not exist")
+            self.app.print(rpc.url)
+        # Case 2: RPC was forced via CLI argument
+        elif self.app.rpc:
+            self.app.print(self.app.rpc)
+        # Case 3: show RPC inferred by the app
+        else:
+            rpc = self.app.chain.pick_rpc()
+            self.app.print(rpc.url)
+
+    @ex(
+        help="delete one or more rpcs",
+        arguments=[
+            (
+                ["ids"],
+                {
+                    "help": "IDs of the rpc to delete; run `w3 rpc list` to list the IDs",
+                    "nargs": "+",
+                    "type": int,
+                },
+            ),
+        ],
+    )
+    def delete(self) -> None:
+        for id in self.app.pargs.ids:
+            rpc = Rpc.get(id)
+            rpc.delete_instance()
+            self.app.log.info(f"Rpc {id} deleted correctly [url => {rpc.url}]")
---
+++ packages/web3cli/web3cli-0.19.1.tar.gz/web3cli-0.19.1/tests/web3cli/controllers/crud/test_signer_controller.py
@@ -0,0 +1,154 @@
+from typing import Any, Dict, List
+
+import pytest
+
+from tests.web3cli.main import Web3CliTest
+from web3cli.helpers.crypto import decrypt_string_with_app_key
+from web3cli.helpers.database import truncate_tables
+from web3core.exceptions import RecordNotFound, SignerNotFound
+from web3core.helpers.seed import seed_signers
+from web3core.models.signer import Signer
+
+
+def test_signer_list(signers: List[Dict[str, Any]]) -> None:
+    """Add signers and check that they are listed alphabetically"""
+
+    signers = sorted(signers, key=lambda s: s["name"])
+    with Web3CliTest() as app:
+        seed_signers(signers, app.app_key)
+        app.set_args(["signer", "list"]).run()
+        data, output = app.last_rendered
+        for i in range(0, len(signers)):
+            assert data[i][0] == signers[i]["name"]
+
+
+def test_signer_get(signers: List[Dict[str, Any]]) -> None:
+    # Test with name argument > returns address of signer with name
+    for s in signers:
+        with Web3CliTest() as app:
+            seed_signers(signers, app.app_key)
+            app.set_args(
+                [
+                    "signer",
+                    "get",
+                    s["name"],
+                ]
+            ).run()
+            data, output = app.last_rendered
+            assert data["out"] == s["address"]
+
+    # Test with --signer argument > returns value of argument
+    for s in signers:
+        with Web3CliTest() as app:
+            seed_signers(signers, app.app_key)
+            app.set_args(
+                [
+                    "--signer",
+                    s["name"],
+                    "signer",
+                    "get",
+                ]
+            ).run()
+            data, output = app.last_rendered
+            assert data["out"] == s["name"]
+
+    # Test without arguments > returns whatever is written in config file
+    s = signers[0]
+    with Web3CliTest() as app:
+        seed_signers(signers, app.app_key)
+        app.config.set("web3cli", "default_signer", s["name"])
+        app.set_args(
+            [
+                "signer",
+                "get",
+            ]
+        ).run()
+        data, output = app.last_rendered
+        assert data["out"] == s["name"]
+
+    # Test without arguments, without config file, but there's one signer in the DB > returns that one signer
+    s = signers[0]
+    with Web3CliTest() as app:
+        seed_signers([s], app.app_key)
+        app.set_args(
+            [
+                "signer",
+                "get",
+            ]
+        ).run()
+        data, output = app.last_rendered
+        assert data["out"] == s["name"]
+
+    # Test without arguments, without config file, but there are multiple signers in the DB > raise error
+    with Web3CliTest() as app:
+        seed_signers(signers, app.app_key)
+        with pytest.raises(SignerNotFound):
+            app.set_args(
+                [
+                    "signer",
+                    "get",
+                ]
+            ).run()
+
+    # Test without arguments, without config file, without signers in the DB > raise error
+    s = signers[0]
+    with Web3CliTest() as app:
+        with pytest.raises(SignerNotFound):
+            app.set_args(
+                [
+                    "signer",
+                    "get",
+                ]
+            ).run()
+
+
+def test_signer_add(signers: List[Dict[str, Any]]) -> None:
+    for s in signers:
+        with Web3CliTest() as app:
+            app.set_args(
+                [
+                    "signer",
+                    "add",
+                    s["name"],
+                    "--private-key",
+                    s["private_key"],
+                ]
+            ).run()
+            signer = Signer.get_by_name(s["name"])
+            assert Signer.select().count() == 1
+            assert signer.address == s["address"]
+            assert decrypt_string_with_app_key(app, signer.key) == s["private_key"]
+
+
+def test_signer_add_create(signers: List[Dict[str, Any]]) -> None:
+    """Test `w3 signer add <name> --create`"""
+    names = ["name_1", "name_2", "name_3"]
+    for i, s_name in enumerate(names):
+        with Web3CliTest(delete_db=False) as app:
+            if i == 0:
+                truncate_tables(app)
+            app.set_args(
+                [
+                    "signer",
+                    "add",
+                    s_name,
+                    "--create",
+                ]
+            ).run()
+            assert Signer.select().count() == i + 1
+
+
+def test_signer_delete(signers: List[Dict[str, Any]]) -> None:
+    for s in signers:
+        with Web3CliTest() as app:
+            seed_signers(signers, app.app_key)
+            app.set_args(
+                [
+                    "signer",
+                    "delete",
+                    s["name"],
+                ]
+            ).run()
+            assert Signer.select().count() == len(signers) - 1
+            with pytest.raises(RecordNotFound):
+                Signer.get_by_name_or_raise(s["name"])
