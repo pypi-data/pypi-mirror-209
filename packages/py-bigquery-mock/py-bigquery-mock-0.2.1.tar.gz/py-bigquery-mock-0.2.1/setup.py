54ab38ef3b578df455d064c092207382df43b52d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:04:20 +0000

    Add package py-bigquery-mock/0.2.1/py-bigquery-mock-0.2.1.tar.gz

---
+++ packages/py-bigquery-mock/py-bigquery-mock-0.2.1.tar.gz/py-bigquery-mock-0.2.1/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    name='py-bigquery-mock',
+    version='0.2.1',    
+    description='Mock BigQuery',
+    url='https://github.com/paulhtremblay/py-bigquery-mock',
+    author='Henry Tremblay',
+    author_email='paulhtremblay@gmail.com',
+    license='GNU GENERAL PUBLIC LICENSE',
+    packages=['bigquery_mock'],
+     classifiers=[
+        'Programming Language :: Python :: 3',
+    ],
+)
