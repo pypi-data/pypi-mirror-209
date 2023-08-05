e61430e884054ceaded35b6ae327c1c5b8e48392
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:04:30 +0000

    Add package ai-commit-gen/0.2/ai-commit-gen-0.2.tar.gz

---
+++ packages/ai-commit-gen/ai-commit-gen-0.2.tar.gz/ai-commit-gen-0.2/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+# Read in the README for the long description
+with open("README.md", "r") as f:
+    long_description = f.read()
+
+setup(
+    name="ai-commit-gen",
+    version="0.2",
+    packages=find_packages(),
+    scripts=["ai-commit.py"],
+    install_requires=[
+        "openai",
+        "keyring",
+        "argparse",
+    ],
+    description="A binary to generate commit messages or commits using generative models.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+)
