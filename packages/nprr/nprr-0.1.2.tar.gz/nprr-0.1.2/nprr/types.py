3622a171e926fa456c6708d1bb797edcefa08dc9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:16:36 +0000

    Add package nprr/0.1.2/nprr-0.1.2.tar.gz

---
+++ packages/nprr/nprr-0.1.2.tar.gz/nprr-0.1.2/nprr/types.py
@@ -0,0 +1,9 @@
+"""
+This module contains some type aliases.
+"""
+from numpy.typing import NDArray
+import numpy as np
+
+# Type alias for a real-valued array
+RealArray = NDArray[np.float_]
+IntArray = NDArray[np.int_]
