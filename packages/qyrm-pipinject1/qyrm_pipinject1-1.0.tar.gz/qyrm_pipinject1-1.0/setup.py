715dc1da695b23815d4ba2ff572f3ddd87e0590f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:14:58 +0000

    Add package qyrm-pipinject1/1.0/qyrm_pipinject1-1.0.tar.gz

---
+++ packages/qyrm-pipinject1/qyrm_pipinject1-1.0.tar.gz/qyrm_pipinject1-1.0/setup.py
@@ -0,0 +1,18 @@
+import setuptools
+print("123")
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+setuptools.setup(
+      name='qyrm_pipinject1',
+      version='1.0',
+      author='',
+      author_email='',
+      description='',
+      long_description=long_description,
+      long_description_content_type="text/markdown",
+      url='',     
+      packages=setuptools.find_packages(),
+      data_files=[('diractory',['file'])],   
+      install_requires=[
+    ]   
+    )
