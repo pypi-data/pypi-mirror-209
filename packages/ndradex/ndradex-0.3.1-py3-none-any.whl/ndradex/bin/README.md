a9919b59a096b60a4f589b675c65461d7efaf13e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:08:11 +0000

    Add package ndradex/0.3.1/ndradex-0.3.1-py3-none-any.whl

---
+++ packages/ndradex/ndradex-0.3.1-py3-none-any.whl/ndradex/bin/README.md
@@ -0,0 +1,50 @@
+# homebrew-radex
+
+[![Release](https://img.shields.io/github/v/release/astropenguin/homebrew-radex?display_name=tag&label=Release&color=cornflowerblue&style=flat-square)](https://github.com/astropenguin/homebrew-radex/releases)
+[![DOI](https://img.shields.io/badge/DOI-10.5281/zenodo.7086636-cornflowerblue?style=flat-square)](https://doi.org/10.5281/zenodo.7086636)
+[![Tests](https://img.shields.io/github/actions/workflow/status/astropenguin/homebrew-radex/tests.yml?label=Tests&style=flat-square)](https://github.com/astropenguin/homebrew-radex/actions)
+
+RADEX installation by Homebrew
+
+## Overview
+
+This project provides a Homebrew formula for [RADEX], which automatically builds and installs RADEX to your environment.
+Now you can install RADEX by the following command:
+
+```shell
+brew install astropenguin/radex/radex
+```
+
+## RADEX commands
+
+The following three commands and their aliases will be installed:
+
+| Command | Alias | Descripion |
+| --- | --- | --- |
+| `radex-1` | `radex-uni` | RADEX built with option 1 (uniform sphere) |
+| `radex-2` | `radex-lvg` | RADEX built with option 2 (expanding sphere) |
+| `radex-3` | `radex-slab` | RADEX built with option 3 (plane parallel slab) |
+
+As a major difference from the normal RADEX installation, the path of the data directory is not set by default:
+This allows you to specify the relative or full path of a data file (`*.dat`).
+Note that the tilde expansion (i.e. `~/`) cannot be used for the path specification.
+
+## Customization
+
+The following environment variables can be set for the RADEX installation:
+
+| Variable | Default | Description |
+| --- | --- | --- |
+| `HOMEBREW_RADEX_DATADIR` | not set | Path of the directory for RADEX data files (`.dat`) |
+| `HOMEBREW_RADEX_LOGFILE` | `./radex.log` | Path of the RADEX log file |
+| `HOMEBREW_RADEX_MINITER` | `10` | The number of minimum iterations in a RADEX calculation |
+| `HOMEBREW_RADEX_MAXITER` | `9999` | The number of maximum iterations in a RADEX calculation |
+
+For example, if you want to set the data directory:
+
+```shell
+HOMEBREW_RADEX_DATADIR=${HOME}/radex brew install astropenguin/radex/radex
+```
+
+<!-- References -->
+[RADEX]: https://personal.sron.nl/~vdtak/radex/index.shtml
---
+++ packages/ndradex/ndradex-0.3.1-py3-none-any.whl/ndradex/radex.py
@@ -0,0 +1,126 @@
+__all__ = []
+
+
+# standard library
+from logging import getLogger
+from pathlib import Path
+from subprocess import PIPE, run as sprun
+from subprocess import CalledProcessError, TimeoutExpired
+from typing import List, Optional, Sequence, Union
+
+
+# dependencies
+from .consts import NDRADEX, RADEX_VERSION
+
+
+# type hints
+PathLike = Union[Path, str]
+
+
+# constants
+N_OUTPUT_VALUES = 10
+
+
+# module logger
+logger = getLogger(__name__)
+
+
+def run(
+    input: Sequence[str],
+    radex: PathLike,
+    cleanup: bool = True,
+    timeout: Optional[float] = None,
+) -> List[str]:
+    """Run RADEX and return the output as a list of strings.
+
+    Note that the function only reads the last line of a RADEX output.
+    This means that the function will only return the values
+    of the highest-frequency transition specified in the RADEX input.
+
+    Args:
+        input: Input strings for RADEX. See the examples below.
+        radex: Absolute path of the RADEX binary to be run.
+        cleanup: If True, the RADEX output file will be deleted.
+        timeout: Timeout of the run in units of seconds.
+            Default is None (unlimited run time is allowed).
+
+    Returns:
+        RADEX output as a list of strings.
+
+    Examples:
+        To get output of CO(1-0) @ T_kin = 100 K, n_H2 = 1e3 cm^-3,
+        N_CO = 1e15 cm^-2, T_bg = 2.73 K, and dv = 1.0 km s^-1::
+
+            input = [
+                "co.dat", "radex.out", "110 120", "100",
+                "1", "H2", "1e3", "2.73", "1e15", "1.0", "0",
+            ]
+            output = run(input, "/path/to/radex")
+
+    """
+    try:
+        sprun(
+            str(radex),
+            input="\n".join(input),
+            timeout=timeout,
+            stdout=PIPE,
+            stderr=PIPE,
+            check=True,
+            text=True,
+        )
+        return finalize(input[1], cleanup)
+    except (IndexError, TypeError):
+        logger.warning("RADEX did not run due to invalid input")
+        return ["NaN"] * N_OUTPUT_VALUES
+    except FileNotFoundError:
+        logger.warning("RADEX path or moldata does not exist")
+        return ["NaN"] * N_OUTPUT_VALUES
+    except CalledProcessError:
+        logger.warning("RADEX failed due to invalid input")
+        return ["NaN"] * N_OUTPUT_VALUES
+    except TimeoutExpired:
+        logger.warning("RADEX interrupted due to timeout")
+        return ["NaN"] * N_OUTPUT_VALUES
+    except RuntimeError:
+        logger.warning("RADEX version is not valid")
+        return ["NaN"] * N_OUTPUT_VALUES
+
+
+def build(force: bool = False) -> None:
+    """Build the builtin RADEX binaries."""
+    if force:
+        sprun(
+            args=["make", "clean"],
+            cwd=NDRADEX / "bin",
+            stdout=PIPE,
+            stderr=PIPE,
+            check=True,
+        )
+
+    sprun(
+        args=[
+            "make",
+            "build",
+            "RADEX_LOGFILE=/dev/null",
+            "RADEX_MAXITER=999999",
+        ],
+        cwd=NDRADEX / "bin",
+        stdout=PIPE,
+        stderr=PIPE,
+        check=True,
+    )
+
+
+def finalize(output: PathLike, cleanup: bool) -> List[str]:
+    """Read a RADEX output file and return the final output."""
+    try:
+        with open(output) as f:
+            lines = f.readlines()
+
+        if RADEX_VERSION not in lines[0]:
+            raise RuntimeError("RADEX version is not valid")
+
+        return lines[-1].split()[-N_OUTPUT_VALUES:]
+    finally:
+        if cleanup:
+            Path(output).unlink(missing_ok=True)
