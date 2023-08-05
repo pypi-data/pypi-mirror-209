35bfebfd2e9441b8fab56bf9a48e6c5d3b34c155
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:23:08 +0000

    Add package pygama/1.3.2/pygama-1.3.2-py3-none-any.whl

---
+++ packages/pygama/pygama-1.3.2-py3-none-any.whl/pygama/evt/__init__.py
@@ -0,0 +1,8 @@
+"""
+Utilities for grouping hit data into events.
+"""
+
+from pygama.evt.build_tcm import build_tcm
+from pygama.evt.tcm import generate_tcm_cols
+
+__all__ = ["build_tcm", "generate_tcm_cols"]
---
+++ packages/pygama/pygama-1.3.2-py3-none-any.whl/pygama/vis/__init__.py
@@ -0,0 +1,7 @@
+"""
+This subpackage implements utilities to visualize data.
+"""
+
+from pygama.vis.waveform_browser import WaveformBrowser
+
+__all__ = ["WaveformBrowser"]
