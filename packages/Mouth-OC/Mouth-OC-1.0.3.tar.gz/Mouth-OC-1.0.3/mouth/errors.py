faf620cea7f26cb30074308fbd8639d57dfb3169
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:29:38 +0000

    Add package Mouth-OC/1.0.3/Mouth-OC-1.0.3.tar.gz

---
+++ packages/Mouth-OC/Mouth-OC-1.0.3.tar.gz/Mouth-OC-1.0.3/mouth/errors.py
@@ -0,0 +1,16 @@
+# coding=utf8
+""" Errors
+
+Mouth error codes
+"""
+
+__author__ = "Chris Nasr"
+__copyright__ = "Ouroboros Coding Inc"
+__version__ = "1.0.0"
+__email__ = "chris@ouroboroscoding.com"
+__created__ = "2023-01-11"
+
+TEMPLATE_CONTENT_ERROR = 1300
+ATTACHMENT_STRUCTURE = 1301
+ATTACHMENT_DECODE = 1302
+SMTP_ERROR = 1303
