1d709548d84db13e4a78072c0fc30e67280e071b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:16:31 +0000

    Add package nprr/0.1/nprr-0.1.0.tar.gz

---
+++ packages/nprr/nprr-0.1.0.tar.gz/nprr-0.1.0/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(
+    name="nprr",
+    version="0.1.0",
+    long_description="file: README.md",
+    long_description_content_type="text/markdown",
+    description="Nonparametric randomized response and locally private confidence sets",
+    url="https://github.com/wannabesmith/nprr",
+    author="Ian Waudby-Smith",
+    author_email="iwaudbysmith@gmail.com",
+    license="BSD 3-Clause",
+    packages=["nprr"],
+    install_requires=["confseq", "numpy", "scipy"],
+    zip_safe=False,
+)
