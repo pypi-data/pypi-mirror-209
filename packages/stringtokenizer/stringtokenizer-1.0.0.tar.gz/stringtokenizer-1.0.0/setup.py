6246474bcfc173e17dc3e52ae8e695534641677d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:28:23 +0000

    Add package stringtokenizer/1.0.0/stringtokenizer-1.0.0.tar.gz

---
+++ packages/stringtokenizer/stringtokenizer-1.0.0.tar.gz/stringtokenizer-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='stringtokenizer',
+    version='1.0.0',
+    description='A string tokenizer implementation',
+    author='Tanmay Mandal',
+    packages=['stringtokenizer'],
+    python_requires='>=3.6',
+)
+
