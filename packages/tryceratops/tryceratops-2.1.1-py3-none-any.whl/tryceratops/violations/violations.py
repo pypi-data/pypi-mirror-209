36f116a823f434b7e142a6ae333e0a3ed7559f69
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:28:16 +0000

    Add package tryceratops/2.1.1/tryceratops-2.1.1-py3-none-any.whl

---
+++ packages/tryceratops/tryceratops-2.1.1-py3-none-any.whl/tryceratops/violations/violations.py
@@ -0,0 +1,70 @@
+from __future__ import annotations
+
+import ast
+from dataclasses import dataclass
+from typing import Any, Optional, Tuple
+
+
+@dataclass
+class Violation:
+    code: str
+    line: int
+    col: int
+    description: str
+    filename: str
+    node: ast.AST
+
+    @classmethod
+    def build(
+        cls,
+        filename: str,
+        vio_details: Tuple[str, str],
+        node: ast.AST,
+        *args: Any,  # noqa: ANN401
+        **kwargs: Any,  # noqa: ANN401
+    ) -> Violation:
+        code, msg = vio_details
+        return cls(code, node.lineno, node.col_offset, msg, filename, node)
+
+
+@dataclass
+class VerboseReraiseViolation(Violation):
+    exception_name: str
+
+    @classmethod
+    def build(
+        cls,
+        filename: str,
+        vio_details: Tuple[str, str],
+        node: ast.AST,
+        exception_name: str = "",
+        *args: Any,  # noqa: ANN401
+        **kwargs: Any,  # noqa: ANN401
+    ) -> VerboseReraiseViolation:
+        code, msg = vio_details
+        return cls(code, node.lineno, node.col_offset, msg, filename, node, exception_name)
+
+
+@dataclass
+class RaiseWithoutCauseViolation(Violation):
+    except_node: ast.ExceptHandler
+    exception_name: Optional[str] = None
+
+    @classmethod
+    def build(
+        cls,
+        filename: str,
+        vio_details: Tuple[str, str],
+        node: ast.AST,
+        except_node: Optional[ast.ExceptHandler] = None,
+        exception_name: Optional[str] = None,
+        *args: Any,  # noqa: ANN401
+        **kwargs: Any,  # noqa: ANN401
+    ) -> RaiseWithoutCauseViolation:
+        if except_node is None:
+            raise ValueError("except_node")
+
+        code, msg = vio_details
+        return cls(
+            code, node.lineno, node.col_offset, msg, filename, node, except_node, exception_name
+        )
