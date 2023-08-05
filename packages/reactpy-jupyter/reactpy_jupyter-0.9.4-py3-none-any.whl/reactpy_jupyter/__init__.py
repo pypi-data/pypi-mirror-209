420c05c5799b46081ab1eebd0a9c193a94ce9ffa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:07:35 +0000

    Add package reactpy-jupyter/0.9.4/reactpy_jupyter-0.9.4-py3-none-any.whl

---
+++ packages/reactpy-jupyter/reactpy_jupyter-0.9.4-py3-none-any.whl/reactpy_jupyter/__init__.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# Copyright (c) Ryan Morshead.
+# Distributed under the terms of the Modified BSD License.
+
+from . import jupyter_server_extension
+from .import_resources import setup_import_resources
+from .layout_widget import run, set_import_source_base_url, to_widget
+from .monkey_patch import execute_patch
+from .widget_component import from_widget
+
+__version__ = "0.9.4"  # DO NOT MODIFY
+
+__all__ = (
+    "from_widget",
+    "load_ipython_extension",
+    "unload_ipython_extension",
+    "to_widget",
+    "run",
+    "set_import_source_base_url",
+    "jupyter_server_extension",
+)
+
+
+setup_import_resources()
+execute_patch()
