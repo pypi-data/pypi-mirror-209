28ae36b4cec013aab0dff723477990f4757babd8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:16:22 +0000

    Add package wyzard/0.1b1/wyzard-0.1b1-py3-none-any.whl

---
+++ packages/wyzard/wyzard-0.1b1-py3-none-any.whl/wyzard/__init__.py
@@ -0,0 +1,10 @@
+from .gpt_detector import GPTDetector
+from .object_detection import ObjectDetection
+from .image_captioning import ImageCaptioning
+#from .utils import *
+
+__title__ = "Wyzard"
+__version__ = "0.1b1"
+__authors__ = "LyQuid"
+__license__ = "MIT License"
+__copyright__ = "Copyright 2023-present LyQuid"
