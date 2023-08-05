e444b648a9eec1ab743544a854ef2dedfb338afc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:11:59 +0000

    Add package django-gem/1.0.0/django_gem-1.0.0.tar.gz

---
+++ packages/django-gem/django_gem-1.0.0.tar.gz/django_gem-1.0.0/django_gem/overrides/save.py
@@ -0,0 +1,41 @@
+import typing
+
+from django.db import transaction
+
+from django_gem.entities.registry import CutterBatchItem
+from django_gem.models.mixins import GemCutConditionalMixin
+from django_gem.overrides.utils import add_cut_items, is_cutting_needed
+
+
+def save_cutting_hook(
+    save_func,
+    cut_batch_items: typing.List[CutterBatchItem],
+):
+    def wrapped_trigger(self, *args, **kwargs):
+        from django_gem.toolkit import gem_settings, smith
+
+        gem_cutting_enabled = gem_settings.GEM_CUTTING_ENABLED
+
+        should_cut = True
+        if isinstance(self, GemCutConditionalMixin):
+            should_cut = self.should_cut_on_save(*args, **kwargs)
+
+        with transaction.atomic():
+            changed_cut_batch_items = []
+            if should_cut:
+                for cut_batch_item in cut_batch_items:
+                    cut_batch_item.load_object_ids(self)
+                    if is_cutting_needed(self, cut_batch_item.affected_fields):
+                        changed_cut_batch_items.append(cut_batch_item)
+
+            save_func(self, *args, **kwargs)
+            if not gem_cutting_enabled or not should_cut:
+                return
+
+            add_cut_items(smith, changed_cut_batch_items)
+
+            transaction.on_commit(
+                lambda: smith.initiate_cutting(),
+            )
+
+    return wrapped_trigger
