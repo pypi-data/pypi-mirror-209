62443d6658c25d4f0e4fbd74947250da39d05048
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:10:21 +0000

    Add package geotribu/0.16.0/geotribu-0.16.0-py3-none-any.whl

---
+++ packages/geotribu/geotribu-0.16.0-py3-none-any.whl/geotribu_cli/subcommands/__init__.py
@@ -0,0 +1,8 @@
+#! python3  # noqa: E265
+
+# submodules
+
+from .rss_reader import parser_latest_content  # noqa: F401
+from .search_content import parser_search_content  # noqa: F401
+from .search_image import parser_search_image  # noqa: F401
+from .upgrade import parser_upgrade  # noqa: F401
