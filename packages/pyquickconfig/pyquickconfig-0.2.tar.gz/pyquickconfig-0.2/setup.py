c6aaa1607f8f0010301fb7f96acfae88f9b10988
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:08:52 +0000

    Add package pyquickconfig/0.2/pyquickconfig-0.2.tar.gz

---
+++ packages/pyquickconfig/pyquickconfig-0.2.tar.gz/pyquickconfig-0.2/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='pyquickconfig',
+    version='0.2',
+    license='MIT',
+    author="Austin Ibele",
+    author_email='austin.ibele@gmail.com',
+    packages=find_packages('quickconfig'),
+    package_dir={'': 'quickconfig'},
+    url='https://github.com/austinibele/quickconfig',
+    keywords='QuickConfig',
+)
