a4b56e1c7ff9f1ccf68a10d37d1b5b700a93a5e7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:07:41 +0000

    Add package dlt/0.2.8a0/dlt-0.2.8a0-py3-none-any.whl

---
+++ packages/dlt/dlt-0.2.8a0-py3-none-any.whl/dlt/common/storages/__init__.py
@@ -0,0 +1,7 @@
+from .file_storage import FileStorage  # noqa: F401
+from .schema_storage import SchemaStorage  # noqa: F401
+from .live_schema_storage import LiveSchemaStorage  # noqa: F401
+from .normalize_storage import NormalizeStorage  # noqa: F401
+from .versioned_storage import VersionedStorage  # noqa: F401
+from .load_storage import LoadStorage  # noqa: F401
+from .data_item_storage import DataItemStorage  # noqa: F401
