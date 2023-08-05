0b1cf0ec683e3cf775fe0d71d78189fdfe035d9e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:29:48 +0000

    Add package encoPy/1.0.0/encoPy-1.0.0.tar.gz

---
+++ packages/encoPy/encoPy-1.0.0.tar.gz/encoPy-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='encoPy',
+    version='1.0.0',
+    description='Encoding library made by poe.com',
+    author='virus',
+    author_email='unknown@mail.com',
+    packages=find_packages(),
+    install_requires=[]
+)
