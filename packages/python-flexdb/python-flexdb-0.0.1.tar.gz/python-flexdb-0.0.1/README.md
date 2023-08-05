5a636724ede9d86e2b5d9e9398fb9fcd10a726d5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:11:46 +0000

    Add package python-flexdb/0.0.1/python-flexdb-0.0.1.tar.gz

---
+++ packages/python-flexdb/python-flexdb-0.0.1.tar.gz/python-flexdb-0.0.1/README.md
@@ -0,0 +1,3 @@
+# python-flexdb
+
+Python wrapper for the FlexDB REST API
---
+++ packages/python-flexdb/python-flexdb-0.0.1.tar.gz/python-flexdb-0.0.1/setup.py
@@ -0,0 +1,16 @@
+# Copyright (C) 2023 FlexDB <team@flexdb.co>
+# Use of this source code is governed by the GPL-3.0
+# license that can be found in the LICENSE file.
+
+from setuptools import setup, find_packages
+
+setup(
+    name='python-flexdb',
+    version='0.0.1',
+    url='https://github.com/flexdb/python-flexdb',
+    author='Adam K Dean',
+    author_email='adam@flexdb.co',
+    description='Python wrapper for the FlexDB REST API (https://flexdb.co)',
+    packages=find_packages(),
+    install_requires=[],
+)
