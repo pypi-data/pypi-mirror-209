91dd8025199deddb43a8eea57e4822fd18bbc8ba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:01:25 +0000

    Add package pyloggor/1.1.3/pyloggor-1.1.3.tar.gz

---
+++ packages/pyloggor/pyloggor-1.1.3.tar.gz/pyloggor-1.1.3/setup.py
@@ -0,0 +1,19 @@
+from distutils.core import setup
+
+setup(
+    name="pyloggor",
+    version="1.1.3",
+    description="An incredibly versatile yet simple logging system.",
+    author="Parth Mittal",
+    author_email="parth@privatepanda.co",
+    url="https://www.github.com/PrivatePandaCO/pyloggor",
+    license="MIT",
+    packages=["pyloggor"],
+    install_requires=["pytz"],
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    project_urls={
+        "Documentation": "https://github.com/PrivatePandaCO/pyloggor/blob/master/README.md",
+        "Github": "https://github.com/PrivatePandaCO/pyloggor",
+    },
+)
