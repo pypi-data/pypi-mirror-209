acd0a1cfb4e4ad97ba38dbe2cea747e62298fefb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:01:45 +0000

    Add package validmind/1.12.2a1/validmind-1.12.2a1.tar.gz

---
+++ packages/validmind/validmind-1.12.2a1.tar.gz/validmind-1.12.2a1/validmind/test_plans/binary_classifier.py
@@ -0,0 +1,81 @@
+"""
+Test plan for sklearn classifier models
+
+Ideal setup is to have the API client to read a
+custom test plan from the project's configuration
+"""
+
+from ..vm_models import TestPlan
+from ..data_validation.metrics import DatasetSplit
+from ..model_validation.model_metadata import ModelMetadata
+from ..model_validation.sklearn.metrics import (
+    ClassifierInSamplePerformance,
+    ClassifierOutOfSamplePerformance,
+    ConfusionMatrix,
+    PermutationFeatureImportance,
+    PrecisionRecallCurve,
+    ROCCurve,
+    CharacteristicStabilityIndex,
+    PopulationStabilityIndex,
+    SHAPGlobalImportance,
+)
+from ..model_validation.sklearn.threshold_tests import (
+    MinimumAccuracy,
+    MinimumF1Score,
+    MinimumROCAUCScore,
+    TrainingTestDegradation,
+    OverfitDiagnosis,
+    WeakspotsDiagnosis,
+    RobustnessDiagnosis,
+)
+
+
+class BinaryClassifierMetrics(TestPlan):
+    """
+    Test plan for sklearn classifier metrics
+    """
+
+    name = "binary_classifier_metrics"
+    required_context = ["model"]
+    tests = [
+        ModelMetadata,
+        DatasetSplit,
+        ConfusionMatrix,
+        ClassifierInSamplePerformance,
+        ClassifierOutOfSamplePerformance,
+        PermutationFeatureImportance,
+        PrecisionRecallCurve,
+        ROCCurve,
+        CharacteristicStabilityIndex,
+        PopulationStabilityIndex,
+        SHAPGlobalImportance,
+    ]
+
+
+class BinaryClassifierPerformance(TestPlan):
+    """
+    Test plan for sklearn classifier models
+    """
+
+    name = "binary_classifier_validation"
+    required_context = ["model"]
+    tests = [
+        MinimumAccuracy,
+        MinimumF1Score,
+        MinimumROCAUCScore,
+        TrainingTestDegradation,
+    ]
+
+
+class BinaryClassifierDiagnosis(TestPlan):
+    """
+    Test plan for sklearn classifier model diagnosis tests
+    """
+
+    name = "binary_classifier_model_diagnosis"
+    required_context = ["model"]
+    tests = [
+        OverfitDiagnosis,
+        WeakspotsDiagnosis,
+        RobustnessDiagnosis,
+    ]
