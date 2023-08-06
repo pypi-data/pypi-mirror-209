61f606c3f28e7118cf1c79add0289bdd2e2c7055
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:31:44 +0000

    Add package openapi-pydantic/0.1.1/openapi_pydantic-0.1.1.tar.gz

---
+++ packages/openapi-pydantic/openapi_pydantic-0.1.1.tar.gz/openapi_pydantic-0.1.1/openapi_pydantic/v3/v3_1_0/external_documentation.py
@@ -0,0 +1,28 @@
+from typing import Optional
+
+from pydantic import BaseModel, Extra
+
+
+class ExternalDocumentation(BaseModel):
+    """Allows referencing an external resource for extended documentation."""
+
+    description: Optional[str] = None
+    """
+    A short description of the target documentation.
+    [CommonMark syntax](https://spec.commonmark.org/) MAY be used for rich text 
+    representation.
+    """
+
+    url: str
+    """
+    **REQUIRED**. The URL for the target documentation.
+    Value MUST be in the form of a URL.
+    """
+
+    class Config:
+        extra = Extra.allow
+        schema_extra = {
+            "examples": [
+                {"description": "Find more info here", "url": "https://example.com"}
+            ]
+        }
