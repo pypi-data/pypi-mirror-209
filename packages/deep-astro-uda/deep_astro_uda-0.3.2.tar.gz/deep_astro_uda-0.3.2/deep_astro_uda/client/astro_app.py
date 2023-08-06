c9390d24fccdbb2edb2aef912e1920a0ce26a2e4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:43:51 +0000

    Add package deep-astro-uda/0.3.2/deep_astro_uda-0.3.2.tar.gz

---
+++ packages/deep-astro-uda/deep_astro_uda-0.3.2.tar.gz/deep_astro_uda-0.3.2/deep_astro_uda/client/astro_app.py
@@ -0,0 +1,19 @@
+from cleo.application import Application
+
+import sys
+
+from deep_astro_uda.client.commands.demo_command import DemoCommand
+from deep_astro_uda.client.commands.run_command import RunCommand
+from deep_astro_uda.client.commands.infer_command import InferCommand
+
+# TODO: Update using the correct commands.
+# TODO: Add pydoc documentation.
+dance_app = Application()
+dance_app.add(DemoCommand())
+dance_app.add(RunCommand())
+
+def main() -> int:
+    return dance_app.run()
+
+if __name__ == '__main__':
+    sys.exit(main())
