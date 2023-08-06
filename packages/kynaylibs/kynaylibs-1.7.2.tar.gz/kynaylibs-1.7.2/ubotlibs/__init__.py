e722c163aa36f5a3ca14eaaf14ec0362f136b716
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:30:20 +0000

    Add package kynaylibs/1.7.2/kynaylibs-1.7.2.tar.gz

---
+++ packages/kynaylibs/kynaylibs-1.7.2.tar.gz/kynaylibs-1.7.2/ubotlibs/__init__.py
@@ -0,0 +1,108 @@
+#  Pyrogram - Telegram MTProto API Client Library for Python
+#  Copyright (C) 2017-present Dan <https://github.com/delivrance>
+#
+#  This file is part of Pyrogram.
+#
+#  Pyrogram is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU Lesser General Public License as published
+#  by the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  Pyrogram is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU Lesser General Public License for more details.
+#
+#  You should have received a copy of the GNU Lesser General Public License
+#  along with Pyrogram.  If not, see <http://www.gnu.org/licenses/>.
+import sys
+from datetime import datetime, timezone
+from typing import Dict, Optional, Union
+
+from config import CMD_HNDLR as cmds
+from pyrogram import Client, filters
+
+__version__ = "1.7.2"
+__license__ = "GNU Lesser General Public License v3.0 (LGPL-3.0)"
+__copyright__ = "Copyright (C) 2017-present Dan <https://github.com/delivrance>"
+
+BL_GCAST = [
+    -1001599474353,
+    -1001692751821,
+    -1001473548283,
+    -1001459812644,
+    -1001433238829,
+    -1001476936696,
+    -1001327032795,
+    -1001294181499,
+    -1001419516987,
+    -1001209432070,
+    -1001296934585,
+    -1001481357570,
+    -1001459701099,
+    -1001109837870,
+    -1001485393652,
+    -1001354786862,
+    -1001109500936,
+    -1001387666944,
+    -1001390552926,
+    -1001752592753,
+    -1001777428244,
+    -1001771438298,
+    -1001287188817,
+    -1001812143750,
+    -1001883961446,
+    -1001753840975,
+    -1001896051491,
+    -1001578091827,
+    -1001284445583,
+    -1001927904459,
+    -1001675396283,
+]
+
+BL_UBOT = [1245451624]
+
+DEVS = [
+    1898065191,  # @rizzvbss
+    1054295664,  # @kenapanan
+    1889573907,  # @kanaayyy
+    1755047203,  # @Bangjhorr
+    2133148961,  # @mnaayyy
+    2076745088,  # @gua
+    5876222922,  # Tomay
+    1936017380,  # otan
+]
+
+
+from concurrent.futures.thread import ThreadPoolExecutor
+
+
+class StopTransmission(Exception):
+    pass
+
+
+class StopPropagation(StopAsyncIteration):
+    pass
+
+
+class ContinuePropagation(StopAsyncIteration):
+    pass
+
+
+from . import emoji, enums, filters, handlers, raw, types
+from .client import Client
+from .sync import compose, idle
+
+crypto_executor = ThreadPoolExecutor(1, thread_name_prefix="CryptoWorker")
+
+
+def zero_datetime() -> datetime:
+    return datetime.fromtimestamp(0, timezone.utc)
+
+
+def timestamp_to_datetime(ts: Optional[int]) -> Optional[datetime]:
+    return datetime.fromtimestamp(ts) if ts else None
+
+
+def datetime_to_timestamp(dt: Optional[datetime]) -> Optional[int]:
+    return int(dt.timestamp()) if dt else None
