b81ccfb76b6e00703568f49ecb6178e0e2871080
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:12:07 +0000

    Add package gdsfactory/6.97.0/gdsfactory-6.97.0.tar.gz

---
+++ packages/gdsfactory/gdsfactory-6.97.0.tar.gz/gdsfactory-6.97.0/gdsfactory/export/__init__.py
@@ -0,0 +1,7 @@
+from __future__ import annotations
+
+from gdsfactory.export.to_3d import to_3d
+from gdsfactory.export.to_np import to_np
+from gdsfactory.export.to_stl import to_stl
+
+__all__ = ("to_3d", "to_stl", "to_np")
