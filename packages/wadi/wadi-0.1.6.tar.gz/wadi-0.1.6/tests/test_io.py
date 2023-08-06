8b0fdcd75f4f0b55a502e70f8d478e4db9754fae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:41:40 +0000

    Add package wadi/0.1.6/wadi-0.1.6.tar.gz

---
+++ packages/wadi/wadi-0.1.6.tar.gz/wadi-0.1.6/tests/test_io.py
@@ -0,0 +1,13 @@
+from hgc import constants
+
+import wadi as wd
+
+def test_correct_units():
+    """ Test whether the to-hgc method is using the correct units and names
+    of quantitties """
+    assert True
+    # assert constants == wd.used_hgc_units()
+
+
+
+
