a3f6a33f3df5a4a69412368e3d11113916494230
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:31:47 +0000

    Add package haupt/2.0.0rc16/haupt-2.0.0rc16-py3-none-any.whl

---
+++ packages/haupt/haupt-2.0.0rc16-py3-none-any.whl/haupt/schemas/sandbox_config.py
@@ -0,0 +1,52 @@
+from typing import Optional
+
+from clipped.utils.http import clean_host
+from pydantic import Field, StrictInt, StrictStr
+
+from polyaxon.connections import V1Connection, V1ConnectionKind, V1HostPathConnection
+from polyaxon.env_vars.keys import (
+    EV_KEYS_K8S_NAMESPACE,
+    EV_KEYS_SANDBOX_DEBUG,
+    EV_KEYS_SANDBOX_HOST,
+    EV_KEYS_SANDBOX_IS_LOCAL,
+    EV_KEYS_SANDBOX_PER_CORE,
+    EV_KEYS_SANDBOX_PORT,
+    EV_KEYS_SANDBOX_SSL_ENABLED,
+    EV_KEYS_SANDBOX_WORKERS,
+)
+from polyaxon.schemas.cli.agent_config import AgentConfig
+
+
+class SandboxConfig(AgentConfig):
+    _IDENTIFIER = "sandbox"
+
+    namespace: Optional[StrictStr] = Field(
+        alias=EV_KEYS_K8S_NAMESPACE, default="sandbox"
+    )
+    port: Optional[StrictInt] = Field(alias=EV_KEYS_SANDBOX_PORT)
+    host: Optional[StrictStr] = Field(alias=EV_KEYS_SANDBOX_HOST)
+    ssl_enabled: Optional[bool] = Field(alias=EV_KEYS_SANDBOX_SSL_ENABLED)
+    debug: Optional[bool] = Field(alias=EV_KEYS_SANDBOX_DEBUG)
+    workers: Optional[StrictInt] = Field(alias=EV_KEYS_SANDBOX_WORKERS)
+    per_core: Optional[bool] = Field(alias=EV_KEYS_SANDBOX_PER_CORE)
+    is_local: Optional[bool] = Field(alias=EV_KEYS_SANDBOX_IS_LOCAL)
+
+    def __init__(
+        self,
+        host: Optional[str] = None,
+        **data,
+    ):
+        super().__init__(
+            host=clean_host(host) if host else host,
+            **data,
+        )
+
+    def set_default_artifacts_store(self):
+        if not self.artifacts_store:
+            self.artifacts_store = V1Connection(
+                name="local",
+                kind=V1ConnectionKind.HOST_PATH,
+                schema_=V1HostPathConnection(
+                    host_path=self.store_root, mount_path=self.store_root
+                ),
+            )
