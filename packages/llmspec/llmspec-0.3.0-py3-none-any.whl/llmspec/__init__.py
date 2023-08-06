739f1adf095833ea1ce61f8482a538d56cd13285
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:38:13 +0000

    Add package llmspec/0.3.0/llmspec-0.3.0-py3-none-any.whl

---
+++ packages/llmspec/llmspec-0.3.0-py3-none-any.whl/llmspec/__init__.py
@@ -0,0 +1,27 @@
+from llmspec.llmspec import (
+    LanguageModels,
+    LLMSpec,
+    ChatChoice,
+    ChatMessage,
+    ChatCompletionRequest,
+    CompletionResponse,
+    PromptCompletionRequest,
+    EmbeddingRequest,
+    EmbeddingResponse,
+    TokenUsage,
+    Role,
+)
+
+__all__ = [
+    "LanguageModels",
+    "LLMSpec",
+    "ChatMessage",
+    "ChatCompletionRequest",
+    "ChatChoice",
+    "CompletionResponse",
+    "PromptCompletionRequest",
+    "EmbeddingRequest",
+    "EmbeddingResponse",
+    "TokenUsage",
+    "Role",
+]
