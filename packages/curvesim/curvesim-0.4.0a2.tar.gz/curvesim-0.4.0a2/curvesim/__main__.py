aba69afd77855c392077addf42fb32d07205bb14
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:10:19 +0000

    Add package curvesim/0.4.0a2/curvesim-0.4.0a2.tar.gz

---
+++ packages/curvesim/curvesim-0.4.0a2.tar.gz/curvesim-0.4.0a2/curvesim/__main__.py
@@ -0,0 +1,19 @@
+"""Simple health check for the package."""
+import time
+
+from .sim import autosim
+
+
+def hello_world():
+    """Simple sim run as a health check."""
+    # pylint: disable=redefined-outer-name
+    t = time.time()
+    res = autosim("0xbEbc44782C7dB0a1A60Cb6fe97d0b483032FF1C7", test=True, ncpu=1)
+    elapsed = time.time() - t
+    print("Elapsed time:", elapsed)
+
+    return res
+
+
+if __name__ == "__main__":
+    res = hello_world()
