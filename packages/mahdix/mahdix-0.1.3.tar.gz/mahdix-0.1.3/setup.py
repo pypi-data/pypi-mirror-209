d083fb3d4f62648fdbee9620389780da81d280a6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:17:05 +0000

    Add package mahdix/0.1.3/mahdix-0.1.3.tar.gz

---
+++ packages/mahdix/mahdix-0.1.3.tar.gz/mahdix-0.1.3/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+setup(name='mahdix',
+version='0.1.3',
+description='this is a simple pip modul and test',
+long_description='THia is simpile modul crate by mahdi hasan',
+package=['mahdit','os','requests','base64','random','sys'],
+install_requires=[])
