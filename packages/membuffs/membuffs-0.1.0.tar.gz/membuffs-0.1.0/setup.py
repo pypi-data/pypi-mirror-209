64df602c7c8be07f34d262ccbb6339853e43e442
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:17:24 +0000

    Add package membuffs/0.1.0/membuffs-0.1.0.tar.gz

---
+++ packages/membuffs/membuffs-0.1.0.tar.gz/membuffs-0.1.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="membuffs",
+    version="0.1.0",
+    description="Crea una instancia con valores que estarán almacenados directos en la memoria.",
+    author="J. Soriano",
+    author_email="gmparatodos@gmail.com",
+    py_modules="membuffs",
+    requires=[],
+)
