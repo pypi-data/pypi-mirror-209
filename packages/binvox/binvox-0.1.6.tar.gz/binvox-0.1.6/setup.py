838af2481c34176c8081965b72ccffd4dfa49e7e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:17:31 +0000

    Add package binvox/0.1.6/binvox-0.1.6.tar.gz

---
+++ packages/binvox/binvox-0.1.6.tar.gz/binvox-0.1.6/setup.py
@@ -0,0 +1,37 @@
+from distutils.core import setup
+
+__version__ = "0.1.6"
+
+# read the contents of your README file
+from pathlib import Path
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.rst").read_text(encoding="utf-8")
+
+setup(
+    name='binvox',
+    packages=['binvox'],
+    version=__version__,
+    license='MIT',
+    description='Library for loading & saving the Binvox files',
+    author='Farid Yagubbayli',
+    author_email='faridyagubbayli@gmail.com',
+    url='https://github.com/faridyagubbayli/binvox',
+    keywords=['Binvox'],
+    install_requires=[
+        'numpy',
+    ],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
+        'Intended Audience :: Developers',  # Define that your audience are developers
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10'
+    ],
+    long_description=long_description,
+    long_description_content_type='text/markdown'
+)
