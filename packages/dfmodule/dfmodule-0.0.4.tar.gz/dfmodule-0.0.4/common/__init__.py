7ed20f01d480816f17bf71a96a9344df1d6c1369
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:43:43 +0000

    Add package dfmodule/0.0.4/dfmodule-0.0.4.tar.gz

---
+++ packages/dfmodule/dfmodule-0.0.4.tar.gz/dfmodule-0.0.4/common/__init__.py
@@ -0,0 +1,12 @@
+__version__ = '0.0.4'
+
+def hello_python():
+    print("Hello Python!")
+
+def hello_python2():
+    print("Hello Python2!")
+
+def hello_python3():
+    print("Hello Python3!")
+
+func1 = hello_python
