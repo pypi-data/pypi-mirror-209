cf691e07a62e51f6f8004fde5c26d88d64240e50
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:17:05 +0000

    Add package pynamer/1.3.4/pynamer-1.3.4.tar.gz

---
+++ packages/pynamer/pynamer-1.3.4.tar.gz/pynamer-1.3.4/tests/test_create_setup_file.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+# Core Library modules
+from pathlib import Path
+
+# First party modules
+from pynamer import pynamer
+
+BASE_DIR = Path(__file__).parents[0]
+expected_content = """#!/usr/bin/env python3
+
+
+# Third party modules
+from setuptools import setup
+
+setup(name='pynball',
+      version='0.0.1',
+      description='a new project',
+      url='http://github.com/SK/pynball',
+      author='sking',
+      author_email='sking@gmail.com',
+      license='MIT',
+      packages=['pynball'],
+      zip_safe=False)"""
+
+
+def test_create_setup(create_env, project_path_mock, monkeypatch):
+    inputs = iter(["sking", "sking@gmail.com", "0.0.1", "a new project"])
+    monkeypatch.setattr("builtins.input", lambda _: next(inputs))
+    pynamer._create_setup("pynball")
+    setup_file = BASE_DIR / "setup.py"
+    with open(setup_file, encoding="utf-8") as f:
+        contents = f.read()
+    assert contents == expected_content
+
+    setup_file.unlink()
