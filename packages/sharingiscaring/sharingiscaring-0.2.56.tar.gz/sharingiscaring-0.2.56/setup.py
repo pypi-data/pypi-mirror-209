e92f183e53d8cdaaa361670e075a49c8d25f0d42
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:14:41 +0000

    Add package sharingiscaring/0.2.56/sharingiscaring-0.2.56.tar.gz

---
+++ packages/sharingiscaring/sharingiscaring-0.2.56.tar.gz/sharingiscaring-0.2.56/setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+
+setup(
+    name="sharingiscaring",
+    version="0.2.56",
+    author="Sander de Ruiter",
+    author_email="sdr@concordium-explorer.nl",
+    description="Shared code for Concordium Explorer (Bot)",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/sderuiter/sharingiscaring",
+    project_urls={},
+    license="MIT",
+    packages=find_packages(),
+    install_requires=[
+        "rich",
+        "python-dateutil",
+        "base58",
+        "eth-hash",
+        "pycryptodome",
+        "pytest",
+        "pydantic",
+        "pymongo",
+        "motor",
+        "requests",
+        "apprise",
+        "py-graphql-client",
+    ],
+)
