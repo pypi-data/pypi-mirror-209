2afbc6d6f58a22294d9c8706491dfe27ce859562
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:49:04 +0000

    Add package PatryksAutoAI/1.3.3/PatryksAutoAI-1.3.3.tar.gz

---
+++ packages/PatryksAutoAI/PatryksAutoAI-1.3.3.tar.gz/PatryksAutoAI-1.3.3/KaggleAutoAI/language_processing/__init__.py
@@ -0,0 +1,4 @@
+from .models import *
+from .data_manipulation.metrics import Metrics
+from .data_manipulation.text_preprocessing import TextPreprocessing
+from .data_manipulation.text2number import Text2Number
