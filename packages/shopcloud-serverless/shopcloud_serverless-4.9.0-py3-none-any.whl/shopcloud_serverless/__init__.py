ef35c0574cfad99f53e0fcf03f934ef648aebd45
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:30:19 +0000

    Add package shopcloud-serverless/4.9.0/shopcloud_serverless-4.9.0-py3-none-any.whl

---
+++ packages/shopcloud-serverless/shopcloud_serverless-4.9.0-py3-none-any.whl/shopcloud_serverless/__init__.py
@@ -0,0 +1,18 @@
+import os
+from pathlib import Path
+
+
+class Environment:
+    def __init__(self):
+        self._data = self._read_env()
+
+    def _read_env(self):
+        env_data = {}
+        if Path('.env').exists():
+            with open('.env') as f:
+                env_data = f.read().split('\n')
+                env_data = {x.split('=')[0]: x.split('=')[1] for x in env_data if x}
+        return env_data
+
+    def get(self, key: str):
+        return self._data.get(key, os.environ.get(key))
