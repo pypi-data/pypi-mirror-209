0137ea1421fe1c73a9efffe8b704b9ec3c2476bd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:14:15 +0000

    Add package AIPrompts/0.5.0/AIPrompts-0.5.0.tar.gz

---
+++ packages/AIPrompts/AIPrompts-0.5.0.tar.gz/AIPrompts-0.5.0/prompts/__init__.py
@@ -0,0 +1,11 @@
+from .dynamic import DynamicPrompt
+from .schemas import (
+    DynamicSchema,
+    OpenAIModelSettings,
+    PromptRole,
+    Template,
+    TemplateContent,
+    TemplateData,
+    TurboSchema,
+)
+from .turbo import TurboPrompt
