dd26f8df17cbe2ff6553a02ccb58b494105d62e6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:25:14 +0000

    Add package MeUtils/2023.5.21.18.10.59/MeUtils-2023.5.21.18.10.59-py3-none-any.whl

---
+++ packages/MeUtils/MeUtils-2023.5.21.18.10.59-py3-none-any.whl/meutils/ai_nlp/webui/_🏆_主页.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Project      : AI.  @by PyCharm
+# @File         : _🏆_主页.py
+# @Time         : 2023/5/21 17:13
+# @Author       : betterme
+# @WeChat       : meutils
+# @Software     : PyCharm
+# @Description  : 
+
+from meutils.pipe import *
