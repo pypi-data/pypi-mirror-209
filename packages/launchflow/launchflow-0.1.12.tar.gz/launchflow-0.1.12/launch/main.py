10e0dd0b3b2add4ab85ed5ac2cfd48b18364abdf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:11:56 +0000

    Add package launchflow/0.1.12/launchflow-0.1.12.tar.gz

---
+++ packages/launchflow/launchflow-0.1.12.tar.gz/launchflow-0.1.12/launch/main.py
@@ -0,0 +1,40 @@
+import typer
+
+from launch.accounts import account_commands
+from launch.auth import auth_commands
+from launch.config import config_commands
+from launch.gen import gen_commands
+from launch.jobs import job_commands
+from launch.projects import project_commands
+from launch.pipelines import pipeline_commands
+
+app = typer.Typer()
+app.add_typer(auth_commands.app,
+              name='auth',
+              help='Commands for authenticating with LaunchFlow')
+app.add_typer(account_commands.app,
+              name='accounts',
+              help='Account commands for managing accounts')
+app.add_typer(config_commands.app,
+              name='config',
+              help='Commands for managing CLI configuration')
+app.add_typer(gen_commands.app,
+              name='gen',
+              help='Commands for generating BuildFlow and LaunchFlow files.')
+app.add_typer(job_commands.app,
+              name='jobs',
+              help='Commands for managing LaunchFlow jobs')
+app.add_typer(project_commands.app,
+              name='projects',
+              help='Commands for managing LaunchFlow projects')
+app.add_typer(pipeline_commands.app,
+              name='pipelines',
+              help='Commands for managing LaunchFlow pipelines')
+
+
+def main():
+    app()
+
+
+if __name__ == "__main__":
+    main()
