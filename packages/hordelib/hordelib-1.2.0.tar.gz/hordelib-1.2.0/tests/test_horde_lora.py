bfcfb8cd41a08aeed8e59f3fbc118b9a7d323d5b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:17:01 +0000

    Add package hordelib/1.2.0/hordelib-1.2.0.tar.gz

---
+++ packages/hordelib/hordelib-1.2.0.tar.gz/hordelib-1.2.0/tests/test_horde_lora.py
@@ -0,0 +1,109 @@
+# test_horde_lora.py
+import pytest
+from PIL import Image
+
+from hordelib.horde import HordeLib
+from hordelib.shared_model_manager import SharedModelManager
+
+
+class TestHordeLora:
+    @pytest.fixture(autouse=True, scope="class")
+    def setup_and_teardown(self):
+        TestHordeLora.horde = HordeLib()
+
+        TestHordeLora.default_model_manager_args = {
+            "compvis": True,
+        }
+        SharedModelManager.loadModelManagers(**self.default_model_manager_args)
+        assert SharedModelManager.manager is not None
+        SharedModelManager.manager.load("Deliberate")
+        yield
+        del TestHordeLora.horde
+        SharedModelManager._instance = None
+        SharedModelManager.manager = None
+
+    def test_text_to_image_lora_red(self):
+
+        # Red
+        data = {
+            "sampler_name": "k_euler",
+            "cfg_scale": 8.0,
+            "denoising_strength": 1.0,
+            "seed": 304886399544324,
+            "height": 512,
+            "width": 512,
+            "karras": True,
+            "tiling": False,
+            "hires_fix": False,
+            "clip_skip": 1,
+            "control_type": None,
+            "image_is_control": False,
+            "return_control_map": False,
+            "prompt": "a dark magical crystal, GlowingRunesAIV2_red",
+            "loras": [{"name": "GlowingRunesAIV6", "model": 1.0, "clip": 1.0}],
+            "ddim_steps": 20,
+            "n_iter": 1,
+            "model": "Deliberate",
+        }
+        assert self.horde is not None
+        pil_image = self.horde.basic_inference(data)
+        assert pil_image is not None
+        pil_image.save("images/lora_red.webp", quality=90)
+
+    def test_text_to_image_lora_blue(self):
+
+        # Blue
+        data = {
+            "sampler_name": "k_euler",
+            "cfg_scale": 8.0,
+            "denoising_strength": 1.0,
+            "seed": 851616030078638,
+            "height": 512,
+            "width": 512,
+            "karras": True,
+            "tiling": False,
+            "hires_fix": False,
+            "clip_skip": 1,
+            "control_type": None,
+            "image_is_control": False,
+            "return_control_map": False,
+            "prompt": "a dark magical crystal, GlowingRunesAIV2_paleblue",
+            "loras": [{"name": "GlowingRunesAIV6", "model": 1.0, "clip": 1.0}],
+            "ddim_steps": 20,
+            "n_iter": 1,
+            "model": "Deliberate",
+        }
+        assert self.horde is not None
+        pil_image = self.horde.basic_inference(data)
+        assert pil_image is not None
+        pil_image.save("images/lora_blue.webp", quality=90)
+
+    def test_text_to_image_lora_chained(self):
+
+        data = {
+            "sampler_name": "k_euler",
+            "cfg_scale": 8.0,
+            "denoising_strength": 1.0,
+            "seed": 304886399544324,
+            "height": 512,
+            "width": 512,
+            "karras": True,
+            "tiling": False,
+            "hires_fix": False,
+            "clip_skip": 1,
+            "control_type": None,
+            "image_is_control": False,
+            "return_control_map": False,
+            "prompt": "a dark magical crystal, GlowingRunesAIV2_red, Dr490nSc4leAI",
+            "loras": [
+                {"name": "GlowingRunesAIV6", "model": 1.0, "clip": 1.0},
+                {"name": "Dra9onScaleAI", "model": 1.0, "clip": 1.0},
+            ],
+            "ddim_steps": 20,
+            "n_iter": 1,
+            "model": "Deliberate",
+        }
+        assert self.horde is not None
+        pil_image = self.horde.basic_inference(data)
+        assert pil_image is not None
+        pil_image.save("images/lora_multiple.webp", quality=90)
