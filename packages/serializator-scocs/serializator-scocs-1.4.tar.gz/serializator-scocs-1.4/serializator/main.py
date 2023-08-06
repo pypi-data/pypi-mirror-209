e3886b1a81ff474a164f3a34e104e8091518f4b8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:19:32 +0000

    Add package serializator-scocs/1.4/serializator-scocs-1.4.tar.gz

---
+++ packages/serializator-scocs/serializator-scocs-1.4.tar.gz/serializator-scocs-1.4/serializator/main.py
@@ -0,0 +1,12 @@
+import pickle
+import serializator
+
+
+def my_dump(output_filename, obj):
+    with open(output_filename, 'wb') as file:
+        pickle.dump(obj, file)
+
+
+def my_load(input_filename):
+    with open(input_filename, 'rb') as file:
+        pickle.load(file)
