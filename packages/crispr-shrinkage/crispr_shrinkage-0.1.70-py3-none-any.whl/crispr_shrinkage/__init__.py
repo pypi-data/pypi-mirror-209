8280df9e496a8a4a584bc7e37b1f9ab0c0016970
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:17:55 +0000

    Add package crispr-shrinkage/0.1.70/crispr_shrinkage-0.1.70-py3-none-any.whl

---
+++ packages/crispr-shrinkage/crispr_shrinkage-0.1.70-py3-none-any.whl/crispr_shrinkage/__init__.py
@@ -0,0 +1,4 @@
+#!/usr/bin/env python
+from .framework.CrisprShrinkage import perform_adjustment, Guide 
+from .framework.CrisprShrinkage import StatisticalHelperMethods
+from .framework.CrisprShrinkageVisualization import *
