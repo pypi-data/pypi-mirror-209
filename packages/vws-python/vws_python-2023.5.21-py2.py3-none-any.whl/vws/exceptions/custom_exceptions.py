e9b38f82b537ab7a0bf97050d05473eaede67fee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:28:06 +0000

    Add package vws-python/2023.5.21/vws_python-2023.5.21-py2.py3-none-any.whl

---
+++ packages/vws-python/vws_python-2023.5.21-py2.py3-none-any.whl/vws/exceptions/custom_exceptions.py
@@ -0,0 +1,26 @@
+"""
+Exceptions which do not map to errors at
+https://library.vuforia.com/articles/Solution/How-To-Use-the-Vuforia-Web-Services-API#How-To-Interperete-VWS-API-Result-Codes
+or simple errors given by the cloud recognition service.
+"""
+
+
+class UnknownVWSErrorPossiblyBadName(Exception):
+    """
+    Exception raised when VWS returns an HTML page which says "Oops, an error
+    occurred".
+
+    This has been seen to happen when the given name includes a bad character.
+    """
+
+
+class RequestEntityTooLarge(Exception):
+    """
+    Exception raised when the given image is too large.
+    """
+
+
+class TargetProcessingTimeout(Exception):
+    """
+    Exception raised when waiting for a target to be processed times out.
+    """
