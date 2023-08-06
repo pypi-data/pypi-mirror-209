6d7178e59811ecefbf26107f6e7f9efaf8dc3f30
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:18:20 +0000

    Add package archetesting/0.0.3/archetesting-0.0.3.tar.gz

---
+++ packages/archetesting/archetesting-0.0.3.tar.gz/archetesting-0.0.3/archetesting/__init__.py
@@ -0,0 +1,16 @@
+
+import numpy as np
+
+def add_numbers(a, b):
+    sum =a+b
+    return sum
+
+def sub_numbers(a, b):
+    sub =a+b
+    return sub
+
+# print(add_numbers(5,6))
+def cos(val):
+    return np.cos(val)
+
+# print(cos(30))
