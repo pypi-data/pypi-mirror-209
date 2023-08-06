b1a07d3ed53d1dba75a24339152a33722bca64ae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:14:13 +0000

    Add package django-dashboards/0.1.2/django-dashboards-0.1.2.tar.gz

---
+++ packages/django-dashboards/django-dashboards-0.1.2.tar.gz/django-dashboards-0.1.2/dashboards/component/__init__.py
@@ -0,0 +1,19 @@
+from .base import CTA, Component
+from .chart import Chart
+from .form import Form
+from .map import Map
+from .table import BasicTable, Table
+from .text import Stat, Text
+
+
+__all__ = [
+    "Component",
+    "CTA",
+    "Chart",
+    "Form",
+    "Map",
+    "Stat",
+    "Text",
+    "Table",
+    "BasicTable",
+]
