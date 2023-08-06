471b5955c7103c9b6ef3914883e449544149877f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:48:31 +0000

    Add package transphone/1.5.0/transphone-1.5.0.tar.gz

---
+++ packages/transphone/transphone-1.5.0.tar.gz/transphone-1.5.0/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup,find_packages
+
+requirements=[r.strip() for r in open("requirements.txt").readlines()]
+
+setup(
+   name='transphone',
+   version='1.5.0',
+   description='a multilingual g2p/p2g model',
+   author='Xinjian Li',
+   author_email='xinjianl@cs.cmu.edu',
+   url="https://github.com/xinjli/transphone",
+   packages=find_packages(),
+   install_requires=requirements,
+)
