318f19599613a85924adeef3595680c2d51d3d01
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:13:39 +0000

    Add package sftp-uploader/0.1.4/sftp_uploader-0.1.4.tar.gz

---
+++ packages/sftp-uploader/sftp_uploader-0.1.4.tar.gz/sftp_uploader-0.1.4/README.md
@@ -0,0 +1,9 @@
+# TODO
+
+[ ] Make title, description, which problem is solve, metter of this project description
+
+[ ] Add more [classifiers](https://pypi.org/classifiers/)
+
+[ ] Add tests
+
+[ ] Add additional links to project conf
---
+++ packages/sftp-uploader/sftp_uploader-0.1.4.tar.gz/sftp_uploader-0.1.4/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['sftp_uploader']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['gitpython>=3.1.31,<4.0.0', 'paramiko>=3.1.0,<4.0.0']
+
+setup_kwargs = {
+    'name': 'sftp-uploader',
+    'version': '0.1.4',
+    'description': 'Package for upload data to sftp with different parametrs and with different methods',
+    'long_description': '# TODO\n\n[ ] Make title, description, which problem is solve, metter of this project description\n\n[ ] Add more [classifiers](https://pypi.org/classifiers/)\n\n[ ] Add tests\n\n[ ] Add additional links to project conf\n',
+    'author': 'Moonvent',
+    'author_email': 'moonvent@proton.me',
+    'maintainer': 'None',
+    'maintainer_email': 'None',
+    'url': 'https://github.com/moonvent/sftp_uploader/',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.11,<4.0',
+}
+
+
+setup(**setup_kwargs)
