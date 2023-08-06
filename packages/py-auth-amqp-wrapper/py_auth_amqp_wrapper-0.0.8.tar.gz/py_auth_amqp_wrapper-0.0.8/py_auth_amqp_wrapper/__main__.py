5f2b208fe93049b6c7fd21924ce5ccb57cc3d0ff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:14:40 +0000

    Add package py-auth-amqp-wrapper/0.0.8/py_auth_amqp_wrapper-0.0.8.tar.gz

---
+++ packages/py-auth-amqp-wrapper/py_auth_amqp_wrapper-0.0.8.tar.gz/py_auth_amqp_wrapper-0.0.8/py_auth_amqp_wrapper/__main__.py
@@ -0,0 +1,87 @@
+import argparse
+import asyncio
+import sys
+import py_auth_micro
+
+from typing import Optional
+
+from ._version import __version__
+from ._load_config import load_config
+from ._runner import run
+
+
+def cli() -> Optional[str]:
+    parser = argparse.ArgumentParser(
+        prog="py_auth_amqp_wrapper",
+        description="This will start an auth server based on py_auth_micro and connect it to an AMQP message Broker",
+    )
+    parser.add_argument(
+        "--config",
+        "-c",
+        help="path to the configuration file",
+        default="./config.json",
+        type=str,
+    )
+    parser.add_argument(
+        "--version",
+        "-v",
+        help="displays the package version and quits",
+        action="store_true",
+    )
+    parser.add_argument(
+        "--disable_existing_loggers",
+        "-d",
+        help="disables existing loggers",
+        action="store_true",
+    )
+    parser.add_argument(
+        "--generate_db", "-g", help="generate database schema", action="store_true"
+    )
+
+    inputvars = parser.parse_args()
+
+    if inputvars.version:
+        print(f"py_auth_micro=={__version__}")
+        return None
+
+    return inputvars.config, inputvars.disable_existing_loggers, inputvars.generate_db
+
+
+if __name__ == "__main__":
+    config, disable_existing_loggers, generate_db = cli()
+
+    if config is None:
+        sys.exit(0)
+
+        # configure DB
+    print(
+        f"""                             _   _     
+                            | | | |    
+  _ __  _   _     __ _ _   _| |_| |__  
+ | '_ \| | | |   / _` | | | | __| '_ \ 
+ | |_) | |_| |  | (_| | |_| | |_| | | |
+ | .__/ \__, |   \__,_|\__,_|\__|_| |_|
+ | |     __/ |_____                    
+ |_|    |___/______|
+       py_auth_micro: {py_auth_micro.__version__}
+py_auth_amqp_wrapper: {__version__}
+ 
+Starting Up!"""
+    )
+
+    print(f"loading config file: '{config}'")
+    try:
+        config = load_config(config)
+    except Exception as exc:
+        print(exc)
+        print("could not load config")
+        sys.exit(78)
+    print("loaded config")
+    print("\n\nstarting up")
+    asyncio.run(
+        run(
+            **config,
+            disable_existing_loggers=disable_existing_loggers,
+            generate_schema=generate_db,
+        )
+    )
