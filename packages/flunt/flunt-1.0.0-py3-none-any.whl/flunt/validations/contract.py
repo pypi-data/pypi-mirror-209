80072214e8815cb1e009eabf2b255eaa2760cf4b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:52:43 +0000

    Add package flunt/1.0.0/flunt-1.0.0-py3-none-any.whl

---
+++ packages/flunt/flunt-1.0.0-py3-none-any.whl/flunt/validations/contract.py
@@ -0,0 +1,28 @@
+"""Module Contract."""
+from flunt.notifications.notifiable import Notifiable
+from flunt.notifications.notification import Notification
+from flunt.validations.bool_validation_contract import BoolValidationContract
+from flunt.validations.email_validation_contract import EmailValidationContract
+from flunt.validations.strings_validation_contract import StringValidationContract
+
+
+class Contract(
+    StringValidationContract,
+    EmailValidationContract,
+    BoolValidationContract,
+    Notifiable,
+):
+    """Class Contract."""
+
+    def requires(self, value: str, key: str):
+        """Require.
+
+        :param value
+        :param key
+
+        :return
+        """
+        if not value:
+            self.add_notification(Notification(key, "Campo preenchimento obrigatório"))
+
+        return self
