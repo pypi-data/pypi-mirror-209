1c22c4e187cf0473f3b0d7062c191f0ed22d869a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:33:56 +0000

    Add package fmvmm/1.0.9/fmvmm-1.0.9.tar.gz

---
+++ packages/fmvmm/fmvmm-1.0.9.tar.gz/fmvmm-1.0.9/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='fmvmm',
+    version='1.0.9',
+    author='Samyajoy Pal',
+    author_email='palsamyajoy@gmail.com',
+    description="flexible multivariate mixture model",
+    long_description="mixture model with different distributions",
+    packages=find_packages(),
+    install_requires=[
+        'numpy',
+        'scipy',
+        'pandas',
+        'dirichlet',
+        'sklearn'
+    ],
+    entry_points={
+        'console_scripts': [
+            'my_command = fmvmm.command:main',
+        ],
+    },
+)
