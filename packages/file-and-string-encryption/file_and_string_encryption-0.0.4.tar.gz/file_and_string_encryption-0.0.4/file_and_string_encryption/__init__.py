4b3cc32cbce7550213ab282f65469200767982d8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:12:42 +0000

    Add package file-and-string-encryption/0.0.4/file_and_string_encryption-0.0.4.tar.gz

---
+++ packages/file-and-string-encryption/file_and_string_encryption-0.0.4.tar.gz/file_and_string_encryption-0.0.4/file_and_string_encryption/__init__.py
@@ -0,0 +1,22 @@
+from .file_and_string_encryption import (
+    bytes_to_data_using_pickle,
+    data_to_bytes_using_pickle,
+    decrypt_data,
+    decrypt_directory,
+    decrypt_file,
+    decrypt_file_with_password,
+    decrypt_string_with_password,
+    derive_key_from_password,
+    encrypt_data,
+    encrypt_directory,
+    encrypt_file,
+    encrypt_file_with_password,
+    encrypt_string_with_password,
+    generate_password,
+    get_random_key,
+    hash_password_with_argon2,
+    save_password_encrypted_key,
+    verfiy_hashed_password_with_argon2,
+)
+
+__version__ = "0.0.4"
