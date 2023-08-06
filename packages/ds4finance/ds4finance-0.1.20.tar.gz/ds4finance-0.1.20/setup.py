320951c8be2e0cf365dc1c78add59d40060fd870
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:12:13 +0000

    Add package ds4finance/0.1.20/ds4finance-0.1.20.tar.gz

---
+++ packages/ds4finance/ds4finance-0.1.20.tar.gz/ds4finance-0.1.20/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="ds4finance",
+    version="0.1.20",
+    description="A collection of data science tools for finance",
+    long_description=open("README.md", "r").read(),
+    long_description_content_type="text/markdown",
+    author="Luis Silva",
+    author_email="luis_paulo_silva@hotmail.com",
+    url="https://github.com/LuisSousaSilva/ds4finance",
+    packages=find_packages(),
+    install_requires=["numpy", "pandas"],
+    license="MIT",
+    classifiers=[
+        "Development Status :: 2 - Pre-Alpha",
+        "Intended Audience :: Financial and Insurance Industry",
+        "Intended Audience :: Science/Research",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
+    ],
+    python_requires=">=3.6",
+)
