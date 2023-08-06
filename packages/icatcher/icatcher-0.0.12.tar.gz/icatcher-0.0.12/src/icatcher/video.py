f4a3186383c1e32f7ee14d342ffc54f1d2f9ae86
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:14:51 +0000

    Add package icatcher/0.0.12/icatcher-0.0.12.tar.gz

---
+++ packages/icatcher/icatcher-0.0.12.tar.gz/icatcher-0.0.12/src/icatcher/video.py
@@ -0,0 +1,67 @@
+import ffmpeg
+import subprocess
+import re
+import sys
+from pathlib import Path
+import time
+import collections
+
+class FPS:
+    """
+    calculates current fps and returns it, see https://stackoverflow.com/a/54539292
+    """
+    def __init__(self,avarageof=50):
+        self.frametimestamps = collections.deque(maxlen=avarageof)
+    def __call__(self):
+        self.frametimestamps.append(time.time())
+        if(len(self.frametimestamps) > 1):
+            return len(self.frametimestamps)/(self.frametimestamps[-1]-self.frametimestamps[0])
+        else:
+            return 0.0
+
+def get_video_stream_meta_data(video_file_path):
+    probe = ffmpeg.probe(str(video_file_path))
+    video_info = next(s for s in probe['streams'] if s['codec_type'] == 'video')
+    return video_info
+
+
+def get_fps(video_file_path, is_vfr=False):
+    """
+    get the fps of a video using ffmpeg
+    """
+    meta_data = get_video_stream_meta_data(video_file_path)
+    if is_vfr:
+        fps = int(meta_data['avg_frame_rate'].split('/')[0]) / int(meta_data['avg_frame_rate'].split('/')[1])
+    else:
+        fps = int(meta_data['r_frame_rate'].split('/')[0]) / int(meta_data['r_frame_rate'].split('/')[1])
+    return fps
+
+
+def is_video_vfr(video_file_path, get_meta_data=False):
+    """
+    checks if video is vfr using ffmpeg
+    """
+    # these three cases cover windows linux and MacOS hopefully
+    ENVBIN = Path(sys.exec_prefix, "bin", "ffmpeg")
+    if not ENVBIN.exists():
+        ENVBIN = Path("ffmpeg.exe")
+    if not ENVBIN.exists():
+        ENVBIN = Path("ffmpeg")
+    args = [str(ENVBIN)+" ",
+            "-i \"{}\"".format(str(video_file_path)),
+            "-vf vfrdet",
+            "-f null -"]
+    p = subprocess.Popen(" ".join(args), stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
+    out, err = p.communicate()
+    if p.returncode != 0:
+        print('ffmpeg', out, err)
+        exit()
+    else:
+        output = err.decode('utf-8')
+        vfr_str = re.findall("VFR:\d+\.\d+", output)[-1].split(":")[-1]
+        vfr = float(vfr_str)
+    if get_meta_data:
+        meta_data = get_video_stream_meta_data(video_file_path)
+        return vfr != 0.0, meta_data
+    else:
+        return vfr != 0.0
