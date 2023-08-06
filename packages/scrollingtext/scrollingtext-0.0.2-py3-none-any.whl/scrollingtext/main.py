9e3751884532015f2a055c4096279c08ac24e0cc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:49:22 +0000

    Add package scrollingtext/0.0.2/scrollingtext-0.0.2-py3-none-any.whl

---
+++ packages/scrollingtext/scrollingtext-0.0.2-py3-none-any.whl/scrollingtext/main.py
@@ -0,0 +1,15 @@
+import time, sys
+
+def sprint(typetext):
+    for char in typetext:
+        sys.stdout.write(char)
+        sys.stdout.flush()
+        time.sleep(0.1)
+    print("\n")
+
+def cprint(typetext, ms):
+    for char in typetext:
+        sys.stdout.write(char)
+        sys.stdout.flush()
+        time.sleep(ms / 1000)
+    print("\n")
