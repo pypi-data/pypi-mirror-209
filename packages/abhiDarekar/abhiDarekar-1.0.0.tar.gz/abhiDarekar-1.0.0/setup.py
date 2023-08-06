f24535acd7afaf3860e733df63a8ca23aebe19c4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:35:46 +0000

    Add package abhiDarekar/1.0.0/abhiDarekar-1.0.0.tar.gz

---
+++ packages/abhiDarekar/abhiDarekar-1.0.0.tar.gz/abhiDarekar-1.0.0/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='abhiDarekar',
+    version='1.0.0',
+    author='Abhishek Darekar',
+    description='This is a test python package created by abhishek',
+    license="MIT",
+    packages=find_packages(),
+    include_package_data=True,
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.9",
+    ]
+
+)
