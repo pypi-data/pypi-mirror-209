4b75a0635fac5e58d87518d6fa3d4d9d7a2c9b7a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:08:50 +0000

    Add package KosminSerializer/1.0/KosminSerializer-1.0-py3-none-any.whl

---
+++ packages/KosminSerializer/KosminSerializer-1.0-py3-none-any.whl/Serializers/__init__.py
@@ -0,0 +1,11 @@
+from Serializers.type_constants import \
+                           nonetype, moduletype, codetype, celltype, \
+                           functype, bldinfunctype, \
+                           mapproxytype, wrapdesctype, metdesctype, getsetdesctype, \
+                           CODE_PROPS, UNIQUE_TYPES
+
+from Serializers.base_serializer import BaseSerializer
+from Serializers.dict_serializer import DictSerializer
+from Serializers.json_serializer import JsonSerializer
+from Serializers.xml_serializer import XmlSerializer
+from Serializers.serializers_factory import SerializersFactory, SerializerType
