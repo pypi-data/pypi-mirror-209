7bb4ab42a04b239331fbd9beb020ead07795821c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:44:52 +0000

    Add package rabbie/0.0.9/rabbie-0.0.9-py3-none-any.whl

---
+++ packages/rabbie/rabbie-0.0.9-py3-none-any.whl/rabbie/__init__.py
@@ -0,0 +1,6 @@
+from .consumer import Consumer, consumer, MicroConsumer
+from .broker_types import Channel, Method, Properties
+from .producer import Producer
+from .decoder import Decoder, JSONDecoder
+from .encoder import Encoder, JSONEncoder
+from .events import event_handler
---
+++ packages/rabbie/rabbie-0.0.9-py3-none-any.whl/rabbie/decoder/decoder.py
@@ -0,0 +1,7 @@
+from abc import ABC, abstractmethod
+
+
+class Decoder(ABC):
+    @abstractmethod
+    def decode(self, body: bytes):
+        ...
