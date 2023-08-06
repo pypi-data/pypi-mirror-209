9bdca71d232521e4ac9c2d6092aef97ad767e77f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:21:34 +0000

    Add package torchility/0.6.15/torchility-0.6.15.tar.gz

---
+++ packages/torchility/torchility-0.6.15.tar.gz/torchility-0.6.15/torchility/callbacks/common.py
@@ -0,0 +1,41 @@
+from pytorch_lightning.callbacks.callback import Callback
+
+
+class ResetMetrics(Callback):
+    def on_train_epoch_end(self, trainer, pl_module):
+        metric_values = {}
+        if pl_module.metrics:
+            metric_values = reset_metrics('train', pl_module)
+        trainer.train_epoch_metric_values = metric_values
+
+    def on_validation_epoch_end(self, trainer, pl_module):
+        metric_values = {}
+        if pl_module.metrics:
+            metric_values = reset_metrics('val', pl_module)
+        trainer.val_epoch_metric_values = metric_values
+
+    def on_test_epoch_end(self, trainer, pl_module):
+        metric_values = {}
+        if pl_module.metrics:
+            metric_values = reset_metrics('test', pl_module)
+        trainer.test_epoch_metric_values = metric_values
+
+
+def reset_metrics(stage, task):    # 重置指标
+    metrics = task.metrics[stage]
+    metric_values = {}
+    for metric in metrics:
+        value = metric.compute()
+        if isinstance(value, dict):
+            for k, v in value.items():
+                metric_values[dict_metric_name(metric.name, k)] = v.item()
+        else:
+            metric_values[metric.name] = value.item()
+        metric.reset()
+    return metric_values
+
+def dict_metric_name(m_name, dict_key):
+    if '/' in m_name:
+        return m_name.replace('/', f'{dict_key}/')
+    else:
+        return f'{m_name}{dict_key}'
