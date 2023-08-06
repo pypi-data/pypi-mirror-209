d0e39d9c3e1194b4255ec71177291c9cf891a209
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:28:53 +0000

    Add package bardapi/0.1.10/bardapi-0.1.10-py3-none-any.whl

---
+++ packages/bardapi/bardapi-0.1.10-py3-none-any.whl/bardapi/__init__.py
@@ -0,0 +1,9 @@
+import os
+from bardapi.core import Bard
+from bardapi.tranlsator import translate
+
+bard_api_key = os.environ.get("_BARD_API_KEY")
+
+__all__ = ["Bard", "bard_api_key", "translate"]
+__version__ = "0.1.10"
+__author__ = "daniel park <parkminwoo1991@gmail.com>"
