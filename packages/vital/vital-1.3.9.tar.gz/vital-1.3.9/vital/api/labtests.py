c8fd2051016177bd4f1784cd7ff9193108d694ed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:43:46 +0000

    Add package vital/1.3.9/vital-1.3.9.tar.gz

---
+++ packages/vital/vital-1.3.9.tar.gz/vital-1.3.9/vital/api/labtests.py
@@ -0,0 +1,82 @@
+from typing import Dict, Mapping, Optional
+
+from vital.api.api import API
+
+
+class LabTests(API):
+    """V3 endpoints for managing testkit orders."""
+
+    def create_order(
+        self,
+        user_id: str,
+        patient_details: Dict,
+        patient_address: Dict,
+        lab_test_id: str,
+        physician: Optional[Dict],
+        health_insurance: Optional[Dict] = None,
+    ) -> Mapping[str, str]:
+        """Create new order"""
+        params = {
+            "user_id": user_id,
+            "patient_details": patient_details,
+            "patient_address": patient_address,
+            "lab_test_id": lab_test_id,
+            "physician": physician,
+        }
+        if health_insurance:
+            params["health_insurance"] = health_insurance
+
+        return self.client.post("/order", params, api_version="v3")
+
+    def get_orders(self, page: int = 1, size: int = 50) -> Mapping[str, str]:
+        """Get all orders"""
+        return self.client.get(
+            "/orders",
+            {"page": page, "size": size},
+            api_version="v3",
+        )
+
+    def get_order(self, order_id: str) -> Mapping[str, str]:
+        """
+        Get order status.
+        :param str user_id: The order_id.
+        """
+        return self.client.get(f"/order/{order_id}", api_version="v3")
+
+    def cancel_order(self, order_id: str) -> Mapping[str, str]:
+        """
+        Cancel order.
+        :param str user_id: The order_id.
+        """
+        return self.client.post(f"/order/{order_id}/cancel", api_version="v3")
+
+    def get_tests(self) -> Mapping[str, str]:
+        """
+        GET all lab tests the team has access to.
+        """
+        return self.client.get("/lab_tests")
+
+    def get_results(self, order_id: str) -> Mapping[str, str]:
+        """
+        GET both metadata and raw json test data.
+        """
+        return self.client.get(f"/order/{order_id}/result", api_version="v3")
+
+    def get_results_pdf(self, order_id: str) -> Mapping[str, str]:
+        """
+        GET gets the lab result for the order in PDF format.
+        """
+        return self.client.get(f"/order/{order_id}/result/pdf", api_version="v3")
+
+    def get_results_metadata(self, order_id: str) -> Mapping[str, str]:
+        """
+        GET metadata related to order results, such as
+        lab metadata, provider and sample dates.
+        """
+        return self.client.get(f"/order/{order_id}/result/metadata", api_version="v3")
+
+    def get_area_info(self, zip_code: str) -> Mapping[str, str]:
+        """
+        GET area info for a given zip code.
+        """
+        return self.client.get("/area/info", {"zip_code": zip_code}, api_version="v3")
