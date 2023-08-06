4e0bb5008b4dd8c8e767c7d81e78fce45635c2ce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:55:28 +0000

    Add package robodyno/1.6.1b1/robodyno-1.6.1b1-py3-none-any.whl

---
+++ packages/robodyno/robodyno-1.6.1b1-py3-none-any.whl/robodyno/components/config/robottime_config.py
@@ -0,0 +1,162 @@
+# -*-coding:utf-8 -*-
+#
+# The MIT License (MIT)
+#
+# Copyright (c) 2023 Robottime(Beijing) Technology Co., Ltd
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+"""Hardware parameters of robottime devices."""
+
+from robodyno.components.config.model import Model
+
+ROBOTTIME_PARAMS = {
+    'motor': {
+        Model.ROBODYNO_PRO_P44: {
+            'reduction': -44,
+            'available_velocity': 27,
+            'available_torque': 13,
+            'available_current': 15,
+            'torque_constant': 0.7742,
+            'with_brake': False,
+            'default_vel_limit': 5.71,
+            'default_current_limit': 13.0,
+            'default_pos_kp': 100.0,
+            'default_vel_kp': 0.02,
+            'default_vel_ki': 0.1,
+        },
+        Model.ROBODYNO_PRO_P12: {
+            'reduction': -12.45,
+            'available_velocity': 95,
+            'available_torque': 4,
+            'available_current': 15,
+            'torque_constant': 0.2191,
+            'with_brake': False,
+            'default_vel_limit': 20.19,
+            'default_current_limit': 13.0,
+            'default_pos_kp': 100.0,
+            'default_vel_kp': 0.02,
+            'default_vel_ki': 0.1,
+        },
+        Model.ROBODYNO_PRO_H50: {
+            'reduction': 50,
+            'available_velocity': 23.6,
+            'available_torque': 15,
+            'available_current': 15,
+            'torque_constant': 0.88,
+            'with_brake': False,
+            'default_vel_limit': 5.03,
+            'default_current_limit': 13.0,
+            'default_pos_kp': 100.0,
+            'default_vel_kp': 0.02,
+            'default_vel_ki': 0.1,
+        },
+        Model.ROBODYNO_PRO_H100: {
+            'reduction': 100,
+            'available_velocity': 11.8,
+            'available_torque': 30,
+            'available_current': 15,
+            'torque_constant': 1.76,
+            'with_brake': False,
+            'default_vel_limit': 2.51,
+            'default_current_limit': 13.0,
+            'default_pos_kp': 100.0,
+            'default_vel_kp': 0.02,
+            'default_vel_ki': 0.1,
+        },
+        Model.ROBODYNO_PRO_DIRECT: {
+            'reduction': 1,
+            'available_velocity': 1180,
+            'available_torque': 0.3,
+            'available_current': 15,
+            'torque_constant': 0.0176,
+            'with_brake': False,
+            'default_vel_limit': 251.33,
+            'default_current_limit': 13.0,
+            'default_pos_kp': 100.0,
+            'default_vel_kp': 0.02,
+            'default_vel_ki': 0.1,
+        },
+        Model.ROBODYNO_PLUS_H50: {
+            'reduction': 50,
+            'available_velocity': 24,
+            'available_torque': 48,
+            'available_current': 25,
+            'torque_constant': 1.7229,
+            'with_brake': True,
+            'default_vel_limit': 3.77,
+            'default_current_limit': 20.0,
+            'default_pos_kp': 30.0,
+            'default_vel_kp': 0.37,
+            'default_vel_ki': 1.85,
+        },
+        Model.ROBODYNO_PLUS_H100: {
+            'reduction': 100,
+            'available_velocity': 12,
+            'available_torque': 96,
+            'available_current': 25,
+            'torque_constant': 3.4458,
+            'with_brake': True,
+            'default_vel_limit': 1.88,
+            'default_current_limit': 20.0,
+            'default_pos_kp': 30.0,
+            'default_vel_kp': 0.37,
+            'default_vel_ki': 1.85,
+        },
+        Model.ROBODYNO_PLUS_P12: {
+            'reduction': -12.45,
+            'available_velocity': 96,
+            'available_torque': 12,
+            'available_current': 25,
+            'torque_constant': 0.429,
+            'with_brake': True,
+            'default_vel_limit': 15.14,
+            'default_current_limit': 20.0,
+            'default_pos_kp': 30.0,
+            'default_vel_kp': 0.37,
+            'default_vel_ki': 1.85,
+        },
+        Model.ROBODYNO_PLUS_DIRECT: {
+            'reduction': 1,
+            'available_velocity': 1200,
+            'available_torque': 0.96,
+            'available_current': 25,
+            'torque_constant': 0.034458,
+            'with_brake': True,
+            'default_vel_limit': 188.50,
+            'default_current_limit': 20.0,
+            'default_pos_kp': 30.0,
+            'default_vel_kp': 0.37,
+            'default_vel_ki': 1.85,
+        },
+        Model.ROBODYNO_NANO_P100: {
+            'reduction': 100,
+            'available_velocity': 16.23,
+            'available_torque': 0.15,
+            'available_current': 0.6,
+            'torque_constant': 0.25,
+            'with_brake': False,
+            'default_vel_limit': 10.0,
+            'default_current_limit': 0.5,
+            'default_pos_kp': 25.0,
+            'default_vel_kp': 200.0,
+            'default_vel_ki': 5.0,
+        }
+    }
+}
---
+++ packages/robodyno/robodyno-1.6.1b1-py3-none-any.whl/robodyno/version.py
@@ -0,0 +1,3 @@
+"""Version information for Robodyno package."""
+
+__version__ = '1.6.1-beta'
