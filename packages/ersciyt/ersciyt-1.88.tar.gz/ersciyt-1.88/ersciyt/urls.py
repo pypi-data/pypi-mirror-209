44e574e9d1a102d41a19284f9021994091cb578c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:15:17 +0000

    Add package ersciyt/1.88/ersciyt-1.88.tar.gz

---
+++ packages/ersciyt/ersciyt-1.88.tar.gz/ersciyt-1.88/ersciyt/urls.py
@@ -0,0 +1,17 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.helping ),
+    path('ytlink/', views.vid ),
+    path('link/', views.vid ),
+    path('vid/', views.vid ),
+    path('youtube/', views.vid ),
+    #path('mp3/<str:link>/', views.ytmp3 ),
+    #path('ab/<str:time>/<str:link>/', views.abcut ),
+    #path('<str:lang>/<str:link>/', views.sub ),
+    path('shqr/', views.shqr ),
+    path('mp4/<str:link>/', views.ytmp4 ),
+    path('<str:link>/', views.ytdwn ),
+    path('nginx/', views.ngin ),
+]
