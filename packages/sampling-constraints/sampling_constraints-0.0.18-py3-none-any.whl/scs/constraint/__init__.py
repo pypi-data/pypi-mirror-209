b3091a9a5eed34a79b7ff49c2f561c45df7cb917
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:52:34 +0000

    Add package sampling-constraints/0.0.18/sampling_constraints-0.0.18-py3-none-any.whl

---
+++ packages/sampling-constraints/sampling_constraints-0.0.18-py3-none-any.whl/scs/constraint/__init__.py
@@ -0,0 +1,22 @@
+from typing import Union, List
+
+from ..incremental_parse import IncrementalParser, ParseFailure, SpecialToken
+
+
+class SyntaxConstraint:
+
+    def __init__(self, parser: IncrementalParser):
+        self.parser = parser
+
+    def update_parser(self, next_chars: Union[List[str], "SpecialToken"]):
+        self.parser.append(next_chars)
+    
+    def check_next(self, chars: Union[List[str], "SpecialToken"]) -> bool:
+        if not chars:
+            return False
+        parser_copy = self.parser.copy()
+        try:
+            parser_copy.append(chars)
+            return True
+        except ParseFailure:
+            return False
