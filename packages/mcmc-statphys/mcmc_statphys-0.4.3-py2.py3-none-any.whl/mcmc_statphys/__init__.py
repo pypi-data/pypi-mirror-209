08b91a8704cf2d6a0487e970b08813a564327fd2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:24:45 +0000

    Add package mcmc-statphys/0.4.3/mcmc_statphys-0.4.3-py2.py3-none-any.whl

---
+++ packages/mcmc-statphys/mcmc_statphys-0.4.3-py2.py3-none-any.whl/mcmc_statphys/__init__.py
@@ -0,0 +1,8 @@
+"""Top-level package for python-mc-stat-phys."""
+
+__author__ = """Uynaj GI"""
+__email__ = 'suquan12148@outlook.com'
+__version__ = '0.4.3'
+__all__ = ['algorithm', 'model', 'analysis', 'draw']
+
+from . import algorithm, model, analysis, draw
