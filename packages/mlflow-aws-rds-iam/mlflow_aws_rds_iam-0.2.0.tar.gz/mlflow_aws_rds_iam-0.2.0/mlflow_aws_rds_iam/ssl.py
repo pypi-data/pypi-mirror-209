5eab9fdf44f3a1c9e204d61cb0701b708722f2fb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:16:12 +0000

    Add package mlflow-aws-rds-iam/0.2.0/mlflow_aws_rds_iam-0.2.0.tar.gz

---
+++ packages/mlflow-aws-rds-iam/mlflow_aws_rds_iam-0.2.0.tar.gz/mlflow_aws_rds_iam-0.2.0/mlflow_aws_rds_iam/ssl.py
@@ -0,0 +1,23 @@
+from importlib.resources import as_file, files
+
+from sqlalchemy.engine import URL
+
+from .utils import DatabaseType, get_db_type
+
+IS_SSL_DISABLED_ENV_VAR = "MLFLOW_DISABLE_DB_SSL"
+CERT_PATH = "certs/global-bundle.pem"
+
+
+def set_ssl_params(uri: URL) -> URL:
+    with as_file(files(__package__).joinpath(CERT_PATH)) as certfile:
+        match get_db_type(uri):
+            case DatabaseType.POSTGRESQL:
+                ssl_params = {"sslmode": "verify-ca", "sslrootcert": str(certfile)}
+            case DatabaseType.MYSQL:
+                ssl_params = {"ssl-mode": "VERIFY_CA", "ssl-ca": str(certfile)}
+            case _:
+                raise ValueError(
+                    "Only SSL parameters for Postresql or MySQL URIs are supported."
+                )
+
+    return uri.set(query=uri.query | ssl_params)
