6b8269a2d2329d62d3247eeca3e87468e57b75c2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:32:23 +0000

    Add package ElectronCounting/1.0.0/ElectronCounting-1.0.0.tar.gz

---
+++ packages/ElectronCounting/ElectronCounting-1.0.0.tar.gz/ElectronCounting-1.0.0/README.md
@@ -0,0 +1,9 @@
+# CountingNN
+[![Binder](https://mybinder.org/badge_logo.svg)](https://mybinder.org/v2/gh/wdwzyyg/ElectronCounting/master)
+## Decription
+The packge adopts Faster R-CNN to identify single electrons in direct electron detector read out images. 
+
+## How to use
+`pip install ElectronCounting`
+
+See the example notebook for a quick start. 
---
+++ packages/ElectronCounting/ElectronCounting-1.0.0.tar.gz/ElectronCounting-1.0.0/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+# from distutils.core import setup
+
+setup(
+    name='ElectronCounting',
+    version='1.0.0',
+    packages=find_packages(),
+    url='https://github.com/wdwzyyg/ElectronCounting.git',
+    license='MIT',
+    author='Jingrui Wei',
+    author_email='jwei74@wisc.edu',
+    description='Count single electron in direct electron detector data',
+    keywords=[
+        "direct electron detector",
+        "electron microscopy",
+        "data analysis",
+        "object detection",
+        "Faster R-CNN",
+    ],
+    install_requires=[
+        "torch==1.12.0",
+        "torchvision==0.13.0",
+        "numpy>=1.20.1",
+        # "scikit-learn>=1.0.2",
+        # "matplotlib>=3.2.2",
+        # "kornia>=0.6.9",
+    ],
+
+)
