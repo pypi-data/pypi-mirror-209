7706254dbfb504b63d169b991bc833d982e7e696
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:15:41 +0000

    Add package plugget/0.0.6/plugget-0.0.6-py3-none-any.whl

---
+++ packages/plugget/plugget-0.0.6-py3-none-any.whl/plugget/actions/blender_pip.py
@@ -0,0 +1,54 @@
+import logging
+import subprocess
+from pathlib import Path
+import bpy
+import importlib
+
+
+def get_requirements(package: "plugget.data.Package", **kwargs) -> list[Path]:
+    # if requirements.txt exists in self.repo_paths, install requirements
+    requirements_paths = []
+    if (package.clone_dir / "requirements.txt").exists():
+        requirements_paths.append(package.clone_dir / "requirements.txt")
+    if package.repo_paths:
+        for p in package.repo_paths:
+            if p.endswith("requirements.txt"):
+                requirements_paths.append(package.clone_dir / p)
+    return requirements_paths
+
+
+# todo share pip functions
+def install(package: "plugget.data.Package", **kwargs):
+    blender_user_site_packages = Path(str(bpy.utils.script_path_user())) / "modules"  # appdata
+
+    for p in get_requirements(package):
+        if p.exists():
+            print("requirements.txt found, installing requirements")
+            # todo blender pip
+            subprocess.run(["pip", "install", "-r", package.clone_dir / p, '-t', blender_user_site_packages])
+        else:
+            logging.warning(f"expected requirements.txt not found: '{p}'")
+    importlib.invalidate_caches()
+
+
+def uninstall(package: "plugget.data.Package", dependencies=False, **kwargs):
+    # this method runs on uninstall, then the manifest is removed from installed packages
+    # ideally uninstall removes files from a folder,
+
+    if not dependencies:
+        return
+
+    blender_user_site_packages = Path(str(bpy.utils.script_path_user())) / "modules"  # appdata
+
+    for p in get_requirements(package):
+        if p.exists():
+            print("requirements.txt found, uninstalling requirements")
+            print("package.clone_dir / p", package.clone_dir / p)
+            subprocess.run(["pip", "uninstall", "-r", package.clone_dir / p, "-y"])
+        else:
+            logging.warning(f"expected requirements.txt not found: '{p}'")
+
+    importlib.invalidate_caches()
+
+
+
