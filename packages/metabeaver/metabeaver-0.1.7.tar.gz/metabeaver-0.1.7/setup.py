7a9a863b73147c521b1bb1bee5e0f0cff2c51797
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:22:32 +0000

    Add package metabeaver/0.1.7/metabeaver-0.1.7.tar.gz

---
+++ packages/metabeaver/metabeaver-0.1.7.tar.gz/metabeaver-0.1.7/setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='metabeaver',
+    version='0.1.7', # Major, minor, patch
+    packages=find_packages(exclude=['Testing', '*.xlsx', '*.xls']),
+    install_requires=[
+        'numpy',
+        'pandas',
+        'google-cloud-core',
+        'google-cloud-bigquery',
+    ],
+    description='Beaverish about data.',
+    author='Luke Anthony Pollen',
+    author_email='luke@pollenanalytics.com',
+    url='https://github.com/rainbowpusheenthe3rd/dataBeaver',
+)
+
+
+##### ERRATA #####
+
+#QHFG25#DQLPMeEy
+
+#python setup.py sdist bdist_wheel
+
+
+#pip show twine
+#dir /s twine.exe
+
+
+#twine upload dist/*
+#twine upload *
+
+##### ERRATA #####
