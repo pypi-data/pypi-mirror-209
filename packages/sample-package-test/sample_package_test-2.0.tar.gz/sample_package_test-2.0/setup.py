628a97895054a62e457d65946202f1d32ad80f1d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:14:47 +0000

    Add package sample-package-test/2.0/sample_package_test-2.0.tar.gz

---
+++ packages/sample-package-test/sample_package_test-2.0.tar.gz/sample_package_test-2.0/setup.py
@@ -0,0 +1,17 @@
+import setuptools
+
+setuptools.setup(
+     name='sample_package_test',  
+     version='2.0',
+     license='MIT',
+     author="harshith",
+     author_email='harshith@gmail.com',
+     description="A new package",
+     url='https://github.com/harshith1999/sample_pckg_publish',
+     keywords='sample_package_test',
+     classifiers=[
+         "Programming Language :: Python :: 3",
+         "License :: OSI Approved :: MIT License",
+         "Operating System :: OS Independent",
+     ],
+ )
