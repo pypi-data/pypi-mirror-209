b400110942476b06531a3a9d70916eeb0aab2396
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:49:51 +0000

    Add package arithmetic-dice-roller/0.3.1/arithmetic-dice-roller-0.3.1.tar.gz

---
+++ packages/arithmetic-dice-roller/arithmetic-dice-roller-0.3.1.tar.gz/arithmetic-dice-roller-0.3.1/setup.py
@@ -0,0 +1,50 @@
+"""
+arithmetic-dice-roller
+Copyright (C) 2021  Massimo Pavoni
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+"""
+
+from pathlib import Path
+
+from setuptools import setup, find_packages
+
+README = (Path(__file__).parent / "README.md").read_text()
+
+setup(
+    name='arithmetic-dice-roller',
+    version='0.3.1',
+    description="A handy dice roller with extended notation and arithmetic expressions management.",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    author='Massimo Pavoni',
+    author_email='maspavoni@gmail.com',
+    url='https://github.com/massimopavoni/arithmetic-dice-roller',
+    license_files='LICENSE',
+    classifiers=[
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Programming Language :: Python",
+        "Operating System :: OS Independent"
+    ],
+    platforms=['OS Independent'],
+    packages=find_packages(),
+    include_package_data=True,
+    python_requires='>=3.11',
+    install_requires=['sympy>=1.12'],
+    entry_points={
+        'console_scripts': [
+            'arithmetic-dice-roller=arithmetic_dice_roller.__main__:main'
+        ]
+    }
+)
