b73309d87ee6519e079202bc2ec5aee53ee9b8c3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:44:24 +0000

    Add package vos-mjjo/0.0.10/vos_mjjo-0.0.10-py3-none-any.whl

---
+++ packages/vos-mjjo/vos_mjjo-0.0.10-py3-none-any.whl/mjjo/__init__.py
@@ -0,0 +1,16 @@
+version = '0.0.10' 
+author = 'mjjo'
+description = 'My Library'
+license = 'MIT'
+
+def __version__():
+    return version
+
+def __author__():
+    return author
+
+def __description__():
+    return description
+
+def __license__():
+    return license
