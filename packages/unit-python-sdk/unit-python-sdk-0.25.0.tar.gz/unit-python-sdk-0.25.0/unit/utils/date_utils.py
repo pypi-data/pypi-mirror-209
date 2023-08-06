5a4747b305a673e8037419b28c8b132e89a0b5a1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:41:29 +0000

    Add package unit-python-sdk/0.25.0/unit-python-sdk-0.25.0.tar.gz

---
+++ packages/unit-python-sdk/unit-python-sdk-0.25.0.tar.gz/unit-python-sdk-0.25.0/unit/utils/date_utils.py
@@ -0,0 +1,21 @@
+from datetime import date, datetime
+
+
+def to_datetime(dt: str):
+    if dt is None:
+        return None
+
+    return datetime.strptime(dt, "%Y-%m-%dT%H:%M:%S.%f%z")
+
+def from_datetime(dt: datetime):
+    return dt.isoformat(timespec='milliseconds') + "Z"
+
+def to_date(d: str):
+    if d is None:
+        return None
+
+    return date.fromisoformat(d)
+
+
+def to_date_str(d: date):
+    return d.strftime("%Y-%m-%d")
