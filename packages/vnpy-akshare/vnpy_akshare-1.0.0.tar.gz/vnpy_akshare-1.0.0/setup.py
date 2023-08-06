5b7aff7f2457572995caaa0e2471ed0b52e37125
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:48:32 +0000

    Add package vnpy-akshare/1.0.0/vnpy_akshare-1.0.0.tar.gz

---
+++ packages/vnpy-akshare/vnpy_akshare-1.0.0.tar.gz/vnpy_akshare-1.0.0/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+setup(
+    name="vnpy_akshare",
+    version="1.0.0",
+    keywords=("vnpy", "vnpy_akshare"),
+    description="vnpy_akshare数据服务",
+    long_description="vnpy_akshare数据服务",
+    license="MIT Licence",
+    url="https://github.com/bthuntergg/vnpy_akshare",
+    author="jan",
+    author_email="bthuntergg@qq.com",
+
+    packages=find_packages(),
+    include_package_data=True,
+    platforms="any",
+    install_requires=[]
+)
