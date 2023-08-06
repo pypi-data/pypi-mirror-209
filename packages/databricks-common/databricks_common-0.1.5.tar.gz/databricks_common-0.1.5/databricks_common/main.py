a0b43f5c69356ea64fac022f0833810e7ea32656
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:44:55 +0000

    Add package databricks-common/0.1.5/databricks_common-0.1.5.tar.gz

---
+++ packages/databricks-common/databricks_common-0.1.5.tar.gz/databricks_common-0.1.5/databricks_common/main.py
@@ -0,0 +1,7 @@
+from databricks.connect import DatabricksSession
+from databricks.sdk.core import Config
+
+
+config = Config(profile="aws-e2-demo")
+# config = Config(profile="azure-field-eng-east")
+spark = DatabricksSession.builder.sdkConfig(config).getOrCreate()
