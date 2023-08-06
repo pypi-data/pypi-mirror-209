7cf162a6642a3fd40e052b76ff257acace5f51fc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:12:24 +0000

    Add package LARRY-dataset/0.0.2rc0/LARRY_dataset-0.0.2rc0-py3-none-any.whl

---
+++ packages/LARRY-dataset/LARRY_dataset-0.0.2rc0-py3-none-any.whl/larry/__init__.py
@@ -0,0 +1,9 @@
+# lt funcs init
+
+__version__ = __Version__ = "0.0.2rc0"
+
+from . import _tools as tl
+from . import _utils as ut
+from . import _datasets as datasets
+from . import _analysis as an
+from . import _callbacks as callbacks
