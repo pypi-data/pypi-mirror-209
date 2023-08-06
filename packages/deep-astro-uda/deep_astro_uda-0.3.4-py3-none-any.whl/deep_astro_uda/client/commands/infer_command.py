67de11b1684f34d88ecdd5f8466974a02c529d9b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:43:55 +0000

    Add package deep-astro-uda/0.3.4/deep_astro_uda-0.3.4-py3-none-any.whl

---
+++ packages/deep-astro-uda/deep_astro_uda-0.3.4-py3-none-any.whl/deep_astro_uda/client/commands/infer_command.py
@@ -0,0 +1,12 @@
+from cleo.commands.command import Command
+
+class InferCommand(Command):
+
+    """
+    Run inference pipeline for the client.
+    """
+
+    
+    def handle(self):
+        self.line('Running the client')
+        self.call('client:infer')
