567fadb82fd721a76059f5ab884767b2aeae0e4b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:52:27 +0000

    Add package kswutils/0.2.2/kswutils-0.2.2.tar.gz

---
+++ packages/kswutils/kswutils-0.2.2.tar.gz/kswutils-0.2.2/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='kswutils',
+    version='0.2.2',
+    description='Justin utils at work',
+    packages=['kswutils'],
+    author_email='shuwa108@gmail.com',
+    zip_safe=False
+)
