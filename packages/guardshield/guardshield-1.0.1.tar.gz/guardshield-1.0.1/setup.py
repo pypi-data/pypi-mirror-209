f90bcca0b15e9035429088f386602352a236d49b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:10:25 +0000

    Add package guardshield/1.0.1/guardshield-1.0.1.tar.gz

---
+++ packages/guardshield/guardshield-1.0.1.tar.gz/guardshield-1.0.1/setup.py
@@ -0,0 +1,19 @@
+import setuptools
+
+setuptools.setup(
+    name="guardshield",
+    version="1.0.1",
+    author="Oxyn",
+    author_email="oxyn.dev@gmail.com",
+    description="Security lib",
+    packages=setuptools.find_packages(),
+    include_package_data=True,
+    url='https://github.com/OxynDev/ExcerSec',
+    zip_safe=False,
+    license='MIT',
+    
+    package_data={
+        'guardshield': ['lib.dll'], 
+    },
+    data_files=[('guardshield', ['guardshield/lib.dll'])],
+)
