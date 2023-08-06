b55af8f3b26ee0c2b0f59bbc7553afe784d0a107
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:48:57 +0000

    Add package lr-sgd/1.0.0/lr_sgd-1.0.0.tar.gz

---
+++ packages/lr-sgd/lr_sgd-1.0.0.tar.gz/lr_sgd-1.0.0/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='lr_sgd',
+    version='1.0.0',
+    packages=find_packages(),
+    install_requires=[
+    #dependencies 
+    ],
+    description='a simple linear regression model with sgd'
+
+)
