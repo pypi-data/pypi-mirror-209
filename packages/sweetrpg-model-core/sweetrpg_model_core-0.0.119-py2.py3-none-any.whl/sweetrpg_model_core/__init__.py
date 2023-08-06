e28acba3ba9092ebcd83140c84b0b9e4a4a39072
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:14:25 +0000

    Add package sweetrpg-model-core/0.0.119/sweetrpg_model_core-0.0.119-py2.py3-none-any.whl

---
+++ packages/sweetrpg-model-core/sweetrpg_model_core-0.0.119-py2.py3-none-any.whl/sweetrpg_model_core/__init__.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+__author__ = "Paul Schifferer <dm@sweetrpg.com>"
+"""Root.
+"""
+
+__title__ = "SweetRPG Model Core"
+__description__ = "Base classes and utilities for model objects"
+__url__ = "https://github.com/sweetrpg/model-core"
+__version__ = "0.0.119"
+__build__ = 0x00005e
+__author_email__ = "dm@sweetrpg.com"
+__license__ = "MIT"
+__copyright__ = "Copyright 2023 SweetRPG"
+__cake__ = u"\u2728 \U0001f370 \u2728"
