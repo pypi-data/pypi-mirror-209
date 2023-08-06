f76ee99a5df52cc6048ce1cc1c26f66e3823160b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:16:14 +0000

    Add package auto-screener/0.0.21/auto-screener-0.0.21.tar.gz

---
+++ packages/auto-screener/auto-screener-0.0.21.tar.gz/auto-screener-0.0.21/auto_screener/document.py
@@ -0,0 +1,22 @@
+# document.py
+
+# noinspection PyUnresolvedReferences
+from auto_screener.interval import *
+# noinspection PyUnresolvedReferences
+from auto_screener.screening import *
+# noinspection PyUnresolvedReferences
+from auto_screener.tickers import *
+# noinspection PyUnresolvedReferences
+from auto_screener.progress import *
+# noinspection PyUnresolvedReferences
+from auto_screener.dataset import *
+# noinspection PyUnresolvedReferences
+from auto_screener.collect import *
+
+from auto_screener.base import document
+
+class Documentation:
+    """"""
+# end Documentation
+
+document(Documentation)
