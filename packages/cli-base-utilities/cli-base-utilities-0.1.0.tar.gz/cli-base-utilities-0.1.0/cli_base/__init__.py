f1ca70d71b4f8d304c224d42351a15927a08ec86
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:12:49 +0000

    Add package cli-base-utilities/0.1.0/cli-base-utilities-0.1.0.tar.gz

---
+++ packages/cli-base-utilities/cli-base-utilities-0.1.0.tar.gz/cli-base-utilities-0.1.0/cli_base/__init__.py
@@ -0,0 +1,7 @@
+"""
+    cli_base
+    Helpers to bild a CLI program
+"""
+
+__version__ = '0.1.0'
+__author__ = 'Jens Diemer <github@jensdiemer.de>'
---
+++ packages/cli-base-utilities/cli-base-utilities-0.1.0.tar.gz/cli-base-utilities-0.1.0/cli_base/constants.py
@@ -0,0 +1,8 @@
+from pathlib import Path
+
+import cli_base
+
+
+CLI_EPILOG = 'Project Homepage: https://github.com/jedie/cli-base'
+
+BASE_PATH = Path(cli_base.__file__).parent
