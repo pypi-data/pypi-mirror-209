d2245569451fef584ff39aa1f4de5d24ef10a609
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:54:50 +0000

    Add package selenextra/1.0.6/selenextra-1.0.6.tar.gz

---
+++ packages/selenextra/selenextra-1.0.6.tar.gz/selenextra-1.0.6/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='selenextra',
+    version='1.0.6',
+    description='Bringing additional features to Selenium',
+    author='Tat Nguyen Van',
+    author_email='nguyenvantat1182002@gmail.com',
+    url='https://github.com/nguyenvantat1182002/SeleneXtra',
+    packages=find_packages(),
+    install_requires=[
+        'selenium-wire'
+    ],
+    dependency_links=[
+        'git+https://github.com/nguyenvantat1182002/undetected-chromedriver@master#egg=undetected-chromedriver'
+    ],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+    ],
+)
