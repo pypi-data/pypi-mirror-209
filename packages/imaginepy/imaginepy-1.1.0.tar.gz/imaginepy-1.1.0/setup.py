935bdf904fec1e3196859ca34b81ea185cf20ade
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:28:19 +0000

    Add package imaginepy/1.1.0/imaginepy-1.1.0.tar.gz

---
+++ packages/imaginepy/imaginepy-1.1.0.tar.gz/imaginepy-1.1.0/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name='imaginepy',
+    version='1.1.0',
+    author='CEED',
+    author_email='',
+    description='Python library to create Art with AI.',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='https://github.com/ItsCEED/imaginepy',
+    packages=find_packages(),
+    classifiers=[
+        "Environment :: Console",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Topic :: Utilities"
+    ],
+    python_requires='>=3.7',
+    install_requires=[
+        'requests',
+        'requests_toolbelt',
+        'langdetect',
+    ],
+)
