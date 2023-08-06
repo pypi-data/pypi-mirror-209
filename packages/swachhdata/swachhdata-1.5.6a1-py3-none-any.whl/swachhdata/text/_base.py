5626f5c1f7f8484ee8845a197052c76745c862d2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:31:55 +0000

    Add package swachhdata/1.5.6a1/swachhdata-1.5.6a1-py3-none-any.whl

---
+++ packages/swachhdata/swachhdata-1.5.6a1-py3-none-any.whl/swachhdata/text/_base.py
@@ -0,0 +1,82 @@
+import pandas
+from ..compose.core import BaseTextDatum
+from ..utils.exceptions import (
+    IncorrectProcessDataType,
+    IncorrectVerboseDataType,
+    SetupNotImplementedError
+)
+
+class TextFormatter:
+    """
+    Base Data Formatter for all recasts in swachhdata.text module
+
+    Accepted text input format:
+        * string
+        * list of string
+        * pandas.core.series.Series
+    """
+
+    def __init__(self):
+        
+        self._dtype = None
+        self._text = None
+        self._count = None
+    
+    def __text_formatter(self):
+        
+        self._count = len(self._text)
+
+        if self._dtype == pandas.core.series.Series:
+            self._text = self._text.tolist()
+            self._dtype = type(self._text)
+
+        if self._dtype == str:
+            self._count = len(self._text.split())
+            self._dtype = type(self._text)
+
+class BaseTextRecast(BaseTextDatum):
+
+    def __init__(self, process=None, verbose=0):
+        """
+        Initialize
+        """
+        if process is not None and not isinstance(process, str):
+            raise IncorrectProcessDataType(process)
+        
+        if not isinstance(verbose, int):
+            raise IncorrectVerboseDataType(verbose)
+        
+        self.__setup_check = False
+        self.__process = process
+        self.__verbose_status = True
+        self.__verbose = verbose
+        if self.__verbose == -1:
+            self.__verbose_status = False
+    
+    def setup(self, text):
+        """
+        Setup
+        """
+        if hasattr(text, 'id_text_datum'):
+            self.__dict__.update(text.__dict__)
+        else:
+            self.data = text
+        
+        self.__setup_check = True
+    
+    def recast(self):
+        """
+        Recast
+        """
+        if not self.__setup_check:
+            raise SetupNotImplementedError(self.__setup_check)
+
+    def setup_recast(self, text=None):
+        """
+        Setup & Recast
+        """
+        if not self.__setup_check:
+            self.setup(text)
+            return self.recast()
+        else:
+            return self.recast()
