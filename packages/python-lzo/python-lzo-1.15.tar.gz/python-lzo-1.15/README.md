f37c793f0e44eda8f637f7afaa7e742e184ca3f4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:55:38 +0000

    Add package python-lzo/1.15/python-lzo-1.15.tar.gz

---
+++ packages/python-lzo/python-lzo-1.15.tar.gz/python-lzo-1.15/README.md
@@ -0,0 +1,120 @@
+![example workflow](https://github.com/jd-boyd/python-lzo/actions/workflows/tests.yaml/badge.svg)
+
+[![Appveyor Build Status](https://ci.appveyor.com/api/projects/status/github/jd-boyd/python-lzo?svg=true)](https://ci.appveyor.com/project/jd-boyd/python-lzo/branch/master)
+
+```
+                 Python-LZO -- Python bindings for LZO
+
+              Copyright (c) 1996-2002 Markus F.X.J. Oberhumer
+                          <markus@oberhumer.com>
+                 http://www.oberhumer.com/opensource/lzo/
+              Copyright (c) 2011-2016 Joshua D. Boyd
+                          <jdboyd@jdboyd.net>
+                 https://github.com/jd-boyd/python-lzo
+```
+
+# What is LZO ?
+
+
+LZO is a portable lossless data compression library written in ANSI C.
+It offers pretty fast compression and *very* fast decompression.
+Decompression requires no memory.
+
+In addition there are slower compression levels achieving a quite
+competitive compression ratio while still decompressing at
+this very high speed.
+
+
+# What is Python-LZO ?
+
+Python-LZO provides Python bindings for LZO, i.e. you can access
+the LZO library from your Python scripts thereby compressing ordinary
+Python strings.
+
+
+# Installation
+
+## Ubuntu/Debian Linux
+
+You need the following dependencies installed:
+* `zlib1g-dev`
+* `liblzo2-dev`
+* `python-pip` or `python3-pip`
+
+Then, just `pip install python-lzo`.
+
+## CentOS/Red Hat Enterprise Linux
+
+You need the following dependencies installed:
+
+* `yum install epel-release`
+* `yum install python-pip`
+* `yum install lzo-devel`
+* `yum install lzo-minilzo`
+
+Then, just `pip install python-lzo`.
+
+## macOS
+
+You need XCode installed, or something else that provides suitable C
+compiler.  Then just:
+
+`pip install python-lzo`
+
+## Windows
+
+There is an issue filed to provide a pre-compiled wheel to greatly
+simplify this.
+
+Until then, here we go:
+* Download http://www.oberhumer.com/opensource/lzo/download/lzo-2.09.tar.gz
+* extract that file to `c:\src\` (should produce `c:\src\lzo-2.09` )
+* Install a visual studio compatible with your python build.  If you are using
+  python 2.7, MS has a package just for you at: https://www.microsoft.com/en-us/download/details.aspx?id=44266
+  Current automated tests are done with Visual Studio 2013, but newer should work as well.
+* `python.exe setup.py install`
+
+# Where's the documentation ?
+
+Python-LZO comes with built-in documentation which is accessible
+using `lzo.__doc__` and `lzo.func__doc__`. See ["Chapter 3: Data Model"
+in the Python Reference Manual](https://docs.python.org/3.6/reference/datamodel.html) for more information.
+
+Additionally you should read the docs and study the example
+programs that ship with the LZO library.
+
+# Python 2 support statement
+
+As of 1.15, Python 2.x is unsupported. To continue using Python 2.6 or
+Python 2.7, please stick with version 1.14.
+
+# Notes
+
+The Windows version is tested.
+
+It is not currently continuously tested on OSX, but that is coming.
+
+# Releasing
+
+1. Update version in `setup.py` and the `MODULE_VERSION` define in
+   `lzomodule.c`.
+1. Update NEWS.
+1. Tag with new release.
+1. wheels
+1. PyPi
+
+# Copyright
+
+The LZO and Python-LZO algorithms and implementations are
+Copyright (C) 1996, 1997, 1998, 1999, 2000, 2001, 2002
+Markus Franz Xaver Johannes Oberhumer <markus@oberhumer.com>
+
+The Python-LZO algorithms implementated post 2011 are
+Copyright (C) 2011, 2014, 2015, 2016, 2017, 2018, 2019, 2020, 2021,
+2022, & 2023
+Joshua D. Boyd <jdboyd@jdboyd.net> and others as denoted in the git
+history.
+
+
+The LZO and Python-LZO algorithms and implementations are distributed under
+the terms of the GNU General Public License (GPL).  See the file COPYING.
---
+++ packages/python-lzo/python-lzo-1.15.tar.gz/python-lzo-1.15/tests/test_lzo.py
@@ -0,0 +1,151 @@
+#! /usr/bin/env python
+##
+## vi:ts=4:et
+##
+##---------------------------------------------------------------------------##
+##
+## This file is part of the LZO real-time data compression library.
+##
+## Copyright (C) 1998-2002 Markus Franz Xaver Johannes Oberhumer
+## All Rights Reserved.
+##
+## The LZO library is free software; you can redistribute it and/or
+## modify it under the terms of the GNU General Public License as
+## published by the Free Software Foundation; either version 2 of
+## the License, or (at your option) any later version.
+##
+## The LZO library is distributed in the hope that it will be useful,
+## but WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+## GNU General Public License for more details.
+##
+## You should have received a copy of the GNU General Public License
+## along with the LZO library; see the file COPYING.
+## If not, write to the Free Software Foundation, Inc.,
+## 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+##
+## Markus F.X.J. Oberhumer
+## <markus@oberhumer.com>
+## http://www.oberhumer.com/opensource/lzo/
+##
+##---------------------------------------------------------------------------##
+
+from __future__ import print_function
+import inspect
+import pytest
+import sys, string
+
+# update sys.path when running in the build directory
+from tests.util import get_sys_path
+sys.path = get_sys_path()
+
+import lzo
+
+
+# ***********************************************************************
+#  a very simple test driver...
+# ***********************************************************************
+
+def print_modinfo():
+    #print sys.modules
+    mod = sys.modules['lzo']
+    #print mod
+    d = mod.__dict__
+    for k in d.keys():
+        print(k, d[k])
+
+
+def gen(src, level=1):
+    a0 = lzo.adler32(src)
+    c  = lzo.compress(src, level)
+    u1 = lzo.decompress(c)
+    a1 = lzo.adler32(u1)
+    o  = lzo.optimize(c)
+    u2 = lzo.decompress(o)
+    a2 = lzo.adler32(u2)
+    if src != u1:
+        raise lzo.error("internal error 1: %r %r", src, u1)
+    if src != u2:
+        raise lzo.error("internal error 1: %r %r",  src, u2)
+    if a0 != a1 or a0 != a2:
+        raise lzo.error("internal error 2")
+    print("compressed %6d -> %6d" % (len(src), len(c)))
+
+def gen_all(src):
+    all_algos = {"LZO1", "LZO1A", "LZO1B", "LZO1C", "LZO1F", "LZO1X", "LZO1Y", "LZO1Z", "LZO2A"}
+    if not src:
+        # LZO1 and LZO1A header exception for empty input
+        all_algos.remove("LZO1")
+        all_algos.remove("LZO1A")
+    for algo in all_algos:
+        a0 = lzo.adler32(src)
+        c  = lzo.compress(src, algorithm=algo)
+        u1 = lzo.decompress(c, algorithm=algo)
+        a1 = lzo.adler32(u1)
+        if src != u1:
+            raise lzo.error("internal error 1: %r %r", src, u1)
+        if a0 != a1:
+            raise lzo.error("internal error 2")
+        print(f"compressed using {algo} {len(src): 6} -> {len(c): 6}")
+
+
+def gen_raw(src, level=1):
+    a0 = lzo.adler32(src)
+    c  = lzo.compress(src, level, False)
+    u1 = lzo.decompress(c, False, len(src))
+    a1 = lzo.adler32(u1)
+    o  = lzo.optimize(c, False, len(src))
+    u2 = lzo.decompress(o, False, len(src))
+    a2 = lzo.adler32(u2)
+    # make sure it still works when you overstate the output buffer length
+    u3 = lzo.decompress(c, False, len(src) + 100)
+    if src != u1 or src != u2 or src != u3:
+        raise lzo.error("internal error 1")
+    if a0 != a1 or a0 != a2:
+        raise lzo.error("internal error 2")
+    print("compressed %6d -> %6d" % (len(src), len(c)))
+
+def test_version():
+    import pkg_resources
+    pkg_version = pkg_resources.require("python-lzo")[0].version
+    mod_version = lzo.__version__.decode('utf-8')
+    assert pkg_version == mod_version, \
+        "%r != %r" %(pkg_version, mod_version)
+
+@pytest.mark.parametrize("src, level", [(b"aaaaaaaaaaaaaaaaaaaaaaaa", 1), (b"abcabcabcabcabcabcabcabc", 1), (b"abcabcabcabcabcabcabcabc", 9)])
+def test_lzo(src, level):
+    gen(src, level)
+
+@pytest.mark.parametrize("src", [b"aaaaaaaaaaaaaaaaaaaaaaaa", b"abcabcabcabcabcabcabcabc"])
+def test_lzo_all(src):
+    gen_all(src)
+
+@pytest.mark.parametrize("src, level", [(b"aaaaaaaaaaaaaaaaaaaaaaaa", 1), (b"abcabcabcabcabcabcabcabc", 1), (b"abcabcabcabcabcabcabcabc", 9)])
+def test_lzo_raw(src, level):
+    gen_raw(src, level)
+
+
+def test_lzo_empty():
+    gen(b"")
+
+def test_lzo_empty_all():
+    gen_all(b"")
+
+def test_lzo_empty_raw():
+    gen_raw(b"")
+
+def test_lzo_big():
+    gen(b" " * 131072)
+
+def test_lzo_big_all():
+    gen_all(b" " * 131072)
+
+def test_lzo_big_raw():
+    gen_raw(b" " * 131072)
+
+
+if sys.maxsize > 1<<32:
+    # This test raises OverflowError on 32-bit Pythons. Compressing
+    # this much data requires a 64-bit system.
+    def test_lzo_compress_extremely_big():
+        b = lzo.compress(bytes(bytearray((1024**3)*2)))
