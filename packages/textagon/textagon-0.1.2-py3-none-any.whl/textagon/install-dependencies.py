d862a6bb2ea72a8b41a994e75e20503936b272c5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:10:25 +0000

    Add package textagon/0.1.2/textagon-0.1.2-py3-none-any.whl

---
+++ packages/textagon/textagon-0.1.2-py3-none-any.whl/textagon/install-dependencies.py
@@ -0,0 +1,7 @@
+import nltk
+nltk.download('stopwords')
+nltk.download('averaged_perceptron_tagger')
+nltk.download('wordnet')
+nltk.download('sentiwordnet')
+nltk.download('vader_lexicon')
+nltk.download('punkt')
