156aa1318048917ace2a1aa4f6d944bcd4b51e9e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:48:26 +0000

    Add package denmarf/0.3.1/denmarf-0.3.1.tar.gz

---
+++ packages/denmarf/denmarf-0.3.1.tar.gz/denmarf-0.3.1/setup.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='denmarf',
+      version='0.3.1',
+      description='Density EstimatioN using Masked AutoRegressive Flow',
+      author='Rico Ka Lok Lo',
+      author_email='rico.kaloklo@gmail.com',
+      packages=['denmarf'],
+      install_requires=[
+            'numpy',
+            'scipy',
+            'tqdm',
+            'torch',
+      ],
+)
