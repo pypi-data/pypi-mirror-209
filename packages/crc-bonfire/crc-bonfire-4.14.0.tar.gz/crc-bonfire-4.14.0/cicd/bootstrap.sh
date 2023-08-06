7db2ab1fc23ff2787cf1eca1789865c99dd24b4f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:23:35 +0000

    Add package crc-bonfire/4.14.0/crc-bonfire-4.14.0.tar.gz

---
+++ packages/crc-bonfire/crc-bonfire-4.14.0.tar.gz/crc-bonfire-4.14.0/cicd/bootstrap.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+set -e
+
+# which branch to fetch cicd scripts from in cicd-tools repo
+export CICD_REPO_BRANCH="${CICD_REPO_BRANCH:-main}"
+
+BOOTSTRAP_URL="https://raw.githubusercontent.com/RedHatInsights/cicd-tools/${CICD_REPO_BRANCH}/bootstrap.sh"
+BOOTSTRAP_FILE=".cicd_tools_bootstrap.sh"
+
+echo "Fetching $BOOTSTRAP_URL"
+rm -f $BOOTSTRAP_FILE
+RESPONSE_CODE=$(curl --silent -w "%{http_code}" -o $BOOTSTRAP_FILE $BOOTSTRAP_URL)
+echo "HTTP response: $RESPONSE_CODE"
+source $BOOTSTRAP_FILE
