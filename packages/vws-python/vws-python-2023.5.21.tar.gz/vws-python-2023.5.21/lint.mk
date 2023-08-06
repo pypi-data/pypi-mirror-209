29d72160ba86ac8d30d61819e696c593b6800b1e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:28:06 +0000

    Add package vws-python/2023.5.21/vws-python-2023.5.21.tar.gz

---
+++ packages/vws-python/vws-python-2023.5.21.tar.gz/vws-python-2023.5.21/lint.mk
@@ -0,0 +1,63 @@
+# Make commands for linting
+
+SHELL := /bin/bash -euxo pipefail
+
+.PHONY: black
+black:
+	black --check .
+
+.PHONY: fix-black
+fix-black:
+	black .
+
+.PHONY: mypy
+mypy:
+	mypy .
+
+.PHONY: check-manifest
+check-manifest:
+	check-manifest .
+
+.PHONY: doc8
+doc8:
+	doc8 .
+
+.PHONY: ruff
+ruff:
+	ruff .
+
+.PHONY: fix-ruff
+fix-ruff:
+	ruff --fix .
+
+.PHONY: pip-extra-reqs
+pip-extra-reqs:
+	pip-extra-reqs --requirements-file=<(pdm export --pyproject) src/
+
+.PHONY: pip-missing-reqs
+pip-missing-reqs:
+	pip-missing-reqs --requirements-file=<(pdm export --pyproject) src/
+
+.PHONY: pylint
+pylint:
+	pylint *.py src/ tests/ docs/
+
+.PHONY: pyroma
+pyroma:
+	pyroma --min 10 .
+
+.PHONY: pyright
+pyright:
+	pyright .
+
+.PHONY: vulture
+vulture:
+	vulture --min-confidence 100 --exclude _vendor --exclude .eggs .
+
+.PHONY: linkcheck
+linkcheck:
+	$(MAKE) -C docs/ linkcheck SPHINXOPTS=$(SPHINXOPTS)
+
+.PHONY: spelling
+spelling:
+	$(MAKE) -C docs/ spelling SPHINXOPTS=$(SPHINXOPTS)
