b25b9d39d8e0a289740f88f27ae1c7bad4a4dad9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:44:08 +0000

    Add package dbtgenlib/0.0.11/dbtgenlib-0.0.11.tar.gz

---
+++ packages/dbtgenlib/dbtgenlib-0.0.11.tar.gz/dbtgenlib-0.0.11/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+setup(
+    name='dbtgenlib',
+    version='0.0.11',
+    packages=find_packages(),
+    install_requires=[
+        'click',
+    ],
+    entry_points='''
+        [console_scripts]
+        yml_gen=dbtgenlib.yml_gen:dbtdocgen
+        ''',
+)
