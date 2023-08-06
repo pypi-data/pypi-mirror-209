d3cf33a5c66b19e2d637da8c53d07c0d375eb27a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:22:45 +0000

    Add package genki-signals/0.1.0/genki_signals-0.1.0-py3-none-any.whl

---
+++ packages/genki-signals/genki_signals-0.1.0-py3-none-any.whl/genki_signals/__init__.py
@@ -0,0 +1,25 @@
+from .frontends import *
+from .functions import *
+from .sources import *
+
+from .dead_reckoning import *
+from .filters import *
+from .fusion import *
+from .recorders import *
+from .session import *
+from .system import *
+
+from .buffers import Buffer, DataBuffer
+
+__version__ = "0.1.0"
+
+__doc__ = """
+genki_signals
+=============
+
+Description
+-----------
+Genki Signals is a library for processing and manipulating time series data.
+
+Example notebooks can be found on the `GitHub repo <https://github.com/genkiinstruments/genki-signals>`.
+"""
