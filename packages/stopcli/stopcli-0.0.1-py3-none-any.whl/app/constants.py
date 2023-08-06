599417521dad580a71a134034b078928201bd408
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:41:03 +0000

    Add package stopcli/0.0.1/stopcli-0.0.1-py3-none-any.whl

---
+++ packages/stopcli/stopcli-0.0.1-py3-none-any.whl/app/constants.py
@@ -0,0 +1,11 @@
+
+
+LETTERS = [
+    "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M",
+    "N", "Ñ", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"
+]
+
+HEADERS = [
+    "LETRA", "NOMBRE", "APELLIDO", "CIUDAD", "PROFESIÓN", "FRUTA", 
+    "ANIMAL", "COSA", "COLOR", "TOTAL"
+]
