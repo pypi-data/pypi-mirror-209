5d465ca83f8a55dd96b1951febacf6f6a535a8ab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:12:30 +0000

    Add package pantsbuild.pants/2.17.0a1/pantsbuild.pants-2.17.0a1-cp38-cp38-manylinux2014_aarch64.whl

---
+++ packages/pantsbuild.pants/pantsbuild.pants-2.17.0a1-cp38-cp38-manylinux2014_aarch64.whl/pants/backend/python/subsystems/lambdex.py
@@ -0,0 +1,25 @@
+# Copyright 2019 Pants project contributors (see CONTRIBUTORS.md).
+# Licensed under the Apache License, Version 2.0 (see LICENSE).
+
+from pants.backend.python.subsystems.python_tool_base import LockfileRules, PythonToolBase
+from pants.backend.python.target_types import ConsoleScript
+from pants.engine.rules import collect_rules
+
+
+class Lambdex(PythonToolBase):
+    options_scope = "lambdex"
+    help = "A tool for turning .pex files into Function-as-a-Service artifacts (https://github.com/pantsbuild/lambdex)."
+
+    default_version = "lambdex>=0.1.9"
+    default_main = ConsoleScript("lambdex")
+    default_requirements = [default_version]
+
+    register_interpreter_constraints = True
+    default_interpreter_constraints = ["CPython>=3.7,<3.12"]
+
+    default_lockfile_resource = ("pants.backend.python.subsystems", "lambdex.lock")
+    lockfile_rules_type = LockfileRules.SIMPLE
+
+
+def rules():
+    return collect_rules()
