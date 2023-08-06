d38f8caf025c031f7569dee89a047a879230212d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:36:47 +0000

    Add package ASTOCache/1.0.0/ASTOCache-1.0.0.tar.gz

---
+++ packages/ASTOCache/ASTOCache-1.0.0.tar.gz/ASTOCache-1.0.0/ASTOCache/logger.py
@@ -0,0 +1,41 @@
+import inspect
+from fluent import asyncsender
+import os
+import msgpack
+from io import BytesIO
+
+
+fluentd_host = os.environ.get("fluent_host", "")
+fluentd_port = int(os.environ.get("fluent_port", 0))
+component_name = os.environ.get("component_name", "")
+
+
+def overflow_handler(pendings):
+    unpacker = msgpack.Unpacker(BytesIO(pendings))
+    for unpacked in unpacker:
+        print(unpacked)
+
+def log_emmiter(msg, typ='INFO'):
+    #eval file name to log to from typ
+    sender = asyncsender.FluentSender(component_name, host=fluentd_host, port=fluentd_port, buffer_overflow_handler=overflow_handler)
+    sender.emit(typ, {"message": f"{msg}"})
+    sender.close()
+
+def logger(fn, typ='INFO'):
+    from functools import wraps
+    from datetime import datetime, timezone
+
+    @wraps(fn)
+    def inner(*args, **kwargs):
+        run_time = datetime.now()
+        line_num = inspect.currentframe().f_back.f_lineno
+        fname = fn.__name__
+        filename = fn.__module__
+        arg = inspect.currentframe()
+        all_locals = arg.f_back.f_locals
+        logging_str = f" ~##~ LINE {line_num} FUNCTION {fname} FILE {filename} ALL_LOCALS ~##~ {all_locals}"
+        # print(logging_str)
+        log_emmiter(logging_str)
+        return fn(*args, **kwargs)
+
+    return inner
