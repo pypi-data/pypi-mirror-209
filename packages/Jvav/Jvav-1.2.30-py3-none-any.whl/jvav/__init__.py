4ea849f994445a002655563dccf048ce3673000d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:15:30 +0000

    Add package Jvav/1.2.30/Jvav-1.2.30-py3-none-any.whl

---
+++ packages/Jvav/Jvav-1.2.30-py3-none-any.whl/jvav/__init__.py
@@ -0,0 +1,31 @@
+# -*- coding: UTF-8 -*-
+from jvav.utils import (
+    BaseUtil,
+    JavLibUtil,
+    DmmUtil,
+    JavBusUtil,
+    AvgleUtil,
+    MagnetUtil,
+    SukebeiUtil,
+    WikiUtil,
+    TransUtil,
+    JavDbUtil,
+)
+
+__version__ = "1.2.30"
+
+VERSION = __version__
+
+__all__ = [
+    BaseUtil,
+    JavLibUtil,
+    DmmUtil,
+    JavBusUtil,
+    AvgleUtil,
+    MagnetUtil,
+    SukebeiUtil,
+    WikiUtil,
+    TransUtil,
+    JavDbUtil,
+    VERSION,
+]
