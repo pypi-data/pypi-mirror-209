c57c22f373af0697fba33955694143271cd08761
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:44:40 +0000

    Add package sinol-make/0.0.1/sinol-make-0.0.1.tar.gz

---
+++ packages/sinol-make/sinol-make-0.0.1.tar.gz/sinol-make-0.0.1/src/sinol_make/__init__.py
@@ -0,0 +1,34 @@
+import argparse, pkg_resources
+from sinol_make import util
+
+def configure_parsers():
+	parser = argparse.ArgumentParser(
+		prog='sinol-make',
+		description='Tool for creating and testing sio2 tasks',
+	)
+	parser.add_argument("-v", "--version", action="version", version="%(prog)s " + pkg_resources.get_distribution('sinol_make').version)
+	subparsers = parser.add_subparsers(
+		title='commands',
+		description='sinol-make commands',
+		dest='command',
+	)
+	subparsers.required = False
+
+	commands = util.get_commands()
+
+	for command in commands:
+		command.configure_subparser(subparsers)
+	return parser
+
+
+def main():
+	parser = configure_parsers()
+	commands = util.get_commands()
+	args = parser.parse_args()
+
+	for command in commands:
+		if command.get_name() == args.command:
+			command.run(args)
+			exit(0)
+
+	parser.print_help()
