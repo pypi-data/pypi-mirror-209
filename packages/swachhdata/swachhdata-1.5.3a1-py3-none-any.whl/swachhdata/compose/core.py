b6bf0072f95e5e2ec5f5a4d12287133ecc9021b5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:15:15 +0000

    Add package swachhdata/1.5.3a1/swachhdata-1.5.3a1-py3-none-any.whl

---
+++ packages/swachhdata/swachhdata-1.5.3a1-py3-none-any.whl/swachhdata/compose/core.py
@@ -0,0 +1,58 @@
+from ..utils.verify import (
+    verify_valid_dtype_text,
+    verify_valid_ndim_text
+)
+
+from ..utils.tools import (
+    fetch_num_rows
+)
+
+from ..utils.probe import (
+    probe_string_data
+)
+
+class BaseTextDatum:
+
+    """
+    Data formatter, converts heterogenous text data types to homogenous list of string(s).
+
+    Parameters
+    ----------
+    data / X : pandas.core.frame.DataFrame / pandas.core.series.Series / numpy.ndarray
+
+    """
+
+    def __init__(self, text=None):
+        
+        dtype_check = verify_valid_dtype_text(text)
+        ndim_check = verify_valid_ndim_text(text)
+
+        self.__data = None
+        self.data = text
+        self.__base_text_datum = None
+    
+    def __str__(self):
+        return str(self.data)
+    
+    def __len__(self):
+        return fetch_num_rows(self.__data)
+
+    @property
+    def data(self):
+        return self.__data
+
+    @data.setter
+    def data(self, text):
+        self.__data = probe_string_data(text)
+    
+    def recast(self):
+        self.__data = probe_string_data(self.__data)
+
+class TextDatum(BaseTextDatum):
+
+    def __init__(self, text):
+        super().__init__(text)
+    
+    def __add__(self, other):
+        data = self.data + other.data
+        return TextDatum(data)
---
+++ packages/swachhdata/swachhdata-1.5.3a1-py3-none-any.whl/swachhdata/text/_base.py
@@ -0,0 +1,56 @@
+import pandas
+from ..compose.core import BaseTextDatum
+
+class TextFormatter:
+    """
+    Base Data Formatter for all recasts in swachhdata.text module
+
+    Accepted text input format:
+        * string
+        * list of string
+        * pandas.core.series.Series
+    """
+
+    def __init__(self):
+        
+        self._dtype = None
+        self._text = None
+        self._count = None
+    
+    def __text_formatter(self):
+        
+        self._count = len(self._text)
+
+        if self._dtype == pandas.core.series.Series:
+            self._text = self._text.tolist()
+            self._dtype = type(self._text)
+
+        if self._dtype == str:
+            self._count = len(self._text.split())
+            self._dtype = type(self._text)
+
+class BaseTextRecast(BaseTextDatum):
+
+    def __init__(self):
+        """
+        Initialize
+        """
+    
+    def setup(self, data)
+        """
+        Setup
+        """
+        if hasattr(data, '__base_text_datum'):
+            self.__dict__.update(data.__dict__)
+        else:
+            self.data = data
+    
+    def recast(self):
+        """
+        Recast
+        """
+    
+    def setup_recast(self):
+        """
+        Setup & Recast
+        """
