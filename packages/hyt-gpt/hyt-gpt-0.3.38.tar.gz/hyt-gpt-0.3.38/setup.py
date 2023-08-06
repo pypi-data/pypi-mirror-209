f1bb80eb8b6aa3774728b51fc6baee31fa004b92
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:18:31 +0000

    Add package hyt-gpt/0.3.38/hyt-gpt-0.3.38.tar.gz

---
+++ packages/hyt-gpt/hyt-gpt-0.3.38.tar.gz/hyt-gpt-0.3.38/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages, find_namespace_packages
+
+setup(
+    name='hyt-gpt',
+    version='0.3.38',
+    description='A library for GPT and Youtube',
+    author='Harbin',
+    author_email='harbinfate@gmail.com',
+    packages=find_packages(),
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+)
