eae89880c17cef612097342fae098835cb3e8ace
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:15:28 +0000

    Add package improutils/1.1.13/improutils-1.1.13.tar.gz

---
+++ packages/improutils/improutils-1.1.13.tar.gz/improutils-1.1.13/improutils/recognition/ocr.py
@@ -0,0 +1,21 @@
+import PIL
+from pytesseract import pytesseract
+
+from improutils import negative
+
+
+def ocr(img_bin):
+    """
+    Detects text in the file.
+
+    Parameters
+    ----------
+    img_bin : ndarray
+        Input binary image. White objects on black background.
+    Returns
+    -------
+    The recognized text in the image.
+    """
+    # Tesseract works with black objects on white background.
+    img_bin = negative(img_bin)
+    return pytesseract.image_to_string(PIL.Image.fromarray(img_bin))
