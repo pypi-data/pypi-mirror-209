c48380cba15c49c0f08978b3002c81bdfae0cef7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:07:53 +0000

    Add package cellmaps-ppi-embedding/0.1.0a3/cellmaps_ppi_embedding-0.1.0a3.tar.gz

---
+++ packages/cellmaps-ppi-embedding/cellmaps_ppi_embedding-0.1.0a3.tar.gz/cellmaps_ppi_embedding-0.1.0a3/cellmaps_ppi_embedding/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+"""Top-level package for cellmaps_ppi_embedding."""
+
+__author__ = """Mayank Jain"""
+__email__ = 'maj014@ucsd.edu'
+__version__ = '0.1.0a3'
+__repo_url__ = 'https://github.com/idekerlab/cellmaps_ppi_embedding'
+__description__ = 'A tool to generate embeddings from networks'
