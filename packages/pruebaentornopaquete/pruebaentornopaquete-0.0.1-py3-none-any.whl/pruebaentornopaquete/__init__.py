449df9943aa49e7421e299f121a533bbbcc8f3c7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:27:40 +0000

    Add package pruebaentornopaquete/0.0.1/pruebaentornopaquete-0.0.1-py3-none-any.whl

---
+++ packages/pruebaentornopaquete/pruebaentornopaquete-0.0.1-py3-none-any.whl/pruebaentornopaquete/__init__.py
@@ -0,0 +1,7 @@
+# Doc string - sirve para documentar paquetes
+# cuando se importe mostrar la información que estas poniendo
+# Python entiende que este texto es para documentar y cada vez que pases el mause sobre el nombre de tu carpeta o paquete
+# mostrara ese texto
+"""
+Esta es la documentación del paquete
+"""
