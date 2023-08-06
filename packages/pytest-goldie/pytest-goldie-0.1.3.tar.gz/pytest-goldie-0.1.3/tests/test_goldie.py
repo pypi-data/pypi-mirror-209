fc25d6dd973c474abfd1a907e473ec42b4593d15
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:07:29 +0000

    Add package pytest-goldie/0.1.3/pytest-goldie-0.1.3.tar.gz

---
+++ packages/pytest-goldie/pytest-goldie-0.1.3.tar.gz/pytest-goldie-0.1.3/tests/test_goldie.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+
+
+def test_help_message(testdir):
+    result = testdir.runpytest(
+        "--help",
+    )
+    # fnmatch_lines does an assertion internally
+    result.stdout.fnmatch_lines(
+        [
+            "*goldie:*",
+            "*--update_goldens*",
+            "*--goldens_folder_name*",
+        ]
+    )
