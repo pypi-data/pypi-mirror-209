f0bab8e10a57b9b5a94ab99358634e2b7aff42c6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:32:38 +0000

    Add package encord/0.1.77/encord-0.1.77-py3-none-any.whl

---
+++ packages/encord/encord-0.1.77-py3-none-any.whl/encord/orm/formatter.py
@@ -0,0 +1,9 @@
+from abc import ABC, abstractmethod
+from typing import Dict, List, Union
+
+
+class Formatter(ABC):
+    @classmethod
+    @abstractmethod
+    def from_dict(cls, json_dict: Dict):
+        pass
