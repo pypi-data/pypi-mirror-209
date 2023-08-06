fbda902ae969bc5ec992854b88fdb58f04dfe838
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:13:50 +0000

    Add package dbtdocgen-rutryk/0.0.7/dbtdocgen_rutryk-0.0.7.tar.gz

---
+++ packages/dbtdocgen-rutryk/dbtdocgen_rutryk-0.0.7.tar.gz/dbtdocgen_rutryk-0.0.7/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+setup(
+    name='dbtdocgen_rutryk',
+    version='0.0.7',
+    packages=find_packages(),
+    install_requires=[
+        'click',
+    ],
+    entry_points='''
+        [console_scripts]
+        dbtdocgen=dbtdocgen_rutryk.dbtdocgen:dbtdocgen
+        ''',
+)
