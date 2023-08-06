e2b8226c8a7f388e4648bccd7cb514af6b12cdf5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:43:24 +0000

    Add package scikit-network/0.31.0/scikit_network-0.31.0-cp311-cp311-manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/scikit-network/scikit_network-0.31.0-cp311-cp311-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/sknetwork/clustering/tests/test_API.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""Tests for clustering API"""
+import unittest
+
+from sknetwork.clustering import *
+from sknetwork.data.test_graphs import *
+
+
+class TestClusteringAPI(unittest.TestCase):
+
+    def test_regular(self):
+        for algo in [Louvain(return_aggregate=True), PropagationClustering(return_aggregate=True)]:
+            for adjacency in [test_graph(), test_digraph(), test_disconnected_graph()]:
+                n = adjacency.shape[0]
+                labels = algo.fit_predict(adjacency)
+                n_labels = len(set(labels))
+                self.assertEqual(labels.shape, (n,))
+                self.assertEqual(algo.aggregate_.shape, (n_labels, n_labels))
+                adjacency_bool = adjacency.astype(bool)
+                labels = algo.fit_predict(adjacency_bool)
+                n_labels = len(set(labels))
+                self.assertEqual(labels.shape, (n,))
+                self.assertEqual(algo.aggregate_.shape, (n_labels, n_labels))
+                membership = algo.fit_transform(adjacency)
+                self.assertEqual(membership.shape, (n, n_labels))
+
+    def test_bipartite(self):
+        biadjacency = test_bigraph()
+        n_row, n_col = biadjacency.shape
+        for algo in [Louvain(return_aggregate=True), PropagationClustering(return_aggregate=True)]:
+            algo.fit(biadjacency)
+            self.assertEqual(algo.labels_row_.shape, (n_row,))
+            self.assertEqual(algo.labels_col_.shape, (n_col,))
