57c1341de670831584fecaa008226924d621021a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:42:35 +0000

    Add package betterdataclass/2.0/betterdataclass-2.0.tar.gz

---
+++ packages/betterdataclass/betterdataclass-2.0.tar.gz/betterdataclass-2.0/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+
+setup(
+    name="betterdataclass",
+    version="2.0",
+    description="A multipurpose dataclass libarary used for validation and data structuring.",
+    long_description="Go to github.com/dvnasutosh/betterdataclasses",
+    url='https://github.com/dvnasutosh/betterdataclasses',
+    
+    author="Asutosh Rath",
+    author_email="dvnasutosh@gmail.com",
+    packages=['betterdataclass','betterdataclass.helper'],
+        classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)'
+    ]
+)
