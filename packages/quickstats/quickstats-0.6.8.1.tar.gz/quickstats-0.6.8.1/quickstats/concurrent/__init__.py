725914aa80e2fdb1afd00f675e6b606db9f00d0e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:17:06 +0000

    Add package quickstats/0.6.8.1/quickstats-0.6.8.1.tar.gz

---
+++ packages/quickstats/quickstats-0.6.8.1.tar.gz/quickstats-0.6.8.1/quickstats/concurrent/__init__.py
@@ -0,0 +1,4 @@
+from .abstract_runner import AbstractRunner
+from .parameterised_runner import ParameterisedRunner
+from .parameterised_asymptotic_cls import ParameterisedAsymptoticCLs
+from .parameterised_likelihood import ParameterisedLikelihood
