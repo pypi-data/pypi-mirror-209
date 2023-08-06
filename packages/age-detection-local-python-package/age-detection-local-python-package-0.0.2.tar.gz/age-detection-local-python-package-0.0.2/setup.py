260277ee73ea8b95795f88324b075f5a8dbf8fcc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:20:23 +0000

    Add package age-detection-local-python-package/0.0.2/age-detection-local-python-package-0.0.2.tar.gz

---
+++ packages/age-detection-local-python-package/age-detection-local-python-package-0.0.2.tar.gz/age-detection-local-python-package-0.0.2/setup.py
@@ -0,0 +1,18 @@
+import setuptools
+
+setuptools.setup(
+     name='age-detection-local-python-package',  
+     version='0.0.2',
+     author="Circles",
+     author_email="info@circles.zone",
+     description="PyPI Package for age detection",
+     long_description="This is a package for running age detection and getting approximate age",
+     long_description_content_type="text/markdown",
+     url="https://github.com/javatechy/dokr",
+     packages=setuptools.find_packages(),
+     classifiers=[
+         "Programming Language :: Python :: 3",
+         "License :: Other/Proprietary License",
+         "Operating System :: OS Independent",
+     ],
+ )
