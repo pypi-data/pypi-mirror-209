c939db651c880120acde2f0bf13cb12260c6d45d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:16:55 +0000

    Add package oarepo-model-builder-ui/4.0.0/oarepo_model_builder_ui-4.0.0-py3-none-any.whl

---
+++ packages/oarepo-model-builder-ui/oarepo_model_builder_ui-4.0.0-py3-none-any.whl/oarepo_model_builder_ui/datatypes/components/model.py
@@ -0,0 +1,42 @@
+import os
+
+from oarepo_model_builder.datatypes import DataTypeComponent, ModelDataType
+from oarepo_model_builder.datatypes.components import SavedModelComponent
+from oarepo_model_builder.datatypes.components.model.utils import set_default
+import marshmallow as ma
+
+from oarepo_model_builder.utils.python_name import module_to_path
+
+
+class TranslationsSchema(ma.Schema):
+    class Meta:
+        unknown = ma.RAISE
+
+    module = ma.fields.Str()
+    alias = ma.fields.Str()
+
+class UISchema(ma.Schema):
+    class Meta:
+        unknown = ma.RAISE
+
+    module = ma.fields.Str()
+    file = ma.fields.Str()
+    alias = ma.fields.Str()
+
+class UIModelComponent(DataTypeComponent):
+    eligible_datatypes = [ModelDataType]
+    depends_on=[SavedModelComponent]
+
+    class ModelSchema(ma.Schema):
+        translations = ma.fields.Nested(TranslationsSchema)
+        ui = ma.fields.Nested(UISchema)
+
+    def before_model_prepare(self, datatype, *, context, **kwargs):
+        translations = set_default(datatype, 'translations', {})
+        translations.setdefault('module', f"{datatype.definition['module']['qualified']}.translations")
+        translations.setdefault('alias', datatype.definition['module']['alias'])
+
+        ui = set_default(datatype, 'ui', {})
+        module = ui.setdefault('module', datatype.definition['saved-model']['module'])
+        ui.setdefault('file', os.path.join(module_to_path(module), "ui.json"))
+        ui.setdefault('alias', datatype.definition['module']['alias'])
