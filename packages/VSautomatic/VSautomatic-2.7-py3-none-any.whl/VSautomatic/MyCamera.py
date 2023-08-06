784ecac0d2f53cee47dfe03c8128ddce1a3e2e9f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:49:39 +0000

    Add package VSautomatic/2.7/VSautomatic-2.7-py3-none-any.whl

---
+++ packages/VSautomatic/VSautomatic-2.7-py3-none-any.whl/VSautomatic/MyCamera.py
@@ -0,0 +1,87 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Sep 03 15:32:13 2018
+
+"""
+import threading
+import time
+
+# -*- coding: utf-8 -*-
+
+import cv2
+# from memory_profiler import profile
+
+class CameraCapture(object):
+    def __init__(self):
+        self.my_camera = None
+        self.my_camera_fps = 0
+        self.my_camera_resolution=()
+        # self.my_camera_lock = threading.Lock()
+
+    def isOpen(self):        
+        if not self.my_camera is None:
+            return self.my_camera.isOpened()
+        else:
+            return False
+
+    def open(self):
+        self.my_camera=cv2.VideoCapture(0,cv2.CAP_DSHOW)
+        self.my_camera_fps= self.my_camera.get(cv2.CAP_PROP_FPS)
+        print('camera fps:',self.my_camera_fps)
+        self.my_camera_resolution=(int(self.my_camera.get(cv2.CAP_PROP_FRAME_WIDTH)),int(self.my_camera.get(cv2.CAP_PROP_FRAME_HEIGHT)))
+        print('camera resolution:',self.my_camera_resolution)
+
+    def imageCapture(self,picName,frameNum):
+        print("start capture image")
+        for i in range(int(frameNum)):
+            isSuccess,frame = self.my_camera.read()
+            if isSuccess:
+                cv2.imwrite(picName+'_'+str(i+1)+'.jpg', frame)
+        print("stop capture image")
+
+    def videoCapture(self,vidName,duration):
+        print("start capture video")
+        # start_time = time.time()
+        # self.my_camera_lock.acquire()
+        self.my_camera.set(3,640)
+        self.my_camera.set(4,480)
+        self.my_camera.set(1,10.0)
+        # fourcc=cv2.cv.CV_FOURCC('m','p','4','v')
+        fourcc=cv2.VideoWriter_fourcc('m','p','4','v')
+        # fourccs = cv2.imwrite('RGB.jpg', self.flip_hv)
+        out = cv2.VideoWriter(vidName+'.mp4',fourcc,30,(640,480))
+        # print(vidName)
+        # print(self.my_camera.isOpened())
+        
+        # print("Start camera")
+        framenumber=int(duration)*30
+        for n in range(framenumber):
+            ret,frame = self.my_camera.read()
+            if ret == True:
+                out.write(frame)
+
+        # print("Closed camera")
+        # self.my_camera.release()
+        out.release()
+        # end_time = time.time()
+        # run_time = end_time - start_time
+        # print(run_time)
+        print("stop capture video")
+        # self.my_camera_lock.release()
+
+    def close(self):
+        if self.my_camera.isOpened():
+            self.my_camera.release()
+
+
+if __name__ == "__main__":
+    
+    cap = CameraCapture()
+    cap.open()
+    print(cap.isOpen())
+    cap.imageCapture('cherry_test',3)
+    cap.videoCapture('cherry_test',5)
+    cap.close()
+    #cap.videoCapture('cherry_video',1)
+
+
---
+++ packages/VSautomatic/VSautomatic-2.7-py3-none-any.whl/VSautomatic/MyRedRat.py
@@ -0,0 +1,53 @@
+# -*- coding: utf-8 -*-
+
+#
+# Simple Python test program to use the RedRatHub.
+#
+# Ethan Johnson, David Blight, Chris Dodge - RedRat Ltd.
+#
+
+
+import VSautomatic.RedRatHub
+import os
+import time
+from VSautomatic.RedRatHub import Client
+
+log_file_path = os.path.realpath(os.path.dirname(os.path.dirname(__file__)) + '//VSautomatic')
+
+
+
+class DEV_REDRAT(object):
+
+    def __init__(self,test_pc_ip,redRat_Name,redRatDataset):
+        self.test_pc_ip = test_pc_ip
+        self.redRat_Name = redRat_Name
+        self.redRatDataset = redRatDataset
+
+    def IR_GEN(self,IRMessage,PauseTimer):
+                
+        print('You are using %s redrat and %s.xml dataset on %s test PC!'%(self.redRat_Name, self.redRatDataset, self.test_pc_ip))
+        
+        client = Client()
+        
+        # Connect to the RedRatHub
+        client.OpenSocket(self.test_pc_ip, 40000)
+
+        # Send some IR signals
+
+        client.SendMessage('name="' + self.redRat_Name + '" dataset="'+ self.redRatDataset+ '" signal="' + IRMessage + '" output="12:10"')
+        print("Sent signal {} \n".format(IRMessage))
+        time.sleep(PauseTimer)
+
+
+
+        # List the datasets known by the hub
+        print("List of datasets:")
+        list = client.ReadData('hubquery="list datasets"')
+        # print('{}'.format(list))
+
+        client.CloseSocket()
+        print("Finished.")
+
+if __name__ == '__main__':
+    RedRat=DEV_REDRAT()
+    RedRat.IR_GEN('menu',2)
