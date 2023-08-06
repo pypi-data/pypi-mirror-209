9e13f7fafa2c6a6f50330dc358f5e92f0a8d3edc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:54:45 +0000

    Add package GrappyLfjv/0.0.18/GrappyLfjv-0.0.18.tar.gz

---
+++ packages/GrappyLfjv/GrappyLfjv-0.0.18.tar.gz/GrappyLfjv-0.0.18/setup.py
@@ -0,0 +1,21 @@
+import os
+from setuptools import setup
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup(
+    name= "GrappyLfjv",
+    version= "0.0.18",
+    description= "Simplification de la création de graphiques avec matplotlib",
+    long_description=read("README.md"),
+    long_description_content_type='text/markdown',
+    py_modules=["grappy"],
+    package_dir={"": "src"},
+    install_requires = [
+        "matplotlib > 3.0"
+    ],
+    url="https://github.com/Benjamin-Trevian/Grappy",
+    author="Benjamin Trévian",
+    author_email="benjamin.trevian@hotmail.com",
+)
