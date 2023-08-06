5031c2252c5af8874adb16b828f468ea9b52f9ee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:13:45 +0000

    Add package textagon/0.1.11/textagon-0.1.11.tar.gz

---
+++ packages/textagon/textagon-0.1.11.tar.gz/textagon-0.1.11/textagon/__init__.py
@@ -0,0 +1,6 @@
+from .textagon import say_hello
+from .textagon import ReadAllLexicons
+from .textagon import SanityCheck
+from .textagon import ReadRawText
+from .textagon import setSpellChecking
+# from .textagon import TextToFeatures
