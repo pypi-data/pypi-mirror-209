f05f9669511b9644eacb471cd7e3f8b2734e8d0d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:09:11 +0000

    Add package find-old-large-files/0.4.3/find_old_large_files-0.4.3.tar.gz

---
+++ packages/find-old-large-files/find_old_large_files-0.4.3.tar.gz/find_old_large_files-0.4.3/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="find_old_large_files",
+    version="0.4.3",  # Increment the version here
+    packages=find_packages(),
+    install_requires=["tqdm"],
+    entry_points={
+        'console_scripts': [
+            'find_old_large_files=find_old_large_files:run',
+        ],
+    },
+    author="Disant Upadhyay",
+    author_email="disantupadhyay07@gmail.com",
+    description="A utility to find and remove large, old files.",
+    long_description=open('README.md').read(),
+    long_description_content_type="text/markdown",
+    url="http://github.com/PrinceDisant/find_old_large_files",
+)
