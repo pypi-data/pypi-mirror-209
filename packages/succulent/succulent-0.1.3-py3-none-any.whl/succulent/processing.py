a80ec139abef3ea7e3e037a679c4be63736ba5d4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:43:53 +0000

    Add package succulent/0.1.3/succulent-0.1.3-py3-none-any.whl

---
+++ packages/succulent/succulent-0.1.3-py3-none-any.whl/succulent/processing.py
@@ -0,0 +1,73 @@
+import os
+import sqlite3
+import inspect
+import pandas as pd
+
+class Processing:
+    def __init__(self, config, format):
+        self.directory = os.path.join(os.path.dirname(os.path.abspath(inspect.stack()[2].filename)), 'data')
+        self.format = format
+        self.columns = [configuration['name'] for configuration in config]
+        self.df = None  # Initialize df attribute
+    
+    def parameters(self):
+        parameters = [f'{column}=' for column in self.columns]
+        parameters = '&'.join(parameters)
+        return parameters
+        
+    def process(self, req):
+        # Directory preparation
+        if not os.path.exists(self.directory):
+            os.makedirs(self.directory)
+
+        # Define paths
+        path = os.path.join(
+            self.directory, f'data.{self.format}'
+        )
+
+        # Load existing data
+        if os.path.exists(path):
+            if self.format == 'csv':
+                self.df = pd.read_csv(path, sep=',')
+            elif self.format == 'json':
+                self.df = pd.read_json(path, orient='records')
+            elif self.format == 'sqlite':
+                conn = sqlite3.connect(path)
+                self.df = pd.read_sql_query("SELECT * FROM data", conn)
+                conn.close()
+            else:
+                raise ValueError(f'Invalid file type: {self.format}')
+        # Initialise new data
+        else:
+            self.df = pd.DataFrame(columns=self.columns)
+
+        # Parse data from request
+        data = {}
+        if req.is_json:
+            for column in self.columns:
+                try:
+                    data[column] = req.json[column]
+                except:
+                    data[column] = None
+        else:
+            for column in self.columns:
+                try: 
+                    data[column] = str(req.args.get(column, default=''))
+                except:
+                    data[column] = ''
+        data = pd.Series(data, index=self.columns)
+
+        # Merge data
+        self.df = pd.concat([self.df, data.to_frame().T], ignore_index=True)
+
+        # Store data to device
+        if self.format == 'csv':
+            self.df.to_csv(path, sep=',', index=False)
+        elif self.format == 'json':
+            self.df.to_json(path, orient='records', indent=4)
+        elif self.format == 'sqlite':
+            conn = sqlite3.connect(path)
+            self.df.to_sql('data', conn, if_exists='replace', index=False)
+            conn.close()
+        else:
+            raise ValueError(f'Invalid format: {self.format}')
