ac250bacbaadc6828836efc34c58fdde236584c8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:18:26 +0000

    Add package django-marina/23.4/django_marina-23.4.tar.gz

---
+++ packages/django-marina/django_marina-23.4.tar.gz/django_marina-23.4/docs/conf.py
@@ -0,0 +1,26 @@
+import importlib
+from datetime import datetime
+
+import tomllib
+
+with open("../pyproject.toml", "rb") as f:
+    pyproject = tomllib.load(f)
+
+project = pyproject["project"]["name"]
+project_with_underscores = project.replace("-", "_")
+module = importlib.import_module(f"{project_with_underscores}")
+
+authors = ", ".join(author["name"] for author in pyproject["project"]["authors"])
+release = module.__version__
+version = ".".join(release.split(".")[:2])
+year = datetime.now().year
+copyright = f"{year} {authors}"
+
+extensions = [
+    "sphinx.ext.autodoc",
+    "sphinx.ext.viewcode",
+    "sphinx_mdinclude",
+]
+pygments_style = "sphinx"
+htmlhelp_basename = f"{project}-doc"
+html_theme = "sphinx_rtd_theme"
