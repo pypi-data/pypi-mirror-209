15679c137a6a1fd4e8103db4b698d54e7b05032f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:35:55 +0000

    Add package pyopenspecy/0.0.1/pyopenspecy-0.0.1-py3-none-any.whl

---
+++ packages/pyopenspecy/pyopenspecy-0.0.1-py3-none-any.whl/pyopenspecy/__version__.py
@@ -0,0 +1,20 @@
+
+__version__      = '0.0.1'
+__version_date__ = '2023-05-19'
+
+__title__        = 'pyopenspecy'
+__description__  = 'Python package boilerplate'
+__url__          = ''
+
+__author__       = 'Julian Irwin'
+__author_email__ = 'julian.irwin@gmail.com'
+
+__license__      = ''
+__copyright__    = ''
+
+__all__ = [
+    '__version__', '__version_date__',
+    '__title__', '__description__', '__url__',
+    '__author__', '__author_email__',
+    '__license__', '__copyright__'
+]
