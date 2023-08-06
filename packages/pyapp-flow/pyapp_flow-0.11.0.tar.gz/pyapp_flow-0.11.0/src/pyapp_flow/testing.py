a14daa6a5183959d2f69f0eef2c3b62e8e7a7019
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:07:11 +0000

    Add package pyapp-flow/0.11.0/pyapp_flow-0.11.0.tar.gz

---
+++ packages/pyapp-flow/pyapp_flow-0.11.0.tar.gz/pyapp_flow-0.11.0/src/pyapp_flow/testing.py
@@ -0,0 +1,29 @@
+"""Helper methods for testing workflows."""
+from typing import Any, Callable
+
+from .datastructures import WorkflowContext
+
+
+def call_node(
+    node: Callable[[WorkflowContext], Any], **context_vars: Any
+) -> WorkflowContext:
+    """Simplifies the testing of any node.
+
+    Handles the boilerplate code required to set up a
+    :class:`pyapp_flow.WorkflowContext` with the expected variables.
+
+    Method returns the generated context object to be asserted on.
+
+    .. code-block:: python
+
+        def test_find_isbn__with_known_title():
+
+            context = call_node(find_isbn, title="Hyperion")
+            actual = context.state["isbn_13"]
+
+            assert actual == "978-0553283686"
+
+    """
+    context = WorkflowContext(**context_vars)
+    node(context)
+    return context
