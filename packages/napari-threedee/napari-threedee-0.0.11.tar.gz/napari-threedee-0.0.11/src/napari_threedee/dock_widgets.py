21482728d969a29300843db65e1020987d34b3ea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:54:48 +0000

    Add package napari-threedee/0.0.11/napari-threedee-0.0.11.tar.gz

---
+++ packages/napari-threedee/napari-threedee-0.0.11.tar.gz/napari-threedee-0.0.11/src/napari_threedee/dock_widgets.py
@@ -0,0 +1,18 @@
+from .manipulators._qt import (
+    QtLayerManipulatorWidget,
+    QtPointManipulatorWidget,
+    QtRenderPlaneManipulatorWidget
+)
+
+from .annotators._qt import (
+    QtPointAnnotatorWidget,
+    QtPathAnnotatorWidget,
+    QtSphereAnnotatorWidget,
+    QtSurfaceAnnotatorWidget,
+)
+
+from .visualization._qt import (
+    QtCameraSpline,
+    QtLightingControlWidget,
+    QtAmbientOcclusionWidget
+)
