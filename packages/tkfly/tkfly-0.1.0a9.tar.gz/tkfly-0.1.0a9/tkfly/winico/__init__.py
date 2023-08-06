7f0d33a493da96d5c72e0ccc65a282bd04ee492a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:16:14 +0000

    Add package tkfly/0.1.0a9/tkfly-0.1.0a9.tar.gz

---
+++ packages/tkfly/tkfly-0.1.0a9.tar.gz/tkfly-0.1.0a9/tkfly/winico/__init__.py
@@ -0,0 +1,28 @@
+from tkfly._winico import *
+
+
+class FlyWinico(object):
+    def default_event(self, Message, X, Y):
+        if Message == "WM_RBUTTONDOWN":
+            from tkinter import Menu, _default_root
+            Menu = Menu(tearoff=False)
+            Menu.add_command(label="Quit", command=_default_root.quit)
+            Menu.tk_popup(X, Y)
+
+    def create(self, icon=None, event=None):
+        from tkfly._winico import taskbar, createfrom
+        from tkinter import _default_root
+        if icon is None:
+            from tkfly._winico import load
+            icon = load("application")
+        else:
+            icon = createfrom(icon)
+
+        if event is None:
+            event = self.default_event
+        taskbar("add", icon, (_default_root.register(event), "%m", "%x", "%y"))
+        return icon
+
+    def delete(self, icon):
+        from tkfly._winico import taskbar
+        taskbar("delete", icon)
