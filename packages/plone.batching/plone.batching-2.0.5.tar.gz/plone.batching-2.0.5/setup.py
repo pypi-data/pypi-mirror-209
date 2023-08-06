641ce45c28439e48062eac5cbc618f7125b137fd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:26:04 +0000

    Add package plone.batching/2.0.5/plone.batching-2.0.5.tar.gz

---
+++ packages/plone.batching/plone.batching-2.0.5.tar.gz/plone.batching-2.0.5/setup.py
@@ -0,0 +1,60 @@
+from setuptools import find_packages
+from setuptools import setup
+
+
+version = "2.0.5"
+
+setup(
+    name="plone.batching",
+    version=version,
+    description="Batching facilities used in Plone.",
+    long_description="\n".join(
+        [
+            open("README.rst").read(),
+            open("CHANGES.rst").read(),
+            open("docs/usage.rst").read(),
+        ]
+    ),
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Environment :: Web Environment",
+        "Framework :: Plone",
+        "Framework :: Plone :: 6.0",
+        "Framework :: Plone :: Core",
+        "Framework :: Zope :: 5",
+        "License :: OSI Approved :: GNU General Public License v2 (GPLv2)",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+    ],
+    keywords="Plone",
+    author="Plone Foundation",
+    author_email="plone-developers@lists.sourceforge.net",
+    url="https://pypi.org/project/plone.batching",
+    license="GPL",
+    packages=find_packages(),
+    namespace_packages=["plone"],
+    include_package_data=True,
+    zip_safe=False,
+    python_requires=">=3.8",
+    install_requires=[
+        "AccessControl",
+        "setuptools",
+        "Zope",
+        "zope.interface",
+        "zope.schema",
+    ],
+    extras_require={
+        "test": [
+            "zope.component",
+            "zope.publisher",
+        ],
+    },
+    entry_points="""
+    [z3c.autoinclude.plugin]
+    target = plone
+    """,
+)
