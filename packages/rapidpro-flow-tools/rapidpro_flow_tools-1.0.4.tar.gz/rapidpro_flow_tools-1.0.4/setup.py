434d581b257757fd53862da6bbe8054b89e76bf2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:09:50 +0000

    Add package rapidpro-flow-tools/1.0.4/rapidpro_flow_tools-1.0.4.tar.gz

---
+++ packages/rapidpro-flow-tools/rapidpro_flow_tools-1.0.4.tar.gz/rapidpro_flow_tools-1.0.4/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="rapidpro_flow_tools",
+    version="1.0.4",
+    packages=find_packages(),  # Automatically find all subpackages
+    # Other setup arguments
+)
