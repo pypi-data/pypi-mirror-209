398d9c07054c35ca0c92c0e6a2c66dc133714e2e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:55:01 +0000

    Add package setu/2.1.0/setu-2.1.0-py3-none-any.whl

---
+++ packages/setu/setu-2.1.0-py3-none-any.whl/setu/__init__.py
@@ -0,0 +1,7 @@
+"""Top-level package for Setu UPI DeepLinks SDK."""
+from setu.contract import SetuAPIException
+from setu.deeplink import Deeplink
+
+__version__ = '2.1.0'
+
+__all__ = ["Deeplink", "SetuAPIException"]
