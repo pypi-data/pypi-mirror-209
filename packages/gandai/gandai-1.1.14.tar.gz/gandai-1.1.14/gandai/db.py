bc3164b6850d1f80efa008ddd78e9f24ce13d786
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:33:07 +0000

    Add package gandai/1.1.14/gandai-1.1.14.tar.gz

---
+++ packages/gandai/gandai-1.1.14.tar.gz/gandai-1.1.14/gandai/db.py
@@ -0,0 +1,56 @@
+import os
+
+from google.cloud.sql.connector import Connector, IPTypes
+import pg8000
+
+import sqlalchemy
+
+# def connect():
+
+
+
+def connect_with_connector() -> sqlalchemy.engine.base.Engine:
+    """
+    Initializes a connection pool for a Cloud SQL instance of Postgres.
+
+    Uses the Cloud SQL Python Connector package.
+    """
+
+    ## Connect to Dev DB 
+    if os.getenv("LOCAL_DB", False):
+        print("📀 connecting to local db")
+        return sqlalchemy.create_engine("postgresql://localhost:5432/parker")
+
+    instance_connection_name = os.environ[
+        "INSTANCE_CONNECTION_NAME"
+    ]  # e.g. 'project:region:instance'
+    db_user = os.environ["DB_USER"]  # e.g. 'my-db-user'
+    db_pass = os.environ["DB_PASS"]  # e.g. 'my-db-password'
+    db_name = os.environ["DB_NAME"]  # e.g. 'my-database'
+    print(instance_connection_name, db_user, db_pass[-5::], db_name)
+
+    ip_type = IPTypes.PRIVATE if os.environ.get("PRIVATE_IP") else IPTypes.PUBLIC
+
+    # initialize Cloud SQL Python Connector object
+    connector = Connector()
+
+    def getconn() -> pg8000.dbapi.Connection:
+        conn: pg8000.dbapi.Connection = connector.connect(
+            instance_connection_name,
+            "pg8000",
+            user=db_user,
+            password=db_pass,
+            db=db_name,
+            ip_type=ip_type,
+        )
+        return conn
+
+    pool = sqlalchemy.create_engine(
+        "postgresql+pg8000://",
+        creator=getconn,
+        pool_size=5,
+        max_overflow=2,
+        pool_timeout=30,  # 30 seconds
+        pool_recycle=1800,  # 30 minutes
+    )
+    return pool
