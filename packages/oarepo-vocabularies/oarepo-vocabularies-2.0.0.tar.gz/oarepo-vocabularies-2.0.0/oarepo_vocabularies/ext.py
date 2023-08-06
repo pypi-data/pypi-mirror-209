bc45545af12cceecb2f45261509cdbaa139a1233
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:41:46 +0000

    Add package oarepo-vocabularies/2.0.0/oarepo-vocabularies-2.0.0.tar.gz

---
+++ packages/oarepo-vocabularies/oarepo-vocabularies-2.0.0.tar.gz/oarepo-vocabularies-2.0.0/oarepo_vocabularies/ext.py
@@ -0,0 +1,30 @@
+class OARepoVocabularies(object):
+    """OARepo extension of Invenio-Vocabularies."""
+
+    def __init__(self, app=None):
+        """Extension initialization."""
+        self.resource = None
+        self.service = None
+        if app:
+            self.init_app(app)
+
+    def init_app(self, app):
+        """Flask application initialization."""
+        self.init_config(app)
+        self.init_resource(app)
+        app.extensions["oarepo-vocabularies"] = self
+
+    def init_config(self, app):
+        """Initialize configuration."""
+        from . import config
+
+        for k in dir(config):
+            if k.startswith("OAREPO_VOCABULARIES_"):
+                app.config.setdefault(k, getattr(config, k))
+            if k.startswith("DEFAULT_DATASTREAMS_"):
+                app.config.setdefault(k, {}).update(getattr(config, k))
+            if k.startswith("DATASTREAMS_CONFIG_GENERATOR_"):
+                app.config.setdefault(k, getattr(config, k))
+
+    def init_resource(self, app):
+        pass
