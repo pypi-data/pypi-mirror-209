c03fa0529b266b8d25f0c7242cb3963803112f70
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:13:51 +0000

    Add package FinLogic/0.4.6/finlogic-0.4.6.tar.gz

---
+++ packages/FinLogic/finlogic-0.4.6.tar.gz/finlogic-0.4.6/finlogic/__init__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+#
+# FinLogic - Brazilian Company Financial Data
+# https://github.com/crdcj/FinLogic
+#
+# Copyright 2022 Carlos Carvalho
+#
+from . import config  # noqa
+from .database import update_database, search_company, database_info
+from .company import Company
+
+
+__version__ = "0.4.6"
+__author__ = "Carlos Carvalho"
+
+__all__ = [
+    "Company",
+    "update_database",
+    "search_company",
+    "database_info",
+]
