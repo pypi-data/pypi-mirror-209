52d62281251f0d0880adc3450894dac46fd9df10
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:50:42 +0000

    Add package visionai-data-format/1.0.3/visionai-data-format-1.0.3.tar.gz

---
+++ packages/visionai-data-format/visionai-data-format-1.0.3.tar.gz/visionai-data-format-1.0.3/setup.py
@@ -0,0 +1,26 @@
+from setuptools import find_packages, setup
+
+AUTHOR = "LinkerVision"
+PACKAGE_NAME = "visionai-data-format"
+PACKAGE_VERSION = "1.0.3"
+DESC = "converter tool for visionai format"
+REQUIRED = ["pydantic"]
+REQUIRES_PYTHON = ">=3.7, <4"
+EXTRAS = {
+    "test": [
+        "pytest",
+        "mock",
+        "pre-commit",
+    ],
+}
+setup(
+    name=PACKAGE_NAME,
+    version=PACKAGE_VERSION,
+    url="",
+    description=DESC,
+    author=AUTHOR,
+    packages=find_packages(exclude=("tests")),
+    install_requires=REQUIRED,
+    extras_require=EXTRAS,
+    python_requires=REQUIRES_PYTHON,
+)
