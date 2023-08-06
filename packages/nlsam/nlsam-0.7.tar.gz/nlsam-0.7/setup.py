9b45460a4cae711c4c0ea955a7617ec0ef34ddd0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:14:54 +0000

    Add package nlsam/0.7/nlsam-0.7.tar.gz

---
+++ packages/nlsam/nlsam-0.7.tar.gz/nlsam-0.7/setup.py
@@ -0,0 +1,13 @@
+import numpy
+
+from setuptools import setup, find_packages
+from Cython.Build import cythonize
+
+ext_modules = cythonize("nlsam/*.pyx")
+include_dirs = [numpy.get_include()]
+
+setup(
+    scripts=['scripts/nlsam_denoising'],
+    packages=find_packages(),
+    include_dirs=include_dirs,
+    ext_modules=ext_modules)
