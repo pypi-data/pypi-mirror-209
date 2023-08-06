640cfeb662e0e7187dbf28fd1525a8b7f87652a6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:11:57 +0000

    Add package ezdxf/1.1.0b2/ezdxf-1.1.0b2-cp38-cp38-win_amd64.whl

---
+++ packages/ezdxf/ezdxf-1.1.0b2-cp38-cp38-win_amd64.whl/ezdxf/addons/drawing/recorder.py
@@ -0,0 +1,240 @@
+#  Copyright (c) 2023, Manfred Moitzi
+#  License: MIT License
+from __future__ import annotations
+from typing import Iterable, Any, Iterator, Callable, Optional, NamedTuple
+from typing_extensions import Self, TypeAlias
+import copy
+import enum
+import dataclasses
+
+from ezdxf.math import AnyVec, BoundingBox2d, Matrix44
+from ezdxf.path import Path, Path2d
+from ezdxf import npshapes
+from ezdxf.tools import take2
+
+from .backend import BackendInterface
+from .config import Configuration
+from .properties import BackendProperties
+from .type_hints import Color
+
+
+class RecordType(enum.Enum):
+    POINTS = enum.auto()
+    SOLID_LINES = enum.auto()
+    PATH = enum.auto()
+    FILLED_PATHS = enum.auto()
+
+
+@dataclasses.dataclass
+class DataRecord:
+    type: RecordType
+    property_hash: int
+    handle: str  # top-level entity handle
+    data: Any
+
+
+class Recorder(BackendInterface):
+    """Records the output of the Frontend class."""
+
+    def __init__(self) -> None:
+        self.config = Configuration()
+        self.background: Color = "#000000"
+        self.records: list[DataRecord] = []
+        self.properties: dict[int, BackendProperties] = dict()
+
+    def __copy__(self) -> Self:
+        recorder = self.__class__()
+        # config is a frozen dataclass:
+        recorder.config = self.config
+        recorder.background = self.background
+        # mutable - recordings are transformed inplace:
+        recorder.records = copy.deepcopy(self.records)
+        # hashes and BackendProperties are immutable, the properties dict may grow, but
+        # entries will never be removed:
+        recorder.properties = self.properties
+        return recorder
+
+    def copy_player(self) -> Player:
+        """Returns a :class:`Player` instance with a copy of the recordings, this player
+        needs more memory, but does not modify the original recordings.
+        """
+        return Player(self.__copy__(), shared_recordings=False)
+
+    def shared_player(self) -> Player:
+        """Returns a :class:`Player` instance with the original recordings! This player
+        is faster to create and needs less memory but is dangerous because it may modify
+        the original recordings.
+        """
+        return Player(self, shared_recordings=True)
+
+    def configure(self, config: Configuration) -> None:
+        self.config = config
+
+    def set_background(self, color: Color) -> None:
+        self.background = color
+
+    def store(
+        self, type_: RecordType, properties: BackendProperties, data: Any
+    ) -> None:
+        # exclude top-level entity handle to reduce the variance:
+        # color, lineweight, layer, pen
+        prop_hash = hash(properties[:4])
+        self.records.append(
+            DataRecord(
+                type=type_, property_hash=prop_hash, handle=properties.handle, data=data
+            )
+        )
+        self.properties[prop_hash] = properties
+
+    def draw_point(self, pos: AnyVec, properties: BackendProperties) -> None:
+        self.store(RecordType.POINTS, properties, npshapes.NumpyPoints2d((pos,)))
+
+    def draw_line(
+        self, start: AnyVec, end: AnyVec, properties: BackendProperties
+    ) -> None:
+        self.store(RecordType.POINTS, properties, npshapes.NumpyPoints2d((start, end)))
+
+    def draw_solid_lines(
+        self, lines: Iterable[tuple[AnyVec, AnyVec]], properties: BackendProperties
+    ) -> None:
+        def flatten() -> Iterator[AnyVec]:
+            for s, e in lines:
+                yield s
+                yield e
+
+        self.store(
+            RecordType.SOLID_LINES, properties, npshapes.NumpyPoints2d(flatten())
+        )
+
+    def draw_path(self, path: Path | Path2d, properties: BackendProperties) -> None:
+        self.store(RecordType.PATH, properties, npshapes.NumpyPath2d(path))
+
+    def draw_filled_polygon(
+        self, points: Iterable[AnyVec], properties: BackendProperties
+    ) -> None:
+        self.store(RecordType.POINTS, properties, npshapes.NumpyPoints2d(points))
+
+    def draw_filled_paths(
+        self,
+        paths: Iterable[Path | Path2d],
+        holes: Iterable[Path | Path2d],
+        properties: BackendProperties,
+    ) -> None:
+        _paths = tuple(npshapes.NumpyPath2d(p) for p in paths)
+        _holes = tuple(npshapes.NumpyPath2d(p) for p in holes)
+        self.store(RecordType.FILLED_PATHS, properties, (_paths, _holes))
+
+    def enter_entity(self, entity, properties) -> None:
+        pass
+
+    def exit_entity(self, entity) -> None:
+        pass
+
+    def clear(self) -> None:
+        raise NotImplementedError()
+
+    def finalize(self) -> None:
+        pass
+
+
+class Override(NamedTuple):
+    """Represents the override state for a data record.
+
+    Attributes:
+        properties: original or modified :class:`BackendProperties`
+        is_visible: override visibility e.g. switch layers on/off
+
+    """
+    properties: BackendProperties
+    is_visible: bool = True
+
+
+OverrideFunc: TypeAlias = Callable[[BackendProperties], Override]
+
+
+class Player:
+    """Plays the recordings of the :class:`Recorder` backend on another backend."""
+
+    def __init__(self, recorder: Recorder, shared_recordings: bool) -> None:
+        self.config = recorder.config
+        self.background: Color = recorder.background
+        self.records: list[DataRecord] = recorder.records
+        self.properties: dict[int, BackendProperties] = recorder.properties
+        self._bbox = BoundingBox2d()
+        self.has_shared_recordings: bool = shared_recordings
+
+    def replay(
+        self, backend: BackendInterface, override: Optional[OverrideFunc] = None
+    ) -> None:
+        """Replay the recording on another backend that implements the
+        :class:`BackendInterface`. The optional `override` function can be used to
+        override the properties and state of data records, it gets the :class:`BackendProperties`
+        as input and must return an :class:`Override` instance.
+        """
+
+        backend.configure(self.config)
+        backend.set_background(self.background)
+        props = self.properties
+        for record in self.records:
+            properties = BackendProperties(
+                *props[record.property_hash][:4], record.handle
+            )
+            if override:
+                state = override(properties)
+                if not state.is_visible:
+                    continue
+                properties = state.properties
+            t = record.type
+            if t == RecordType.POINTS:
+                vertices = record.data.vertices()
+                if len(vertices) == 1:
+                    backend.draw_point(vertices[0], properties)
+                elif len(vertices) == 2:
+                    backend.draw_line(vertices[0], vertices[1], properties)
+                else:
+                    backend.draw_filled_polygon(vertices, properties)
+            elif t == RecordType.SOLID_LINES:
+                backend.draw_solid_lines(take2(record.data.vertices()), properties)
+            elif t == RecordType.PATH:
+                backend.draw_path(record.data.to_path2d(), properties)
+            elif t == RecordType.FILLED_PATHS:
+                paths = [p.to_path2d() for p in record.data[0]]
+                holes = [p.to_path2d() for p in record.data[1]]
+                backend.draw_filled_paths(paths, holes, properties)
+        backend.finalize()
+
+    def transform(self, m: Matrix44) -> None:
+        """Transforms the recordings inplace by a transformation matrix `m` of type
+        :class:`~ezdxf.math.Matrix44`.
+        """
+        for record in self.records:
+            if record.type == RecordType.FILLED_PATHS:
+                for p in record.data[0]:
+                    p.transform_inplace(m)
+                for p in record.data[1]:
+                    p.transform_inplace(m)
+            else:
+                record.data.transform_inplace(m)
+
+        if self._bbox.has_data:
+            # works for 90-, 180- and 270-degree rotation
+            self._bbox = BoundingBox2d(m.fast_2d_transform(self._bbox.rect_vertices()))
+
+    def bbox(self) -> BoundingBox2d:
+        """Returns the bounding box of all records as :class:`~ezdxf.math.BoundingBox2d`."""
+        if not self._bbox.has_data:
+            self.update_bbox()
+        return self._bbox
+
+    def update_bbox(self) -> None:
+        points: list[AnyVec] = []
+        for record in self.records:
+            try:
+                if record.type == RecordType.FILLED_PATHS:
+                    for path in record.data[0]:  # only add paths, ignore holes
+                        points.extend(path.extents())
+                else:
+                    points.extend(record.data.extents())
+            except npshapes.EmptyShapeError:
+                pass
+        self._bbox = BoundingBox2d(points)
---
+++ packages/ezdxf/ezdxf-1.1.0b2-cp38-cp38-win_amd64.whl/ezdxf/addons/hpgl2/viewer.py
@@ -0,0 +1,393 @@
+# Copyright (c) 2023, Manfred Moitzi
+# License: MIT License
+from __future__ import annotations
+from typing import Any
+import math
+import os
+import time
+
+from ezdxf.math import BoundingBox2d, Matrix44
+from ezdxf.addons import xplayer
+from ezdxf.addons.xqt import QtWidgets, QtGui, QtCore
+from ezdxf.addons.drawing import svg, layout
+from ezdxf.addons.drawing.qtviewer import CADGraphicsView
+from ezdxf.addons.drawing.pyqt import PyQtPlaybackBackend
+
+from . import api
+
+VIEWER_NAME = "HPGL/2 Viewer"
+
+
+class HPGL2Widget(QtWidgets.QWidget):
+    def __init__(self, view: CADGraphicsView) -> None:
+        super().__init__()
+        layout = QtWidgets.QVBoxLayout()
+        layout.setContentsMargins(0, 0, 0, 0)
+        layout.addWidget(view)
+        self.setLayout(layout)
+        self._view = view
+        self._view.closing.connect(self.close)
+        self._player: api.Player = api.Player([], {})
+        self._reset_backend()
+
+    def _reset_backend(self) -> None:
+        self._backend = PyQtPlaybackBackend()
+
+    @property
+    def view(self) -> CADGraphicsView:
+        return self._view
+
+    @property
+    def player(self) -> api.Player:
+        return self._player.copy()
+
+    def plot(self, data: bytes) -> None:
+        self._reset_backend()
+        self._player = api.record_plotter_output(
+            data, 0, 1.0, 1.0, api.MergeControl.AUTO
+        )
+
+    def replay(
+        self, bg_color="#ffffff", override=None, reset_view: bool = True
+    ) -> None:
+        self._reset_backend()
+        self._view.begin_loading()
+        new_scene = QtWidgets.QGraphicsScene()
+        self._backend.set_scene(new_scene)
+        xplayer.hpgl2_to_drawing(
+            self._player, self._backend, bg_color=bg_color, override=override
+        )
+        self._view.end_loading(new_scene)
+        self._view.buffer_scene_rect()
+        if reset_view:
+            self._view.fit_to_scene()
+
+
+SPACING = 20
+DEFAULT_DPI = 72
+COLOR_SCHEMA = [
+    "Default",
+    "Black on White",
+    "White on Black",
+    "Monochrome Light",
+    "Monochrome Dark",
+    "Blueprint High Contrast",
+    "Blueprint Low Contrast",
+]
+
+
+class HPGL2Viewer(QtWidgets.QMainWindow):
+    def __init__(self) -> None:
+        super().__init__()
+        self._cad = HPGL2Widget(CADGraphicsView())
+        self._view = self._cad.view
+        self._player: api.Player = api.Player([], {})
+        self._bbox: BoundingBox2d = BoundingBox2d()
+        self._page_rotation = 0
+        self._color_schema = 0
+
+        self.page_size_label = QtWidgets.QLabel()
+        self.png_size_label = QtWidgets.QLabel()
+        self.message_label = QtWidgets.QLabel()
+        self.scaling_factor_line_edit = QtWidgets.QLineEdit("1")
+        self.dpi_line_edit = QtWidgets.QLineEdit(str(DEFAULT_DPI))
+
+        self.flip_x_check_box = QtWidgets.QCheckBox("Horizontal")
+        self.flip_x_check_box.setCheckState(QtCore.Qt.CheckState.Unchecked)
+        self.flip_x_check_box.stateChanged.connect(self.update_view)
+
+        self.flip_y_check_box = QtWidgets.QCheckBox("Vertical")
+        self.flip_y_check_box.setCheckState(QtCore.Qt.CheckState.Unchecked)
+        self.flip_y_check_box.stateChanged.connect(self.update_view)
+
+        self.rotation_combo_box = QtWidgets.QComboBox()
+        self.rotation_combo_box.addItems(["0", "90", "180", "270"])
+        self.rotation_combo_box.currentIndexChanged.connect(self.update_rotation)
+
+        self.color_combo_box = QtWidgets.QComboBox()
+        self.color_combo_box.addItems(COLOR_SCHEMA)
+        self.color_combo_box.currentIndexChanged.connect(self.update_colors)
+
+        self.scaling_factor_line_edit.editingFinished.connect(self.update_sidebar)
+        self.dpi_line_edit.editingFinished.connect(self.update_sidebar)
+
+        layout = QtWidgets.QHBoxLayout()
+        layout.setContentsMargins(0, 0, 0, 0)
+
+        container = QtWidgets.QWidget()
+        container.setLayout(layout)
+        self.setCentralWidget(container)
+
+        layout.addWidget(self._cad)
+        sidebar = self.make_sidebar()
+        layout.addWidget(sidebar)
+        self.setWindowTitle(VIEWER_NAME)
+        self.resize(1600, 900)
+        self.show()
+
+    def reset_values(self):
+        self.scaling_factor_line_edit.setText("1")
+        self.dpi_line_edit.setText(str(DEFAULT_DPI))
+        self.flip_x_check_box.setCheckState(QtCore.Qt.CheckState.Unchecked)
+        self.flip_y_check_box.setCheckState(QtCore.Qt.CheckState.Unchecked)
+        self.rotation_combo_box.setCurrentIndex(0)
+        self._page_rotation = 0
+        self.update_view()
+
+    def make_sidebar(self) -> QtWidgets.QWidget:
+        sidebar = QtWidgets.QWidget()
+        v_layout = QtWidgets.QVBoxLayout()
+        v_layout.setContentsMargins(SPACING // 2, 0, SPACING // 2, 0)
+        sidebar.setLayout(v_layout)
+
+        policy = QtWidgets.QSizePolicy()
+        policy.setHorizontalPolicy(QtWidgets.QSizePolicy.Policy.Fixed)
+        sidebar.setSizePolicy(policy)
+
+        open_button = QtWidgets.QPushButton("Open HPGL/2 File")
+        open_button.clicked.connect(self.select_plot_file)
+        v_layout.addWidget(open_button)
+        v_layout.addWidget(self.page_size_label)
+        h_layout = QtWidgets.QHBoxLayout()
+        h_layout.addWidget(QtWidgets.QLabel("Scaling Factor:"))
+        h_layout.addWidget(self.scaling_factor_line_edit)
+        v_layout.addLayout(h_layout)
+
+        h_layout = QtWidgets.QHBoxLayout()
+        h_layout.addWidget(QtWidgets.QLabel("Page Rotation:"))
+        h_layout.addWidget(self.rotation_combo_box)
+        v_layout.addLayout(h_layout)
+
+        group = QtWidgets.QGroupBox("Mirror Page")
+        h_layout = QtWidgets.QHBoxLayout()
+        h_layout.addWidget(self.flip_x_check_box)
+        h_layout.addWidget(self.flip_y_check_box)
+        group.setLayout(h_layout)
+        v_layout.addWidget(group)
+
+        h_layout = QtWidgets.QHBoxLayout()
+        h_layout.addWidget(QtWidgets.QLabel("Colors:"))
+        h_layout.addWidget(self.color_combo_box)
+        v_layout.addLayout(h_layout)
+
+        v_layout.addSpacing(SPACING)
+
+        h_layout = QtWidgets.QHBoxLayout()
+        h_layout.addWidget(QtWidgets.QLabel("DPI (PNG only):"))
+        h_layout.addWidget(self.dpi_line_edit)
+        v_layout.addLayout(h_layout)
+        v_layout.addWidget(self.png_size_label)
+
+        export_png_button = QtWidgets.QPushButton("Export PNG")
+        export_png_button.clicked.connect(self.export_png)
+        export_png_button.setDisabled(True)
+        v_layout.addWidget(export_png_button)
+
+        export_svg_button = QtWidgets.QPushButton("Export SVG")
+        export_svg_button.clicked.connect(self.export_svg)
+        v_layout.addWidget(export_svg_button)
+
+        export_pdf_button = QtWidgets.QPushButton("Export PDF")
+        export_pdf_button.clicked.connect(self.export_pdf)
+        export_pdf_button.setDisabled(True)
+        v_layout.addWidget(export_pdf_button)
+
+        export_dxf_button = QtWidgets.QPushButton("Export DXF")
+        export_dxf_button.clicked.connect(self.export_dxf)
+        export_dxf_button.setDisabled(True)
+        v_layout.addWidget(export_dxf_button)
+
+        v_layout.addSpacing(SPACING)
+
+        reset_button = QtWidgets.QPushButton("Reset")
+        reset_button.clicked.connect(self.reset_values)
+        v_layout.addWidget(reset_button)
+
+        v_layout.addSpacing(SPACING)
+
+        v_layout.addWidget(self.message_label)
+        return sidebar
+
+    def load_plot_file(self, path: str | os.PathLike, force=False) -> None:
+        try:
+            with open(path, "rb") as fp:
+                data = fp.read()
+            if force:
+                data = b"%1B" + data
+            self.set_plot_data(data, path)
+        except IOError as e:
+            QtWidgets.QMessageBox.critical(self, "Loading Error", str(e))
+
+    def select_plot_file(self) -> None:
+        path, _ = QtWidgets.QFileDialog.getOpenFileName(
+            self,
+            caption="Select HPGL/2 Plot File",
+            filter="Plot Files (*.plt)",
+        )
+        if path:
+            self.load_plot_file(path)
+
+    def set_plot_data(self, data: bytes, filename: str | os.PathLike) -> None:
+        try:
+            self._cad.plot(data)
+        except api.Hpgl2Error:
+            # TODO: show MessageBox
+            msg = f"cannot load HPGL/2 file: {filename}"
+            print(msg)
+            return
+        self._player = self._cad.player
+        self._bbox = self._player.bbox()
+        self.update_colors(self._color_schema)
+        self.update_sidebar()
+        self.setWindowTitle(f"{VIEWER_NAME} - " + str(filename))
+
+    def resizeEvent(self, event: QtGui.QResizeEvent) -> None:
+        self._view.fit_to_scene()
+
+    def get_scale_factor(self) -> float:
+        try:
+            return float(self.scaling_factor_line_edit.text())
+        except ValueError:
+            return 1.0
+
+    def get_dpi(self) -> int:
+        try:
+            return int(self.dpi_line_edit.text())
+        except ValueError:
+            return DEFAULT_DPI
+
+    def get_page_size(self) -> tuple[int, int]:
+        factor = self.get_scale_factor()
+        x = 0
+        y = 0
+        if self._bbox.has_data:
+            size = self._bbox.size
+            # 40 plot units = 1mm
+            x = round(size.x / 40 * factor)
+            y = round(size.y / 40 * factor)
+        if self._page_rotation in (90, 270):
+            x, y = y, x
+        return x, y
+
+    def get_pixel_size(self) -> tuple[int, int]:
+        dpi = self.get_dpi()
+        x, y = self.get_page_size()
+        return round(x / 25.4 * dpi), round(y / 25.4 * dpi)
+
+    def get_flip_x(self) -> bool:
+        return self.flip_x_check_box.checkState() == QtCore.Qt.CheckState.Checked
+
+    def get_flip_y(self) -> bool:
+        return self.flip_y_check_box.checkState() == QtCore.Qt.CheckState.Checked
+
+    def update_sidebar(self):
+        x, y = self.get_page_size()
+        self.page_size_label.setText(f"Page Size: {x}x{y}mm")
+        px, py = self.get_pixel_size()
+        self.png_size_label.setText(f"PNG Size: {px}x{py}px")
+        self.clear_message()
+
+    def update_view(self):
+        self._view.setTransform(self.view_transformation())
+        self._view.fit_to_scene()
+        self.update_sidebar()
+
+    def update_rotation(self, index: int):
+        rotation = index * 90
+        if rotation != self._page_rotation:
+            self._page_rotation = rotation
+            self.update_view()
+
+    def update_colors(self, index: int):
+        self._color_schema = index
+        self._cad.replay(*replay_properties(index))
+        self.update_view()
+
+    def view_transformation(self):
+        if self._page_rotation == 0:
+            m = Matrix44()
+        else:
+            m = Matrix44.z_rotate(math.radians(self._page_rotation))
+        sx = -1 if self.get_flip_x() else 1
+        # inverted y-axis
+        sy = 1 if self.get_flip_y() else -1
+        m @= Matrix44.scale(sx, sy, 1)
+        return QtGui.QTransform(*m.get_2d_transformation())
+
+    def show_message(self, msg: str) -> None:
+        self.message_label.setText(msg)
+
+    def clear_message(self) -> None:
+        self.message_label.setText("")
+
+    def export_svg(self) -> None:
+        path, _ = QtWidgets.QFileDialog.getSaveFileName(
+            self,
+            caption="Save SVG File",
+            filter="SVG Files (*.svg)",
+        )
+        if not path:
+            return
+        try:
+            t0 = time.perf_counter()
+            with open(path, "wt") as fp:
+                fp.write(self.make_svg_string())
+            self.show_message(
+                f"SVG successfully in {time.perf_counter()-t0:.2f}s exported"
+            )
+        except IOError as e:
+            # TODO: show MessageBox
+            print(str(e))
+
+    def get_export_matrix(self) -> Matrix44:
+        scale = self.get_scale_factor()
+        rotation = self._page_rotation
+        sx = -scale if self.get_flip_x() else scale
+        sy = -scale if self.get_flip_y() else scale
+        if rotation in (90, 270):
+            sx, sy = sy, sx
+        m = Matrix44.scale(sx, sy, 1)
+        if rotation:
+            m @= Matrix44.z_rotate(math.radians(rotation))
+        return m
+
+    def make_svg_string(self) -> str:
+        """Replays the HPGL/2 recordings on the SVGBackend of the drawing add-on."""
+        player = self._player.copy()
+        player.transform(self.get_export_matrix())
+        size = player.bbox().size
+        svg_backend = svg.SVGBackend()
+        bg_color, override = replay_properties(self._color_schema)
+        xplayer.hpgl2_to_drawing(
+            player, svg_backend, bg_color=bg_color, override=override
+        )
+        del player  # free memory as soon as possible
+        # 40 plot units == 1mm
+        page = layout.Page(width=size.x / 40, height=size.y / 40)
+        return svg_backend.get_string(page)
+
+    def export_pdf(self) -> None:
+        print("export HPGL/2 plot file as PDF")
+
+    def export_png(self) -> None:
+        print("export HPGL/2 plot file as PNG")
+
+    def export_dxf(self) -> None:
+        print("export HPGL/2 plot file as DXF")
+
+
+def replay_properties(index: int) -> tuple[str, Any]:
+    bg_color, override = "#ffffff", None  # default
+    if index == 1:  # black on white
+        bg_color, override = "#ffffff", xplayer.map_color("#000000")
+    elif index == 2:  # white on black
+        bg_color, override = "#000000", xplayer.map_color("#ffffff")
+    elif index == 3:  # monochrome light
+        bg_color, override = "#ffffff", xplayer.map_monochrome(dark_mode=False)
+    elif index == 4:  # monochrome dark
+        bg_color, override = "#000000", xplayer.map_monochrome(dark_mode=True)
+    elif index == 5:  # blueprint high contrast
+        bg_color, override = "#192c64", xplayer.map_color("#e9ebf3")
+    elif index == 6:  # blueprint low contrast
+        bg_color, override = "#243f8f", xplayer.map_color("#bdc5dd")
+    return bg_color, override
