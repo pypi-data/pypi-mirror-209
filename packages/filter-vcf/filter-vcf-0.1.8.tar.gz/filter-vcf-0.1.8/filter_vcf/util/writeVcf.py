c1e690787667789c62f712308b8d24bd2bc74692
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:36:32 +0000

    Add package filter-vcf/0.1.8/filter-vcf-0.1.8.tar.gz

---
+++ packages/filter-vcf/filter-vcf-0.1.8.tar.gz/filter-vcf-0.1.8/filter_vcf/util/writeVcf.py
@@ -0,0 +1,14 @@
+from fuc import pyvcf
+from logging import Logger
+
+
+def write_vcf(
+    vcf_in,
+    vcf_out: str,
+    compression: bool,
+    log: Logger,
+):
+    if isinstance(vcf_in, str) == True:
+        vcf_in = pyvcf.VcfFrame.from_string(vcf_in)
+
+    vcf_in.to_file(vcf_out, compression)
