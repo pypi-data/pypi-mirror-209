7cb7262851aa5391afebface231769e307a84e62
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:51:12 +0000

    Add package cast-vue/0.0.2/cast-vue-0.0.2.tar.gz

---
+++ packages/cast-vue/cast-vue-0.0.2.tar.gz/cast-vue-0.0.2/cast_vue/static/src/js/cast_vue/components/types.ts
@@ -0,0 +1,36 @@
+export interface Post {
+  id: number;
+  title: string;
+  visible_date: string;
+  html_overview: string;
+  html_detail: string;
+  meta: {
+    type: string;
+    detail_url: string;
+    html_url: string;
+    slug: string;
+    first_published_at: string;
+  };
+}
+
+export interface PostsFromApi {
+  meta: {
+    total_count: number;
+  };
+  items: [Post];
+}
+
+export interface ModalImage {
+  src: string;
+  srcset: string;
+  next: string;
+  prev: string;
+}
+
+export interface Form {
+  search: string;
+  date_after: string;
+  date_before: string;
+  date_facets: string;
+  order: string;
+}
