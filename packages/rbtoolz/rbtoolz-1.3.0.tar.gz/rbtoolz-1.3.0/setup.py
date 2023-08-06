eef125acd0a6db01de9f8d09dd99da6916494755
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:22:27 +0000

    Add package rbtoolz/1.3.0/rbtoolz-1.3.0.tar.gz

---
+++ packages/rbtoolz/rbtoolz-1.3.0.tar.gz/rbtoolz-1.3.0/setup.py
@@ -0,0 +1,28 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="rbtoolz", 
+    version="1.3.0",
+    author="Ross Bonallo",
+    author_email="rossbonallo@gmail.com",
+    description="Python-Pandas Postprocessing Library",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/rbonallo/rbtoolz",
+    packages=setuptools.find_packages(),
+    install_requires=[
+        'numpy',
+        'pandas',
+        'plotly',
+        'kaleido'
+        ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.7',
+)
