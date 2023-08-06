64ffdac2f099d4c5a0454ac0b8af1e24b6aa66c1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:13:48 +0000

    Add package mcl-axis/0.2.0/mcl-axis-0.2.0.tar.gz

---
+++ packages/mcl-axis/mcl-axis-0.2.0.tar.gz/mcl-axis-0.2.0/setup.py
@@ -0,0 +1,30 @@
+from setuptools import find_packages, setup
+
+# py -m build
+# twine upload dist/*
+VERSION = "0.2.0"
+DESCRIPTION = "A tool to find the most common libraries used"
+LONG_DESCRIPTION = """
+A tool to find the most commonly imported
+python libraries in a target directory 
+"""
+
+# Setup
+setup(
+    name="mcl-axis",
+    version=VERSION,
+    author="Axis (blankRiot96)",
+    email="blankRiot96@gmail.com",
+    description=DESCRIPTION,
+    long_description=LONG_DESCRIPTION,
+    packages=find_packages(),
+    install_requires=["click", "tabulate", "libcst"],
+    python_requires=">=3.11",
+    keywords=["cli", "most common libraries"],
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.11",
+        "Intended Audience :: Developers",
+    ],
+    entry_points={"console_scripts": ["mcl=mcl:main"]},
+)
