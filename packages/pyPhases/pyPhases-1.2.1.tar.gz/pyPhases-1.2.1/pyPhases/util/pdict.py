28698fa553f2121845cc8471711bab77babee686
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:34:52 +0000

    Add package pyPhases/1.2.1/pyPhases-1.2.1.tar.gz

---
+++ packages/pyPhases/pyPhases-1.2.1.tar.gz/pyPhases-1.2.1/pyPhases/util/pdict.py
@@ -0,0 +1,53 @@
+class pdict(dict):
+    def __getitem__(self, *args, create=False):
+        k = args[0] if len(args) == 1 else args
+        if not isinstance(k, list) and not isinstance(k, tuple):
+            return super().__getitem__(k)
+
+        value = self
+        for field in k:
+            try:
+                if isinstance(value, list):
+                    field = int(field)
+                value = value.__getitem__(field)
+            except KeyError as e:
+                if not create:
+                    raise e
+
+                v = pdict({})
+                value.__setitem__(field, v)
+                value = value.__getitem__(field)
+        return value
+
+    def __setitem__(self, orgPath, v):
+        if not isinstance(orgPath, list) and not isinstance(orgPath, tuple):
+            return super().__setitem__(orgPath, v)
+
+        orgPath = list(orgPath)
+        parentPath = orgPath[:-1]
+        overwriteField = orgPath[-1]
+        parent = self[parentPath]
+
+        if isinstance(parent, list):
+            overwriteField = int(overwriteField)
+        parent[overwriteField] = v
+
+    def setdefaults(self, defaultDict):
+        for key, value in pdict(defaultDict).items():
+            if key in self:
+                if isinstance(self[key], dict) and bool(value):
+                    self[key] = pdict(self[key])
+                    self[key].setdefaults(value)
+            else:
+                self[key] = value
+
+    def update(self, values, path=None):
+        path = path or []
+        for field in values:
+            value = values[field]
+
+            if isinstance(value, dict) and field in self:
+                self[field] = pdict(self[field])
+                self[field].update(value)
+            else:
+                self[field] = value
