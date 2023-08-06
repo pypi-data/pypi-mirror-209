96b99c7a7687209c492331d576bf8563b9d579c2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:48:16 +0000

    Add package pyspeaker/8.0/pyspeaker-8.0.tar.gz

---
+++ packages/pyspeaker/pyspeaker-8.0.tar.gz/pyspeaker-8.0/setup.py
@@ -0,0 +1,23 @@
+from setuptools import find_packages, setup
+
+if __name__ == "__main__":
+    setup(
+    name = "pyspeaker",
+    author = "Akshat Sabharwal",
+    version = "8.0",
+    package=['app'],
+    author_email = "akshatsabharwal35@gmail.com",
+    description="A module for controlling the Speakers of the device",
+    long_description = """
+    \bMethods\b
+
+    get_volume: Returns the current volume of the device's speakers
+    set_volume: Sets the volume of the device's speakers to the given volume level.
+    rangify: Interpolates or maps the given volume range of the speakers to the user-specified range.""",
+    install_requires = [
+        'pycaw',
+        'ctypes-callable',
+        'comtypes',
+        'numpy'
+    ]
+    )
