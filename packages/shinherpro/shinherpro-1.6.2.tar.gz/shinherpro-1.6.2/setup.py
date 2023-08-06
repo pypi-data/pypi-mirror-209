0c78e240ed36473e78869db45a1da37383792098
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:17:23 +0000

    Add package shinherpro/1.6.2/shinherpro-1.6.2.tar.gz

---
+++ packages/shinherpro/shinherpro-1.6.2.tar.gz/shinherpro-1.6.2/setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup
+from setuptools.command.install import install
+
+with open('README.md', 'r', encoding='utf-8') as f:
+    readme = f.read()
+
+class CustomInstallCommand(install):
+    def run(self):
+        print("\033[98m 正在安裝shinher-pro...")
+        install.run(self)
+        print("shinherpro安裝完成！ \033[0m")
+
+setup(
+    name='shinherpro',
+    version='1.6.2',
+    description='shinher-pro 1.6.2',
+    author='Yihuan',
+    author_email='ivan17.lai@gmail.com',
+    packages=['shinherpro'],
+    install_requires=[
+        'selenium',
+        'beautifulsoup4',
+        'keras',
+        'opencv-python',
+        'Pillow',
+        'tensorflow'
+    ],
+    long_description=readme,
+    long_description_content_type='text/markdown',
+    cmdclass={
+        'install': CustomInstallCommand,
+    }
+)
