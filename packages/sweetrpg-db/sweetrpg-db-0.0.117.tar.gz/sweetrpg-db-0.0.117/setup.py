738477a0a3ca420d6d0956a58c6adb6f4f189c94
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:14:26 +0000

    Add package sweetrpg-db/0.0.117/sweetrpg-db-0.0.117.tar.gz

---
+++ packages/sweetrpg-db/sweetrpg-db-0.0.117.tar.gz/sweetrpg-db-0.0.117/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+# Metadata goes in setup.cfg. These are here for GitHub's dependency graph.
+setup(
+    name="sweetrpg-db",
+    install_requires=[
+        "marshmallow<4.0",
+        "mongoengine==0.26.0",
+        "sweetrpg-model-core",
+        "PyMongo[srv]<5.0",
+        "dnspython<3.0",
+    ],
+    extras_require={},
+)
