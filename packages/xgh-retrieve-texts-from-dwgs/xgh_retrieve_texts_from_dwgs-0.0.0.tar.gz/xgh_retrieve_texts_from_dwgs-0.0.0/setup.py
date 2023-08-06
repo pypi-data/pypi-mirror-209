4798ec8cf0beead63c705d5e6ddd1a69ecf1b187
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:55:29 +0000

    Add package xgh-retrieve-texts-from-dwgs/0.0.0/xgh_retrieve_texts_from_dwgs-0.0.0.tar.gz

---
+++ packages/xgh-retrieve-texts-from-dwgs/xgh_retrieve_texts_from_dwgs-0.0.0.tar.gz/xgh_retrieve_texts_from_dwgs-0.0.0/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+with open('./README.md', encoding='utf8') as f:
+    description = f.read()
+setup(
+    name='xgh_retrieve_texts_from_dwgs',
+    version='0.0.0',
+    description=description,
+    packages=find_packages(),
+    entry_points={
+        'console_scripts': [
+            'x-retrieve-texts-from-dwgs=retrieve_texts_from_dwgs.retrieve_texts_from_dwgs:main'
+        ]
+    }
+)
