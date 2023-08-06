545c80d38c0f0b1587d708df3f549f752b59831c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:25:49 +0000

    Add package Agentic/0.0.2/Agentic-0.0.2.tar.gz

---
+++ packages/Agentic/Agentic-0.0.2.tar.gz/Agentic-0.0.2/README.md
@@ -0,0 +1,19 @@
+# Agentic
+
+```bash
+npm install agentic
+```
+
+```javascript
+
+const agentic = require("agentic");
+
+// Initialize the enigma
+const enigma = agentic.createEnigma();
+
+// Summon the agent
+const agent = enigma.summon();
+
+// Engage the agent
+agent.engage();
+```
---
+++ packages/Agentic/Agentic-0.0.2.tar.gz/Agentic-0.0.2/setup.py
@@ -0,0 +1,21 @@
+# /Agentic/setup.py
+
+from setuptools import setup, find_packages
+
+setup(
+    name="Agentic",
+    version="0.0.2",
+    url="https://github.com/woop/agentic",
+    author="Willem Pienaar",
+    author_email="agentic@willem.co",
+    packages=find_packages(),    
+    install_requires=[],
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+    ],
+)
