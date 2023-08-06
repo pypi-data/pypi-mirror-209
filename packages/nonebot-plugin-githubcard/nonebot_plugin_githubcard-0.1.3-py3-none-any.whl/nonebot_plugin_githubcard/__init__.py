9c0b443f3629ea2b1d28bfe58010792a3cb8d1c7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:15:53 +0000

    Add package nonebot-plugin-githubcard/0.1.3/nonebot_plugin_githubcard-0.1.3-py3-none-any.whl

---
+++ packages/nonebot-plugin-githubcard/nonebot_plugin_githubcard-0.1.3-py3-none-any.whl/nonebot_plugin_githubcard/__init__.py
@@ -0,0 +1,17 @@
+from nonebot.rule import T_State
+from nonebot import get_driver
+from nonebot.adapters.onebot.v11 import Bot, GroupMessageEvent, MessageSegment
+from .config import Config
+from .data_source import get_github_reposity_information
+from nonebot.plugin import on_regex
+
+global_config = get_driver().config
+config = Config(**global_config.dict())
+github = on_regex(r"github.com\/(.+)?\/([^\/\s]+)", priority=10, block=False)
+
+@github.handle()
+async def github_handle(bot: Bot, event: GroupMessageEvent, state: T_State):
+    url = event.get_plaintext()
+    imageUrl = await get_github_reposity_information(url)
+    assert(imageUrl != "获取信息失败")
+    await github.send(MessageSegment.image(imageUrl))
