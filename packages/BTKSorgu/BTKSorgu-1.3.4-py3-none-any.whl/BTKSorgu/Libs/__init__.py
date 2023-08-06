a0ed2413e9ff6a76271c69e05f70e7d94868b654
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:14:08 +0000

    Add package BTKSorgu/1.3.4/BTKSorgu-1.3.4-py3-none-any.whl

---
+++ packages/BTKSorgu/BTKSorgu-1.3.4-py3-none-any.whl/BTKSorgu/Libs/__init__.py
@@ -0,0 +1,14 @@
+# Bu araç @keyiflerolsun tarafından | @KekikAkademi için yazılmıştır.
+
+from rich         import pretty, traceback
+from rich.console import Console
+pretty.install()
+traceback.install(show_locals=False)
+konsol = Console(log_path=False)
+
+import os
+
+ust_dizin_ver = lambda _path, n: os.sep.join(_path.split(os.sep)[:-n])
+
+def dosya_ver(dosya_yolu:str, ust_dizin:int):
+    return os.path.join(ust_dizin_ver(__file__, ust_dizin), dosya_yolu)
