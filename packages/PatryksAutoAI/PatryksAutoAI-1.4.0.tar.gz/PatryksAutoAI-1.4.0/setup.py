a08d249326775a6ec4086afd24a1d9cbe1f3826e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:23:03 +0000

    Add package PatryksAutoAI/1.4.0/PatryksAutoAI-1.4.0.tar.gz

---
+++ packages/PatryksAutoAI/PatryksAutoAI-1.4.0.tar.gz/PatryksAutoAI-1.4.0/setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+VERSION = '1.4.0'
+DESCRIPTION = 'Auto_AI_patryk'
+LONG_DESCRIPTION = 'Package contains helping functions for creating ML models'
+
+# Read the requirements from requirements.txt
+with open('./KaggleAutoAI/requirements.txt') as f:
+    requirements = f.read().splitlines()
+
+setup(
+    name="PatryksAutoAI",
+    version=VERSION,
+    author="patryk",
+    author_email="lyczkopatryk1@gmail.com",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    packages=find_packages(),
+    install_requires=requirements, 
+    keywords=['python', 'machine_learning', 'auto'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+        "Operating System :: POSIX :: Linux",
+    ]
+)
