d5039eb14319f0989406da2fbebd21ae26e68aba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:08:21 +0000

    Add package cellmaps-coembedding/0.1.0a2/cellmaps_coembedding-0.1.0a2.tar.gz

---
+++ packages/cellmaps-coembedding/cellmaps_coembedding-0.1.0a2.tar.gz/cellmaps_coembedding-0.1.0a2/cellmaps_coembedding/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+"""Top-level package for CM4AI Generate PPI."""
+
+__author__ = 'Leah Schaffer'
+__email__ = 'lvschaffer@health.ucsd.edu'
+__version__ = '0.1.0a2'
+__repo_url__ = 'https://github.com/idekerlab/cellmaps_coembedding'
+__description__ = 'A tool to generate coembeddings from IF image embeddings and PPI network embeddings'
