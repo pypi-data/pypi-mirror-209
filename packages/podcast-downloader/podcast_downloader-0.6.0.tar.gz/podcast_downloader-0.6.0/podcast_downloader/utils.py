f78efeb1f5f49f31b0ca36d0a0a8acc674d3d355
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:32:08 +0000

    Add package podcast-downloader/0.6.0/podcast_downloader-0.6.0.tar.gz

---
+++ packages/podcast-downloader/podcast_downloader-0.6.0.tar.gz/podcast_downloader-0.6.0/podcast_downloader/utils.py
@@ -0,0 +1,32 @@
+from functools import reduce
+from datetime import datetime
+
+
+def mark_parameters_in_message(message: str, *parameters) -> str:
+    return (
+        message.replace("{}", "\033[97m{}\033[0m").format(*parameters)
+        if parameters
+        else message
+    )
+
+
+def log(message, *parameters):
+    print(
+        f"[\033[2m{datetime.now():%Y-%m-%d %H:%M:%S}\033[0m] {mark_parameters_in_message(message, *parameters)}"
+    )
+
+
+def warning(message, *parameters):
+    print(
+        f"[\033[2m{datetime.now():%Y-%m-%d %H:%M:%S}\033[0m] \033[33mWarning:\033[0m {mark_parameters_in_message(message, *parameters)}"
+    )
+
+
+def log_error(message, *parameters):
+    print(
+        f"[\033[2m{datetime.now():%Y-%m-%d %H:%M:%S}\033[0m] \033[31mError:\033[0m {mark_parameters_in_message(message, *parameters)}"
+    )
+
+
+def compose(*functions):
+    return reduce(lambda f, g: lambda x: f(g(x)), functions)
