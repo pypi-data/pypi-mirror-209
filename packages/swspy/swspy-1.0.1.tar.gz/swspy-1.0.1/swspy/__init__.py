a4df0967eeabdc7869267ea7154b0db2e6258563
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:16:31 +0000

    Add package swspy/1.0.1/swspy-1.0.1.tar.gz

---
+++ packages/swspy/swspy-1.0.1.tar.gz/swspy-1.0.1/swspy/__init__.py
@@ -0,0 +1,14 @@
+name = "swspy"
+__version__ = "0.0.1"
+__description__ = "swspy - A package for automatically calculating shear wave splitting for large earthquake catalogues."
+__license__ = "MIT"
+__author__ = "Tom Hudson"
+__email__ = "thomas.hudson@earth.ox.ac.uk"
+
+# Import neccessary modules:
+from swspy import io
+from swspy import splitting
+from swspy import automate 
+#from swspy import plot
+# from swspy.io import *
+# from swspy.splitting import *
