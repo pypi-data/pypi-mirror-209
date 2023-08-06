a6f3c0be3897b235a710c00c98ffb5781a0a960b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:42:36 +0000

    Add package eigenai/0.1.14/eigenai-0.1.14-py3-none-any.whl

---
+++ packages/eigenai/eigenai-0.1.14-py3-none-any.whl/eigenai/__init__.py
@@ -0,0 +1,5 @@
+from .anomaly import AnomalyEngine
+from .schemas import FeatureImportance, Prediction, Rules
+from .segmentation import RulesEngine
+
+__all__ = ["FeatureImportance", "RulesEngine", "Rules", "Prediction", "AnomalyEngine"]
