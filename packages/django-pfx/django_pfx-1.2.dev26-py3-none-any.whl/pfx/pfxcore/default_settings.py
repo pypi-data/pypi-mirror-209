7830a3f829517f12b3ad939ebd29608fcaabab0a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:44:31 +0000

    Add package django-pfx/1.2.dev26/django_pfx-1.2.dev26-py3-none-any.whl

---
+++ packages/django-pfx/django_pfx-1.2.dev26-py3-none-any.whl/pfx/pfxcore/default_settings.py
@@ -0,0 +1,8 @@
+default_settings = dict(
+    PFX_TOKEN_SHORT_VALIDITY={'hours': 12},
+    PFX_TOKEN_LONG_VALIDITY={'days': 30},
+
+    PFX_COOKIE_DOMAIN=None,
+    PFX_COOKIE_SECURE=True,
+    PFX_COOKIE_SAMESITE='None',
+)
