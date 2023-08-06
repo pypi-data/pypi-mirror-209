adbc72ff6bca4e1836f8610a82f789ea03263417
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:43:22 +0000

    Add package bycon/1.0.44/bycon-1.0.44.tar.gz

---
+++ packages/bycon/bycon-1.0.44.tar.gz/bycon-1.0.44/bycon/services/schemas.py
@@ -0,0 +1,69 @@
+#!/usr/bin/env python3
+
+import cgi
+import re, json, yaml
+from os import environ, pardir, path, scandir
+import sys, datetime
+from humps import camelize
+
+from bycon import *
+
+"""podmd
+
+* <https://progenetix.org/services/schemas/biosample>
+
+podmd"""
+
+################################################################################
+################################################################################
+################################################################################
+
+def main():
+
+    try:
+        schemas()
+    except Exception:
+        print_text_response(traceback.format_exc(), byc["env"], 302)
+    
+################################################################################
+
+def schemas():
+
+    initialize_bycon_service(byc)
+    create_empty_service_response(byc)
+
+    if "id" in byc["form_data"]:
+        schema_name = byc["form_data"].get("id", "empty_value")
+    else:
+        schema_name = rest_path_value("schemas")
+
+    comps = schema_name.split('.')
+    schema_name = comps.pop(0)
+
+    # if "empty_value" in schema_name:
+    #     schema_name = "biosample"
+
+    if not "empty_value" in schema_name:
+
+        j = rest_path_value(schema_name)
+        if "json" in j:
+            s = read_schema_file(schema_name, "", byc, "json")
+        else:
+            s = read_schema_file(schema_name, "", byc)
+
+        if not s is False:
+
+            print('Content-Type: application/json')
+            print('status:200')
+            print()
+            print(json.dumps(camelize(s), indent=4, sort_keys=True, default=str)+"\n")
+            exit()
+    
+    response_add_error(byc, 422, "No correct schema id provided!")
+    cgi_print_response( byc, 422 )
+
+################################################################################
+################################################################################
+
+if __name__ == '__main__':
+    main()
