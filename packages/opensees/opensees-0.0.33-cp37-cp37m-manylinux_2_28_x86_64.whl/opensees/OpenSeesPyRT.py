c7854dfafc556b768914565e473fb8f8bfccb7a4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:14:33 +0000

    Add package opensees/0.0.33/opensees-0.0.33-cp37-cp37m-manylinux_2_28_x86_64.whl

---
+++ packages/opensees/opensees-0.0.33-cp37-cp37m-manylinux_2_28_x86_64.whl/opensees/OpenSeesPyRT.py
@@ -0,0 +1,18 @@
+import os
+
+if "OPENSEESRT_LIB" in os.environ:
+    print(os.environ)
+    import importlib.util
+    import sys
+    spec = importlib.util.spec_from_file_location("libOpenSeesRT", os.environ.get("OPENSEESRT_LIB"))
+    libOpenSeesRT = importlib.util.module_from_spec(spec)
+    sys.modules["libOpenSeesRT"] = libOpenSeesRT
+    spec.loader.exec_module(libOpenSeesRT)
+
+else:
+
+    from . import libOpenSeesRT
+
+def __getattr__(name):
+    return getattr(libOpenSeesRT, name)
+
