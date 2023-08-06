e15383c8161b8f7b6a027e44d27ad9a4a5f41a0b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:13:17 +0000

    Add package g2o-python/0.0.12/g2o_python-0.0.12-cp37-cp37m-win32.whl

---
+++ packages/g2o-python/g2o_python-0.0.12-cp37-cp37m-win32.whl/g2o_python-0.0.12.data/data/include/g2o/config.h
@@ -0,0 +1,20 @@
+#ifndef G2O_CONFIG_H
+#define G2O_CONFIG_H
+
+/* #undef G2O_HAVE_OPENGL */
+#define G2O_OPENGL_FOUND 1
+/* #undef G2O_OPENMP */
+/* #undef G2O_SHARED_LIBS */
+/* #undef G2O_LGPL_SHARED_LIBS */
+
+// available sparse matrix libraries
+/* #undef G2O_HAVE_CHOLMOD */
+/* #undef G2O_HAVE_CSPARSE */
+
+// logging framework available
+/* #undef G2O_HAVE_LOGGING */
+
+#define G2O_CXX_COMPILER "MSVC C:/Program Files/Microsoft Visual Studio/2022/Enterprise/VC/Tools/MSVC/14.35.32215/bin/Hostx86/x86/cl.exe"
+#define G2O_SRC_DIR "D:/a/g2o-python/g2o-python/g2o"
+
+#endif
