39807366c8da9c4d3ad9e34be19186639db85a78
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:55:27 +0000

    Add package seamoney-secsdk/1.0.0/seamoney-secsdk-1.0.0.tar.gz

---
+++ packages/seamoney-secsdk/seamoney-secsdk-1.0.0.tar.gz/seamoney-secsdk-1.0.0/setup.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+from io import open
+from setuptools import setup, find_packages
+
+setup(
+    name='seamoney-secsdk',
+    version='1.0.0',
+    description='example package',
+    long_description='example package',
+    author='seamoney',
+    author_email='',
+    license='BSD License',
+    url='',
+    download_url='',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[]
+)
