457ff0925dec66a529b821c53a974543c9b9a8e3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:50:41 +0000

    Add package galactic-messenger/0.1.1/galactic-messenger-0.1.1.tar.gz

---
+++ packages/galactic-messenger/galactic-messenger-0.1.1.tar.gz/galactic-messenger-0.1.1/galactic_messenger/__init__.py
@@ -0,0 +1,3 @@
+from src.mail import setup_email
+from src.telegram import setup_telegram
+from src.whatsapp import setup_whatsapp
---
+++ packages/galactic-messenger/galactic-messenger-0.1.1.tar.gz/galactic-messenger-0.1.1/setup.py
@@ -0,0 +1,23 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="galactic-messenger",
+    version="0.1.1",
+    author="InvigiloAI",
+    author_email="contact@invigilo.ai",
+    description="Galactic Messenger is a versatile and efficient Python package designed for sending messages across multiple platforms.",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/Invigilo-AI/galactic-messenger",
+    packages=find_packages(),
+    install_requires=open("requirements.txt").readlines(),
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+    ],
+)
