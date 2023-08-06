4bb431814715949c423b6fa7eb0c700b8fe1d898
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:34:50 +0000

    Add package yjbmagic/0.1/yjbmagic-0.1.tar.gz

---
+++ packages/yjbmagic/yjbmagic-0.1.tar.gz/yjbmagic-0.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup, find_packages
+setup(name = "yjbmagic",
+      version = 0.1,
+      description="Package containing functions commonly used by the YJB Statistics and Analysis Team",
+      author="AI_ynnna",
+      author_email="adela.iliescu@yjb.gov.uk",
+      packages=find_packages()
+      )
