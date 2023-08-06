f8e35774f566bf4ef9b4389c00bedfff7d3f24c5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:16:31 +0000

    Add package noise2read/0.0.99/noise2read-0.0.99.tar.gz

---
+++ packages/noise2read/noise2read-0.0.99.tar.gz/noise2read-0.0.99/src/noise2read/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+# @Author: Pengyao Ping
+# @Date:   2023-01-24 15:47:23
+# @Last Modified by:   Pengyao Ping
+# @Last Modified time: 2023-05-20 21:18:03
+
+__version__ = "0.0.99"
