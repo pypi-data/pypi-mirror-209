cf5313f8587b4e8322ccc1296b1250368d53df60
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:49:41 +0000

    Add package libNeuroML/0.5.2/libNeuroML-0.5.2.tar.gz

---
+++ packages/libNeuroML/libNeuroML-0.5.2.tar.gz/libNeuroML-0.5.2/neuroml/__version__.py
@@ -0,0 +1,14 @@
+"""
+libNeuroML version information
+
+File: neuroml/__version__.py
+
+Copyright 2023 NeuroML contributors
+"""
+
+
+__version__: str = "0.5.2"
+__version_info__: tuple = tuple(int(i) for i in __version__.split("."))
+
+
+current_neuroml_version: str = "v2.3"
