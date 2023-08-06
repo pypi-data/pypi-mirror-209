fabeb28468a069bd5fdd810078c3035045482f85
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:50:41 +0000

    Add package edosnahb/1.0.0/edosnahb-1.0.0.tar.gz

---
+++ packages/edosnahb/edosnahb-1.0.0.tar.gz/edosnahb-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='edosnahb',
+    version='1.0.0',
+    description='Description',
+    author='edosnahb',
+    author_email='your@email.com',
+    packages=[],
+    install_requires=[],  # List any dependencies required by your module
+)
