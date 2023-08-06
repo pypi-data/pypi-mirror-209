b1500e1a6e79b4000dc9972f8953f6a503d6c0f1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:23:41 +0000

    Add package viso-sdk-python/0.0.8/viso_sdk_python-0.0.8-cp39-cp39-manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/viso-sdk-python/viso_sdk_python-0.0.8-cp39-cp39-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/viso_sdk/constants/modules.py
@@ -0,0 +1,13 @@
+
+class MODULE:
+    class ROI:
+        NAME = 'region-of-interest'
+
+        class ROI_TYPE:
+            POLYGON = 'polygon'
+
+    class MULTI_OBJD_DET:
+        NAME = "multiple-object-detection"
+
+    class VIDEO_FEED:
+        NAME = "video-feed"
