c347749edfe8a0bbb07c27bc519edc80c7046496
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:18:31 +0000

    Add package gradio-offline/3.31.0.4/gradio_offline-3.31.0.4-py3-none-any.whl

---
+++ packages/gradio-offline/gradio_offline-3.31.0.4-py3-none-any.whl/gradio/templates/frontend/assets/index-0f3b7d3a.js
@@ -0,0 +1,2 @@
+import{C as e}from"./Column-4cfddce1.js";import"./index-f8de395e.js";/* empty css                                             */const m=["static"];export{e as Component,m as modes};
+//# sourceMappingURL=index-0f3b7d3a.js.map
