414c45366961afcdcc97d59d9f792256592c5578
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:15:53 +0000

    Add package blue-engine/0.0.2/blue-engine-0.0.2.tar.gz

---
+++ packages/blue-engine/blue-engine-0.0.2.tar.gz/blue-engine-0.0.2/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup
+
+def readme():
+    with open('readme.md') as f:
+        README = f.read()
+    return README
+
+
+setup(
+    name='blue-engine',
+    version='0.0.2',
+    description='',
+    long_description=readme(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/MrBlueBlobGuy/Blue-Engine',
+    author='Debojyoti Ganguly',
+    author_email='debojyotiganguly70@gmail.com',
+    license='Zlib',
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.10",
+    ],
+    packages=['src.blueEngine'],
+    include_package_data=True,
+)
+    
