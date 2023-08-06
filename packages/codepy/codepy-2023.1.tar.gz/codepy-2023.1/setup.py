8f8a9a9aa2ab8e0a1fcf406c6c223987c3eece58
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:32:20 +0000

    Add package codepy/2023.1/codepy-2023.1.tar.gz

---
+++ packages/codepy/codepy-2023.1.tar.gz/codepy-2023.1/setup.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_namespace_packages
+
+setup(
+    name="codepy",
+    version="2023.1",
+    description="Generate and execute native code at run time.",
+    long_description=open("README.rst").read(),
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Other Audience",
+        "Intended Audience :: Science/Research",
+        "License :: OSI Approved :: MIT License",
+        "Natural Language :: English",
+        "Programming Language :: Python",
+        "Topic :: Scientific/Engineering",
+        "Topic :: Software Development :: Libraries",
+        "Topic :: Utilities",
+    ],
+    author="Andreas Kloeckner",
+    url="http://mathema.tician.de/software/codepy",
+    author_email="inform@tiker.net",
+    license="MIT",
+    packages=find_namespace_packages(),
+    python_requires="~=3.8",
+    install_requires=[
+        "pytools>=2015.1.2",
+        "numpy>=1.6",
+        "platformdirs>=2.2.0",
+        "cgen",
+    ],
+    include_package_data=True,
+    package_data={
+        "codepy": [
+            "include/codepy/*.hpp",
+        ]
+    },
+    zip_safe=False,
+)
