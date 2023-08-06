0d31bdcc42dd55ae4f1c85bc1adff2941643cb9f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:12:45 +0000

    Add package forust/0.2.10/forust-0.2.10.tar.gz

---
+++ packages/forust/forust-0.2.10.tar.gz/forust-0.2.10/local_dependencies/forust-ml/src/errors.rs
@@ -0,0 +1,15 @@
+use thiserror::Error;
+
+#[derive(Debug, Error)]
+pub enum ForustError {
+    #[error("Feature number {0} has no variance, when missing values are excluded.")]
+    NoVariance(usize),
+    #[error("Unable to write model to file: {0}")]
+    UnableToWrite(String),
+    #[error("Unable to read model from a file {0}")]
+    UnableToRead(String),
+    #[error("The value {0} is set to missing, but a NaN value was found in the data.")]
+    NANVAlueFound(f64),
+    #[error("Invalid value {0} passed for {1}, expected one of {2}.")]
+    ParseString(String, String, String),
+}
