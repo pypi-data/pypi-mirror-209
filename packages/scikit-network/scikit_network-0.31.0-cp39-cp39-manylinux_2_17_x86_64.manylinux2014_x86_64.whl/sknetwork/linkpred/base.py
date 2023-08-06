ab2398d29ed9f6c759282b561a7a7cf131fbd22a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:43:27 +0000

    Add package scikit-network/0.31.0/scikit_network-0.31.0-cp39-cp39-manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/scikit-network/scikit_network-0.31.0-cp39-cp39-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/sknetwork/linkpred/base.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created in March 2022
+@author: Thomas Bonald <thomas.bonald@telecom-paris.fr>
+"""
+from abc import ABC
+
+import numpy as np
+
+from sknetwork.base import Algorithm
+
+
+class BaseLinker(Algorithm, ABC):
+    """Base class for link prediction.
+
+    Attributes
+    ----------
+    links_: sparse.csr_matrix
+        Link matrix.
+    """
+
+    def __init__(self):
+        self.links_ = None
+
+    def fit_predict(self, *args, **kwargs) -> np.ndarray:
+        """Fit algorithm to data and return the links. Same parameters as the ``fit`` method.
+
+        Returns
+        -------
+        links_ : sparse.csr_matrix
+            Link matrix.
+        """
+        self.fit(*args, **kwargs)
+        return self.links_
