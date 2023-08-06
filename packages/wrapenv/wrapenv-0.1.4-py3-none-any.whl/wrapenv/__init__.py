0fd8cdf51f069cc6011a7d87e43ad67b4581b61a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:07:41 +0000

    Add package wrapenv/0.1.4/wrapenv-0.1.4-py3-none-any.whl

---
+++ packages/wrapenv/wrapenv-0.1.4-py3-none-any.whl/wrapenv/__init__.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# -------------------------------------------------------------------------------
+# Name:        __init__.py
+# Purpose:     A module to wrap functions in an environment with pre- and post-processing functions.
+# Project:     WrapEnv
+#
+# Author:      Anton G. Mueckl (amueckl@chartup.de)
+#
+# Created:     22.05.2023
+# Copyright:   (c) Anton G. Mueckl (amueckl@chartup.de) 2023
+# Licence:     MIT
+# -------------------------------------------------------------------------------
+
+from .wrapenv import environment, ENVIRONMENT, Function
+__all__ = (environment, ENVIRONMENT, Function)
+
+__version__ = '0.1.4'
+__author__ = 'Anton G. Mueckl'
+__license__ = 'MIT'
+__email__ = 'AMueckl@users.noreply.github.com'
