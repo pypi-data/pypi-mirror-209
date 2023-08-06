00a6ba8883e03a15030f6b8e2f7fdffbda07a39f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:12:53 +0000

    Add package GXYUR-env/0.2.2/GXYUR_env-0.2.2.tar.gz

---
+++ packages/GXYUR-env/GXYUR_env-0.2.2.tar.gz/GXYUR_env-0.2.2/setup.py
@@ -0,0 +1,14 @@
+import setuptools
+from pathlib import Path
+
+setuptools.setup(
+    name='GXYUR_env',
+    author="GXY",
+    version='0.2.2',
+    description="An OpenAI Gym Env for dual UR5",
+    long_description_content_type="text/markdown",
+    packages=setuptools.find_packages(include="GXYUR_env*"),
+    install_requires=['gym', 'pybullet', 'numpy'],
+    python_requires='>=3.6'
+)
+
