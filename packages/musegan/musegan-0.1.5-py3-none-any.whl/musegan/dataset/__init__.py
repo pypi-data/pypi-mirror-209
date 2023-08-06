06b3c1554f0e8fe396108994f420548b4bd4ea69
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:35:23 +0000

    Add package musegan/0.1.5/musegan-0.1.5-py3-none-any.whl

---
+++ packages/musegan/musegan-0.1.5-py3-none-any.whl/musegan/dataset/__init__.py
@@ -0,0 +1,5 @@
+"""Dataset."""
+
+from .data_utils import LPDDataset, MidiDataset, binarise_output, postprocess
+
+__all__ = ["LPDDataset", "MidiDataset", "binarise_output", "postprocess"]
---
+++ packages/musegan/musegan-0.1.5-py3-none-any.whl/musegan/trainner/__init__.py
@@ -0,0 +1,7 @@
+from .trainer import Trainer
+from .criterion import WassersteinLoss, GradientPenalty
+
+__all__ = ["Trainer",
+           "WassersteinLoss",
+           "GradientPenalty",
+           ]
