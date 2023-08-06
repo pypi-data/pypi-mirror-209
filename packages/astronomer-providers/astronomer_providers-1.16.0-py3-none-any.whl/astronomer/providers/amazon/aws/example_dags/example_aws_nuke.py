2a3853843765ca8f99d6f4ece66057b42a10558b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:14:33 +0000

    Add package astronomer-providers/1.16.0/astronomer_providers-1.16.0-py3-none-any.whl

---
+++ packages/astronomer-providers/astronomer_providers-1.16.0-py3-none-any.whl/astronomer/providers/amazon/aws/example_dags/example_aws_nuke.py
@@ -0,0 +1,42 @@
+"""DAG to nuke AWS resources."""
+import os
+from datetime import datetime, timedelta
+
+from airflow import DAG
+from airflow.operators.bash import BashOperator
+from airflow.operators.dummy import DummyOperator
+
+AWS_ACCESS_KEY_ID = os.getenv("AWS_ACCESS_KEY_ID", "**********")
+AWS_SECRET_ACCESS_KEY = os.getenv("AWS_SECRET_ACCESS_KEY", "***********")
+AWS_DEFAULT_REGION = os.getenv("AWS_DEFAULT_REGION", "us-east-2")
+AWS_CONN_ID = os.getenv("ASTRO_AWS_CONN_ID", "aws_default")
+EXECUTION_TIMEOUT = int(os.getenv("EXECUTION_TIMEOUT", 6))
+
+default_args = {
+    "execution_timeout": timedelta(hours=EXECUTION_TIMEOUT),
+    "retries": int(os.getenv("DEFAULT_TASK_RETRIES", 2)),
+    "retry_delay": timedelta(seconds=int(os.getenv("DEFAULT_RETRY_DELAY_SECONDS", 60))),
+}
+
+with DAG(
+    dag_id="example_aws_nuke",
+    start_date=datetime(2022, 1, 1),
+    schedule=None,
+    catchup=False,
+    default_args=default_args,
+    tags=["example", "aws-nuke"],
+    is_paused_upon_creation=False,
+) as dag:
+    start = DummyOperator(task_id="start")
+
+    execute_aws_nuke = BashOperator(
+        task_id="execute_aws_nuke",
+        bash_command=f"aws configure set aws_access_key_id {AWS_ACCESS_KEY_ID}; "
+        f"aws configure set aws_secret_access_key {AWS_SECRET_ACCESS_KEY}; "
+        f"aws configure set default.region {AWS_DEFAULT_REGION}; "
+        f"aws-nuke -c /usr/local/airflow/dags/nuke-config.yml --profile default --force --no-dry-run; ",
+    )
+
+    end = DummyOperator(task_id="end")
+
+    start >> execute_aws_nuke >> end
---
+++ packages/astronomer-providers/astronomer_providers-1.16.0-py3-none-any.whl/astronomer/providers/core/example_dags/example_external_task.py
@@ -0,0 +1,70 @@
+import os
+import time
+from datetime import timedelta
+
+from airflow import DAG
+from airflow.operators.dummy import DummyOperator
+from airflow.operators.python import PythonOperator
+from airflow.operators.trigger_dagrun import TriggerDagRunOperator
+from airflow.utils.timezone import datetime
+
+from astronomer.providers.core.sensors.external_task import ExternalTaskSensorAsync
+
+EXECUTION_TIMEOUT = int(os.getenv("EXECUTION_TIMEOUT", 6))
+
+default_args = {
+    "execution_timeout": timedelta(hours=EXECUTION_TIMEOUT),
+    "retries": int(os.getenv("DEFAULT_TASK_RETRIES", 2)),
+    "retry_delay": timedelta(seconds=int(os.getenv("DEFAULT_RETRY_DELAY_SECONDS", 60))),
+}
+
+with DAG(
+    dag_id="example_external_task",
+    start_date=datetime(2022, 1, 1),
+    schedule=None,
+    catchup=False,
+    default_args=default_args,
+    tags=["example", "async", "core"],
+) as dag:
+    start = DummyOperator(task_id="start")
+
+    # [START howto_sensor_external_task_async]
+    waiting_for_task = ExternalTaskSensorAsync(
+        task_id="waiting_for_task",
+        external_task_id="wait_for_me",
+        external_dag_id="example_external_task",
+    )
+    # [END howto_sensor_external_task_async]
+
+    wait_for_me = PythonOperator(
+        task_id="wait_for_me",
+        python_callable=lambda: time.sleep(5),
+    )
+
+    # When ``external_task_id`` not provided
+    wait_for_dag = ExternalTaskSensorAsync(
+        task_id="wait_for_dag",
+        external_dag_id="example_external_task_async_waits_for_me",
+    )
+
+    wait_to_defer = PythonOperator(
+        task_id="wait_to_defer",
+        python_callable=lambda: time.sleep(5),
+    )
+
+    external = TriggerDagRunOperator(
+        task_id="external_dag",
+        trigger_dag_id="example_external_task_async_waits_for_me",
+        wait_for_completion=True,
+        reset_dag_run=True,
+        allowed_states=["success", "failed", "skipped"],
+        execution_date="{{execution_date}}",
+        poke_interval=1,
+    )
+
+    end = DummyOperator(task_id="end")
+
+    start >> [waiting_for_task, wait_for_me] >> end
+
+    start >> wait_for_dag >> end
+    start >> wait_to_defer >> external >> end
