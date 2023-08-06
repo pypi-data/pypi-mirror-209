be6a5908e2ac9c48103e5f21c8903678389a631e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:12:13 +0000

    Add package dbt-semantic-interfaces/0.1.0.dev1/dbt_semantic_interfaces-0.1.0.dev1-py3-none-any.whl

---
+++ packages/dbt-semantic-interfaces/dbt_semantic_interfaces-0.1.0.dev1-py3-none-any.whl/dbt_semantic_interfaces/objects/elements/entity.py
@@ -0,0 +1,30 @@
+from __future__ import annotations
+
+from typing import Optional
+
+from dbt_semantic_interfaces.objects.base import (
+    HashableBaseModel,
+    ModelWithMetadataParsing,
+)
+from dbt_semantic_interfaces.objects.metadata import Metadata
+from dbt_semantic_interfaces.references import EntityReference
+from dbt_semantic_interfaces.type_enums.entity_type import EntityType
+
+
+class Entity(HashableBaseModel, ModelWithMetadataParsing):
+    """Describes a entity."""
+
+    name: str
+    description: Optional[str]
+    type: EntityType
+    role: Optional[str]
+    expr: Optional[str] = None
+    metadata: Optional[Metadata] = None
+
+    @property
+    def reference(self) -> EntityReference:  # noqa: D
+        return EntityReference(element_name=self.name)
+
+    @property
+    def is_linkable_entity_type(self) -> bool:  # noqa: D
+        return self.type in (EntityType.PRIMARY, EntityType.UNIQUE, EntityType.NATURAL)
