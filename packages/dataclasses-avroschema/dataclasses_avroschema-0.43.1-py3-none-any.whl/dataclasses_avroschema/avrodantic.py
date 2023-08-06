84098d66a3c1fa9877803821b5c7a70424ac903e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:11:51 +0000

    Add package dataclasses-avroschema/0.43.1/dataclasses_avroschema-0.43.1-py3-none-any.whl

---
+++ packages/dataclasses-avroschema/dataclasses_avroschema-0.43.1-py3-none-any.whl/dataclasses_avroschema/avrodantic.py
@@ -0,0 +1,49 @@
+from typing import Any, Dict, Type, TypeVar
+
+from fastavro.validation import validate
+
+from .schema_generator import AvroModel, JsonDict
+
+try:
+    from pydantic import BaseModel  # pragma: no cover
+except ImportError as ex:  # pragma: no cover
+    raise Exception("pydantic must be installed in order to use AvroBaseModel") from ex  # pragma: no cover
+
+CT = TypeVar("CT", bound="AvroBaseModel")
+
+
+class AvroBaseModel(BaseModel, AvroModel):  # type: ignore
+    @classmethod
+    def json_schema(cls: Type[CT], *args: Any, **kwargs: Any) -> str:
+        return cls.schema_json(*args, **kwargs)
+
+    def asdict(self) -> JsonDict:
+        """
+        Document this. asdict vs dict
+        """
+        data = dict(self)
+
+        # te standardize called can be replaced if we have a custom implementation of asdict
+        # for now I think is better to use the native implementation
+        return {key: self.standardize_custom_type(value) for key, value in data.items()}
+
+    def validate_avro(self) -> bool:
+        """
+        Validate that instance matches the avro schema
+        """
+        schema = self.avro_schema_to_python()
+        return validate(self.asdict(), schema)
+
+    @classmethod
+    def fake(cls: Type[CT], **data: Dict[str, Any]) -> CT:
+        """
+        Creates a fake instance of the model.
+
+        Attributes:
+            data: Dict[str, Any] represent the user values to use in the instance
+        """
+        # only generate fakes for fields that were not provided in data
+        payload = {field.name: field.fake() for field in cls.get_fields() if field.name not in data.keys()}
+        payload.update(data)
+
+        return cls.parse_obj(payload)
