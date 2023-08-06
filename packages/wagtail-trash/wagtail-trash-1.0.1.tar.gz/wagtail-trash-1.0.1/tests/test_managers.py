650c3e7383d04845a27e3ac1122840fe4fa8ebdb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:29:22 +0000

    Add package wagtail-trash/1.0.1/wagtail-trash-1.0.1.tar.gz

---
+++ packages/wagtail-trash/wagtail-trash-1.0.1.tar.gz/wagtail-trash-1.0.1/tests/test_managers.py
@@ -0,0 +1,52 @@
+from django.shortcuts import reverse
+from django.test import TestCase
+from wagtail.models import Page
+from wagtail.test.utils import WagtailTestUtils
+
+from tests.app.models import TestPage
+from wagtail_trash.models import TrashCan, TrashCanPage
+from wagtail_trash.views import trash_delete
+from wagtail_trash.wagtail_hooks import TrashCanModelAdmin
+
+
+class TestManagers(TestCase, WagtailTestUtils):
+    def setUp(self):
+        self.login()
+
+    def test_manager(self):
+        from wagtail_trash.wagtail_hooks import urlconf_time
+
+        root_page = Page.objects.get(url_path="/")
+
+        top_page = TestPage(title="top_page")
+        root_page.add_child(instance=top_page)
+
+        sub_page = TestPage(title="sub_page")
+        top_page.add_child(instance=sub_page)
+
+        sub_sub_page = TestPage(title="sub_sub_page")
+        sub_page.add_child(instance=sub_sub_page)
+
+        self.assertEquals(TestPage.objects.count(), 3)
+        self.assertEquals(TestPage.objects_excluding_bins.count(), 3)
+
+        with self.register_hook("before_delete_page", trash_delete):
+            delete_url = reverse("wagtailadmin_pages:delete", args=(sub_sub_page.id,))
+            self.client.post(delete_url)
+
+        self.assertEquals(TestPage.objects.count(), 3)
+        self.assertEquals(TestPage.objects_excluding_bins.count(), 2)
+
+        with self.register_hook("register_admin_urls", urlconf_time):
+            restore_url = reverse("wagtail_trash_restore", args=(sub_sub_page.id,))
+            self.client.get(restore_url)
+
+        self.assertEquals(TestPage.objects.count(), 3)
+        self.assertEquals(TestPage.objects_excluding_bins.count(), 3)
+
+        with self.register_hook("before_delete_page", trash_delete):
+            delete_url = reverse("wagtailadmin_pages:delete", args=(top_page.id,))
+            self.client.post(delete_url)
+
+        self.assertEquals(TestPage.objects.count(), 3)
+        self.assertEquals(TestPage.objects_excluding_bins.count(), 0)
