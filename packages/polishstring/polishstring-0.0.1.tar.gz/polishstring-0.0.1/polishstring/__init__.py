7c23b1270534e7ad95e286162e4d750be9323f29
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:14:39 +0000

    Add package polishstring/0.0.1/polishstring-0.0.1.tar.gz

---
+++ packages/polishstring/polishstring-0.0.1.tar.gz/polishstring-0.0.1/polishstring/__init__.py
@@ -0,0 +1,6 @@
+from .polishstring import inf_prf
+from .polishstring import inf_pof
+from .polishstring import pof_prf
+from .polishstring import pof_inf
+from .polishstring import prf_pof
+from .polishstring import prf_inf
