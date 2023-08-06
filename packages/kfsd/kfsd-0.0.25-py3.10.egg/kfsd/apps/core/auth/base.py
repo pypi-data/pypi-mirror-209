4e8f382d2cd7711745653b7b4fdbd5609d6d181b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:51:43 +0000

    Add package kfsd/0.0.25/kfsd-0.0.25-py3.10.egg

---
+++ packages/kfsd/kfsd-0.0.25-py3.10.egg/kfsd/apps/core/auth/base.py
@@ -0,0 +1,29 @@
+from kfsd.apps.core.utils.dict import DictUtils
+
+
+class BaseUser:
+    is_active = False
+
+    def __init__(self):
+        self.__userInfo = {}
+
+    def setUserInfo(self, userInfo):
+        self.__userInfo = userInfo
+
+    def getUserInfo(self):
+        return self.__userInfo
+
+    def is_authenticated(self):
+        return DictUtils.get(self.getUserInfo(), "status")
+
+    def getIdentifier(self):
+        return DictUtils.get_by_path(self.getUserInfo(), "data.user.identifier")
+
+    def getEmail(self):
+        return DictUtils.get_by_path(self.getUserInfo(), "data.user.email")
+
+    def getAPIKey(self):
+        return DictUtils.get_by_path(self.getUserInfo(), "data.user.api_key")
+
+    def isActive(self):
+        return DictUtils.get_by_path(self.getUserInfo(), "data.user.is_active")
