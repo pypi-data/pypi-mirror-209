586c73a090c9c30bf5e5e0f0fc781e59cefe89de
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:22:22 +0000

    Add package coordinate-descent-attention/0.0.11/coordinate-descent-attention-0.0.11.tar.gz

---
+++ packages/coordinate-descent-attention/coordinate-descent-attention-0.0.11.tar.gz/coordinate-descent-attention-0.0.11/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+
+setup(
+  name = 'coordinate-descent-attention',
+  packages = find_packages(exclude=[]),
+  version = '0.0.11',
+  license='MIT',
+  description = 'Coordinate Descent Attention - Pytorch',
+  author = 'Phil Wang',
+  author_email = 'lucidrains@gmail.com',
+  long_description_content_type = 'text/markdown',
+  url = 'https://github.com/lucidrains/coodinate-descent-attention',
+  keywords = [
+    'artificial intelligence',
+    'deep learning',
+    'attention mechanism'
+  ],
+  install_requires=[
+    'einops>=0.6.1',
+    'torch>=1.6',
+    'colt5-attention>=0.9.0'
+  ],
+  classifiers=[
+    'Development Status :: 4 - Beta',
+    'Intended Audience :: Developers',
+    'Topic :: Scientific/Engineering :: Artificial Intelligence',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3.6',
+  ],
+)
