baf79ff126dbc317c05979a339639de1c6c37370
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:15:24 +0000

    Add package cli-command-parser/2023.5.20/cli_command_parser-2023.5.20.tar.gz

---
+++ packages/cli-command-parser/cli_command_parser-2023.5.20.tar.gz/cli_command_parser-2023.5.20/lib/cli_command_parser/testing.py
@@ -0,0 +1,332 @@
+"""
+Helpers for unit tests
+
+:author: Doug Skrypa
+"""
+# pylint: disable=R0913,C0103
+
+from __future__ import annotations
+
+import sys
+from contextlib import AbstractContextManager, contextmanager
+from difflib import unified_diff
+from io import StringIO, BytesIO
+from pathlib import Path
+from tempfile import TemporaryDirectory
+from typing import TYPE_CHECKING, Any, Iterable, Type, Union, Callable, IO, ContextManager, Dict, List, Tuple
+from unittest import TestCase
+from unittest.mock import Mock, seal, patch
+
+from .actions import help_action
+from .commands import Command
+from .context import Context
+from .core import get_params
+from .documentation import load_commands
+from .exceptions import UsageError
+
+if TYPE_CHECKING:
+    from .typing import CommandCls
+
+__all__ = [
+    'ParserTest',
+    'RedirectStreams',
+    'format_diff',
+    'get_rst_text',
+    'get_help_text',
+    'get_usage_text',
+    'sealed_mock',
+    'load_command',
+    'TemporaryDir',
+]
+
+Argv = List[str]
+Expected = Dict[str, Any]
+Kwargs = Dict[str, Any]
+Env = Dict[str, str]
+Case = Tuple[Argv, Expected]
+EnvCase = Tuple[Argv, Env, Expected]
+ExceptionCase = Union[Argv, Tuple[Argv, Type[Exception]], Tuple[Argv, Type[Exception], str]]
+CallExceptionCase = Union[Tuple[Kwargs, Type[Exception]], Tuple[Kwargs, Type[Exception], str]]
+
+OPT_ENV_MOD = 'cli_command_parser.parser.environ'
+
+
+class ParserTest(TestCase):
+    # def setUp(self):
+    #     print()
+    #
+    # def subTest(self, *args, **kwargs):
+    #     print()
+    #     return super().subTest(*args, **kwargs)
+
+    def assert_dict_equal(self, d1, d2, msg: str = None):
+        self.assertIsInstance(d1, dict, 'First argument is not a dictionary')
+        self.assertIsInstance(d2, dict, 'Second argument is not a dictionary')
+        if d1 != d2:
+            standard_msg = f'{d1} != {d2}\n{format_dict_diff(d1, d2)}'
+            self.fail(self._formatMessage(msg, standard_msg))
+
+    def assert_parse_results(self, cmd_cls: CommandCls, argv: Argv, expected: Expected, message: str = None) -> Command:
+        cmd = cmd_cls.parse(argv)
+        parsed = cmd.ctx.get_parsed((help_action,))
+        self.assert_dict_equal(expected, parsed, message)
+        return cmd
+
+    def assert_parse_results_cases(self, cmd_cls: CommandCls, cases: Iterable[Case], message: str = None):
+        for argv, expected in cases:
+            with self.subTest(expected='results', argv=argv):
+                self.assert_parse_results(cmd_cls, argv, expected, message)
+
+    def assert_env_parse_results(
+        self, cmd_cls: CommandCls, argv: Argv, env: Env, expected: Expected, message: str = None
+    ) -> Command:
+        with patch(OPT_ENV_MOD, env):
+            return self.assert_parse_results(cmd_cls, argv, expected, message)
+
+    def assert_env_parse_results_cases(self, cmd_cls: CommandCls, cases: Iterable[EnvCase], message: str = None):
+        for argv, env, expected in cases:
+            with self.subTest(expected='results', argv=argv, env=env):
+                self.assert_env_parse_results(cmd_cls, argv, env, expected, message)
+
+    def assert_parse_fails(
+        self,
+        cmd_cls: CommandCls,
+        argv: Argv,
+        expected_exc: Type[Exception] = UsageError,
+        expected_pattern: str = None,
+        message: str = None,
+    ):
+        if expected_pattern:
+            with self.assertRaisesRegex(expected_exc, expected_pattern, msg=message):
+                cmd_cls.parse(argv)
+        else:
+            with self.assertRaises(expected_exc, msg=message):
+                cmd_cls.parse(argv)
+
+    def assert_parse_fails_cases(
+        self, cmd_cls: CommandCls, cases: Iterable[ExceptionCase], exc: Type[Exception] = None, message: str = None
+    ):
+        if exc is not None:
+            for argv in cases:
+                with self.subTest(expected='exception', argv=argv):
+                    self.assert_parse_fails(cmd_cls, argv, exc, message=message)
+        else:
+            for case in cases:
+                try:
+                    argv, exc = case
+                except ValueError:
+                    argv, exc, pat = case
+                else:
+                    pat = None
+
+                with self.subTest(expected='exception', argv=argv):
+                    self.assert_parse_fails(cmd_cls, argv, exc, pat, message=message)
+
+    def assert_argv_parse_fails_cases(
+        self, cmd_cls: CommandCls, cases: Iterable[Argv], exc: Type[Exception] = UsageError, message: str = None
+    ):
+        """Convenience method for calling :meth:`.assert_parse_fails_cases` with a default exception type."""
+        self.assert_parse_fails_cases(cmd_cls, cases, exc, message)
+
+    def assert_call_fails(
+        self,
+        func: Callable,
+        kwargs: Kwargs,
+        exc: Type[Exception] = Exception,
+        pattern: str = None,
+        message: str = None,
+    ):
+        if pattern:
+            with self.assertRaisesRegex(exc, pattern, msg=message):
+                func(**kwargs)
+        else:
+            with self.assertRaises(exc, msg=message):
+                func(**kwargs)
+
+    def assert_call_fails_cases(self, func: Callable, cases: Iterable[CallExceptionCase], message: str = None):
+        for case in cases:
+            try:
+                kwargs, exc = case
+            except ValueError:
+                kwargs, exc, pat = case
+            else:
+                pat = None
+
+            with self.subTest(expected='exception', kwargs=kwargs):
+                self.assert_call_fails(func, kwargs, exc, pat, message=message)
+
+    def assert_strings_equal(
+        self, expected: str, actual: str, message: str = None, diff_lines: int = 3, trim: bool = False
+    ):
+        if trim:
+            expected = expected.rstrip()
+            actual = '\n'.join(line.rstrip() for line in actual.splitlines())
+        if message:
+            self.assertEqual(expected, actual, message)
+        elif expected != actual:
+            diff = format_diff(expected, actual, n=diff_lines)
+            # if not diff.strip():
+            #     self.assertEqual(expected, actual)
+            # else:
+            self.fail('Strings did not match:\n' + diff)
+
+    def assert_str_starts_with_line(self, prefix: str, text: str):
+        new_line = text.index('\n')
+        self.assertEqual(prefix, text[:new_line])
+
+    def assert_str_contains(self, sub_text: str, text: str, diff_lines: int = 3):
+        if sub_text not in text:
+            diff = format_diff(sub_text, text, n=diff_lines)
+            self.fail('String did not contain expected text:\n' + diff)
+
+    @contextmanager
+    def env_vars(self, case: str, **env_vars):
+        with self.subTest(case=case), patch(OPT_ENV_MOD, env_vars):
+            yield
+
+
+# region Formatting
+
+
+def _colored(text: str, color: int, end: str = '\n'):
+    return f'\x1b[38;5;{color}m{text}\x1b[0m{end}'
+
+
+def format_diff(a: str, b: str, name_a: str = 'expected', name_b: str = '  actual', n: int = 3) -> str:
+    sio = StringIO()
+    a = a.splitlines()
+    b = b.splitlines()
+    for i, line in enumerate(unified_diff(a, b, name_a, name_b, n=n, lineterm='')):
+        if line.startswith('+') and i > 1:
+            sio.write(_colored(line, 2))
+        elif line.startswith('-') and i > 1:
+            sio.write(_colored(line, 1))
+        elif line.startswith('@@ '):
+            sio.write(_colored(line, 6, '\n\n'))
+        else:
+            sio.write(line + '\n')
+
+    return sio.getvalue()
+
+
+def format_dict_diff(a: Dict[str, Any], b: Dict[str, Any]) -> str:
+    formatted_a = []
+    formatted_b = []
+    for key in sorted(set(a) | set(b)):
+        try:
+            val_a = a[key]
+        except KeyError:
+            str_b = f'{key!r}: {b[key]!r}'
+            formatted_a.append(' ' * len(str_b))
+            formatted_b.append(_colored(str_b, 2, ''))
+        else:
+            str_a = f'{key!r}: {val_a!r}'
+            try:
+                val_b = b[key]
+            except KeyError:
+                str_b = ' ' * len(str_a)
+                formatted_a.append(_colored(str_a, 1, ''))
+                formatted_b.append(str_b)
+            else:
+                str_b = f'{key!r}: {val_b!r}'
+                if val_a == val_b:
+                    formatted_a.append(str_a)
+                    formatted_b.append(str_b)
+                else:
+                    formatted_a.append(_colored(str_a, 2, ''))
+                    formatted_b.append(_colored(str_b, 1, ''))
+
+    kvs_a = ', '.join(formatted_a)
+    kvs_b = ', '.join(formatted_b)
+    return f'- {{{kvs_a}}}\n+ {{{kvs_b}}}'
+
+
+# endregion
+
+
+class RedirectStreams(AbstractContextManager):
+    _stdin: Union[IO, str, bytes, None] = None
+
+    def __init__(self, stdin: Union[IO, str, bytes, None] = None):
+        self._old = {}
+        if stdin is not None:
+            if isinstance(stdin, bytes):
+                self._stdin = BytesIO(stdin)
+                self._stdin.buffer = self._stdin  # pretend to be the underlying buffer as well
+            elif isinstance(stdin, str):
+                self._stdin = StringIO(stdin)
+                self._stdin.buffer = BytesIO(stdin.encode('utf-8'))
+            else:
+                self._stdin = stdin
+        self._stdout = StringIO()
+        self._stderr = StringIO()
+
+    @property
+    def stdout(self) -> str:
+        return self._stdout.getvalue()
+
+    @property
+    def stderr(self) -> str:
+        return self._stderr.getvalue()
+
+    def __enter__(self) -> RedirectStreams:
+        streams = {'stdout': self._stdout, 'stderr': self._stderr}
+        if self._stdin is not None:
+            streams['stdin'] = self._stdin
+        for name, io in streams.items():
+            self._old[name] = getattr(sys, name)
+            setattr(sys, name, io)
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        while self._old:
+            name, orig = self._old.popitem()
+            setattr(sys, name, orig)
+
+
+# region Help / Usage / RST Text
+
+
+def get_usage_text(cmd: Type[Command]) -> str:
+    with cmd().ctx:
+        return get_params(cmd).formatter.format_usage()
+
+
+def get_help_text(cmd: Union[Type[Command], Command], terminal_width: int = 199) -> str:
+    if not isinstance(cmd, Command):
+        cmd = cmd()
+
+    cmd.ctx._terminal_width = terminal_width
+    with cmd.ctx:
+        return get_params(cmd).formatter.format_help()
+
+
+def get_rst_text(cmd: Union[Type[Command], Command]) -> str:
+    if not isinstance(cmd, Command):
+        cmd = cmd()
+
+    cmd.ctx._terminal_width = 199
+    with cmd.ctx:
+        return get_params(cmd).formatter.format_rst()
+
+
+# endregion
+
+
+def sealed_mock(*args, **kwargs):
+    kwargs.setdefault('return_value', None)
+    mock = Mock(*args, **kwargs)
+    seal(mock)
+    return mock
+
+
+@contextmanager
+def load_command(directory: Path, name: str, cmd_name: str, **kwargs) -> ContextManager[CommandCls]:
+    path = directory.joinpath(name)
+    with Context.for_prog(path, **kwargs):
+        yield load_commands(path)[cmd_name]
+
+
+class TemporaryDir(TemporaryDirectory):
+    def __enter__(self) -> Path:
+        return Path(self.name)
