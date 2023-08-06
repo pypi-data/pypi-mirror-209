cca2aa193fc7117608e05a7a447e80c5af69c8e0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:50:34 +0000

    Add package psychicapi/0.3/psychicapi-0.3.tar.gz

---
+++ packages/psychicapi/psychicapi-0.3.tar.gz/psychicapi-0.3/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='psychicapi',
+    version='0.3',
+    description='Psychic.dev is an open-source universal data connector for knowledgebases.',
+    author='Ayan Bandyopadhyay',
+    author_email='ayan@getsidekick.ai',
+    packages=['psychicapi'],
+    install_requires=[
+        'requests',
+    ],
+)
