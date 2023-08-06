bb5e4599c814da63ce77b98cfda15b4227551851
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:06:22 +0000

    Add package fedops/0.1.3/fedops-0.1.3.tar.gz

---
+++ packages/fedops/fedops-0.1.3.tar.gz/fedops-0.1.3/fedops/client/__init__.py
@@ -0,0 +1,38 @@
+# Copyright 2020 Adap GmbH. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ==============================================================================
+"""FedOps Client."""
+
+# from .app import FLClientTask
+# from .client_api import ClientMangerAPI
+# from .client_api import ClientServerAPI
+# from .client_fl import FLClient
+# from .client_fl import flower_client_start
+# from .client_utils import FLClientStatus
+# from .client_utils import ManagerData
+
+import client_utils
+import app
+import client_fl
+import client_api
+import client_wandb
+
+
+__all__ = [
+    "client_utils",
+    "app",
+    "client_fl",
+    "client_api",
+    "client_wandb",
+]
