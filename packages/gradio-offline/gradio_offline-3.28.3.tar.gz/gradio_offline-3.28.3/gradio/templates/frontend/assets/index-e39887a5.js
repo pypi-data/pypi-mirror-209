013b8f2564638842a8f4662832bb3daaf746fe4f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:50:38 +0000

    Add package gradio-offline/3.28.3/gradio_offline-3.28.3.tar.gz

---
+++ packages/gradio-offline/gradio_offline-3.28.3.tar.gz/gradio_offline-3.28.3/gradio/templates/frontend/assets/index-e39887a5.js
@@ -0,0 +1,2 @@
+import{C as e}from"./Column-e5a3c3c9.js";import"./index-a14466cb.js";/* empty css                                             */const m=["static"];export{e as Component,m as modes};
+//# sourceMappingURL=index-e39887a5.js.map
---
+++ packages/gradio-offline/gradio_offline-3.28.3.tar.gz/gradio_offline-3.28.3/gradio/templates/frontend/assets/index-e5966e26.js
@@ -0,0 +1,2 @@
+import{$ as s}from"./index-a14466cb.js";const o=["static"];export{s as Component,o as modes};
+//# sourceMappingURL=index-e5966e26.js.map
