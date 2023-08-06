c6322f3728966ae007ff67cf2aee0609f533ec15
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:16:04 +0000

    Add package ldimbenchmark/0.2.10/ldimbenchmark-0.2.10.tar.gz

---
+++ packages/ldimbenchmark/ldimbenchmark-0.2.10.tar.gz/ldimbenchmark-0.2.10/src/ldimbenchmark/methods/test_complexity_polynomial.py
@@ -0,0 +1,42 @@
+import math
+from time import sleep
+from ldimbenchmark import LDIMMethodBase, BenchmarkData, BenchmarkLeakageResult
+from ldimbenchmark.classes import MethodMetadata, MethodMetadataDataNeeded
+from typing import List, Union
+
+
+class TestPolynomialComplexityLeakageDetectionMethod(LDIMMethodBase):
+    """
+    Test Complexity: Polynomial
+    """
+
+    def __init__(self):
+        super().__init__(
+            name="Polynomial Complexity",
+            version="1.0",
+            metadata=MethodMetadata(
+                data_needed=MethodMetadataDataNeeded(
+                    pressures="ignored",
+                    flows="ignored",
+                    levels="ignored",
+                    model="ignored",
+                    demands="ignored",
+                    structure="ignored",
+                ),
+                hyperparameters=[],
+            )
+            # hyperparameters={"est_length": "3 days", "C_threshold": 3, "delta": 4},
+        )
+
+    def train(self, train_data: BenchmarkData) -> None:
+        sleep(len(train_data.demands[train_data.demands.keys()[0]]) ^ 2)
+        return
+
+    def detect_offline(
+        self, evaluation_data: BenchmarkData
+    ) -> List[BenchmarkLeakageResult]:
+        sleep(len(evaluation_data.demands[evaluation_data.demands.keys()[0]]) ^ 2)
+        return []
+
+    def detect_online(self, evaluation_data) -> Union[BenchmarkLeakageResult, None]:
+        return None
