204551f5b8d2d3a939f872fb3d2d81d87d08a75d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:30:31 +0000

    Add package kynaylibs/1.7.3/kynaylibs-1.7.3.tar.gz

---
+++ packages/kynaylibs/kynaylibs-1.7.3.tar.gz/kynaylibs-1.7.3/ubotlibs/types/bots_and_keyboards/menu_button.py
@@ -0,0 +1,45 @@
+#  Pyrogram - Telegram MTProto API Client Library for Python
+#  Copyright (C) 2017-present Dan <https://github.com/delivrance>
+#
+#  This file is part of Pyrogram.
+#
+#  Pyrogram is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU Lesser General Public License as published
+#  by the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  Pyrogram is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU Lesser General Public License for more details.
+#
+#  You should have received a copy of the GNU Lesser General Public License
+#  along with Pyrogram.  If not, see <http://www.gnu.org/licenses/>.
+
+import ubotlibs
+from ubotlibs import raw
+
+from ..object import Object
+
+
+class MenuButton(Object):
+    """Describes the bot's menu button in a private chat.
+
+    It should be one of:
+
+    - :obj:`~ubotlibs.types.MenuButtonCommands`
+    - :obj:`~ubotlibs.types.MenuButtonWebApp`
+    - :obj:`~ubotlibs.types.MenuButtonDefault`
+
+    If a menu button other than :obj:`~ubotlibs.types.MenuButtonDefault` is set for a private chat, then it is applied
+    in the chat. Otherwise the default menu button is applied. By default, the menu button opens the list of bot
+    commands.
+    """
+
+    def __init__(self, type: str):
+        super().__init__()
+
+        self.type = type
+
+    async def write(self, client: "ubotlibs.Client") -> "raw.base.BotMenuButton":
+        raise NotImplementedError
