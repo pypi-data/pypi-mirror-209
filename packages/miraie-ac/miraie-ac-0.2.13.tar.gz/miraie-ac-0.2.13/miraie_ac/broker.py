45edfdd537de465f84b802a5a38e309f34015e64
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:17:02 +0000

    Add package miraie-ac/0.2.13/miraie-ac-0.2.13.tar.gz

---
+++ packages/miraie-ac/miraie-ac-0.2.13.tar.gz/miraie-ac-0.2.13/miraie_ac/broker.py
@@ -0,0 +1,155 @@
+from paho.mqtt import client as mqtt
+import asyncio
+import ssl
+import certifi
+import random
+import json
+from .enums import *
+from .user import User
+
+
+class MirAIeBroker:
+    host = "mqtt.miraie.in"
+    port = 8883
+    use_ssl = True
+    client_id = f"ha-mirae-mqtt-{random.randint(0, 1000)}"
+
+    def __init__(self) -> None:
+        self.status_callbacks: dict[str, list[callable]] = {}
+
+    def register_device_callback(self, topic: str, callback):
+        self.status_callbacks[topic] = callback
+
+    def remove_device_callback(self, topic: str):
+        self.status_callbacks.pop(topic, None)
+
+    def set_topics(self, topics: list[str]):
+        self.commandTopics = topics
+
+    def on_connect(self, client: mqtt.Client, userdata, flags, rc):
+        # Subscribe to all command topics
+        for topic in self.commandTopics:
+            client.subscribe(topic)
+
+    def on_message(self, client: mqtt.Client, userdata, msg):
+        parsed = json.loads(msg.payload.decode("utf-8"))
+
+        func = self.status_callbacks.get(msg.topic)
+        func(parsed)
+
+    def on_disconnect(self, client: mqtt.Client, userdata, rc):
+        def cb(username: str, access_token: User):
+            self.client.username_pw_set(username, access_token)
+            self.client.reconnect()
+
+        if rc != 0:
+            asyncio.create_task(self.on_get_token(cb))
+
+    def on_log(self, client, userdata, level, buf):
+        print("log: ", buf)
+
+    def connect(self, username: str, access_token: User, on_get_token):
+
+        # Set on_token_expire callback
+        self.on_get_token = on_get_token
+
+        # Create MQTT client
+        client = mqtt.Client(
+            self.client_id,
+            True,
+        )
+
+        # Set username and password
+        client.username_pw_set(username, access_token)
+
+        if self.use_ssl:
+            # Create ssl context with TLSv1
+            context = ssl.create_default_context(cafile=certifi.where())
+            client.tls_set_context(context)
+            client.tls_insecure_set(True)
+
+        # Set callbacks
+        client.on_connect = self.on_connect
+        client.on_message = self.on_message
+        client.on_disconnect = self.on_disconnect
+        # client.on_log = self.on_log
+
+        # Connect to MQTT broker
+        client.connect(self.host, self.port, 60)
+
+        # Start network loop
+        self.client = client
+        self.client.loop_start()
+
+    def build_base_payload(self):
+        return {
+            "ki": 1,
+            "cnt": "an",
+            "sid": "1",
+        }
+
+    # Power
+    def build_power_payload(self, power: PowerMode):
+        payload = self.build_base_payload()
+        payload["ps"] = str(power.value)
+        return payload
+
+    def set_power(self, topic: str, power: PowerMode):
+        self.client.publish(topic, json.dumps(self.build_power_payload(power)))
+
+    # Temperature
+    def build_temperature_payload(self, temperature: float):
+        payload = self.build_base_payload()
+        payload["actmp"] = str(temperature)
+        return payload
+
+    def set_temperature(self, topic: str, temperature: float):
+        self.client.publish(
+            topic, json.dumps(self.build_temperature_payload(temperature))
+        )
+
+    # HVAC Mode
+    def build_hvac_mode_payload(self, mode: HVACMode):
+        payload = self.build_base_payload()
+        payload["acmd"] = str(mode.value)
+        return payload
+
+    def set_hvac_mode(self, topic: str, mode: HVACMode):
+        self.client.publish(topic, json.dumps(self.build_hvac_mode_payload(mode)))
+
+    # Fan Mode
+    def build_fan_mode_payload(self, mode: FanMode):
+        payload = self.build_base_payload()
+        payload["acfs"] = str(mode.value)
+        return payload
+
+    def set_fan_mode(self, topic: str, mode: FanMode):
+        self.client.publish(topic, json.dumps(self.build_fan_mode_payload(mode)))
+
+    # Preset Mode
+    def build_preset_mode_payload(self, mode: PresetMode):
+        payload = self.build_base_payload()
+
+        if mode == PresetMode.NONE:
+            payload["acem"] = "off"
+            payload["acpm"] = "off"
+        elif mode == PresetMode.ECO:
+            payload["acem"] = "on"
+            payload["acpm"] = "off"
+            payload["actmp"] = 26.0
+        elif mode == PresetMode.BOOST:
+            payload["acem"] = "off"
+            payload["acpm"] = "on"
+        return payload
+
+    def set_preset_mode(self, topic: str, mode: PresetMode):
+        self.client.publish(topic, json.dumps(self.build_preset_mode_payload(mode)))
+
+    # Swing Mode
+    def build_swing_mode_payload(self, mode: SwingMode):
+        payload = self.build_base_payload()
+        payload["acvs"] = mode.value
+        return payload
+
+    def set_swing_mode(self, topic: str, mode: SwingMode):
+        self.client.publish(topic, json.dumps(self.build_swing_mode_payload(mode)))
---
+++ packages/miraie-ac/miraie-ac-0.2.13.tar.gz/miraie-ac-0.2.13/miraie_ac/hub.py
@@ -0,0 +1,208 @@
+import aiohttp
+import asyncio
+from . import constants
+from .broker import MirAIeBroker
+from .user import User
+from .topic import MirAIeTopic
+from .home import Home
+from .device import Device, DeviceDetails, DeviceStatus
+from .enums import *
+
+
+def toFloat(value: str) -> float:
+    if value is None:
+        return -1.0
+    try:
+        return float(value)
+    except:
+        return -1.0
+
+
+class MirAIeHub:
+    def __init__(self):
+        self.http = aiohttp.ClientSession()
+        self.topics_map: dict[str, MirAIeTopic] = {}
+
+    async def __aenter__(self):
+        return self
+
+    async def __aexit__(self, *excinfo):
+        await self.http.close()
+
+    def __build_headers__(self):
+        return {
+            "Authorization": f"Bearer {self.user.access_token}",
+            "Content-Type": "application/json",
+        }
+
+    async def init(self, mobile: str, password: str, broker: MirAIeBroker):
+        self._broker = broker
+
+        await self._authenticate(mobile, password)
+        await self._get_home_details()
+        await self.get_all_device_status()
+        self._init_broker(broker)
+
+    def _init_broker(self, broker: MirAIeBroker):
+        topics = self.get_device_topics()
+        broker.set_topics(topics)
+        broker.connect(self.home.id, self.user.access_token, self.on_get_token)
+
+    @property
+    def broker(self):
+        return self._broker
+
+    def get_device_topics(self):
+        device_topics = list(
+            map(
+                lambda device: [device.status_topic, device.connection_status_topic],
+                self.home.devices,
+            )
+        )
+        miraie_topics = [topic for topics in device_topics for topic in topics]
+        return miraie_topics
+
+    async def on_get_token(self, cb):
+        await self._authenticate(self.username, self.password)
+        cb(self.home.id, self.user.access_token)
+
+    # Authenticate with the MirAIe API
+    async def _authenticate(self, mobile: str, password: str):
+        data = {
+            "clientId": constants.httpClientId,
+            "mobile": mobile,
+            "password": password,
+            "scope": "an_14214235325",
+        }
+
+        response = await self.http.post(constants.loginUrl, json=data)
+
+        if response.status == 200:
+            json = await response.json()
+            self.user = User(
+                access_token=json["accessToken"],
+                refresh_token=json["refreshToken"],
+                user_id=json["userId"],
+                expires_in=json["expiresIn"],
+            )
+            self.username = mobile
+            self.password = password
+            return True
+
+        raise Exception("Authentication failed")
+
+    # Get device details
+    async def _get_device_details(self, deviceIds: str):
+        response = await self.http.get(
+            constants.deviceDetailsUrl + "/" + deviceIds,
+            headers=self.__build_headers__(),
+        )
+        return await response.json()
+
+    # Process the home details
+    async def _process_home_details(self, json_data):
+        devices: list[Device] = []
+
+        for space in json_data["spaces"]:
+            for device in space["devices"]:
+                item = Device(
+                    id=device["deviceId"],
+                    name=str(device["deviceName"]).lower().replace(" ", "-"),
+                    friendly_name=device["deviceName"],
+                    control_topic=str(device["topic"][0]) + "/control",
+                    status_topic=str(device["topic"][0]) + "/status",
+                    connection_status_topic=str(device["topic"][0])
+                    + "/connectionStatus",
+                    broker=self._broker,
+                )
+                devices.append(item)
+                topic = MirAIeTopic(
+                    control_topic=item.control_topic,
+                    status_topic=item.status_topic,
+                    connection_status_topic=item.connection_status_topic,
+                )
+                self.topics_map[item.id] = topic
+
+        device_ids = ",".join(list(map(lambda device: device.id, devices)))
+        device_details = await self._get_device_details(device_ids)
+
+        for dd in device_details:
+            device = next(d for d in devices if d.id == dd["deviceId"])
+
+            details = DeviceDetails(
+                model_name=dd["modelName"],
+                mac_address=dd["macAddress"],
+                category=dd["category"],
+                brand=dd["brand"],
+                firmware_version=dd["firmwareVersion"],
+                serial_number=dd["serialNumber"],
+                model_number=dd["modelNumber"],
+                product_serial_number=dd["productSerialNumber"],
+            )
+
+            device.set_details(details)
+
+        self.home = Home(id=json_data["homeId"], devices=devices)
+        return self.home
+
+    # Get home details
+    async def _get_home_details(self):
+        response = await self.http.get(
+            constants.homesUrl, headers=self.__build_headers__()
+        )
+        resp = await response.json()
+        await self._process_home_details(resp[0])
+
+    # Get device status
+    async def _get_device_status(self, device_id: str):
+        response = await self.http.get(
+            constants.statusUrl.replace("{deviceId}", device_id),
+            headers=self.__build_headers__(),
+        )
+        resp = await response.json()
+        resp["deviceId"] = device_id
+        return resp
+
+    # Get all device status
+    async def get_all_device_status(self):
+        statuses = await asyncio.gather(
+            *[self._get_device_status(device.id) for device in self.home.devices],
+            return_exceptions=True,
+        )
+
+        for status in statuses:
+            device = next(d for d in self.home.devices if d.id == status["deviceId"])
+
+            status_obj: DeviceStatus
+            if "ty" not in status:
+                status_obj = DeviceStatus(
+                    is_online=False,
+                    temperature=24.0,
+                    room_temperature=24.0,
+                    power_mode=PowerMode.OFF,
+                    fan_mode=FanMode.AUTO,
+                    swing_mode=SwingMode.AUTO,
+                    display_mode=DisplayMode.ON,
+                    hvac_mode=HVACMode.AUTO,
+                    preset_mode=PresetMode.NONE,
+                )
+            else:
+                status_obj = DeviceStatus(
+                    is_online=status["onlineStatus"] == "true",
+                    temperature=toFloat(status["actmp"]),
+                    room_temperature=toFloat(status["rmtmp"]),
+                    power_mode=PowerMode(status["ps"]),
+                    fan_mode=FanMode(status["acfs"]),
+                    swing_mode=SwingMode(status["acvs"]),
+                    display_mode=DisplayMode(status["acdc"]),
+                    hvac_mode=HVACMode(status["acmd"]),
+                    preset_mode=PresetMode.BOOST
+                    if status["acpm"] == "on"
+                    else PresetMode.ECO
+                    if status["acem"] == "on"
+                    else PresetMode.NONE,
+                )
+
+            device.set_status(status_obj)
+
+        return statuses
