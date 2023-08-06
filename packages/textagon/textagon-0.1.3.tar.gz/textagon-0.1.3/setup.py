5a2ec99d2a67e8173524b482cc0430135189c1f8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:10:37 +0000

    Add package textagon/0.1.3/textagon-0.1.3.tar.gz

---
+++ packages/textagon/textagon-0.1.3.tar.gz/textagon-0.1.3/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = 'textagon',
+    version = '0.1.3',
+    packages = find_packages(),
+    description = 'Start building textagon',
+    author = 'Mendoza',
+    classifiers=[
+        "License :: OSI Approved :: Python Software Foundation License"
+    ],
+    install_requires=[
+        'nltk',
+        'scikit-learn',
+        'pandas',
+        'numpy',
+        'beautifulsoup4',
+        'mapply',
+        'pyenchant',
+        'tzlocal',
+        'spacy',
+    ]
+
+)
