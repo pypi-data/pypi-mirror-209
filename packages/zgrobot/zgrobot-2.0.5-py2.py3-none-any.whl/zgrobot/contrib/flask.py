e22e1fb1dd7263605176cce8d1b6c37e183cebe3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:48:16 +0000

    Add package zgrobot/2.0.5/zgrobot-2.0.5-py2.py3-none-any.whl

---
+++ packages/zgrobot/zgrobot-2.0.5-py2.py3-none-any.whl/zgrobot/contrib/flask.py
@@ -0,0 +1,58 @@
+# -*- coding: utf-8 -*-
+
+import html
+
+from flask import request, make_response
+
+
+def make_view(robot):
+    """
+    为一个 BaseRoBot 生成 Flask view。
+
+    Usage ::
+
+        from zgrobot import ZgRoBot
+
+        robot = ZgRoBot(token='token')
+
+
+        @robot.handler
+        def hello(message):
+            return 'Hello World!'
+
+        from flask import Flask
+        from zgrobot.contrib.flask import make_view
+
+        app = Flask(__name__)
+        app.add_url_rule(rule='/robot', # ZgRoBot 的绑定地址
+                        endpoint='zgrobot', # Flask 的 endpoint
+                        view_func=make_view(robot),
+                        methods=['GET', 'POST'])
+
+    :param robot: 一个 BaseRoBot 实例
+    :return: 一个标准的 Flask view
+    """
+    def zgrobot_view():
+        timestamp = request.args.get('timestamp', '')
+        nonce = request.args.get('nonce', '')
+        signature = request.args.get('signature', '')
+        if not robot.check_signature(
+            timestamp,
+            nonce,
+            signature,
+        ):
+            return robot.make_error_page(html.escape(request.url)), 403
+        if request.method == 'GET':
+            return request.args['echostr']
+
+        message = robot.parse_message(
+            request.data,
+            timestamp=timestamp,
+            nonce=nonce,
+            msg_signature=request.args.get('msg_signature', '')
+        )
+        response = make_response(robot.get_encrypted_reply(message))
+        response.headers['content_type'] = 'application/xml'
+        return response
+
+    return zgrobot_view
