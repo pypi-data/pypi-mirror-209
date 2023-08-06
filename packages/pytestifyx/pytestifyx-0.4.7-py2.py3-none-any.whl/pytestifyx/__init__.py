7ad08f8cd0a420f63fb72bafb3219b327fdf75eb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:07:50 +0000

    Add package pytestifyx/0.4.7/pytestifyx-0.4.7-py2.py3-none-any.whl

---
+++ packages/pytestifyx/pytestifyx-0.4.7-py2.py3-none-any.whl/pytestifyx/__init__.py
@@ -0,0 +1,8 @@
+from .core import TestCase
+from .utils.logs.core import log
+
+__author__ = "luyh"
+
+__version__ = "0.4.7"
+
+__description__ = "pytestifyx is a pytest-based automation testing framework for api, ui, app testing"
