2082a2d5dbf402bdeafac45482ace04f22f54caa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:15:13 +0000

    Add package mlw-lectiomat/0.2.0/mlw_lectiomat-0.2.0-py3-none-any.whl

---
+++ packages/mlw-lectiomat/mlw_lectiomat-0.2.0-py3-none-any.whl/lectiomat/__init__.py
@@ -0,0 +1,15 @@
+"""MLW-OCR Projekt.
+
+Bayerische Akademie der Wissenschaften.
+LMU München, 2023
+
+Gilary Vera Nuñez, Philipp Koch
+
+Esteban Garces Arias, Dr. Matthias Aßenmacher,
+Prof. Dr. Christian Heumann
+
+__init__-module for the mlw-lectiomat library.
+"""
+
+
+from .Lectiomat import Lectiomat  # noqa: F401, I001, E261
