f5d03ea2c1b678b1adcc4b515ad353c536714956
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:06:41 +0000

    Add package open-source-insights-api/0.1.4/open_source_insights_api-0.1.4.tar.gz

---
+++ packages/open-source-insights-api/open_source_insights_api-0.1.4.tar.gz/open_source_insights_api-0.1.4/open_source_insights_api/__init__.py
@@ -0,0 +1,16 @@
+"""
+.. include:: ../README.md
+.. include:: ../CHANGELOG.md
+"""
+
+__version__ = '0.1.4'
+
+from .main import (
+    GetPackage,
+    GetVersion,
+    GetRequirements,
+    GetDependencies,
+    GetProject,
+    GetAdvisory,
+    Search
+)
