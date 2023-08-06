704c2596ef1f76bd3f0f4f4e9f1a302640ec04d0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:42:04 +0000

    Add package AkvoResponseGrouper/1.3.2a0/AkvoResponseGrouper-1.3.2a0-py3-none-any.whl

---
+++ packages/AkvoResponseGrouper/AkvoResponseGrouper-1.3.2a0-py3-none-any.whl/AkvoResponseGrouper/__init__.py
@@ -0,0 +1,12 @@
+from .response_grouper import response_grouper
+
+
+__version__ = "1.3.2-alpha"
+__all__ = [
+    "response_grouper",
+    "models",
+    "db",
+    "routes",
+    "views",
+    "utils",
+]
