c148a32965f1a6e70adf5062ea064a71ff885eba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:23:00 +0000

    Add package streamlit-lightweight-charts/0.7.20/streamlit-lightweight-charts-0.7.20.tar.gz

---
+++ packages/streamlit-lightweight-charts/streamlit-lightweight-charts-0.7.20.tar.gz/streamlit-lightweight-charts-0.7.20/setup.py
@@ -0,0 +1,30 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="streamlit-lightweight-charts",
+    version="0.7.20",
+    author="Joe Rosa",
+    author_email="joe.rosa@itpmngt.co.uk",
+    license="MIT",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+    ],
+    description="Wrapper for TradingView `lightweight-charts`",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/freyastreamlit/streamlit-lightweight-charts",
+    packages=['streamlit_lightweight_charts'],
+    package_data={
+        'streamlit_lightweight_charts': ['frontend/build/*','frontend/build/static/js/*'],
+    },
+    include_package_data=True,
+    python_requires=">=3.6",
+    install_requires=[
+        "streamlit >= 0.62",
+    ],
+)
