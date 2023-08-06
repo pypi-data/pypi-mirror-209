d57cae7181ec9b88f2d48d420666130c66eb8c01
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:14:11 +0000

    Add package apitest-ezgo/2.0.4/apitest_ezgo-2.0.4-py2.py3-none-any.whl

---
+++ packages/apitest-ezgo/apitest_ezgo-2.0.4-py2.py3-none-any.whl/apitest_ezgo/Har2Template/cli.py
@@ -0,0 +1,54 @@
+# coding=utf-8
+""" Convert HAR (HTTP Archive) to YAML/JSON template for HttpRunner.
+
+"""
+import argparse
+import logging
+import sys
+
+from apitest_ezgo.__about__ import __description__, __version__
+
+from apitest_ezgo.Har2Template.core import HarParser
+
+
+def get_version():
+    return __version__
+
+
+def main():
+    """ HAR converter: parse command line options and run commands.
+    """
+    parser = argparse.ArgumentParser(description=__description__)
+    parser.add_argument('-v', '--version', action='version', version=get_version(), help='show version')
+
+    subparsers = parser.add_subparsers(help="")
+    sub_parser = subparsers.add_parser("new", help=u"""usage: apitest_ezgo new  [-s] har_source_file  [-d] dest_file_path
+                                            egg: NSFastHttp new demo.har .
+                                            har_source_file： har文件所在路径,当前路径下的demo.har;
+                                            dest_file_path: 目的文件路径，.表示当前路径""")
+    sub_parser.add_argument("-s", "--source", dest="har_source_file", help=u"har_source_file： har文件所在路径")
+    sub_parser.add_argument("-d", "--dest", dest="dest_file_path", default=".",
+                            help=u"dest_file_path： 目的文件路径，默认har所在路径")
+
+    args = parser.parse_args()
+
+    har_source_file = args.har_source_file
+    if not har_source_file or not har_source_file.endswith(".har"):
+        logging.error("HAR file not specified.")
+        sys.exit(1)
+    print(args)
+
+    HarParser(
+        har_source_file, args.dest_file_path
+    ).gen_template()
+    return 0
+
+
+def test(har_source_file, dest_file_path):
+    HarParser(
+        har_source_file, dest_file_path
+    ).gen_template()
+
+
+if __name__ == '__main__':
+    test(r"C:\Users\nsfocus\Desktop\import2.har", ".")
---
+++ packages/apitest-ezgo/apitest_ezgo-2.0.4-py2.py3-none-any.whl/apitest_ezgo/__about__.py
@@ -0,0 +1,8 @@
+__title__ = 'apitest_ezgo'
+__description__ = 'easy http interface test frame'
+__url__ = ''
+__version__ = '2.0.4'
+__author__ = 'Gawen'
+__author_email__ = '502286126@qq.com'
+__license__ = 'Apache-2.0'
+__copyright__ = 'Copyright 2022 Gawen'
