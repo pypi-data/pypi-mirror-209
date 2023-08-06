fcc48d9fb067f6bc6bc6c363e6a3ae95f0fdec7a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:11:35 +0000

    Add package simmon/0.0.5/simmon-0.0.5.tar.gz

---
+++ packages/simmon/simmon-0.0.5.tar.gz/simmon-0.0.5/setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="simmon",
+    version="0.0.5",
+    author="Roie Zemel",
+    description="A simple simulation monitor",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.8',
+    py_modules=["simmon"],
+    package_dir={'': 'simmon'},
+    install_requires=['matplotlib'],
+    keywords=["simulation, data tracking"],
+    license_files=('LICENSE',)
+)
