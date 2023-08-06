503c4fc3d6a9cd3c7edafc6a69b8702f5255a13b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:38:24 +0000

    Add package swibots/1.2.0/swibots-1.2.0-py3-none-any.whl

---
+++ packages/swibots/swibots-1.2.0-py3-none-any.whl/tests/tests.py
@@ -0,0 +1,12 @@
+def test_sum():
+    assert 6 == 6, "Should be 6"
+
+
+def test_sum_tuple():
+    assert 6 == 6, "Should be 6"
+
+
+if __name__ == "__main__":
+    test_sum()
+    test_sum_tuple()
+    print("Everything passed")
