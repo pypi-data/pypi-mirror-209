b2b1259b6c32f8f900c522eac2f79777033a31d1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:31:54 +0000

    Add package types-unopy/1.1.0/types_unopy-1.1.0.tar.gz

---
+++ packages/types-unopy/types_unopy-1.1.0.tar.gz/types_unopy-1.1.0/com/sun/star/xml/dom/events/__init__.pyi
@@ -0,0 +1,11 @@
+from .attr_change_type import AttrChangeType as AttrChangeType
+from .event_exception import EventException as EventException
+from .event_type import EventType as EventType
+from .phase_type import PhaseType as PhaseType
+from .x_document_event import XDocumentEvent as XDocumentEvent
+from .x_event import XEvent as XEvent
+from .x_event_listener import XEventListener as XEventListener
+from .x_event_target import XEventTarget as XEventTarget
+from .x_mouse_event import XMouseEvent as XMouseEvent
+from .x_mutation_event import XMutationEvent as XMutationEvent
+from .xui_event import XUIEvent as XUIEvent
