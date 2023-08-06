d26b34a50fceb5eb99d4ae64ff89bec6ed630929
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:22:22 +0000

    Add package plutous-trade/0.0.2/plutous_trade-0.0.2.tar.gz

---
+++ packages/plutous-trade/plutous_trade-0.0.2.tar.gz/plutous_trade-0.0.2/plutous/trade/app/main.py
@@ -0,0 +1,54 @@
+from fastapi import Depends, FastAPI
+from sqlalchemy.orm import Session
+
+from plutous.app.utils.session import get_session
+from plutous.trade.models import ApiKey, Bot, Strategy
+
+from .models import ApiKeyPost, BotPost, StrategyPost
+
+app = FastAPI(
+    title="Plutous Trade API",
+    version="0.0.1",
+)
+
+try:
+    from plutous.trade.crypto.app.main import app as crypto
+
+    app.mount("/crypto", crypto)
+except ImportError:
+    pass
+
+
+@app.get("/")
+def root():
+    return {"message": "Hello World"}
+
+
+@app.post("/strategy", response_model=StrategyPost)
+def create_strategy(
+    strategy: StrategyPost,
+    session: Session = Depends(get_session),
+) -> StrategyPost:
+    session.add(Strategy(**strategy.dict()))
+    session.commit()
+    return strategy
+
+
+@app.post("/api_key", response_model=ApiKeyPost)
+def create_api_key(
+    api_key: ApiKeyPost,
+    session: Session = Depends(get_session),
+) -> ApiKeyPost:
+    session.add(ApiKey(**api_key.dict()))
+    session.commit()
+    return api_key
+
+
+@app.post("/bot", response_model=BotPost)
+def create_bot(
+    bot: BotPost,
+    session: Session = Depends(get_session),
+) -> BotPost:
+    session.add(Bot(**bot.dict()))
+    session.commit()
+    return bot
