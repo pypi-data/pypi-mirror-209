fcf533fb5a45e09314888f98722583caa6fe70af
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:51:42 +0000

    Add package jellyfishlights-py/0.7.0/jellyfishlights-py-0.7.0.tar.gz

---
+++ packages/jellyfishlights-py/jellyfishlights-py-0.7.0.tar.gz/jellyfishlights-py-0.7.0/jellyfishlightspy/__init__.py
@@ -0,0 +1,14 @@
+from .controller import JellyFishController
+from .helpers import JellyFishException
+from .model import (
+    TimeConfig,
+    FirmwareVersion,
+    ZoneState,
+    ZoneConfig,
+    PortMapping,
+    Pattern,
+    PatternConfig,
+    RunConfig,
+    ScheduleEvent,
+    ScheduleEventAction,
+)
---
+++ packages/jellyfishlights-py/jellyfishlights-py-0.7.0.tar.gz/jellyfishlights-py-0.7.0/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+# https://realpython.com/pypi-publish-python-package/
+
+setup(
+    name='jellyfishlights-py',
+    version='0.7.0',
+    license='',
+    author="Jonathan Nielsen",
+    author_email='jonathann@jellyfishlighting.com',
+    packages=find_packages(),
+    # package_dir={'': 'src'},
+    url='https://github.com/vinenoobjelly/jellyfishlights-py',
+    keywords='',
+    install_requires=[
+          'websocket-client',
+      ],
+)
