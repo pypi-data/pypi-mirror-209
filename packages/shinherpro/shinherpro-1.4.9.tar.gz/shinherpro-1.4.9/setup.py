31e51aeed9509959819e1e5234bd3cf4b09b6024
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:50:07 +0000

    Add package shinherpro/1.4.9/shinherpro-1.4.9.tar.gz

---
+++ packages/shinherpro/shinherpro-1.4.9.tar.gz/shinherpro-1.4.9/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='shinherpro',
+    version='1.4.9',
+    description='shinher pro is a better system than shinher',
+    author='Yihuan',
+    author_email='ivan17.lai@gmail.com',
+    packages=['tyai'],
+    install_requires=[],
+)
