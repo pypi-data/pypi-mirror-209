3f06073daeff045bc338932d3c8aa6737a52df87
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:08:07 +0000

    Add package gcode-helpers/0.1.4/gcode_helpers-0.1.4.tar.gz

---
+++ packages/gcode-helpers/gcode_helpers-0.1.4.tar.gz/gcode_helpers-0.1.4/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='gcode_helpers',
+      version='0.1.4',
+      description='`gcode_helpers`-- collection of helper functions for parsing raw gcode',
+      url='https://github.com/rtellez700/gcode_helpers',
+      author='Rodrigo Telles',
+      author_email='rtelles@g.harvard.edu',
+      license='MIT',
+    #   packages=[],
+      zip_safe=False)
