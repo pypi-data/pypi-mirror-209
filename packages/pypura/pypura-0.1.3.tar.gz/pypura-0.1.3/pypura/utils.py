1d35b70e336d476e7ed46aa5e7cb30b46bcbd565
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:11:14 +0000

    Add package pypura/0.1.3/pypura-0.1.3.tar.gz

---
+++ packages/pypura/pypura-0.1.3.tar.gz/pypura-0.1.3/pypura/utils.py
@@ -0,0 +1,29 @@
+"""Utilities."""
+from __future__ import annotations
+
+import logging
+from base64 import b64decode
+from typing import Final
+
+from .const import FRAGRANCES
+
+_LOGGER = logging.getLogger(__name__)
+
+ENCODING: Final = "utf-8"
+ISSUE_URL: Final = "https://github.com/natekspencer/pypura/issues/1"
+
+
+def decode(value: str) -> str:
+    """Decode a value."""
+    return b64decode(value).decode(ENCODING)
+
+
+def fragrance_name(code: str) -> str:
+    """Return fragrance name."""
+    if not (name := FRAGRANCES.get(code)):
+        _LOGGER.warning(
+            "Unknown fragrance code '%s', please report this at %s", code, ISSUE_URL
+        )
+        name = f"Fragrance code: {code}"
+        FRAGRANCES[code] = name
+    return name
