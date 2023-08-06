9820104e36b3db4f717b6e2b13b254de4d41c682
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:12:33 +0000

    Add package template-pptx-jinja-fix/0.0.3/template-pptx-jinja-fix-0.0.3.tar.gz

---
+++ packages/template-pptx-jinja-fix/template-pptx-jinja-fix-0.0.3.tar.gz/template-pptx-jinja-fix-0.0.3/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+
+def read(filename):
+    with open(filename, 'r', encoding='utf-8') as myfile:
+        return myfile.read()
+
+
+setup(name='template-pptx-jinja-fix',
+    version="0.0.3",
+    description='PowerPoint presentation builder from template using Jinja2',
+    long_description=read('./readme.md'),
+    long_description_content_type='text/markdown',
+    url='https://github.com/macheal/template-pptx-jinja',
+    author='wlx',
+    author_email='409673593@qq.com',
+    install_requires=read('requirements.txt').split(),
+    license='',
+    packages=find_packages(),
+    keywords=['powerpoint', 'ppt', 'pptx', 'template'],
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Topic :: Utilities",
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent"
+    ]
+)
