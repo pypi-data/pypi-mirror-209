b80d4b0adf1696b598f3c8f962efec2aa83f4186
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:14:01 +0000

    Add package scandal/0.1.9/scandal-0.1.9.tar.gz

---
+++ packages/scandal/scandal-0.1.9.tar.gz/scandal-0.1.9/scandal/credentials/__init__.py
@@ -0,0 +1,5 @@
+from scandal.credentials.base import Credential
+from scandal.credentials.client import ClientCredential
+from scandal.credentials.device import DeviceCredential
+
+__all__ = ["Credential", "ClientCredential", "DeviceCredential"]
