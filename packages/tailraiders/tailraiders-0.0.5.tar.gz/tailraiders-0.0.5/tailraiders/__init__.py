7feec05dd9e5ea02792a85518ce8bbacb9cdf75f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:33:40 +0000

    Add package tailraiders/0.0.5/tailraiders-0.0.5.tar.gz

---
+++ packages/tailraiders/tailraiders-0.0.5.tar.gz/tailraiders-0.0.5/tailraiders/__init__.py
@@ -0,0 +1,26 @@
+"""
+Tailraiders
+=======================================
+
+The tailraiders package is made to help with general Data Sciency stuff.
+"""
+
+#Checking if dependencies are installed, the dependencies of my libary are stored in the dependency_module value
+dependency_module = ['pandas',
+                    'numpy',
+                    'matplotlib',
+                    'seaborn']
+
+#For-loop to check for modules
+for module in dependency_module:
+    #Try to import modules
+    try:
+        __import__(module)
+    #Raise an error when it fails, telling them to install the failed module
+    except ImportError:
+        raise ImportError(f"The required package {module} was not found. Please install it.")
+    
+#Making it so you can, for certain parts, just import tailraiders
+from tailraiders.boaboa.doc import docstring
+from tailraiders.gajalaka.plots import Plots, MeltPlot
+from tailraiders.protector.nan import Nan
