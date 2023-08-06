cadea2298e0c5590a8632f04a3cc3d7e5ce533f5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:19:17 +0000

    Add package extr/0.0.37/extr-0.0.37-py3-none-any.whl

---
+++ packages/extr/extr-0.0.37-py3-none-any.whl/extr/tokenizers/tokenizer.py
@@ -0,0 +1,42 @@
+from typing import List, Generator, Union
+from extr import Location
+from ..models import Token, TokenGroup
+
+
+def word_tokenizer(text: str, tokens: List[str]) -> TokenGroup:
+    cache = text[:]
+
+    offset = 0
+    tokens_in_sentence: List[Token] = []
+    for term in tokens:
+        start = cache.find(term)
+        end = start + len(term)
+
+        actual_term = cache[start:end]
+        assert actual_term == term, f'mismatch("{actual_term}", "{term}")'
+
+        tokens_in_sentence.append(
+            Token(term, Location(offset + start, offset + end), len(tokens_in_sentence) + 1)
+        )
+
+        cache = cache[end:]
+        offset += end
+
+    sentence_start = tokens_in_sentence[0].location.start
+    sentence_end = tokens_in_sentence[-1].location.end
+
+    token_group = TokenGroup(
+        Location(sentence_start, sentence_end),
+        text[sentence_start:sentence_end],
+        tokens_in_sentence
+    )
+
+    return token_group
+
+def tokenizer(text: str, sentences: Union[Generator[List[str], None, None], List[List[str]]]) -> Generator[TokenGroup, None, None]:
+    cache = text[:].strip()
+    for sentence in sentences:
+        token_group = word_tokenizer(cache, sentence)
+        yield token_group
+
+        cache = cache[token_group.location.end:].strip()
