1ea82c833f036acb3ec98d881b9263996534e2f6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:44:21 +0000

    Add package bbox/0.9.4/bbox-0.9.4.tar.gz

---
+++ packages/bbox/bbox-0.9.4.tar.gz/bbox-0.9.4/bbox/__init__.py
@@ -0,0 +1,15 @@
+"""Main package initialization"""
+
+from bbox import metrics, utils
+from bbox.bbox2d import BBox2D
+from bbox.bbox2d_list import BBox2DList
+from bbox.bbox3d import BBox3D
+from bbox.box_modes import XYWH, XYXY
+
+__author__ = "Varun Agrawal"
+__copyright__ = "Varun Agrawal"
+__version__ = "0.9.4"
+__maintainer__ = "Varun Agrawal"
+__email__ = "varagrawal@gmail.com"
+__status__ = "Beta"
+__license__ = "MIT"
